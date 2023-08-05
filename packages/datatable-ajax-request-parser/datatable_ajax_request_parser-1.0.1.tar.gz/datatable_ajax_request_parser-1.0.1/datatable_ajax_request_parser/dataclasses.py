560c5df69efa5cbbff8d32e39aa8ea0bebf7595d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:15 +0000

    Add package datatable-ajax-request-parser/1.0.1/datatable_ajax_request_parser-1.0.1.tar.gz

---
+++ packages/datatable-ajax-request-parser/datatable_ajax_request_parser-1.0.1.tar.gz/datatable_ajax_request_parser-1.0.1/datatable_ajax_request_parser/dataclasses.py
@@ -0,0 +1,51 @@
+from dataclasses import asdict, dataclass
+from typing import List
+
+
+@dataclass
+class DictHelperMixin:
+    def to_dict(self):
+        return asdict(self)
+
+
+@dataclass
+class DTOrder(DictHelperMixin):
+    index: int
+    column: int
+    direction: str  # asc/desc
+
+    def __str__(self):
+        return f'index: {self.index} column: {self.column}, direction: {self.direction}'
+
+
+@dataclass
+class DTColumn(DictHelperMixin):
+    data: str
+    name: str
+    searchable: bool
+    orderable: bool
+    search_value: str
+    search_is_regex: bool
+
+    def __str__(self):
+        return f'data: {self.data}, name: {self.name}'
+
+
+@dataclass
+class DTRequest(DictHelperMixin):
+    draw: int
+    start: int
+    length: int
+    search_value: str
+    search_regex: bool
+    columns: List[DTColumn]
+    order: List[DTOrder]
+
+
+@dataclass
+class DTResponse(DictHelperMixin):
+    draw: int
+    records_total: int
+    records_filtered: int
+    data: dict
+    error: str
