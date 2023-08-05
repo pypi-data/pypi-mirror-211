d3cfee1d91ec405a2c5c443b6eeb5cb06531eff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:29 +0000

    Add package api-Rocket/1.6.4/api-Rocket-1.6.4.tar.gz

---
+++ packages/api-Rocket/api-Rocket-1.6.4.tar.gz/api-Rocket-1.6.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='api-Rocket',
+	version='1.6.4',
+	author='Redpiar',
+	author_email='Regeonwix@gmail.com',
+	description='api for ton rocket',
+	packages=['api_Rocket'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
