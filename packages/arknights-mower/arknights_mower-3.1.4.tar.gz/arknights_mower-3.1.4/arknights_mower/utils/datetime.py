0147f4dd142a57fe3513079c06c98ed56fc3780a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package arknights-mower/3.1.4/arknights_mower-3.1.4.tar.gz

---
+++ packages/arknights-mower/arknights_mower-3.1.4.tar.gz/arknights_mower-3.1.4/arknights_mower/utils/datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import pytz
+
+def the_same_day(a: datetime = None, b: datetime = None) -> bool:
+    if a is None or b is None:
+        return False
+    return a.year == b.year and a.month == b.month and a.day == b.day
+
+def the_same_time(a: datetime = None, b: datetime = None) -> bool:
+    if a is None or b is None:
+        return False
+    return a.year == b.year and a.month == b.month and a.day == b.day and a.hour ==b.hour and a.minute==b.minute and a.second == b.second
+def get_server_weekday():
+    return datetime.now(pytz.timezone('Asia/Dubai')).weekday()
