e86d5dc9bc450d4bc8111033cb3a6453b5c46a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:09 +0000

    Add package gptcache/0.1.28/gptcache-0.1.28-py3-none-any.whl

---
+++ packages/gptcache/gptcache-0.1.28-py3-none-any.whl/gptcache/config.py
@@ -0,0 +1,41 @@
+from typing import Optional, Callable, List
+
+from gptcache.utils.error import CacheError
+
+
+class Config:
+    """Pass configuration.
+
+    :param log_time_func: optional, customized log time function
+    :type log_time_func: Optional[Callable[[str, float], None]]
+    :param similarity_threshold: a threshold ranged from 0 to 1 to filter search results with similarity score higher \
+     than the threshold. When it is 0, there is no hits. When it is 1, all search results will be returned as hits.
+    :type similarity_threshold: float
+    :param prompts: optional, if the request content will remove the prompt string when the request contains the prompt list
+    :type prompts: Optional[List[str]]
+    :param template: optional, if the request content will remove the template string and only keep the parameter value in the template
+    :type template: Optional[str]
+
+    Example:
+        .. code-block:: python
+
+            from gptcache import Config
+
+            configs = Config(similarity_threshold=0.6)
+    """
+
+    def __init__(
+            self,
+            log_time_func: Optional[Callable[[str, float], None]] = None,
+            similarity_threshold: float = 0.8,
+            prompts: Optional[List[str]] = None,
+            template: Optional[str] = None,
+    ):
+        if similarity_threshold < 0 or similarity_threshold > 1:
+            raise CacheError(
+                "Invalid the similarity threshold param, reasonable range: 0-1"
+            )
+        self.log_time_func = log_time_func
+        self.similarity_threshold = similarity_threshold
+        self.prompts = prompts
+        self.template = template
