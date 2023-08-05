c7ffc4959f40c774fbee93e76f9b20aa6595d845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package learning-loop-node/0.7.53rc2/learning_loop_node-0.7.53rc2.tar.gz

---
+++ packages/learning-loop-node/learning_loop_node-0.7.53rc2.tar.gz/learning_loop_node-0.7.53rc2/learning_loop_node/rest/uploads.py
@@ -0,0 +1,36 @@
+from typing import List, Union
+import aiohttp
+from learning_loop_node.context import Context
+from learning_loop_node.loop import loop
+import logging
+from icecream import ic
+
+
+async def upload_model(context: Context, files: List[str], model_id: str, format: str) -> None:
+    data = aiohttp.FormData()
+
+    for file_name in files:
+        data.add_field('files',  open(file_name, 'rb'))
+    async with loop.put(f'/{context.organization}/projects/{context.project}/models/{model_id}/{format}/file', data=data) as response:
+        if response.status != 200:
+            msg = f'---- could not upload model with id {model_id} and format {format}. Details: {await response.text()}'
+            raise Exception(msg)
+        else:
+            logging.info(f'---- uploaded model with id {model_id} and format {format}.')
+
+
+async def upload_model_for_training(context: Context, files: List[str], training_number: int, format: str) -> Union[dict, None]:
+    data = aiohttp.FormData()
+
+    for file_name in files:
+        data.add_field('files',  open(file_name, 'rb'))
+    async with loop.put(f'/{context.organization}/projects/{context.project}/trainings/{training_number}/models/latest/{format}/file', data=data) as response:
+        if response.status != 200:
+            msg = f'---- could not upload model for training {training_number} and format {format}. Details: {await response.text()}'
+            logging.error(msg)
+            response.raise_for_status()
+        else:
+            uploaded_model = await response.json()
+            logging.info(
+                f'---- uploaded model for training {training_number} and format {format}. Model id is {uploaded_model}')
+            return uploaded_model
---
+++ packages/learning-loop-node/learning_loop_node-0.7.53rc2.tar.gz/learning_loop_node-0.7.53rc2/learning_loop_node/tests/test_helper.py
@@ -0,0 +1,111 @@
+import asyncio
+from glob import glob
+import os
+import time
+from typing import Callable, List, Optional
+import zipfile
+from learning_loop_node.loop import loop
+from urllib.parse import urljoin
+from requests import Session
+import aiohttp
+import logging
+from icecream import ic
+import shutil
+import requests
+
+
+class LiveServerSession(Session):
+    """https://stackoverflow.com/a/51026159/364388"""
+    def __init__(self, *args, **kwargs):
+        super(LiveServerSession, self).__init__(*args, **kwargs)
+        self.prefix_url = loop.web.base_url
+        self.cookies = self.get_cookies()
+
+    def request(self, method, url, *args, **kwargs):
+        url = urljoin(self.prefix_url, url)
+        ic(url)
+        return super(LiveServerSession, self).request(method, url, cookies=self.cookies, *args, **kwargs)
+
+    def get_cookies(self) -> dict:
+        user = os.environ.get('USERNAME', None)
+        password = os.environ.get('PASSWORD', None)
+        files = {
+            'username': (None, user),
+            'password': (None, password),
+        }
+        return requests.post(f'{self.prefix_url}/login', files=files).cookies
+
+
+def get_files_in_folder(folder: str):
+    files = [entry for entry in glob(f'{folder}/**/*', recursive=True) if os.path.isfile(entry)]
+    files.sort()
+    return files
+
+
+async def get_latest_model_id() -> str:
+    async with loop.get(f'/zauberzeug/projects/pytest/trainings') as response:
+        assert response.status == 200
+        trainings = await response.json()
+        return trainings['charts'][0]['data'][0]['model_id']
+
+
+async def assert_upload_model_with_id(file_paths: Optional[List[str]] = None, format: str = 'mocked', model_id: Optional[str] = None) -> str:
+    data = prepare_formdata(file_paths)
+
+    async with loop.put(f'/zauberzeug/projects/pytest/models/{model_id}/{format}/file', data) as response:
+        if response.status != 200:
+            msg = f'unexpected status code {response.status} while putting model'
+            logging.error(msg)
+            raise (Exception(msg))
+        model = await response.json()
+
+        return model['id']
+
+
+def prepare_formdata(file_paths: Optional[List[str]]) -> aiohttp.FormData:
+    module_path = os.path.dirname(os.path.realpath(__file__))
+    if not file_paths:
+        file_paths = [f'{module_path}/test_data/file_1.txt',
+                      f'{module_path}/test_data/file_2.txt',
+                      f'{module_path}/test_data/model.json']
+
+    data = [('files', open(path, 'rb')) for path in file_paths]
+    data = aiohttp.FormData()
+    for path in file_paths:
+        data.add_field('files',  open(path, 'rb'))
+    return data
+
+
+def unzip(file_path, target_folder):
+    shutil.rmtree(target_folder, ignore_errors=True)
+    os.makedirs(target_folder)
+    with zipfile.ZipFile(file_path, 'r') as zip:
+        zip.extractall(target_folder)
+
+
+async def condition(condition: Callable, *, timeout: float = 1.0, interval: float = 0.1):
+    start = time.time()
+    while not condition():
+        if time.time() > start + timeout:
+            raise TimeoutError(f'condition {condition} took longer than {timeout}s')
+        await asyncio.sleep(interval)
+
+
+def update_attributes(obj, **kwargs) -> None:
+    if isinstance(obj, dict):
+        _update_attribute_dict(obj, **kwargs)
+    else:
+        _update_attribute_class_instance(obj, **kwargs)
+
+
+def _update_attribute_class_instance(obj, **kwargs) -> None:
+    for key, value in kwargs.items():
+        if hasattr(obj, key):
+            setattr(obj, key, value)
+        else:
+            raise ValueError(f"Object of type '{type(obj)}' does not have a property '{key}'.")
+
+
+def _update_attribute_dict(obj: dict, **kwargs) -> None:
+    for key, value in kwargs.items():
+        obj[key] = value
