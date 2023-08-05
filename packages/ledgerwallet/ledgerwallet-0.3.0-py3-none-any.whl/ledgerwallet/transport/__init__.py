50c51fbf45f34cd11b609614b15336d50e08be2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:12 +0000

    Add package ledgerwallet/0.3.0/ledgerwallet-0.3.0-py3-none-any.whl

---
+++ packages/ledgerwallet/ledgerwallet-0.3.0-py3-none-any.whl/ledgerwallet/transport/__init__.py
@@ -0,0 +1,24 @@
+from contextlib import contextmanager
+
+from .device import Device
+from .hid import HidDevice
+from .tcp import TcpDevice
+
+DEVICE_CLASSES = [TcpDevice, HidDevice]
+
+
+def enumerate_devices():
+    devices = []
+    for cls in DEVICE_CLASSES:
+        devices.extend(cls.enumerate_devices())
+    return devices
+
+
+@contextmanager
+def open_device(dev: Device):
+    """Open a device in a context manager."""
+    try:
+        dev.open()
+        yield dev
+    finally:
+        dev.close()
