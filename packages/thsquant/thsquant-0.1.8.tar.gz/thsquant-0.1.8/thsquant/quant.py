cbe0225bff9640ab4dbd33d14650d354d27f1f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package thsquant/0.1.8/thsquant-0.1.8.tar.gz

---
+++ packages/thsquant/thsquant-0.1.8.tar.gz/thsquant-0.1.8/thsquant/quant.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+import config
+
+def sayhello():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="ths", help="input a name")
+    args = parser.parse_args()
+
+    print("Hello,,,, {}! Start quant...\nversion={}".format(args.name, config.version))
+
+if __name__ == '__main__':
+    sayhello()
