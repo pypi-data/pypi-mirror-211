4fe95853abcfbb2ff1ce1fa89140e0e057b0f9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package safelog/1.8.2/safelog-1.8.2.tar.gz

---
+++ packages/safelog/safelog-1.8.2.tar.gz/safelog-1.8.2/setup.py
@@ -0,0 +1,52 @@
+'''
+    Set up for safelog
+
+    Copyright 2018-2023 safeapps
+    Last modified: 2023-05-29
+'''
+
+import os.path
+import setuptools
+
+# read long description
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="safelog",
+    version="1.8.2",
+    author="safeapps",
+    maintainer="safeapps",
+    description="Safelog is a multithread, multiprocess, multiinstance logging package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="logging multiprocess multithread multi-instance",
+    license="GNU General Public License v3 (GPLv3)",
+    url="https://github.com/safeapps/safelog/",
+    download_url="https://github.com/safeapps/safelog/",
+    project_urls={
+        "Source Code": "https://github.com/safeapps/safelog/",
+    },
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Logging",
+         ],
+    py_modules=["safelog"],
+    scripts=['sbin/safelog'],
+    entry_points={
+    },
+    data_files=[],
+    setup_requires=['setuptools-markdown'],
+    install_requires=['solidlibs'],
+    python_requires=">=3.5",
+)
