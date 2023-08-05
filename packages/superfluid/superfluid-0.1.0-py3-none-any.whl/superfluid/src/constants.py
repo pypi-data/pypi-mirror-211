1e70b305e160dd815d5ab9dbde88ff366a35aee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package superfluid/0.1.0/superfluid-0.1.0-py3-none-any.whl

---
+++ packages/superfluid/superfluid-0.1.0-py3-none-any.whl/superfluid/src/constants.py
@@ -0,0 +1,28 @@
+import json
+
+with open("./main/superfluid/src/abis/cfa_v1.json") as cfa_v1:
+    cfa_v1_abi = cfa_v1.read()
+
+CFA_V1_ABI = json.loads(cfa_v1_abi)
+
+with open("./main/superfluid/src/abis/cfa_v1_forwarder.json") as cfa_v1_forwarder:
+    cfa_v1_forwarder_abi = cfa_v1_forwarder.read()
+
+CFA_V1_FORWARDER_ABI = json.loads(cfa_v1_forwarder_abi)
+
+with open("./main/superfluid/src/abis/host.json") as host:
+    host_abi = host.read()
+
+HOST_ABI = json.loads(host_abi)
+
+with open("./main/superfluid/src/metadata/networks.json") as networks:
+    networks = networks.read()
+
+NETWORKS = json.loads(networks)
+
+"""
+------- ACL AUTHORIZATION BIT OPERATIONS -------
+"""
+AUTHORIZE_FLOW_OPERATOR_CREATE = 1 << 0
+AUTHORIZE_FLOW_OPERATOR_UPDATE = 1 << 1
+AUTHORIZE_FLOW_OPERATOR_DELETE = 1 << 2
