a6bfcc2f6277fd2eaad1ff7c07348c11c6a6740d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:34 +0000

    Add package spectrally-constrained-lvms/0.1.2/spectrally_constrained_lvms-0.1.2.tar.gz

---
+++ packages/spectrally-constrained-lvms/spectrally_constrained_lvms-0.1.2.tar.gz/spectrally_constrained_lvms-0.1.2/spectrally_constrained_LVMs/__init__.py
@@ -0,0 +1,40 @@
+# Copyright 2023-present Ryan Balshaw
+"""Spectrally-constrained-LVMs. Train linear LVMs with the addition
+ of a spectral constraint with minimal effort."""
+from .cost_functions import NegentropyCost, SympyCost, UserCost, VarianceCost
+from .helper_methods import (
+    BatchSampler,
+    DataProcessor,
+    DeflationOrthogonalisation,
+    QuasiNewton,
+    hankel_matrix,
+)
+from .negen_approx import CubeObject, ExpObject, LogcoshObject, QuadObject
+from .spectral_constraint import SpectralObjective
+from .spectrally_constrained_model import LinearModel
+
+__author__ = "Ryan Balshaw"
+__version__ = "0.1.2"
+__email__ = "ryanbalshaw81@gmail.com"
+__description__ = (
+    "Train linear LVMs with the addition "
+    "of a spectral constraint with minimal effort."
+)
+# __uri__ = "http://spectrally-constrained-lvms.readthedocs.io/"
+__all__ = [
+    "SpectralObjective",
+    "NegentropyCost",
+    "SympyCost",
+    "UserCost",
+    "VarianceCost",
+    "hankel_matrix",
+    "LinearModel",
+    "BatchSampler",
+    "DataProcessor",
+    "DeflationOrthogonalisation",
+    "QuasiNewton",
+    "CubeObject",
+    "ExpObject",
+    "LogcoshObject",
+    "QuadObject",
+]
