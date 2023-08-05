b6193f6e1992e28f3ed232fe82705f8cf5e6aa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:25 +0000

    Add package volworld-common/0.1.40/volworld_common-0.1.40.tar.gz

---
+++ packages/volworld-common/volworld_common-0.1.40.tar.gz/volworld_common-0.1.40/src/volworld_common/test/behave/ContextUtil.py
@@ -0,0 +1,7 @@
+def int_list_str_to_int_list(int_list: str) -> list:
+    int_list = int_list.replace('"', "")
+    lst = int_list.split(',')
+    res = list()
+    for elm in lst:
+        res.append(int(elm.strip()))
+    return res
