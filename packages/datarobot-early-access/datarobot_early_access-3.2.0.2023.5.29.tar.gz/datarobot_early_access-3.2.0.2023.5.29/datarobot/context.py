ea78a5bb4f03ca5ac0a92d004c3095bbf6dcaeee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:37 +0000

    Add package datarobot-early-access/3.2.0.2023.5.29/datarobot_early_access-3.2.0.2023.5.29.tar.gz

---
+++ packages/datarobot-early-access/datarobot_early_access-3.2.0.2023.5.29.tar.gz/datarobot_early_access-3.2.0.2023.5.29/datarobot/context.py
@@ -0,0 +1,79 @@
+#
+# Copyright 2023 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+from contextvars import ContextVar
+from dataclasses import dataclass, field
+from typing import Optional, TYPE_CHECKING, Union
+
+from trafaret import DataError
+
+from .errors import ClientError
+
+if TYPE_CHECKING:
+    from .models.use_cases.use_case import UseCase
+
+DefaultUseCase = Optional[Union["UseCase", str]]
+
+
+@dataclass
+class _ContextGlobals:
+    """
+    Interface for initializing, accessing, and setting variables that should persist.
+
+    This class should not be used directly; rather, reference `datarobot.context.Context`.
+
+    Examples
+    --------
+        .. code-block:: python
+
+            from datarobot.context import Context
+
+            >>> def foobar(value: str) -> None:
+            ...  print(f"hey {value}")
+            ...  Context.use_case = value
+            ...
+            >>> foobar("Jude")
+            hey Jude
+            >>>
+    """
+
+    _use_case: DefaultUseCase = field(init=False, default=None)
+
+    @property
+    def use_case(self) -> DefaultUseCase:
+        """Returns the default Use Case. If a string representing the Use Case ID
+        was provided, lookup, cache, and return the actual entity."""
+        return self.get_use_case()
+
+    @use_case.setter
+    def use_case(self, value: DefaultUseCase = None) -> None:
+        self._use_case = value
+
+    def get_use_case(self, raw: bool = False) -> DefaultUseCase:
+        """Returns the default Use Case. If a string representing the Use Case ID
+        was provided and raw=False, lookup, cache, and return the actual entity.
+        Otherwise, simply return the stored default Use Case in its current form"""
+        if not raw:
+            if isinstance(self._use_case, str):
+                try:
+                    from .models.use_cases.use_case import (  # pylint: disable=import-outside-toplevel
+                        UseCase,
+                    )
+
+                    self._use_case = UseCase.get(use_case_id=self._use_case)
+                except (DataError, ClientError) as e:
+                    raise ValueError("Current use case is invalid.", e)
+        return self._use_case or None
+
+
+_context: ContextVar[_ContextGlobals] = ContextVar("current_context", default=_ContextGlobals())
+
+Context = _context.get()
