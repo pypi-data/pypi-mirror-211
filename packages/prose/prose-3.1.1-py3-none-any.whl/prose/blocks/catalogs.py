84bdf799d530d6894e0f50945da638bd33419905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package prose/3.1.1/prose-3.1.1-py3-none-any.whl

---
+++ packages/prose/prose-3.1.1-py3-none-any.whl/prose/blocks/catalogs.py
@@ -0,0 +1,268 @@
+import warnings
+
+import astropy.units as u
+import numpy as np
+import pandas as pd
+import twirl
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+from astroquery.mast import Catalogs
+
+from prose import Block
+from prose import visualization as viz
+from prose.core.source import PointSource, Sources
+from prose.utils import cross_match, gaia_query, sparsify
+
+__all__ = ["GaiaCatalog", "TESSCatalog"]
+
+
+def image_gaia_query(
+    image, limit=3000, correct_pm=True, wcs=True, circular=True, fov=None
+):
+    if wcs:
+        center = image.wcs.pixel_to_world(*(np.array(image.shape) / 2)[::-1])
+    else:
+        center = image.skycoord
+
+    if fov is None:
+        fov = image.fov
+
+    table = gaia_query(center, fov, "*", limit=limit, circular=circular)
+
+    if correct_pm:
+        skycoord = SkyCoord(
+            ra=table["ra"].quantity,
+            dec=table["dec"].quantity,
+            pm_ra_cosdec=table["pmra"].quantity,
+            pm_dec=table["pmdec"].quantity,
+            distance=table["parallax"].quantity,
+            obstime=Time("2015-06-01 00:00:00.0"),
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            skycoord = skycoord.apply_space_motion(Time(image.date))
+
+        table["ra"] = skycoord.ra
+        table["dec"] = skycoord.dec
+
+    return table
+
+
+class _CatalogBlock(Block):
+    def __init__(self, name, mode=None, limit=10000, **kwargs):
+        super().__init__(**kwargs)
+        self.mode = mode
+        self.catalog_name = name
+        self.limit = limit
+
+    def get_catalog(self, image):
+        raise NotImplementedError()
+
+    def run(self, image):
+        catalog = self.get_catalog(image)
+        radecs = np.array(
+            [catalog["ra"].quantity.to(u.deg), catalog["dec"].quantity.to(u.deg)]
+        )
+        stars_coords = np.array(SkyCoord(*radecs, unit="deg").to_pixel(image.wcs))
+        catalog["x"], catalog["y"] = stars_coords
+        catalog = catalog.to_pandas()
+        image.catalogs[self.catalog_name] = catalog
+        stars_coords = stars_coords.T
+
+        if self.mode == "replace":
+            mask = np.all(stars_coords < image.shape[::-1], 1) & np.all(
+                stars_coords > 0, 1
+            )
+            mask = mask & ~np.any(np.isnan(stars_coords), 1)
+            image.sources = Sources(
+                [
+                    PointSource(coords=s, i=i)
+                    for i, s in enumerate(stars_coords[mask][0 : self.limit])
+                ],
+                source_type="PointSource",
+            )
+            catalog = catalog.iloc[np.flatnonzero(mask)].reset_index()
+
+        elif self.mode == "crossmatch":
+            coords_1 = image.sources.coords
+            coords_2 = catalog[["x", "y"]].values
+
+            _coords_2 = dict(zip(range(len(coords_2)), coords_2))
+            tolerance = 10
+            matches = {}
+
+            for i, coords in enumerate(coords_1):
+                idxs, available_coords = list(zip(*_coords_2.items()))
+                distances = np.linalg.norm(available_coords - coords, axis=1)
+                if np.all(np.isnan(available_coords)):
+                    break
+                closest = np.nanargmin(distances)
+                if distances[closest] < tolerance:
+                    matches[i] = idxs[closest]
+                    del _coords_2[idxs[closest]]
+                else:
+                    matches[i] = None
+
+            new_df_dict = []
+            nans = {name: np.nan for name in image.catalogs[self.catalog_name].keys()}
+
+            for i in range(len(coords_1)):
+                if matches[i] is not None:
+                    new_df_dict.append(
+                        dict(image.catalogs[self.catalog_name].iloc[int(matches[i])])
+                    )
+                    pass
+                else:
+                    new_df_dict.append(nans)
+
+            catalog = pd.DataFrame(new_df_dict)
+
+        image.catalogs[self.catalog_name] = catalog.iloc[0 : self.limit]
+
+
+class PlateSolve(Block):
+    def __init__(
+        self, reference=None, n=30, tolerance=10, radius=1.2, debug=False, **kwargs
+    ):
+        """Plate solve an image using twirl
+
+        |read| :code:`Image.sources`
+
+        |write| :code:`Image.wcs`, :code:`Image.plate_solved_success`
+
+        Parameters
+        ----------
+        reference : Image, optional
+            _description_, by default None
+        n : int, optional
+            _description_, by default 30
+        tolerance : int, optional
+            _description_, by default 10
+        radius : float, optional
+            _description_, by default 1.2
+        debug : bool, optional
+            _description_, by default False
+        """
+        super().__init__(**kwargs)
+        self.radius = radius * u.arcmin.to("deg")
+        self.n = n
+        self.reference = reference
+        self.tolerance = tolerance
+        self.debug = debug
+
+    def run(self, image):
+        stars = image.sources.coords * image.pixel_scale.to("deg").value
+        stars = sparsify(stars, self.radius) / image.pixel_scale.to("deg").value
+
+        if self.reference is None:
+            table = image_gaia_query(
+                image, wcs=False, circular=True, fov=image.fov.max()
+            ).to_pandas()
+            gaias = np.array([table.ra, table.dec]).T
+            gaias = gaias[~np.any(np.isnan(gaias), 1)]
+        else:
+            gaias = self.reference.catalogs["gaia"][["ra", "dec"]].values
+
+        gaias = sparsify(gaias, self.radius)
+
+        new_wcs = twirl._compute_wcs(
+            stars[0 : self.n], gaias[0 : self.n], n=self.n, tolerance=self.tolerance
+        )
+        image.wcs = new_wcs
+        coords = np.array(image.wcs.world_to_pixel(SkyCoord(gaias, unit="deg"))).T
+        idxs = cross_match(image.sources.coords, coords, return_idxs=True)
+        image.computed["plat_solve_success"] = np.count_nonzero(
+            ~np.isnan(idxs[:, 1])
+        ) / len(gaias)
+
+        if self.debug:
+            image.show()
+            coords = np.array(image.wcs.world_to_pixel(SkyCoord(gaias, unit="deg"))).T
+            _gaias = Sources([PointSource(coords=c) for c in coords])
+            _gaias.plot(c="y")
+
+    @property
+    def citations(self):
+        return super().citations + ["twirl"]
+
+
+class GaiaCatalog(_CatalogBlock):
+    def __init__(self, correct_pm=True, limit=10000, mode=None):
+        """Query gaia catalog
+
+        Catalog is written in Image.catalogs as a pandas DataFrame. If mode is ""crossmatch" the index of catalog sources in the DataFrame matches with the index of sources in Image.sources
+
+        |read| :code:`Image.sources` if mode is "crossmatch"
+
+        |write| :code:`Image.catalogs`
+
+        - :code:`Image.sources` if mode is "crossmatch"
+        - :code:`Image.catalogs`
+
+        Parameters
+        ----------
+        correct_pm : bool, optional
+            whether to correct proper motion, by default True
+        limit : int, optional
+            limit number of stars queried, by default 10000
+        mode: str, optional
+            "crossmatch" to match existing Image.sources or "replace" to use queried stars as Image.sources
+        """
+        _CatalogBlock.__init__(self, "gaia", limit=limit, mode=mode)
+        self.correct_pm = correct_pm
+
+    def get_catalog(self, image):
+        max_fov = image.fov.max() * np.sqrt(2)
+        table = image_gaia_query(
+            image, correct_pm=self.correct_pm, limit=500000, circular=True, fov=max_fov
+        )
+        table.rename_column("DESIGNATION", "id")
+        return table
+
+    def run(self, image):
+        _CatalogBlock.run(self, image)
+
+    @property
+    def citations(self):
+        return super().citations + ["astroquery"]
+
+
+class TESSCatalog(_CatalogBlock):
+    def __init__(self, limit=10000, mode=None):
+        """Query TESS (TIC) catalog
+
+        Catalog is written in Image.catalogs as a pandas DataFrame. If mode is ""crossmatch" the index of catalog sources in the DataFrame matches with the index of sources in Image.sources
+
+        |read| :code:`Image.sources` if mode is "crossmatch"
+
+        |write| :code:`Image.catalogs`
+
+        - :code:`Image.sources` if mode is "crossmatch"
+        - :code:`Image.catalogs`
+
+
+        Parameters
+        ----------
+        limit : int, optional
+            limit number of stars queried, by default 10000
+        mode: str, optional
+            "crossmatch" to match existing Image.sources or "replace" to use queried stars as Image.sources
+        """
+        _CatalogBlock.__init__(self, "tess", limit=limit, mode=mode)
+
+    def get_catalog(self, image):
+        max_fov = image.fov.max() * np.sqrt(2) / 2
+        table = Catalogs.query_region(
+            image.skycoord, radius=max_fov, catalog="TIC", verbose=False
+        )
+        table["ra"].unit = "deg"
+        table["dec"].unit = "deg"
+        table.rename_column("ID", "id")
+        return table
+
+    def run(self, image):
+        _CatalogBlock.run(self, image)
+
+    @property
+    def citations(self):
+        return super().citations + ["astroquery"]
---
+++ packages/prose/prose-3.1.1-py3-none-any.whl/prose/blocks/geometry.py
@@ -0,0 +1,212 @@
+from typing import Optional, Union
+
+import numpy as np
+from scipy.spatial import KDTree
+from skimage.transform import AffineTransform
+from twirl import utils as twirl_utils
+
+from prose.core import Block, Image
+
+__all__ = ["Trim", "Cutouts", "Drizzle"]
+
+
+class Trim(Block):
+    """Image trimming
+
+    If trim is not specified, triming is taken from the "overscan" in image metadata
+
+    |write| ``Image.header``
+
+    |modify|
+
+    Parameters
+    ----------
+    skip_wcs : bool, optional
+        whether to skip applying trim to WCS (If None: wcs is skipped only
+        if image is not plated solved), by default None
+    trim : tuple, int or flot, optional
+        (x, y) trim values, by default None which uses the ``trim`` value
+        from the image telescope definition. If an int or a float is provided trim will be be applied to both axes.
+
+    """
+
+    def __init__(self, trim=None, skip_wcs=None, name=None, verbose=False):
+        super().__init__(name, verbose)
+        assert (skip_wcs is None) or isinstance(
+            skip_wcs, bool
+        ), "skip_wcs must be None or bool"
+        self.skip_wcs = skip_wcs
+        if isinstance(trim, (int, float)):
+            trim = (trim, trim)
+        self.trim = trim
+        self._parallel_friendly = True
+
+    def run(self, image):
+        trim = self.trim if self.trim is not None else image.metadata["overscan"]
+        center = image.shape[::-1] / 2
+        shape = image.shape - 2 * np.array(trim)
+        if self.skip_wcs is None:
+            skip_wcs = not image.plate_solved
+        else:
+            skip_wcs = self.skip_wcs
+        cutout = image.cutout(center, shape, wcs=skip_wcs)
+        image.data = cutout.data
+        image.sources = cutout.sources
+        image.wcs = cutout.wcs
+
+
+class Cutouts(Block):
+    def __init__(
+        self,
+        shape: Union[int, tuple] = 50,
+        wcs: bool = False,
+        name: Optional[str] = None,
+        sources: bool = False,
+    ):
+        """Create cutouts around all sources
+
+        |read| :code:`Image.sources`
+
+        |write| :code:`Image.cutouts`
+
+        Parameters
+        ----------
+        shape : int or tuple, optional
+            cutout shape, by default 50
+        wcs : bool, optional
+            whether to compute cutouts WCS, by default False
+        name : str, optional
+            name of the blocks, by default None
+        sources: bool, optional
+            whether to keep sources in cutouts, by default False
+        """
+        super().__init__(name=name)
+        if isinstance(shape, int):
+            shape = (shape, shape)
+        self.shape = shape
+        self.wcs = wcs
+        self.sources = sources
+        self._parallel_friendly = True
+
+    def run(self, image: Image):
+        image.cutouts = [
+            image.cutout(coords, self.shape, wcs=self.wcs, sources=self.sources)
+            for coords in image.sources.coords
+        ]
+        f = 0
+
+
+class SetAffineTransform(Block):
+    def __init__(self, name=None, verbose=False):
+        super().__init__(name, verbose)
+        self._parallel_friendly = True
+
+    def run(self, image):
+        rotation = image.__dict__.get("rotation", 0)
+        translation = image.__dict__.get("translation", (0, 0))
+        scale = image.__dict__.get("scale", 0)
+        image.transform = AffineTransform(
+            rotation=rotation, translation=translation, scale=scale
+        )
+
+    @property
+    def citations(self):
+        return super().citations + ["scikit-image"]
+
+
+class ComputeTransform(Block):
+    """
+    Compute transformation fromm a reference image
+
+    |read| :code:`Image.sources` on both reference and input image
+
+    |write| :code:`Image.transform`
+
+    Parameters
+    ----------
+    ref : Image
+        image containing detected sources
+    n : int, optional
+        number of stars to consider to compute transformation, by default 10
+    """
+
+    def __init__(self, reference_image: Image, n=10, discard=True, **kwargs):
+        super().__init__(**kwargs)
+        ref_coords = reference_image.sources.coords
+        self.ref = ref_coords[0:n].copy()
+        self.n = n
+        self.quads_ref, self.stars_ref = twirl_utils.quads_stars(ref_coords, n=n)
+        self.KDTree = KDTree(self.quads_ref)
+        self.discard = discard
+        self._parallel_friendly = True
+
+    def run(self, image):
+        if len(image.sources.coords) >= 5:
+            result = self.solve(image.sources.coords)
+            if result is not None:
+                image.transform = AffineTransform(result)
+            else:
+                image.discard = True
+        else:
+            image.discard = True
+
+    def solve(self, coords, tolerance=2):
+        s = coords.copy()
+        quads, stars = twirl_utils.quads_stars(s, n=self.n)
+        _, indices = self.KDTree.query(quads)
+
+        # We pick the two asterismrefs leading to the highest stars matching
+        closeness = []
+        for i, m in enumerate(indices):
+            M = twirl_utils._find_transform(self.stars_ref[m], stars[i])
+            new_ref = twirl_utils.affine_transform(M)(self.ref)
+            closeness.append(
+                twirl_utils._count_cross_match(s, new_ref, tolerance=tolerance)
+            )
+
+        i = np.argmax(closeness)
+        m = indices[i]
+        S1 = self.stars_ref[m]
+        S2 = stars[i]
+        M = twirl_utils._find_transform(S1, S2)
+        new_ref = twirl_utils.affine_transform(M)(self.ref)
+
+        matches = twirl_utils.cross_match(
+            new_ref, s, tolerance=tolerance, return_ixds=True
+        ).T
+        if len(matches) == 0:
+            return None
+        else:
+            i, j = matches
+
+        return twirl_utils._find_transform(s[j], self.ref[i])
+
+
+class Drizzle(Block):
+    def __init__(self, reference, pixfrac=1.0, **kwargs):
+        """Produce a dithered image. Requires :code:`drizzle` package.
+
+        All images (including reference must be plate-solved). After :code:`terminate` is called
+        (e.g. when a sequence is entirely ran), the dithered image can be found in self.image
+
+        Parameters
+        ----------
+        reference : prose.Image
+            Reference image on which the stacking is based
+        pixfrac : float, optional
+            fraction of pixel used in dithering, by default 1.
+        """
+        from drizzle import drizzle
+
+        super().__init__(self, **kwargs)
+        self.pixfrac = pixfrac
+        reference.wcs.pixel_shape = reference.shape
+        self.drizzle = drizzle.Drizzle(outwcs=reference.wcs, pixfrac=pixfrac)
+        self.image = reference.copy()
+
+    def run(self, image):
+        WCS = image.wcs
+        self.drizzle.add_image(image.data, image.wcs)
+
+    def terminate(self):
+        self.image.data = self.drizzle.outsci
