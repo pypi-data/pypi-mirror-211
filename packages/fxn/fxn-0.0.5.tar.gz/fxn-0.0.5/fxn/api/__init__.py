d1f36719e59dd0311e26166544a2eab6b8a0a7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package fxn/0.0.5/fxn-0.0.5.tar.gz

---
+++ packages/fxn/fxn-0.0.5.tar.gz/fxn-0.0.5/fxn/api/__init__.py
@@ -0,0 +1,13 @@
+# 
+#   Function
+#   Copyright © 2023 NatML Inc. All Rights Reserved.
+#
+
+from .dtype import Dtype
+from .feature import Feature
+from .featureinput import FeatureInput
+from .prediction import CloudPrediction, EdgePrediction, Prediction
+from .predictor import Acceleration, AccessMode, Parameter, Predictor, PredictorStatus, PredictorType, Signature
+from .profile import Profile
+from .storage import Storage, UploadType
+from .user import User
