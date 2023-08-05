f4e60c6b89c21060cd9825c9c2c16915c71f90c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:21 +0000

    Add package pyprotogen/0.0.8/pyprotogen-0.0.8-py3-none-any.whl

---
+++ packages/pyprotogen/pyprotogen-0.0.8-py3-none-any.whl/pyprotogen/renderer.py
@@ -0,0 +1,59 @@
+import os
+import re
+import shutil
+
+from importlib import resources
+from pathlib import Path
+
+from grpc_tools import protoc
+from pyprotogen import settings
+
+
+def copy_dependencies(output_path: str) -> None:
+    shutil.copytree(
+        settings.DEPENDENCIES_DIR_PATH, Path(output_path).absolute(), dirs_exist_ok=True
+    )
+
+
+def gen_pb2_files(proto_path: str, output_path: str) -> None:
+    grpc_gen_path = Path(output_path).joinpath(settings.GRPC_GEN_PATH)
+    grpc_gen_path.mkdir(parents=True, exist_ok=True)
+
+    proto_files = []
+    path = Path(proto_path)
+    if path.is_file():
+        proto_dir = path.parent
+        proto_files.append(str(path))
+    else:
+        proto_dir = path if path.is_dir() else path.parent
+        for f in proto_dir.iterdir():
+            if f.name.endswith('.proto'):
+                proto_files.append(str(f))
+    args = [
+        '',
+        f'--proto_path={proto_dir}',
+        f'--python_out={grpc_gen_path}',
+        f'--mypy_out={grpc_gen_path}',
+        f'--grpc_python_out={grpc_gen_path}',
+        *proto_files,
+    ]
+    proto_include = resources.path('grpc_tools', '_proto')
+    include = ['-I{}'.format(proto_include)]
+    protoc.main(args + include)
+
+    for file in grpc_gen_path.rglob('*.py'):
+        name, extension = os.path.splitext(file.name)
+        if '.' in name:
+            new_name = f'{name.replace(".", "/")}{extension}'
+            os.rename(file, f'{file.parent}/{new_name}')
+
+    for file in grpc_gen_path.rglob('*.py'):
+        with open(file, "r") as sources:
+            lines = sources.readlines()
+        with open(file, "w") as sources:
+            for line in lines:
+                if re.search(r'^import .*_pb2 as', line) is not None:
+                    line = 'from . ' + line
+                if re.search(r'^from .* import .*_pb2 as', line) is not None:
+                    line = 'from . import' + line.split('import')[1]
+                sources.write(line)
