f542a8e80a4e94c16b7f2e1ab2bc7982652c5c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:40 +0000

    Add package oneone11/0.1.0/oneone11-0.1.0.tar.gz

---
+++ packages/oneone11/oneone11-0.1.0.tar.gz/oneone11-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oneone11',
+    version='0.1.0',
+    author='prashant',
+    author_email='prashantkrjha5@gmail.com',
+    description='1 and 1, 11',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
