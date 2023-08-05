387b74fa79806f6adcb1c1a61a9643f129c964cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:40 +0000

    Add package gncpy/1.0.1/gncpy-1.0.1-pp38-pypy38_pp73-macosx_10_9_x86_64.whl

---
+++ packages/gncpy/gncpy-1.0.1-pp38-pypy38_pp73-macosx_10_9_x86_64.whl/include/gncpy/dynamics/SerializableTypes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Include all headers that contain things that can be serialized within the
+// dynamics folder
+
+#include "gncpy/dynamics/DoubleIntegrator.h"
+#include "gncpy/dynamics/IDynamics.h"
+#include "gncpy/dynamics/ILinearDynamics.h"
+#include "gncpy/dynamics/Parameters.h"
