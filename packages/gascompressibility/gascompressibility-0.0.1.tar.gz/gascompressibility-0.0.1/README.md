d2b47a43beb4d030e78170692860bcc3743f0af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:40 +0000

    Add package gascompressibility/0.0.1/gascompressibility-0.0.1.tar.gz

---
+++ packages/gascompressibility/gascompressibility-0.0.1.tar.gz/gascompressibility-0.0.1/README.md
@@ -0,0 +1,19 @@
+[![Build Status](https://app.travis-ci.com/aegis4048/GasCompressibiltiy-py.svg?branch=master)](https://app.travis-ci.com/aegis4048/GasCompressibiltiy-py)
+
+<img src="/misc/intro_image.png" alt="Alt text" title="Optional title">
+
+# GasCompressibility-py
+GasCompressibility-py is a Python library for calculating the gas compressibility factor, $`Z`$, based on real gas law. 
+
+It is designed with practical oil field application in mind, in which the required inputs ($`T`$, $`P`$, and $`\gamma_{g}`$) can be readily obtained from the surface facility. 
+
+The packages is under [MIT License](https://github.com/aegis4048/GasCompressibiltiy-py/blob/main/LICENSE).
+
+
+# Authors
+
+* __[Eric 'Soobin' Kim](https://github.com/aegis4048)__ - Petroleum engineer with the gas compressor company, [Flogistix](https://flogistix.com/). Primary author of the package. (Contact | aegis4048@gmail.com, Website | [PythonicExcursions](https://aegis4048.github.io/))
+
+# Work in Progress
+Currently working on publishing the library to PIP, thanks for your patience.
+
---
+++ packages/gascompressibility/gascompressibility-0.0.1.tar.gz/gascompressibility-0.0.1/setup.py
@@ -0,0 +1,47 @@
+import pathlib
+from setuptools import setup, find_packages
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE/"README.md").read_text()
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read().strip()
+
+
+def classifiers():
+    with open('classifiers.txt') as f:
+        return f.read().strip().split('\n')
+
+
+setup(
+    name='gascompressibility',
+    version='0.0.1',
+    packages=find_packages(exclude=[
+        "tutorials",
+        "LICENSE",
+        ".gitignore",
+        "README.md",
+        "misc",
+        ".travis.yml",
+        "notes.py",
+    ]),
+    description='GasCompressibility-py is a Python library for calculating the gas compressibility factor, Z, based on real gas law.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=classifiers(),
+    license='MIT',
+    author='Eric Kim',
+    author_email='aegis4048@gmail.com',
+    install_requires=[
+        'numpy>=1.21.5',
+        'scipy>=1.9.1',
+        'matplotlib>=3.5.1',
+    ],
+    url='https://github.com/aegis4048/GasCompressibiltiy-py/tree/main',
+)
