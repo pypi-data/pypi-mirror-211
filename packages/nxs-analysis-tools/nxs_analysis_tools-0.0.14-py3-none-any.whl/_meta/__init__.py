af59ee17d3ece544250eef2844415ec1304fbcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:39 +0000

    Add package nxs-analysis-tools/0.0.14/nxs_analysis_tools-0.0.14-py3-none-any.whl

---
+++ packages/nxs-analysis-tools/nxs_analysis_tools-0.0.14-py3-none-any.whl/_meta/__init__.py
@@ -0,0 +1,10 @@
+'''MagentroPy package metadata.'''
+
+# keep consistent with pyproject.toml
+__project__ = 'nxs-analysis-tools'
+__author__ = 'Steven J. Gomez Alvarado'
+__email__ = 'stevenjgomez@ucsb.edu'
+__copyright__ = f"2023, {__author__}"
+__license__ = 'MIT'
+__version__= '0.0.14'
+__repo_url__ = 'https://github.com/stevenjgomez/nxs_analysis_tools'
