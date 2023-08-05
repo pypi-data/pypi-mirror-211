12ffa88c5745a3ff22a9695b7b3f7ceac6c45c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:13 +0000

    Add package keypact/0.3.0/keypact-0.3.0.tar.gz

---
+++ packages/keypact/keypact-0.3.0.tar.gz/keypact-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(name='keypact',
+version='0.3.0',
+description="""Efficient Key-Value Data Storage with Multithreaded Simultaneous Writing""",
+long_description="".join(open("README.md", encoding="utf-8").readlines()),
+long_description_content_type='text/markdown',
+url='https://github.com/onuratakan/KeyPact',
+author='Onur Atakan ULUSOY',
+author_email='atadogan06@gmail.com',
+license='MIT',
+packages=["keypact"],
+package_dir={'':'src'},
+install_requires=[
+    "fire==0.5.0"
+],
+entry_points = {
+    'console_scripts': ['keypact=keypact.keypact:main'],
+},
+python_requires=">= 3",
+zip_safe=False)
