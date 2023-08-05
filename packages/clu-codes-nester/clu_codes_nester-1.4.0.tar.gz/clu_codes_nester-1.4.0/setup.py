c75ceea99bd2096d20107f96ed421fe9a225709a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package clu-codes-nester/1.4.0/clu_codes_nester-1.4.0.tar.gz

---
+++ packages/clu-codes-nester/clu_codes_nester-1.4.0.tar.gz/clu_codes_nester-1.4.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="clu_codes_nester",
+    version="1.4.0",
+    py_modules=["clu_codes_nester"],
+    author="clu_codes",
+    author_email="clu.codes@gmail.com",
+    description="A simple printer of nested lists",
+)
+
+
+# to update or build, use command 'python3 setup.py sdist upload'
