8b3e3008a54162964bd692e2a0461c1e3588bc44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package asyncio-oss/1.0.0/asyncio-oss-1.0.0.tar.gz

---
+++ packages/asyncio-oss/asyncio-oss-1.0.0.tar.gz/asyncio-oss-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='asyncio-oss',
+    version='1.0.0',
+    description='An asynchronous OSS library.',
+    author='Ozzy',
+    author_email='ozzycharon@gmail.com',
+    license='MIT',
+    install_requires=['aiohttp', 'oss2'],
+    packages=find_packages(),
+)
