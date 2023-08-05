9df272e2149d930f451c538611d063ced5f3d4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:07 +0000

    Add package pingdatautil/0.0.4/pingdatautil-0.0.4.tar.gz

---
+++ packages/pingdatautil/pingdatautil-0.0.4.tar.gz/pingdatautil-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pingdatautil",
+    version="0.0.4",
+    author="Vorapol Ping",
+    description="Ping's Data Utility Package",
+    packages=find_packages(exclude=["test", "test*", "test-.*", "tests"]),
+    install_requires=[
+        "pyodbc >= 4.0.0",
+        "jaydebeapi >= 1.2.3",
+        "pandas >= 1.5.0",
+        "xlsxwriter >= 3.0.0",
+    ]
+)
