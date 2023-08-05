b39a5968a5a51470450d2a1d7ba886c0b170bf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package klp-commons/0.0.42/klp_commons-0.0.42-py2.py3-none-any.whl

---
+++ packages/klp-commons/klp_commons-0.0.42-py2.py3-none-any.whl/klp_commons/streaming/producer.py
@@ -0,0 +1,32 @@
+from kafka import KafkaProducer
+from json import dumps as dumps_
+
+
+class Producer:
+    def __init__(
+                self, 
+                topic = None,
+                kafka_brokers = None,
+                acks = None,
+                api_version = None,
+                client_id = None
+                 ):
+        self.kafka_api_version = api_version
+        self.topic = topic
+        self.acks = acks
+        self.client_id = client_id
+        self.kafka_brokers = kafka_brokers
+        
+    def init_producer(self) -> None:
+
+        self.producer = KafkaProducer(
+            acks = self.acks,
+            api_version=self.kafka_api_version,
+            bootstrap_servers = self.kafka_brokers,
+            value_serializer = lambda x: dumps_(x).encode('utf-8'),
+        )
+
+    def send_message(self, data: dict) -> None:
+        self.producer.send(self.topic, value=data)
+        # block until all async messages are sent
+        self.producer.flush()
