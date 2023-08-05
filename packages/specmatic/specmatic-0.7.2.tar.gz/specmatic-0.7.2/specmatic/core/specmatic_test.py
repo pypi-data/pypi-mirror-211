01d1bc24a054bfc61ecd651e33a7ed18feb63860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package specmatic/0.7.2/specmatic-0.7.2.tar.gz

---
+++ packages/specmatic/specmatic-0.7.2.tar.gz/specmatic-0.7.2/specmatic/core/specmatic_test.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import subprocess
+
+from specmatic.core.specmatic_base import SpecmaticBase
+from specmatic.utils import get_junit_report_dir_path
+
+
+class SpecmaticTest(SpecmaticBase):
+    def __init__(self, host: str = "127.0.0.1", port: int = 5000, project_root: str = '', contract_file_path: str = '',
+                 specmatic_json_file_path: str = ''):
+        super().__init__()
+        self.project_root = project_root
+        self.host = host
+        self.port = port
+        self.contract_file_path = contract_file_path
+        self.specmatic_json_file_path = specmatic_json_file_path
+
+    def _delete_existing_report_if_exists(self):
+        junit_report_dir_path = get_junit_report_dir_path()
+        if os.path.exists(junit_report_dir_path):
+            shutil.rmtree(junit_report_dir_path)
+
+    def _execute_specmatic(self):
+        self.validate_mandatory_fields()
+        jar_path = os.path.dirname(os.path.realpath(__file__)) + "/specmatic.jar"
+        cmd = [
+            "java",
+            "-jar",
+            jar_path,
+            "test"
+        ]
+        if self.specmatic_json_file_path != '':
+            cmd.append("--config=" + self.specmatic_json_file_path)
+        else:
+            if self.contract_file_path != '':
+                cmd.append(self.contract_file_path)
+            else:
+                cmd.append("--config=" + self.project_root + "/specmatic.json")
+
+        cmd += [
+            "--junitReportDir=" + get_junit_report_dir_path(),
+            '--host=' + self.host,
+            "--port=" + str(self.port)
+        ]
+
+        print(f"\n Running specmatic tests for api at {self.host}:{self.port}")
+        subprocess.run(cmd)
+
+    def run(self):
+        self._delete_existing_report_if_exists()
+        self._execute_specmatic()
