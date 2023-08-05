aac4eebb7f1d54c44753bfcdced2ac85b16e57d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:15 +0000

    Add package automation-file-dev/0.0.3/automation_file_dev-0.0.3.tar.gz

---
+++ packages/automation-file-dev/automation_file_dev-0.0.3.tar.gz/automation_file_dev-0.0.3/file_automation/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    "copy_file", "rename_file", "remove_file", "copy_all_file_to_dir", "copy_specify_extension_file",
+    "copy_dir", "create_dir", "copy_specify_extension_file", "remove_dir_tree",
+    "zip_dir", "zip_file", "zip_info", "zip_file_info", "set_zip_password", "unzip_file", "read_zip_file",
+    "unzip_all"
+]
