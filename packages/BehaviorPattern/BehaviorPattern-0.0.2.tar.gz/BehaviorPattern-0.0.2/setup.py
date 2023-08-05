d8ebf711c8039104c082220604992443544f2f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package BehaviorPattern/0.0.2/BehaviorPattern-0.0.2.tar.gz

---
+++ packages/BehaviorPattern/BehaviorPattern-0.0.2.tar.gz/BehaviorPattern-0.0.2/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+import pkg_resources
+
+# 读取README.md文件内容
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# 列出所有依赖包的名称
+dependencies = [
+    'numpy',
+    'pandas',
+    'tqdm',
+    'efficient_apriori==2.0.3',
+    'prefixspan==0.5.2',
+]
+
+# 检查是否已经安装了所有依赖库
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = set(dependencies) - installed
+
+# 如果存在未安装的依赖库，安装未安装的依赖库
+if missing:
+    setuptools.setup(
+        name="BehaviorPattern",
+        version="0.0.2",
+        author="Chen Chen",
+        author_email="cchen56@163.com",
+        description="The tool is designed to mine behavior patterns",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        install_requires=list(missing),
+    )
+else:
+    setuptools.setup(
+        name="BehaviorPattern",
+        version="0.0.2",
+        author="Chen Chen",
+        author_email="cchen56@163.com",
+        description="The tool is designed to mine behavior patterns",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+    )
