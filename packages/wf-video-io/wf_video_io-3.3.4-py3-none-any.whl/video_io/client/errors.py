f2c2133c79193e5ba53d30da27ed43e0531f6521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:54 +0000

    Add package wf-video-io/3.3.4/wf_video_io-3.3.4-py3-none-any.whl

---
+++ packages/wf-video-io/wf_video_io-3.3.4-py3-none-any.whl/video_io/client/errors.py
@@ -0,0 +1,16 @@
+class SyncError(Exception):
+    pass
+
+
+class BadVideoError(Exception):
+    pass
+
+
+class RequestError(Exception):
+    def __init__(self, response):
+        super().__init__(f"unexpected api response - {response.status_code}")
+        self.response = response
+
+
+class UnableToAuthenticate(Exception):
+    pass
