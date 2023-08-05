fa895ce02d9a4e5e307b623b4465f49ea1c8bd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:41 +0000

    Add package serializer-for-oop-progect/1.1.0/serializer-for-oop-progect-1.1.0.tar.gz

---
+++ packages/serializer-for-oop-progect/serializer-for-oop-progect-1.1.0.tar.gz/serializer-for-oop-progect-1.1.0/MySer/JSONSer.py
@@ -0,0 +1,31 @@
+import json
+from typing import Any
+
+
+class JSONEncoderForClass(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        try:
+            d = o.__dict__
+        except (TypeError, AttributeError):
+            pass
+        else:
+            return d
+        return json.JSONEncoder.default(self, o)
+
+
+class MyJSONSer:
+    @staticmethod
+    def dumps(o: Any):
+        return json.dumps(o, cls=JSONEncoderForClass)
+
+    @staticmethod
+    def dump(o: Any, fp):
+        return json.dump(o, fp, cls=JSONEncoderForClass)
+
+    @staticmethod
+    def loads(s):
+        return json.loads(s)
+
+    @staticmethod
+    def load(fp):
+        return json.load(fp)
