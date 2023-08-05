179e9f381f2c9c1da2baec94719eb7c7e4995113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package mask-util/0.0.9/mask_util-0.0.9-py3-none-any.whl

---
+++ packages/mask-util/mask_util-0.0.9-py3-none-any.whl/mask_util/const.py
@@ -0,0 +1,6 @@
+# Sensitive keywords
+from typing import List
+
+KEYWORDS: List[str] = ["password", "passwd", "token", "authorized_token", "access_token",
+                "security_token", "username", "user_name", "uname", "login", "logout", "ip", "ip_address", "ip_add",
+                "domain", "internal_domain", "realm"]
