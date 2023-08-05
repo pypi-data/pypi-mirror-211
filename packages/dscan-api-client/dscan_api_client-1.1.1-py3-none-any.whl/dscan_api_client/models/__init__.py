8679bc84c553d57cda9bba72f5feac25a1b07f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package dscan-api-client/1.1.1/dscan_api_client-1.1.1-py3-none-any.whl

---
+++ packages/dscan-api-client/dscan_api_client-1.1.1-py3-none-any.whl/dscan_api_client/models/__init__.py
@@ -0,0 +1,43 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .domain import Domain
+from .patched_program import PatchedProgram
+from .patched_scan import PatchedScan
+from .patched_scanner import PatchedScanner
+from .patched_subdomain import PatchedSubdomain
+from .patched_task import PatchedTask
+from .program import Program
+from .program_subdomain_check import ProgramSubdomainCheck
+from .program_subdomain_detail_save_check import ProgramSubdomainDetailSaveCheck
+from .scan import Scan
+from .scan_detailed import ScanDetailed
+from .scanner import Scanner
+from .schema_retrieve_format import SchemaRetrieveFormat
+from .schema_retrieve_response_200 import SchemaRetrieveResponse200
+from .status_enum import StatusEnum
+from .subdomain import Subdomain
+from .subdomain_detail import SubdomainDetail
+from .subdomain_with_detail import SubdomainWithDetail
+from .task import Task
+
+__all__ = (
+    "Domain",
+    "PatchedProgram",
+    "PatchedScan",
+    "PatchedScanner",
+    "PatchedSubdomain",
+    "PatchedTask",
+    "Program",
+    "ProgramSubdomainCheck",
+    "ProgramSubdomainDetailSaveCheck",
+    "Scan",
+    "ScanDetailed",
+    "Scanner",
+    "SchemaRetrieveFormat",
+    "SchemaRetrieveResponse200",
+    "StatusEnum",
+    "Subdomain",
+    "SubdomainDetail",
+    "SubdomainWithDetail",
+    "Task",
+)
