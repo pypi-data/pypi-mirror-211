68063853a04dc5d3868d2ba3f4cf094ba65a8f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:40 +0000

    Add package get-cli/0.1.3/get_cli-0.1.3.tar.gz

---
+++ packages/get-cli/get_cli-0.1.3.tar.gz/get_cli-0.1.3/get_cli/controller.py
@@ -0,0 +1,70 @@
+import typer
+from typing import Protocol, List
+from get_cli.service import Service
+from get_cli import __app_name__, __version__
+
+
+class Controller(Protocol):
+    def __init__(self, service: Service):
+        ...
+
+    def get_version(flag: bool):
+        ...
+
+    def get(key: str):
+        ...
+
+    def list_keys(flag: bool):
+        ...
+
+    def list_items(flag: bool):
+        ...
+
+    def update(key_values: List[str]):
+        ...
+
+    def edit_with_editor(editor: str):
+        ...
+
+
+class GetController:
+    def __init__(self, service: Service):
+        self.service = service
+
+    def get_version(self, flag: bool):
+        if flag:
+            print(f"{__app_name__} v{__version__}")
+            raise typer.Exit()
+
+    def get(self, key: str):
+        if key:
+            value = self.service.get(key)
+            if not value:
+                print('Not found')
+                raise typer.Exit()
+            print(value)
+        raise typer.Exit()
+
+    def list_keys(self, flag: bool):
+        if flag:
+            print(self.service.list_keys())
+        raise typer.Exit()
+
+    def list_items(self, flag: bool):
+        if flag:
+            print(self.service.list_items())
+        raise typer.Exit()
+
+    def update(self, key_values: List[str]):
+        if key_values:
+            data = dict([i.split(':') for i in key_values])
+            try:
+                self.service.update(data)
+                print('Success!')
+            except Exception:
+                print('Fail...')
+        raise typer.Exit()
+
+    def edit_with_editor(self, editor: str):
+        self.service.edit_with_editor(editor)
+        raise typer.Exit()
