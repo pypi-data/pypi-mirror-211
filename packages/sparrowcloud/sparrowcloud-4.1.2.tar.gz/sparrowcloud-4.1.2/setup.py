e122c7c6d03f83c5b7928d972f6483be2183b898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package sparrowcloud/4.1.2/sparrowcloud-4.1.2.tar.gz

---
+++ packages/sparrowcloud/sparrowcloud-4.1.2.tar.gz/sparrowcloud-4.1.2/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+# import io
+import os
+import sys
+import shutil
+from setuptools import find_packages, setup
+
+version = "v4.1.2"
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    if os.system("twine check dist/*"):
+        print("twine check failed. Packages might be outdated.")
+        print("Try using `pip install -U twine wheel`.\nExiting.")
+        sys.exit()
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('sparrowcloud.egg-info')
+    sys.exit()
+
+
+setup(
+    name='sparrowcloud',
+    version=version,
+    license='MIT',
+    description='基础Django和drf的微服务框架扩展',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='sparrow',
+    author_email='',  # SEE NOTE BELOW (*)
+    url='https://gitee.com/sparrow614/sparrow_cloud',
+    packages=find_packages(include=[
+                           'sparrow_cloud', 'sparrow_cloud.*', '*.sparrow_cloud.*', '*.sparrow_cloud']),
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.28.2',
+        'coreapi>=2.3.3',
+        'PyJWT>=2.6.0',
+        'openapi_codec>=1.3.2',
+        'dulwich>=0.21.3',
+        'pika>=1.3.1',
+        'cryptography>=39.0.2',
+        'opentracing>=2.4.0',
+        'jaeger-client>=4.8.0',
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        # 'Framework :: Django',
+        # 'Framework :: Django :: 1.9',
+        'Framework :: Django :: 3.2',
+        # 'Intended Audience :: Developers',
+        # 'Operating System :: OS Independent',
+        # 'Programming Language :: Python',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+
+)
---
+++ packages/sparrowcloud/sparrowcloud-4.1.2.tar.gz/sparrowcloud-4.1.2/tests/test_jwtmiddleware.py
@@ -0,0 +1,148 @@
+import jwt
+import os
+import time
+import unittest
+
+JWT_SECRET = "hard_to_guess_string"
+USER_ID = 'abcedfg1234567'
+
+# # 有效的对称加密token
+# class MockRequest(object):
+#     def __init__(self):
+#         self.META = {"HTTP_AUTHORIZATION": self.get_token()}
+
+#     def get_token(self):
+#         payload = {"uid": USER_ID, 
+#                 "app_id": "app_0000",
+#                 "exp": int(time.time()+60*60), 
+#                 "iat": int(time.time()), 
+#                 "iss": "test"
+#             }
+#         return b'Token '+jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+
+# # 过期无效的对称加密token
+# class MockInvalidRequest(object):
+#     def __init__(self):
+#         self.META = {"HTTP_AUTHORIZATION": self.get_token()}
+
+#     def get_token(self):
+#         payload = { "uid": USER_ID, 
+#                 "app_id": "app_0000",
+#                 "exp": int(time.time()-100),
+#                 "iat": int(time.time()-500),
+#                 "iss": "test"
+#             }
+#         return b'Token '+jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+
+# 有效的非对称加密token
+class MockAsyRequest(object):
+    def __init__(self):
+        self.META = {"HTTP_AUTHORIZATION": self.get_token()}
+
+    def get_token(self):
+        payload = { "uid": USER_ID, 
+                "app_id": "app_0000",
+                "exp": int(time.time()+60*60), 
+                "iat": int(time.time()), 
+                "iss": "test"
+            }
+        private_key = open(os.getenv("PRIVATE_KEY_PATH")).read()
+        token = "Token " + jwt.encode(payload, private_key, algorithm='RS256')
+        return bytes(token, encoding="utf8")
+
+# 过期无效的非对称加密token
+class MockInvalidAsyRequest(object):
+    def __init__(self):
+        self.META = {"HTTP_AUTHORIZATION": self.get_token()}
+
+    def get_token(self):
+        payload = { "uid": USER_ID, 
+                "app_id": "app_0000",
+                "exp": int(time.time()-100),
+                "iat": int(time.time()-500),
+                "iss": "test"
+            }
+        private_key = open(os.getenv("PRIVATE_KEY_PATH")).read()
+        token = "Token " + jwt.encode(payload, private_key, algorithm='RS256')
+        return bytes(token, encoding="utf8")
+
+# 空token
+class MockEmptyRequest(object):
+    def __init__(self):
+        self.META = {"HTTP_AUTHORIZATION": b'token '}
+
+class TestJWTMiddleware(unittest.TestCase):
+
+    def setUp(self):
+        os.environ.setdefault("JWT_SECRET", JWT_SECRET)
+        os.environ.setdefault("PRIVATE_KEY_PATH", "./tests/rsa_private.pem")
+        os.environ.setdefault("SC_JWT_PUBLIC_KEY", open("./tests/rsa_public.pem").read())
+        # os.environ.setdefault("SC_JWT_PUBLIC_KEY_PATH", "./tests/rsa_public.pem")
+
+    # def test_normal_token(self):
+    #     '''
+    #     测试对称加密token
+    #     '''
+    #     from sparrow_cloud.middleware.jwt_middleware import JWTMiddleware
+    #     request = MockRequest()
+    #     JWTMiddleware().process_request(request)
+    #     self.assertIn("REMOTE_USER", request.META)
+    #     self.assertIn("payload", request.META)
+    #     self.assertIn("X-Jwt-Payload", request.META)
+    #     self.assertEqual(USER_ID, request.META.get("REMOTE_USER"))
+    #     self.assertIsNotNone(request.META.get("payload"))
+
+    # def test_invaid_nornal_token(self):
+    #     '''
+    #     测试对称加密token过期无效
+    #     '''
+    #     from sparrow_cloud.middleware.jwt_middleware import JWTMiddleware
+    #     request = MockInvalidRequest()
+    #     JWTMiddleware().process_request(request)
+    #     self.assertIn("REMOTE_USER", request.META)
+    #     self.assertIn("payload", request.META)
+    #     self.assertNotIn("X-Jwt-Payload", request.META)
+    #     self.assertIsNone(request.META.get("REMOTE_USER"))
+    #     self.assertIsNone(request.META.get("payload"))
+
+    def test_asy_token(self):
+        '''
+        测试非对称加密
+        '''
+        from sparrow_cloud.middleware.jwt_middleware import JWTMiddleware
+        request = MockAsyRequest()
+        JWTMiddleware().process_request(request)
+        self.assertIn("REMOTE_USER", request.META)
+        self.assertIn("payload", request.META)
+        self.assertIn("X-Jwt-Payload", request.META)
+        self.assertEqual(USER_ID, request.META.get("REMOTE_USER"))
+        self.assertIsNotNone(request.META.get("payload"))
+
+    def test_invalid_asy_token(self):
+        '''
+        测试过期无效的非对称加密
+        '''
+        from sparrow_cloud.middleware.jwt_middleware import JWTMiddleware
+        request = MockInvalidAsyRequest()
+        JWTMiddleware().process_request(request)
+        self.assertIn("REMOTE_USER", request.META)
+        self.assertIn("payload", request.META)
+        self.assertNotIn("X-Jwt-Payload", request.META)
+        self.assertIsNone(request.META.get("REMOTE_USER"))
+        self.assertIsNone(request.META.get("payload"))
+
+    def test_empty_auth_token(self):
+        '''
+        测试携带空的token
+        '''
+        from sparrow_cloud.middleware.jwt_middleware import JWTMiddleware
+        request = MockEmptyRequest()
+        JWTMiddleware().process_request(request)
+        self.assertIn("REMOTE_USER", request.META)
+        self.assertIn("payload", request.META)
+        self.assertNotIn("X-Jwt-Payload", request.META)
+        self.assertIsNone(request.META.get("REMOTE_USER"))
+        self.assertIsNone(request.META.get("payload"))
+
+if __name__ == '__main__':
+    unittest.main()
