e27270737494cca69ca0499d2555d5b946041eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package stale-maccarone/0.0.4/stale_maccarone-0.0.4.tar.gz

---
+++ packages/stale-maccarone/stale_maccarone-0.0.4.tar.gz/stale_maccarone-0.0.4/support/rename_to_stale.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+# hackishly rewrite the project to use a different name (to support self-hosting)
+
+set -eux
+
+find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/import maccarone/import stale_maccarone/'
+find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/from maccarone/from stale_maccarone/'
+
+sed -i 's/name = "maccarone"/name = "stale_maccarone"/' pyproject.toml
+
+mv src/{maccarone,stale_maccarone}
