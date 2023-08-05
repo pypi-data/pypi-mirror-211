51d53a03de9afa9c0c95f80881da6de979edf583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:07 +0000

    Add package zssfunniest/0.1.1/zssfunniest-0.1.1.tar.gz

---
+++ packages/zssfunniest/zssfunniest-0.1.1.tar.gz/zssfunniest-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='zssfunniest',
+      version='0.1.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='zheng ss',
+      author_email='szheng@example.com',
+      license='MIT',
+      packages=['zssfunniest'],
+      zip_safe=False)
