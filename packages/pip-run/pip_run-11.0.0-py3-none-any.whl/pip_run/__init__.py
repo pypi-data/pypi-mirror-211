6d37af8dfbb821e820beb19cb88ef87585e57dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package pip-run/11.0.0/pip_run-11.0.0-py3-none-any.whl

---
+++ packages/pip-run/pip_run-11.0.0-py3-none-any.whl/pip_run/__init__.py
@@ -0,0 +1,17 @@
+import sys
+
+from . import deps
+from . import commands
+from . import launch
+from . import scripts
+
+
+def run(args=sys.argv[1:]):
+    """
+    Main entry point for pip-run.
+    """
+    pip_args, py_args = commands.infer_ipython(commands.separate(args))
+    commands.intercept(pip_args)
+    pip_args.extend(scripts.DepsReader.search(py_args))
+    with deps.load(*deps.not_installed(pip_args)) as home:
+        raise SystemExit(launch.with_path(home, py_args))
