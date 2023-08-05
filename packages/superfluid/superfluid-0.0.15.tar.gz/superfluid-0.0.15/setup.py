55ff1da5836b5731afcf0072db5e2a7ea360a7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package superfluid/0.0.15/superfluid-0.0.15.tar.gz

---
+++ packages/superfluid/superfluid-0.0.15.tar.gz/superfluid-0.0.15/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="superfluid",
+    version="0.0.15",
+    description="Python SDK for the Superfluid Protocol",
+    package_dir={"": "main"},
+    packages=find_packages(where="main"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Godspower-Eze/superfluid.py",
+    author="Godspower-Eze",
+    author_email="Godspowereze260@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=["web3 == 6.3.0", "python-decouple==3.8"],
+    extras_require={
+        "dev": ["twine>=4.0.2"]
+    },
+    python_requires=">=3"
+)
