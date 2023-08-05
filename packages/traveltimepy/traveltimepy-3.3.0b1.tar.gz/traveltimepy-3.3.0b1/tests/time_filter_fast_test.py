0995c1525d102b0585fa61a387db3f34b591e4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package traveltimepy/3.3.0b1/traveltimepy-3.3.0b1.tar.gz

---
+++ packages/traveltimepy/traveltimepy-3.3.0b1.tar.gz/traveltimepy-3.3.0b1/tests/time_filter_fast_test.py
@@ -0,0 +1,28 @@
+from traveltimepy.dto.requests.time_filter_fast import Transportation
+
+
+def test_one_to_many(sdk, locations):
+    results = sdk.time_filter_fast(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        transportation=Transportation(type="public_transport"),
+    )
+
+    assert len(results) > 0
+
+
+def test_many_to_one(sdk, locations):
+    results = sdk.time_filter_fast(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        transportation=Transportation(type="public_transport"),
+        one_to_many=False,
+    )
+
+    assert len(results) > 0
---
+++ packages/traveltimepy/traveltimepy-3.3.0b1.tar.gz/traveltimepy-3.3.0b1/tests/time_filter_test.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from traveltimepy import PublicTransport
+
+
+def test_departures(sdk, locations):
+    results = sdk.time_filter(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        transportation=PublicTransport(),
+        departure_time=datetime.now(),
+    )
+    assert len(results) == 2
+
+
+def test_arrivals(sdk, locations):
+    results = sdk.time_filter(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        transportation=PublicTransport(),
+        departure_time=datetime.now(),
+    )
+    assert len(results) == 2
