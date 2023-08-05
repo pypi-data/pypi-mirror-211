b21b1605d5582afecab2eae59b6a974f1434d20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package torch-adapters/0.0.1/torch_adapters-0.0.1.tar.gz

---
+++ packages/torch-adapters/torch_adapters-0.0.1.tar.gz/torch_adapters-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['torch_adapters', 'torch_adapters.adapters']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch>=2.0.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'torch-adapters',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': '# torch-adapters',
+    'author': 'ma2za',
+    'author_email': 'mazzapaolo2019@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
