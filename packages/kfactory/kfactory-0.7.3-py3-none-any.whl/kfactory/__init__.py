77f312e5393dd1b93fda8cf72a6ac2d03ab5eead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package kfactory/0.7.3/kfactory-0.7.3-py3-none-any.whl

---
+++ packages/kfactory/kfactory-0.7.3-py3-none-any.whl/kfactory/__init__.py
@@ -0,0 +1,53 @@
+"""KFactory package. Utilities for creating photonic devices.
+
+Uses the klayout package as a backend.
+
+"""
+
+# The import order matters, we need to first import the important stuff.
+# isort:skip_file
+
+import klayout.dbcore as kdb
+import klayout.lay as lay
+from .kcell import (
+    KCell,
+    Instance,
+    Port,
+    Ports,
+    cell,
+    kcl,
+    KCLayout,
+    default_save,
+    LayerEnum,
+    show,
+)
+from . import cells, placer, routing, utils, port, pdk
+from .conf import config
+from .pdk import Pdk
+
+__version__ = "0.7.3"
+
+logger = config.logger
+
+__all__ = [
+    "KCell",
+    "Instance",
+    "Port",
+    "Ports",
+    "cell",
+    "kcl",
+    "KCLayout",
+    "default_save",
+    "kdb",
+    "lay",
+    "port",
+    "cells",
+    "placer",
+    "routing",
+    "utils",
+    "show",
+    "config",
+    "LayerEnum",
+    "logger",
+    "pdk",
+]
