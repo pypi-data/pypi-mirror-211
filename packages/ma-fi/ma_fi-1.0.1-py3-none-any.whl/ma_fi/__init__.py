390fc83034238a719c9c03a7bf318f0ac26246a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package ma-fi/1.0.1/ma_fi-1.0.1-py3-none-any.whl

---
+++ packages/ma-fi/ma_fi-1.0.1-py3-none-any.whl/ma_fi/__init__.py
@@ -0,0 +1,6 @@
+import os
+import pandas as pd
+
+# Get the path of the package directory
+package_dir = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(package_dir, "ISIN_sectors_ma.csv")
