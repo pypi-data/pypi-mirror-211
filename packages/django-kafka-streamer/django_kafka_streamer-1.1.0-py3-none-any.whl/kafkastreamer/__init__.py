7f93c76d10e8ea7d25093146efaa3ab2a28487f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:16 +0000

    Add package django-kafka-streamer/1.1.0/django_kafka_streamer-1.1.0-py3-none-any.whl

---
+++ packages/django-kafka-streamer/django_kafka_streamer-1.1.0-py3-none-any.whl/kafkastreamer/__init__.py
@@ -0,0 +1,8 @@
+from .constants import *  # noqa
+from .context import *  # noqa
+from .decorators import *  # noqa
+from .registry import *  # noqa
+from .serializers import *  # noqa
+from .stream import *  # noqa
+
+__version__ = "1.1.0"
---
+++ packages/django-kafka-streamer/django_kafka_streamer-1.1.0-py3-none-any.whl/kafkastreamer/settings.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+DEFAULTS = {
+    "BOOTSTRAP_SERVERS": None,
+    "PRODUCER_OPTIONS": {},
+    "BATCH_SIZE": 500,
+    "DEFAULT_SOURCE": None,
+    "DEFAULT_MESSAGE_SERIALIZER": (
+        "kafkastreamer.serializers.flat_json_message_serializer"
+    ),
+    "DEFAULT_PARTITION_KEY_SERIALIZER": (
+        "kafkastreamer.serializers.object_id_key_serializer"
+    ),
+}
+
+
+def get_setting(setting_name, resolve=False):
+    value = settings.KAFKA_STREAMER.get(
+        setting_name,
+        DEFAULTS[setting_name],
+    )
+    if resolve and type(value) is str:
+        value = import_string(value)
+    return value
