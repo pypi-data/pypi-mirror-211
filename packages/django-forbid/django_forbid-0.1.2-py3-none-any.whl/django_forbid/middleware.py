e6263b7217ff7dace37d7b9004219f292d49e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:47 +0000

    Add package django-forbid/0.1.2/django_forbid-0.1.2-py3-none-any.whl

---
+++ packages/django-forbid/django_forbid-0.1.2-py3-none-any.whl/django_forbid/middleware.py
@@ -0,0 +1,21 @@
+from .skills.forbid_device import ForbidDeviceMiddleware
+from .skills.forbid_location import ForbidLocationMiddleware
+from .skills.forbid_network import ForbidNetworkMiddleware
+
+__skills__ = (
+    ForbidNetworkMiddleware,
+    ForbidLocationMiddleware,
+    ForbidDeviceMiddleware,
+)
+
+
+class ForbidMiddleware:
+    """Middleware to forbid access to the site."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        for skill in __skills__:
+            self.get_response = skill(self.get_response)
+        return self.get_response(request)
