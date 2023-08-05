989345bbf1e8b4c03931fe2f9e28af952077a483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:47 +0000

    Add package docstruct/1.0.226/docstruct-1.0.226.tar.gz

---
+++ packages/docstruct/docstruct-1.0.226.tar.gz/docstruct-1.0.226/docstruct/__init__.py
@@ -0,0 +1,23 @@
+from .bounding_box import BoundingBox
+from .drawer import Drawer
+from .point import Point
+from .segment import Segment, Segment2D
+from .text_block import (
+    Character,
+    Document,
+    Page,
+    Area,
+    Line,
+    Paragraph,
+    TextBlock,
+    Word,
+    Table,
+    TableColumn,
+    TableCell,
+)
+from .vis_line import VisLine, VisLineOrientation
+from .text_block_splitter import TextBlockSplitter
+from .spatial_grid_indexing import SpatialGrid
+
+
+__version__ = "1.0.226"
