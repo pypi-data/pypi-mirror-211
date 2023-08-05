7bd8223e90692c6a8e4797fc1642ccc67e78dd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:36 +0000

    Add package eXirt/1.0.2/eXirt-1.0.2.tar.gz

---
+++ packages/eXirt/eXirt-1.0.2.tar.gz/eXirt-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='eXirt',
+      description='Explainable Artificial Intelligence tool base in Item Response Theory.',
+      author='José de Sousa Ribeiro Filho',
+      author_email='jose.sousa.filho@gmail.com',
+      version='1.0.2',
+      packages=['pyexirt']
+      )
+
