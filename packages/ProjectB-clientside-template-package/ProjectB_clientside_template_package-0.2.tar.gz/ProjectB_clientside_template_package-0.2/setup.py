7817e8e220e492eef73d662f927b1f817e2cb366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:48 +0000

    Add package ProjectB-clientside-template-package/0.2/ProjectB_clientside_template_package-0.2.tar.gz

---
+++ packages/ProjectB-clientside-template-package/ProjectB_clientside_template_package-0.2.tar.gz/ProjectB_clientside_template_package-0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+     name='ProjectB_clientside_template_package',  
+     version='0.2',
+     #scripts=['ProjectB_clientside_template_package'] ,
+     author="ProjectB",
+     author_email="admin@projectb.click",
+     description="This is the pip package of ProjectB_clientside_template_package",
+     long_description_content_type="text/markdown",
+     url="https://github.com/ProjectBSource/ProjectB_clientside_JAVA_template/tree/change_TradeController_features_to_local",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
+
+#https://pypi.org/project/ProjectB-clientside-template-package/0.1/
