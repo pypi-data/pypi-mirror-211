31eea625b126bb0bf73c87bb05ab295e09dd6cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:34 +0000

    Add package cnd-cr-obj-naming/1.0.2/cnd_cr_obj_naming-1.0.2-py3-none-any.whl

---
+++ packages/cnd-cr-obj-naming/cnd_cr_obj_naming-1.0.2-py3-none-any.whl/cr_obj_naming/__init__.py
@@ -0,0 +1,3 @@
+from cr_obj_naming.__version__ import (__version__)  # noqa: F401
+from cr_obj_naming.obj_naming import ObjNaming  # noqa: F401
+from cr_obj_naming.cnd_consul import CndConsul  # noqa: F401
