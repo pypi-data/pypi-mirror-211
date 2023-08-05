0a5d14012fc1ae1ed13152fb963a4260b6128e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:45 +0000

    Add package python-tools-scripts/0.13.5/python-tools-scripts-0.13.5.tar.gz

---
+++ packages/python-tools-scripts/python-tools-scripts-0.13.5.tar.gz/python-tools-scripts-0.13.5/src/ptscripts/__main__.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import logging
+import sys
+
+from ptscripts.parser import Parser
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    """
+    Main CLI entry-point for python tools scripts.
+    """
+    parser = Parser()
+    cwd = str(parser.repo_root)
+    log.debug(f"Searching for tools in {cwd}")
+    if cwd in sys.path:
+        sys.path.remove(cwd)
+    sys.path.insert(0, cwd)
+    try:
+        import tools  # noqa: F401
+    except ImportError:
+        # No tools/ directory in the current CWD
+        pass
+    parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
