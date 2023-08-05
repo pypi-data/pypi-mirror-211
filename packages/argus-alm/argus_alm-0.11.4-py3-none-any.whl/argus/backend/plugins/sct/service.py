f17640b432ccd46d4a7b93fa0db7bf2d94900588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package argus-alm/0.11.4/argus_alm-0.11.4-py3-none-any.whl

---
+++ packages/argus-alm/argus_alm-0.11.4-py3-none-any.whl/argus/backend/plugins/sct/service.py
@@ -0,0 +1,237 @@
+from dataclasses import dataclass
+import logging
+from time import time
+from argus.backend.plugins.sct.testrun import SCTTestRun
+from argus.backend.plugins.sct.udt import (
+    CloudInstanceDetails,
+    CloudResource,
+    EventsBySeverity,
+    NemesisRunInfo,
+    NodeDescription,
+    PackageVersion,
+)
+from argus.backend.util.enums import NemesisStatus, ResourceState
+
+LOGGER = logging.getLogger(__name__)
+
+
+class SCTServiceException(Exception):
+    pass
+
+
+@dataclass(init=True, repr=True)
+class NemesisSubmissionRequest:
+    name: str
+    class_name: str
+    start_time: int
+    node_name: str
+    node_ip: str
+    node_shards: int
+
+
+@dataclass(init=True, repr=True)
+class NemesisFinalizationRequest:
+    name: str
+    start_time: int
+    status: str
+    message: str
+
+
+@dataclass(init=True, repr=True)
+class EventSubmissionRequest:
+    severity: str
+    total_events: int
+    messages: list[str]
+
+
+class SCTService:
+
+    @staticmethod
+    def submit_packages(run_id: str, packages: list[dict]) -> str:
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            for package_dict in packages:
+                package = PackageVersion(**package_dict)
+                run.packages.append(package)
+            run.save()
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "added"
+
+
+    @staticmethod
+    def set_sct_runner(run_id: str, public_ip: str, private_ip: str, region: str, backend: str):
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            run.sct_runner_host = CloudInstanceDetails(
+                public_ip=public_ip,
+                private_ip=private_ip,
+                provider=backend,
+                region=region,
+            )
+            run.save()
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "updated"
+
+    @staticmethod
+    def submit_screenshots(run_id: str, screenshot_links: list[str]) -> str:
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            for link in screenshot_links:
+                run.add_screenshot(link)
+            run.save()
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "submitted"
+
+    @staticmethod
+    def create_resource(run_id: str, resource_details: dict) -> str:
+        instance_details = CloudInstanceDetails(**resource_details.pop("instance_details"))
+        resource = CloudResource(**resource_details, instance_info=instance_details)
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            run.get_resources().append(resource)
+            run.save()
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "created"
+
+    @staticmethod
+    def update_resource_shards(run_id: str, resource_name: str, new_shards: int) -> str:
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            resource = next(res for res in run.get_resources() if res.name == resource_name)
+            resource.get_instance_info().shards_amount = new_shards
+            run.save()
+        except StopIteration as exception:
+            LOGGER.error("Resource %s not found in run %s", resource_name, run_id)
+            raise SCTServiceException("Resource not found", resource_name) from exception
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "updated"
+
+    @staticmethod
+    def terminate_resource(run_id: str, resource_name: str, reason: str) -> str:
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            resource = next(res for res in run.get_resources() if res.name == resource_name)
+            resource.get_instance_info().termination_reason = reason
+            resource.get_instance_info().termination_time = int(time())
+            resource.state = ResourceState.TERMINATED.value
+            run.save()
+        except StopIteration as exception:
+            LOGGER.error("Resource %s not found in run %s", resource_name, run_id)
+            raise SCTServiceException("Resource not found", resource_name) from exception
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "terminated"
+
+    @staticmethod
+    def submit_nemesis(run_id: str, nemesis_details: dict) -> str:
+        nem_req = NemesisSubmissionRequest(**nemesis_details)
+        node_desc = NodeDescription(name=nem_req.node_name, ip=nem_req.node_ip, shards=nem_req.node_shards)
+        nemesis_info = NemesisRunInfo(
+            class_name=nem_req.class_name,
+            name=nem_req.name,
+            start_time=int(nem_req.start_time),
+            end_time=0,
+            duration=0,
+            stack_trace="",
+            status=NemesisStatus.RUNNING.value,
+            target_node=node_desc,
+        )
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            run.add_nemesis(nemesis_info)
+            run.save()
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "created"
+
+    @staticmethod
+    def finalize_nemesis(run_id: str, nemesis_details: dict) -> str:
+        nem_req = NemesisFinalizationRequest(**nemesis_details)
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            nemesis = next(nem for nem in run.get_nemeses() if nem.name ==
+                           nem_req.name and nem.start_time == nem_req.start_time)
+            nemesis.status = NemesisStatus(nem_req.status).value
+            nemesis.stack_trace = nem_req.message
+            nemesis.end_time = int(time())
+            run.save()
+        except StopIteration as exception:
+            LOGGER.error("Nemesis %s (%s) not found for run %s", nem_req.name, nem_req.start_time, run_id)
+            raise SCTServiceException("Nemesis not found", (nem_req.name, nem_req.start_time)) from exception
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "updated"
+
+    @staticmethod
+    def submit_events(run_id: str, events: list[dict]) -> str:
+        wrapped_events = [EventSubmissionRequest(**ev) for ev in events]
+        try:
+            run: SCTTestRun = SCTTestRun.get(id=run_id)
+            for event in wrapped_events:
+                wrapper = EventsBySeverity(severity=event.severity,
+                                           event_amount=event.total_events, last_events=event.messages)
+                run.get_events().append(wrapper)
+            run.save()
+        except SCTTestRun.DoesNotExist as exception:
+            LOGGER.error("Run %s not found for SCTTestRun", run_id)
+            raise SCTServiceException("Run not found", run_id) from exception
+
+        return "added"
+
+    @staticmethod
+    def get_scylla_version_kernels_report(release_name: str):
+        all_release_runs = SCTTestRun.get_version_data_for_release(release_name=release_name)
+        kernels_by_version = {}
+        kernel_metadata = {}
+        for run in all_release_runs:
+            packages = run["packages"]
+            if not packages:
+                continue
+            scylla_pkgs = {p["name"]: p for p in packages if "scylla-server" in p["name"]}
+            scylla_pkg = scylla_pkgs["scylla-server-upgraded"] if scylla_pkgs.get(
+                "scylla-server-upgraded") else scylla_pkgs.get("scylla-server")
+            version = f"{scylla_pkg['version']}-{scylla_pkg['date']}.{scylla_pkg['revision_id']}" if scylla_pkgs else "unknown"
+            kernel_packages = [p for p in packages if "kernel" in p["name"]]
+            kernel_package = kernel_packages[0] if len(kernel_packages) > 0 else None
+            if not kernel_package:
+                continue
+            version_list = set(kernels_by_version.get(version, []))
+            version_list.add(kernel_package["version"])
+            kernels_by_version[version] = list(version_list)
+            metadata = kernel_metadata.get(
+                kernel_package.version,
+                {
+                    "passed": 0,
+                    "failed": 0,
+                    "aborted": 0,
+                }
+            )
+            if run["status"] in ["passed", "failed", "aborted"]:
+                metadata[run["status"]] += 1
+            kernel_metadata[kernel_package["version"]] = metadata
+
+        return {
+            "versions": kernels_by_version,
+            "metadata": kernel_metadata
+        }
---
+++ packages/argus-alm/argus_alm-0.11.4-py3-none-any.whl/argus/backend/plugins/sct/testrun.py
@@ -0,0 +1,175 @@
+import logging
+from datetime import datetime
+from dataclasses import dataclass
+from uuid import UUID
+
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import _DoesNotExist
+from argus.backend.db import ScyllaCluster
+from argus.backend.models.web import ArgusRelease
+from argus.backend.plugins.core import PluginModelBase
+from argus.backend.plugins.sct.resource_setup import ResourceSetup
+from argus.backend.plugins.sct.udt import (
+    CloudInstanceDetails,
+    CloudResource,
+    CloudSetupDetails,
+    EventsBySeverity,
+    NemesisRunInfo,
+    PackageVersion,
+)
+
+LOGGER = logging.getLogger(__name__)
+SCT_REGION_PROPERTY_MAP = {
+    "aws": "region_name",
+    "aws-siren": "region_name",
+    "k8s-eks": "region_name",
+    "gce": "gce_datacenter",
+    "gce-siren": "gce_datacenter",
+    "k8s-gke": "gce_datacenter",
+    "azure": "azure_region_name",
+    "default": "region_name",
+}
+
+
+@dataclass(init=True, repr=True, frozen=True)
+class SCTTestRunSubmissionRequest():
+    schema_version: str
+    run_id: str
+    job_name: str
+    job_url: str
+    started_by: str
+    commit_id: str
+    runner_public_ip: str
+    runner_private_ip: str
+    sct_config: dict
+
+
+class SCTTestRun(PluginModelBase):
+    __table_name__ = "sct_test_run"
+    _plugin_name = "scylla-cluster-tests"
+
+    # Test Details
+    scm_revision_id = columns.Text()
+    started_by = columns.Text()
+    config_files = columns.List(value_type=columns.Text())
+    packages = columns.List(value_type=columns.UserDefinedType(user_type=PackageVersion))
+    scylla_version = columns.Text()
+    yaml_test_duration = columns.Integer()
+
+    # Test Preset Resources
+    sct_runner_host = columns.UserDefinedType(user_type=CloudInstanceDetails)
+    region_name = columns.List(value_type=columns.Text())
+    cloud_setup = columns.UserDefinedType(user_type=CloudSetupDetails)
+
+    # Test Runtime Resources
+    allocated_resources = columns.List(value_type=columns.UserDefinedType(user_type=CloudResource))
+
+    # Test Results
+    events = columns.List(value_type=columns.UserDefinedType(user_type=EventsBySeverity))
+    nemesis_data = columns.List(value_type=columns.UserDefinedType(user_type=NemesisRunInfo))
+    screenshots = columns.List(value_type=columns.Text())
+
+    @classmethod
+    def _stats_query(cls) -> str:
+        return ("SELECT id, test_id, group_id, release_id, status, start_time, build_job_url, build_id, "
+                f"assignee, end_time, investigation_status, heartbeat, scylla_version FROM {cls.table_name()} WHERE release_id = ?")
+
+    @classmethod
+    def load_test_run(cls, run_id: UUID) -> 'SCTTestRun':
+        return cls.get(id=run_id)
+
+    @classmethod
+    def submit_run(cls, request_data: dict) -> 'SCTTestRun':
+        req = SCTTestRunSubmissionRequest(**request_data)
+        return cls.from_sct_config(req=req)
+
+    @classmethod
+    def get_distinct_product_versions(cls, release: ArgusRelease) -> list[str]:
+        cluster = ScyllaCluster.get()
+        statement = cluster.prepare(f"SELECT scylla_version FROM {cls.table_name()} WHERE release_id = ?")
+        rows = cluster.session.execute(query=statement, parameters=(release.id,))
+        unique_versions = {r["scylla_version"] for r in rows if r["scylla_version"]}
+
+        return sorted(list(unique_versions), reverse=True)
+
+    @classmethod
+    def get_version_data_for_release(cls, release_name: str):
+        cluster = ScyllaCluster.get()
+        release = ArgusRelease.get(name=release_name)
+        query = cluster.prepare(f"SELECT scylla_version, packages, status FROM {cls.table_name()} WHERE release_id = ?")
+        rows = cluster.session.execute(query=query, parameters=(release.id,))
+
+        return list(rows)
+
+    @classmethod
+    def from_sct_config(cls, req: SCTTestRunSubmissionRequest):
+        run = cls()
+        run.build_id = req.job_name
+        run.assign_categories()
+        try:
+            run.assignee = run.get_scheduled_assignee()
+        except _DoesNotExist:
+            run.assignee = None
+        run.start_time = datetime.utcnow()
+        run.id = UUID(req.run_id)  # pylint: disable=invalid-name
+        run.scm_revision_id = req.commit_id
+        run.started_by = req.started_by
+        run.build_job_url = req.job_url
+
+        backend = req.sct_config.get("cluster_backend")
+        region_key = SCT_REGION_PROPERTY_MAP.get(backend, SCT_REGION_PROPERTY_MAP["default"])
+        raw_regions = req.sct_config.get(region_key) or "undefined_region"
+        regions = raw_regions.split() if isinstance(raw_regions, str) else raw_regions
+
+        run.cloud_setup = ResourceSetup.get_resource_setup(backend=backend, sct_config=req.sct_config)
+
+        run.config_files = req.sct_config.get("config_files")
+        run.region_name = regions
+        run.save()
+        return run
+
+    def get_resources(self) -> list[CloudResource]:
+        return self.allocated_resources
+
+    def get_nemeses(self) -> list[NemesisRunInfo]:
+        return self.nemesis_data
+
+    def get_events(self) -> list[EventsBySeverity]:
+        return self.events
+
+    def submit_product_version(self, version: str):
+        self.scylla_version = version
+
+    def finish_run(self):
+        self.end_time = datetime.utcnow()
+
+    def submit_logs(self, logs: list[dict]):
+        for log in logs:
+            self.logs.append((log["log_name"], log["log_link"]))
+
+    def add_screenshot(self, screenshot_link: str):
+        self.screenshots.append(screenshot_link)
+
+    def add_nemesis(self, nemesis: NemesisRunInfo):
+        self.nemesis_data.append(nemesis)
+
+    def _add_new_event_type(self, event: EventsBySeverity):
+        self.events.append(event)
+
+    def _collect_event_message(self, event: EventsBySeverity, message: str):
+        if len(event.last_events) >= 100:
+            event.last_events = event.last_events[1:]
+
+        event.event_amount += 1
+        event.last_events.append(message)
+
+    def add_event(self, event_severity: str, event_message: str):
+        try:
+            event = next(filter(lambda v: v.severity ==
+                         event_severity, self.events))
+        except StopIteration:
+            event = EventsBySeverity(
+                severity=event_severity, event_amount=0, last_events=[])
+            self._add_new_event_type(event)
+
+        self._collect_event_message(event, event_message)
