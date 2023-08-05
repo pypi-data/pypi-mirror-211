dc93e52da97d8c29815e1b406e7d59cd74446808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:45 +0000

    Add package custom-json-serializer-1234/1.0/custom-json-serializer-1234-1.0.tar.gz

---
+++ packages/custom-json-serializer-1234/custom-json-serializer-1234-1.0.tar.gz/custom-json-serializer-1234-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="custom-json-serializer-1234",
+    version="1.0",
+    description="serialization/deserialization package",
+    url="https://github.com/donshester/PythonLabs/tree/lab3",
+    author="Me",
+    author_email="vlad.stepanov.2003@bk.ru",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    packages=find_packages(include=["core", "core.*"]),
+    include_package_data=True
+)
