733a4560c115f4d7a0559a7d2d38b6091dddc173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:41 +0000

    Add package rosecape-airflow/0.0.2/rosecape_airflow-0.0.2.tar.gz

---
+++ packages/rosecape-airflow/rosecape_airflow-0.0.2.tar.gz/rosecape_airflow-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Airflow utilities'
+LONG_DESCRIPTION = 'A package that makes it easy to interact with AWS services from Airflow using PythonOperator'
+
+setup(
+    name="rosecape_airflow",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Nick Joanis",
+    author_email="nicholas@rosecape.ca",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='Airflow',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
