8fb9718a91bbf52dc16703866691e21244951cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package psdm-analysis/0.1.4/psdm-analysis-0.1.4.tar.gz

---
+++ packages/psdm-analysis/psdm-analysis-0.1.4.tar.gz/psdm-analysis-0.1.4/psdm_analysis/models/input/node.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from typing import List
+
+from pandas import DataFrame
+
+from psdm_analysis.models.entity import Entities
+from psdm_analysis.models.input.enums import RawGridElementsEnum
+
+
+@dataclass(frozen=True)
+class Nodes(Entities):
+    data: DataFrame
+
+    @staticmethod
+    def get_enum() -> RawGridElementsEnum:
+        return RawGridElementsEnum.NODE
+
+    @staticmethod
+    def attributes() -> List[str]:
+        return Entities.attributes() + [
+            "v_rated",
+            "v_target",
+            "slack",
+            "geo_position",
+            "volt_lvl",
+            "subnet",
+        ]
+
+    @property
+    def geo_position(self):
+        return self.data["geo_position"]
+
+    @property
+    def longitude(self):
+        return self.data["longitude"]
+
+    @property
+    def latitude(self):
+        return self.data["latitude"]
+
+    @property
+    def slack(self):
+        return self.data["slack"]
+
+    @property
+    def subnet(self):
+        return self.data["subnet"]
+
+    @property
+    def v_rated(self):
+        return self.data["v_rated"]
+
+    @property
+    def v_target(self):  # in kV
+        return self.data["v_target"]
+
+    @property
+    def volt_lvl(self):
+        return self.data["volt_lvl"]
+
+    def get_slack_nodes(self):
+        return Nodes(self.data[self.slack])
+
+    def nodes(self):
+        return self.data.index
