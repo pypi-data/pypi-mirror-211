8f3557b5d55aeed21f8aaf07d156038ee3840447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:33 +0000

    Add package traveltimepy/3.3.0b1/traveltimepy-3.3.0b1-py3-none-any.whl

---
+++ packages/traveltimepy/traveltimepy-3.3.0b1-py3-none-any.whl/tests/geocoding_test.py
@@ -0,0 +1,11 @@
+def test_geocoding_search(sdk):
+    response = sdk.geocoding(
+        query="Parliament square", limit=30, within_countries=["gb", "de"]
+    )
+    assert len(response.features) > 0
+    assert len(response.features) < 31
+
+
+def test_geocoding_reverse(sdk):
+    response = sdk.geocoding_reverse(lat=51.507281, lng=-0.132120)
+    assert len(response.features) > 0
