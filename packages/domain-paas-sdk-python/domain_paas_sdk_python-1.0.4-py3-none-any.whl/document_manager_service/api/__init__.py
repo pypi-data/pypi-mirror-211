669737270943eed12c252213c4b2ef804d9e5f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:36 +0000

    Add package domain-paas-sdk-python/1.0.4/domain_paas_sdk_python-1.0.4-py3-none-any.whl

---
+++ packages/domain-paas-sdk-python/domain_paas_sdk_python-1.0.4-py3-none-any.whl/document_manager_service/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from document_manager_service.api.document_api import DocumentApi
+from document_manager_service.api.health_api import HealthApi
