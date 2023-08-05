ceb26ca8e2b32ab4f0f8537c01f6819de0fecbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:11 +0000

    Add package python-aternos/3.0.0/python-aternos-3.0.0.tar.gz

---
+++ packages/python-aternos/python-aternos-3.0.0.tar.gz/python-aternos-3.0.0/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open('README.md', 'rt') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='python-aternos',
+    version='3.0.0',
+    author='Chechkenev Andrey (@DarkCat09)',
+    author_email='aacd0709@mail.ru',
+    description='An unofficial Aternos API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DarkCat09/python-aternos',
+    project_urls={
+        'Documentation': 'https://python-aternos.codeberg.page',
+        'GitHub': 'https://github.com/DarkCat09/python-aternos',
+        'Bug Tracker': 'https://github.com/DarkCat09/python-aternos/issues',
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Intended Audience :: Developers',
+        'Topic :: Internet',
+        'Typing :: Typed',
+    ],
+    install_requires=[
+        'lxml>=4.8.0',
+        'cloudscraper>=1.2.60',
+        'js2py>=0.71',
+        'websockets>=10.1',
+        'regex>=2022.3.15',
+    ],
+    packages=['python_aternos'],
+    python_requires=">=3.7",
+    include_package_data=True,
+)
