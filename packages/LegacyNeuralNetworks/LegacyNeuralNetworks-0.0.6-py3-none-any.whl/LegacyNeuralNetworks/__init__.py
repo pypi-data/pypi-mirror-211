f028e4a8227d26196fb5508f275b5cb119ff305b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package LegacyNeuralNetworks/0.0.6/LegacyNeuralNetworks-0.0.6-py3-none-any.whl

---
+++ packages/LegacyNeuralNetworks/LegacyNeuralNetworks-0.0.6-py3-none-any.whl/LegacyNeuralNetworks/__init__.py
@@ -0,0 +1,4 @@
+from LegacyNeuralNetworks.Fill import Writer
+from LegacyNeuralNetworks.ds_fill import Writer
+from LegacyNeuralNetworks.ann_fill import Writer
+from LegacyNeuralNetworks.LegacyNeuralNetworks import *
