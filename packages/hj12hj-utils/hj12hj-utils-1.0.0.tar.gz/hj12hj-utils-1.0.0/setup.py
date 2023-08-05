cb35d463cf65a590e5abf2f9e2b3bfc5e0527410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package hj12hj-utils/1.0.0/hj12hj-utils-1.0.0.tar.gz

---
+++ packages/hj12hj-utils/hj12hj-utils-1.0.0.tar.gz/hj12hj-utils-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='hj12hj-utils',
+    version='1.0.0',
+    author='hj',
+    author_email='your.email@example.com',
+    description='Description of your package',
+    packages=['utils'],
+)
