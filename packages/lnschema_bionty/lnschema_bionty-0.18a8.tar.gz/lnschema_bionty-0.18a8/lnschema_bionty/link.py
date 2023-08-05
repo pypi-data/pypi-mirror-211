d26371bc33c6de2fe9001628b992c01bff22ae81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:41 +0000

    Add package lnschema_bionty/0.18a8/lnschema_bionty-0.18a8.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.18a8.tar.gz/lnschema_bionty-0.18a8/lnschema_bionty/link.py
@@ -0,0 +1,17 @@
+"""Link tables.
+
+.. autosummary::
+   :toctree: .
+
+   FeaturesGene
+   FeaturesProtein
+   FeaturesCellMarker
+   FileReadout
+"""
+
+from ._link import (  # noqa
+    FeaturesCellMarker,
+    FeaturesGene,
+    FeaturesProtein,
+    FileReadout,
+)
---
+++ packages/lnschema_bionty/lnschema_bionty-0.18a8.tar.gz/lnschema_bionty-0.18a8/noxfile.py
@@ -0,0 +1,33 @@
+import nox
+import requests  # type: ignore
+from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
+from laminci.nox import (
+    build_docs,
+    login_testuser1,
+    run_pre_commit,
+    run_pytest,
+    setup_test_instances_from_main_branch,
+)
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def build(session):
+    login_testuser1(session)
+    setup_test_instances_from_main_branch(session)
+    session.install(".[dev,test]")
+    response = requests.get("https://github.com/laminlabs/lamindb/tree/staging")
+    if response.status_code < 400:
+        session.install("git+https://github.com/laminlabs/lamindb@staging")
+    else:
+        session.install("git+https://github.com/laminlabs/lamindb")
+    run_pytest(session)
+    build_docs(session)
+    upload_docs_artifact()
+    move_built_docs_to_docs_slash_project_slug()
