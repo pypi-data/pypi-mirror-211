51cc23848a3aa592cd34f5a297b704ba334a8638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:20 +0000

    Add package pybi-next/0.4.9/pybi_next-0.4.9-py3-none-any.whl

---
+++ packages/pybi-next/pybi_next-0.4.9-py3-none-any.whl/pybi/core/components/reactiveComponent/slicer.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, List, Dict
+
+
+from pybi.core.components import ComponentTag
+from .base import SingleReactiveComponent
+
+
+if TYPE_CHECKING:
+    from pybi.core.sql import SqlInfo
+
+
+class Slicer(SingleReactiveComponent):
+    def __init__(self, sql: SqlInfo) -> None:
+        super().__init__(ComponentTag.Slicer, sql)
+        self.title = ""
+        self.multiple = True
+        self.__hidden_title = None
+
+    def set_title(self, title: str):
+        self.title = title
+        return self
+
+    def set_hiddenTitle(self, value=True):
+        """
+        不显示标题
+        """
+        self.__hidden_title = value
+        return self
+
+    def set_multiple(self, multiple: bool):
+        self.multiple = multiple
+        return self
+
+    def set_props(self, props: Dict):
+        """
+        [slicer props](https://element-plus.gitee.io/zh-CN/component/select.html#select-attributes)
+        e.g
+        >>> .add_slicer(...).set_props({'placeholder':'my define placeholder'})
+        """
+        return super().set_props(props)
+
+    def _to_json_dict(self):
+        data = super()._to_json_dict()
+
+        if self.__hidden_title:
+            data["hiddenTitle"] = self.__hidden_title
+        return data
