c9150df6c0447f5938d9246c2241dbe82306e2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:38 +0000

    Add package abstract-package-test/0.0.1/abstract_package_test-0.0.1-py3.11.egg

---
+++ packages/abstract-package-test/abstract_package_test-0.0.1-py3.11.egg/abstract_authority_replica_test/setup.py
@@ -0,0 +1,37 @@
+# This is the setup.py for module abstract_authority_replica_test
+from setuptools import setup, find_packages
+from time import time
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='abstract_authority_replica_test',
+    version='0.0.1',
+    author='putkoff',
+    author_email='partners@abstractendeavors.com',
+    description='abstract_authority_replica_test is a Python package.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AbstractEndeavors/abstract_package_test/abstract_authority_replica_test',
+    packages=find_packages(where="src"),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    install_requires=[
+        # Add your project's requirements here, e.g.,
+        # 'numpy>=1.22.0',
+        # 'pandas>=1.3.0',
+    ],
+    entry_points={
+        'console_scripts': ["abstract_authority_replica_test=abstract_authority_replica_test.main:main"]
+    },
+)
