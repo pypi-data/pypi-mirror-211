389344ebbb047d5b00ed132b98f6c9076ebba97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:31 +0000

    Add package bvz-distributions/1.0/bvz_distributions-1.0.tar.gz

---
+++ packages/bvz-distributions/bvz_distributions-1.0.tar.gz/bvz_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="bvz_distributions",
+ version="1.0",
+ description="Gaussian and Binomial distributions",
+ packages=['bvz_distributions'],
+ author="Bas van Zomeren",
+ zip_safe=False)
