16ae73fdfbf0513b6b9a1fdfc585a36ae7607556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:18 +0000

    Add package cdflib/1.0.0/cdflib-1.0.0.tar.gz

---
+++ packages/cdflib/cdflib-1.0.0.tar.gz/cdflib-1.0.0/tests/test_cdfread.py
@@ -0,0 +1,25 @@
+import pathlib
+
+import pytest
+
+from cdflib import CDF
+
+fname = pathlib.Path(__file__) / ".." / "testfiles" / "psp_fld_l2_mag_rtn_1min_20200104_v02.cdf"
+
+
+def test_read():
+    cdf = CDF(fname)
+
+    cdf.cdf_info()
+    varatts = cdf.varattsget("psp_fld_l2_mag_RTN_1min")
+    assert isinstance(varatts, dict)
+
+
+def test_context():
+    with CDF(fname) as cdf:
+        cdf.cdf_info()
+
+
+def test_nonexist_file_errors(tmp_path):
+    with pytest.raises(FileNotFoundError, match="not found"):
+        CDF(tmp_path / "nonexist.cdf")
