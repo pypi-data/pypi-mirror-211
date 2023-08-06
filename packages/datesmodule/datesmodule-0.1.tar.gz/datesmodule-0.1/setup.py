8a30491e357d1c9dc2915f804d755ba99773ec52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:43 +0000

    Add package datesmodule/0.1/datesmodule-0.1.tar.gz

---
+++ packages/datesmodule/datesmodule-0.1.tar.gz/datesmodule-0.1/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 22:36:20 2023
+
+@author: fatemeh
+"""
+
+import setuptools
+setuptools.setup(name='datesmodule',
+      version='0.1',
+      descriptor='datesmodule',
+      author='fatemeh kohandani'
+      )
