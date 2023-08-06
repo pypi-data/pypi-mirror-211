24e8782f9081d71460f65580c54c0ab1a0d57b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:00 +0000

    Add package lp-ap-tools/0.1.2/lp_ap_tools-0.1.2-py3-none-any.whl

---
+++ packages/lp-ap-tools/lp_ap_tools-0.1.2-py3-none-any.whl/lp_ap_tools/lp_ap_tools.py
@@ -0,0 +1,45 @@
+import functools
+import time
+import os
+from rocrate.rocrate import ROCrate
+from pydantic import Field, BaseModel, create_model # include pydantic in new tooling
+from globus_action_provider_tools.data_types import ActionProviderDescription, ActionRequest, ActionStatus
+
+# LP fields to be included within AP input_schema
+# TODO: Build schema, and provide flexibility in chosen fields. 
+LP_FIELDS = {
+   'context_description': (str, Field(..., title="Some required input", description="A useful description")),
+   'researcher_comment': (str, Field(..., title="Some required input", description="A useful description")),
+}
+
+def union_fields(origin_class: BaseModel, LP_fields):
+   original_fields = {name: (field.outer_type_, field.field_info) for name, field in origin_class.__fields__.items()}
+   return create_model(type(origin_class).__name__, **{**original_fields, **LP_FIELDS})
+
+# Decorator integrating computation with RO-Crate
+def LP_artefact(dir_struct: dict,
+                description: str = "description",
+                creator: str = "creator"):
+    
+    # Create RO-Crate, and set metadata
+    # Add metadata, data, code, workflow, provenance, annotations, preview
+    crate = ROCrate()
+
+    def middle(func):
+      @functools.wraps(func)
+      def create_ROcrate(action_id: str, body):
+          print(f"----- Computation for action {action_id} starting -----")
+
+          # Time to execute the code
+          start_time = time.time()
+          computation = func(action_id, body)
+          total_time = time.time() - start_time
+
+          print(f"total computation time: {total_time}")
+          print(f"----- Computation for action {action_id} ending -----")
+
+          # Write RO-Crate to disk
+          crate.write_crate(os.path.join(dir_struct["crates"], f"crate_{action_id}"))
+          return computation
+      return create_ROcrate
+    return middle
