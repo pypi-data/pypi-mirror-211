512b31c0de09aa120a1341fd1406393e46f53e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:38 +0000

    Add package thipster/0.13.1/thipster-0.13.1.tar.gz

---
+++ packages/thipster/thipster-0.13.1.tar.gz/thipster-0.13.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.13.1'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='thipster',
+    version=__version__,
+    install_requires=required,
+    packages=find_packages(
+        exclude=['pipelines'],
+    ),
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-mock',
+        ],
+        'dev': [
+            'pytest',
+            'pytest-mock',
+            'dagger.io',
+            'pre-commit',
+        ],
+        'doc': [
+            'sphinx',
+        ],
+    },
+)
