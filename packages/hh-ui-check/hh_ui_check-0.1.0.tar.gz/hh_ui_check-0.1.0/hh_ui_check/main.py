200e743ec620a29be0c5e542d3cb2e712ab69e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:18 +0000

    Add package hh-ui-check/0.1.0/hh_ui_check-0.1.0.tar.gz

---
+++ packages/hh-ui-check/hh_ui_check-0.1.0.tar.gz/hh_ui_check-0.1.0/hh_ui_check/main.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import cv2
+import numpy as np
+
+from horizen import getHorizen
+from substract import getSubstract
+from overlapping import getOverlapping
+from diff import getDiffImages
+from hist_compare import getHistResult
+
+if __name__ == '__main__':
+  curPath = os.path.abspath('.')
+  im1Path = os.path.join(curPath, '1.jpg')
+  im2Path = os.path.join(curPath, '2.jpg')
+  image1 = cv2.imread(im1Path)
+  image2 = cv2.imread(im2Path)
+
+  getHorizen(image1, image2)
+  getSubstract(image1, image2)
+  getOverlapping(image1, image2)
+  getDiffImages(image1, image2)
+  getHistResult(image1, image2)
+  
