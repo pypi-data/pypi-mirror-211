671926580d39117c1fc70c92b30028c28f6fcc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:44 +0000

    Add package cad-to-dagmc/0.3.2/cad_to_dagmc-0.3.2.tar.gz

---
+++ packages/cad-to-dagmc/cad_to_dagmc-0.3.2.tar.gz/cad_to_dagmc-0.3.2/README.md
@@ -0,0 +1,73 @@
+
+[![N|Python](https://www.python.org/static/community_logos/python-powered-w-100x40.png)](https://www.python.org)
+
+[![CI with install](https://github.com/fusion-energy/cad_to_dagmc/actions/workflows/ci_with_install.yml/badge.svg)](https://github.com/fusion-energy/cad_to_dagmc/actions/workflows/ci_with_install.yml)
+
+[![Upload Python Package](https://github.com/fusion-energy/cad_to_dagmc/actions/workflows/python-publish.yml/badge.svg)](https://github.com/fusion-energy/cad_to_dagmc/actions/workflows/python-publish.yml)
+
+
+[![PyPI](https://img.shields.io/pypi/v/cad_to_dagmc?color=brightgreen&label=pypi&logo=grebrightgreenen&logoColor=green)](https://pypi.org/project/cad_to_dagmc/)
+
+___
+
+A minimal package that uses CadQuery functionality to convert Cad geometry to [DAGMC](https://github.com/svalinn/DAGMC/) h5m files
+
+This particular method of producing DAGMC compatible h5m files from CAD geometry
+is intended to convert STP files or [CadQuery](https://cadquery.readthedocs.io) objects to h5m file.
+
+One unique feature of this package is the ability to combine STP files with CadQuery objects.
+This allows for the addition of parametric geometry to static geometry.
+
+# Install (Conda)
+
+Creates a new empty Conda environment and activate it
+```bash
+conda create --name new_env python=3.9
+conda activate new_env
+```
+
+Installs cad_to_dagmc and dependencies
+```bash
+conda install -c conda-forge cad_to_dagmc
+```
+# Install (Mamba)
+
+Creates a new empty Conda environment and activate it
+```bash
+conda create --name new_env python=3.9
+conda activate new_env
+```
+
+Installs Mamba
+```bash
+conda install -c conda-forge mamba
+```
+
+Installs cad_to_dagmc and dependencies
+```bash
+mamba install -c conda-forge cad_to_dagmc
+```
+
+# Install (Conda + pip)
+
+You will need to install some dependencies that are not available via PyPi.
+This example uses Conda but Mamba could also be used.
+```bash
+conda install -c conda-forge moab
+conda install -c conda-forge gmsh
+conda install -c conda-forge python-gmsh
+```
+
+Then you can install the cad_to_dagmc package with ```pip```
+
+```bash
+pip install cad_to_dagmc
+```
+
+# Usage
+
+To use the h5m geometry you will need a transport code with DAGMC enabled such as OpenMC.
+Just to note that currently the conda install for CadQuery and OpenMC can't be installed in the same conda environment.
+A work around for this is to create the h5m geometry in one conda environment and simulate with OpenMC in another conda environment.
+
+For examples see the [examples folder](https://github.com/fusion-energy/cad_to_dagmc/tree/main/examples)
---
+++ packages/cad-to-dagmc/cad_to_dagmc-0.3.2.tar.gz/cad_to_dagmc-0.3.2/src/cad_to_dagmc/core.py
@@ -0,0 +1,164 @@
+from tempfile import mkstemp
+
+import typing
+from cadquery import importers
+from cadquery import Assembly
+from OCP.GCPnts import GCPnts_QuasiUniformDeflection
+
+# from cadquery.occ_impl import shapes
+import OCP
+import cadquery as cq
+from OCP.TopLoc import TopLoc_Location
+from OCP.BRep import BRep_Tool
+from OCP.TopAbs import TopAbs_Orientation
+
+from .brep_to_h5m import brep_to_h5m
+from .brep_part_finder import (
+    get_part_properties_from_shapes,
+    get_part_properties_from_shapes,
+    get_matching_part_ids,
+)
+
+
+class CadToDagmc:
+    def __init__(self):
+        self.parts = []
+        self.material_tags = []
+
+    def add_stp_file(
+        self,
+        filename: str,
+        material_tags: typing.Iterable[str],
+        scale_factor: float = 1.0,
+    ):
+        """Loads the parts from stp file into the model keeping track of the
+        parts and their material tags.
+
+        Args:
+            filename: the filename used to save the html graph.
+            material_tags: the names of the DAGMC material tags to assign.
+                These will need to be in the same order as the volumes in the
+                STP file and match the material tags used in the neutronics
+                code (e.g. OpenMC).
+            scale_factor: a scaling factor to apply to the geometry that can be
+                used to increase the size or decrease the size of the geometry.
+                Useful when converting the geometry to cm for use in neutronics
+                simulations.
+        """
+
+        part = importers.importStep(str(filename)).val()
+
+        if scale_factor == 1:
+            scaled_part = part
+        else:
+            scaled_part = part.scale(scale_factor)
+        self.add_cadquery_object(object=scaled_part, material_tags=material_tags)
+
+    def add_cadquery_object(
+        self,
+        object: typing.Union[
+            cq.assembly.Assembly, cq.occ_impl.shapes.Compound, cq.occ_impl.shapes.Solid
+        ],
+        material_tags: typing.Iterable[str],
+    ):
+        """Loads the parts from CadQuery object into the model keeping track of
+        the parts and their material tags.
+
+        Args:
+            object: the cadquery object to convert
+            material_tags: the names of the DAGMC material tags to assign.
+                These will need to be in the same order as the volumes in the
+                STP file and match the material tags used in the neutronics
+                code (e.g. OpenMC).
+        """
+
+        if isinstance(object, cq.assembly.Assembly):
+            print("assembly found")
+            object = object.toCompound()
+
+        if isinstance(object, (cq.occ_impl.shapes.Compound, cq.occ_impl.shapes.Solid)):
+            iterable_solids = object.Solids()
+        else:
+            iterable_solids = object.val().Solids()
+        self.parts = self.parts + iterable_solids
+
+        if len(iterable_solids) != len(material_tags):
+            msg = f"Number of volumes {len(iterable_solids)} is not equal to number of material tags {len(material_tags)}"
+            raise ValueError(msg)
+
+        for material_tag in material_tags:
+            self.material_tags.append(material_tag)
+
+    def export_dagmc_h5m_file(
+        self,
+        filename: str = "dagmc.h5m",
+        min_mesh_size: float = 1,
+        max_mesh_size: float = 10,
+        verbose: bool = False,
+        volume_atol: float = 0.000001,
+        center_atol: float = 0.000001,
+        bounding_box_atol: float = 0.000001,
+    ):
+        brep_shape = self._merge_surfaces()
+
+        tmp_brep_filename = mkstemp(suffix=".brep", prefix="paramak_")[1]
+        brep_shape.exportBrep(tmp_brep_filename)
+
+        if verbose:
+            print(f"Brep file saved to {tmp_brep_filename}")
+
+        brep_file_part_properties = get_part_properties_from_shapes(brep_shape)
+
+        shape_properties = get_part_properties_from_shapes(self.parts)
+
+        brep_and_shape_part_ids = get_matching_part_ids(
+            brep_part_properties=brep_file_part_properties,
+            shape_properties=shape_properties,
+            volume_atol=volume_atol,
+            center_atol=center_atol,
+            bounding_box_atol=bounding_box_atol,
+        )
+
+        material_tags_in_brep_order = []
+        for brep_id, shape_id in brep_and_shape_part_ids:
+            material_tags_in_brep_order.append(self.material_tags[shape_id - 1])
+
+        brep_to_h5m(
+            brep_filename=tmp_brep_filename,
+            material_tags=material_tags_in_brep_order,
+            h5m_filename=filename,
+            min_mesh_size=min_mesh_size,
+            max_mesh_size=max_mesh_size,
+        )
+
+    def _merge_surfaces(self):
+        """Merges surfaces in the geometry that are the same. More details on
+        the merging process in the DAGMC docs
+        https://svalinn.github.io/DAGMC/usersguide/cubit_basics.html"""
+
+        # solids = geometry.Solids()
+
+        bldr = OCP.BOPAlgo.BOPAlgo_Splitter()
+
+        if len(self.parts) == 1:
+            # merged_solid = cq.Compound(solids)
+            return self.parts[0]
+
+        for solid in self.parts:
+            # checks if solid is a compound as .val() is not needed for compounds
+            if isinstance(
+                solid, (cq.occ_impl.shapes.Compound, cq.occ_impl.shapes.Solid)
+            ):
+                bldr.AddArgument(solid.wrapped)
+            else:
+                bldr.AddArgument(solid.val().wrapped)
+
+        bldr.SetNonDestructive(True)
+
+        bldr.Perform()
+
+        bldr.Images()
+
+        merged_solid = cq.Compound(bldr.Shape())
+
+        return merged_solid
