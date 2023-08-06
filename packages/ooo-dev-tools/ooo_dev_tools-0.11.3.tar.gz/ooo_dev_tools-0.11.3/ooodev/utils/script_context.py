81fa7b464c24bdf2889fc03604247d5a8aefd615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:26:55 +0000

    Add package ooo-dev-tools/0.11.3/ooo_dev_tools-0.11.3.tar.gz

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.11.3.tar.gz/ooo_dev_tools-0.11.3/ooodev/utils/script_context.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+# Origin class https://gitlab.com/LibreOfficiant/ide_utils/-/blob/master/IDE_utils.py
+# Original Author: LibreOfficiant: https://gitlab.com/LibreOfficiant
+from __future__ import annotations
+from typing import cast, TYPE_CHECKING
+import uno
+import os
+
+_ON_RTD = os.environ.get("READTHEDOCS", None) == "True"
+if _ON_RTD:
+    from ..mock import unohelper
+else:
+    import unohelper
+
+from com.sun.star.script.provider import XScriptContext
+
+if TYPE_CHECKING:
+    from com.sun.star.frame import XDesktop
+    from com.sun.star.frame import XModel
+    from com.sun.star.uno import XComponentContext
+
+
+class ScriptContext(unohelper.Base, XScriptContext):  # type: ignore
+    """Substitute (Libre|Open)Office XSCRIPTCONTEXT built-in
+
+    Can be used in IDEs such as Anaconda, Geany, KDevelop, PyCharm, VS Code..
+    in order to run/debug Python macros.
+
+    Implements: com.sun.star.script.provider.XScriptContext
+    """
+
+    def __init__(self, ctx: XComponentContext, desktop: XDesktop | None, doc):
+        self.ctx = ctx
+        self.desktop = desktop
+        self.doc = doc
+
+    def getComponentContext(self) -> XComponentContext:
+        return self.ctx
+
+    def getDesktop(self) -> XDesktop:
+        # return self.ctx.getServiceManager().createInstanceWithContext("com.sun.star.frame.Desktop", self.ctx)
+        if self.desktop is None:
+            uno_type = uno.getTypeByName("com.sun.star.frame.XDesktop")
+            interface = self.ctx.getServiceManager().createInstanceWithContext("com.sun.star.frame.Desktop", self.ctx)
+            self.desktop = cast(XDesktop, interface.queryInterface(uno_type))
+        return self.desktop
+
+    def getDocument(self) -> XModel:
+        # return self.getDesktop().getCurrentComponent()
+        if self.doc is None:
+            uno_type = uno.getTypeByName("com.sun.star.frame.XModel")
+            component = self.getDesktop().getCurrentComponent()
+            self.doc = cast(XModel, component.queryInterface(uno_type))
+        return self.doc
+
+    def getInvocationContext(self):
+        raise NotImplementedError
