921b467ed6de7646f71b58b976f691f0e1fd976b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:29 +0000

    Add package embedding-explorer/0.2.1/embedding_explorer-0.2.1-py3-none-any.whl

---
+++ packages/embedding-explorer/embedding_explorer-0.2.1-py3-none-any.whl/embedding_explorer/blueprints/dashboard.py
@@ -0,0 +1,66 @@
+"""Blueprint for overview dashboard."""
+from typing import Callable, Dict, List, Tuple
+from urllib.parse import quote
+
+import dash_mantine_components as dmc
+from dash_extensions.enrich import Dash, DashBlueprint, dash, dcc, html
+
+from embedding_explorer.blueprints.explorer import create_explorer
+from embedding_explorer.components.model_card import create_card
+from embedding_explorer.model import StaticEmbeddings
+
+
+def create_dashboard(
+    models: Dict[str, StaticEmbeddings], fuzzy_search: bool = False
+):
+    """Creates dashboard for all static embedding models.
+
+    Parameters
+    ----------
+    models: dict of str to StaticEmbeddings
+        Mapping of names to models.
+    """
+    print("Creating Dashboard")
+    dashboard = DashBlueprint()
+
+    # Collecting cards and registering pages
+    cards = []
+    pages = {}
+    for model_name, model in models.items():
+        cards.append(create_card(model=model, model_name=model_name))
+        page = create_explorer(
+            model=model, model_name=model_name, fuzzy_search=fuzzy_search
+        )
+        page.register_callbacks(dashboard)
+        pages[model_name] = page.layout
+
+    dashboard.layout = html.Div(
+        children=[
+            html.Div(
+                "Choose an embedding model to inspect:",
+                className="text-2xl pt-8 pb-3 px-8",
+            ),
+            dmc.Grid(
+                children=[dmc.Col(card.layout, span=1) for card in cards],
+                gutter="lg",
+                grow=True,
+                columns=3,
+                className="p-5",
+            ),
+        ]
+    )
+
+    main_blueprint = DashBlueprint()
+    main_blueprint.layout = html.Div(dash.page_container)
+    dashboard.register_callbacks(main_blueprint)
+
+    def register_pages():
+        dash.register_page(
+            "home", path="/", layout=dashboard.layout, redirect_from=["/home"]
+        )
+        for model_name, layout in pages.items():
+            dash.register_page(
+                quote(model_name), "/" + quote(model_name), layout=layout
+            )
+
+    return main_blueprint, register_pages
---
+++ packages/embedding-explorer/embedding_explorer-0.2.1-py3-none-any.whl/embedding_explorer/components/word_selector.py
@@ -0,0 +1,133 @@
+"""Code for the word selector component."""
+from typing import List
+
+import dash_mantine_components as dmc
+import numpy as np
+from dash_extensions.enrich import (
+    DashBlueprint,
+    Input,
+    Output,
+    State,
+    dcc,
+    exceptions,
+    html,
+)
+from neofuzz import Process
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+from typing_extensions import TypedDict
+
+
+class Option(TypedDict):
+    value: int
+    label: str
+    search: str
+
+
+class DummyProcess:
+    def query(self, search_terms, limit):
+        return np.array([]), np.array([])
+
+
+def create_word_selector(
+    vocab: np.ndarray, model_name: str = "", fuzzy_search: bool = False
+) -> DashBlueprint:
+    """Creates word selector component blueprint."""
+
+    word_selector = DashBlueprint()
+    vocab_lookup = {word: index for index, word in enumerate(vocab)}
+    if fuzzy_search:
+        print("Indexing vocabulary for fuzzy search")
+        vectorizer = make_pipeline(
+            TfidfVectorizer(
+                analyzer="char", ngram_range=(1, 4), max_features=20_000
+            ),
+            NMF(n_components=25),
+        )
+        fuzzy_process = Process(
+            vectorizer,
+            metric="cosine",
+            low_memory=True,
+        )
+        fuzzy_process.index(vocab)
+        print("Indexing done")
+    else:
+        fuzzy_process = DummyProcess()
+
+    search_bar = dcc.Dropdown(
+        # label="Seeds",
+        # description="Select words that are going to be used as"
+        # "the basis of semantic association.",
+        id=f"{model_name}_word_selector",
+        placeholder="Search for words...",
+        value=[],
+        # data=[],
+        options=[],
+        searchable=True,
+        clearable=True,
+        # size="lg",
+        multi=True,
+    )
+    word_selector.layout = dmc.Accordion(
+        chevronPosition="right",
+        variant="contained",
+        children=dmc.AccordionItem(
+            [
+                dmc.AccordionControl(
+                    html.Div(
+                        [
+                            dmc.Text("Seed words"),
+                            dmc.Text(
+                                "Words that will be used as the basis of association.",
+                                size="sm",
+                                weight=400,
+                                color="dimmed",
+                            ),
+                        ]
+                    ),
+                ),
+                dmc.AccordionPanel(search_bar),
+            ],
+            value="search",
+        ),
+    )
+
+    @word_selector.callback(
+        Output(f"{model_name}_word_selector", "options"),
+        Input(f"{model_name}_word_selector", "search_value"),
+        State(f"{model_name}_word_selector", "value"),
+    )
+    def update_options(
+        search_value: str, selected_values: List[int]
+    ) -> List[Option]:
+        if not search_value:
+            raise exceptions.PreventUpdate
+        # Collecting already chosen options
+        selected_options = [
+            dict(value=index, label=vocab[index], search=search_value)
+            for index in selected_values
+        ]
+        # Lowercasing search value
+        search_value = search_value.lower()
+        # Trying to find exact match
+        if search_value in vocab_lookup:
+            exact_match = dict(
+                value=vocab_lookup[search_value],
+                label=search_value,
+                search=search_value,
+            )
+            return [exact_match] + selected_options
+        # Fuzzy finding 5 closest
+        fuzzy_indices, _ = fuzzy_process.query(
+            search_terms=[search_value], limit=5
+        )
+        fuzzy_indices = np.ravel(fuzzy_indices)
+        # Collecting options
+        fuzzy_matches = [
+            dict(value=index, label=vocab[index], search=search_value)
+            for index in fuzzy_indices
+        ]
+        return fuzzy_matches + selected_options
+
+    return word_selector
