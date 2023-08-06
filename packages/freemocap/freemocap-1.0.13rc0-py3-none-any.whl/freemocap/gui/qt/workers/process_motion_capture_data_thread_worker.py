f8a130cfefc7cd6fba0fe177449fc2748a85f45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:40 +0000

    Add package freemocap/1.0.13rc0/freemocap-1.0.13rc0-py3-none-any.whl

---
+++ packages/freemocap/freemocap-1.0.13rc0-py3-none-any.whl/freemocap/gui/qt/workers/process_motion_capture_data_thread_worker.py
@@ -0,0 +1,40 @@
+import logging
+import multiprocessing
+
+from PyQt6.QtCore import pyqtSignal, QThread
+
+from freemocap.core_processes.process_motion_capture_videos.process_recording_folder import (
+    process_recording_folder,
+)
+from freemocap.parameter_info_models.recording_processing_parameter_models import RecordingProcessingParameterModel
+
+logger = logging.getLogger(__name__)
+
+
+class ProcessMotionCaptureDataThreadWorker(QThread):
+    finished = pyqtSignal()
+    in_progress = pyqtSignal(str)
+
+    def __init__(self, session_processing_parameters: RecordingProcessingParameterModel,
+                 kill_event: multiprocessing.Event, parent=None):
+        super().__init__()
+        self._session_processing_parameters = session_processing_parameters
+        self._kill_event = kill_event
+        self._process = multiprocessing.Process(target=process_recording_folder, args=(self._session_processing_parameters, self._kill_event))
+
+    def run(self):
+        logger.info(
+            f"Beginning processing of motion capture data with parameters: {self._session_processing_parameters.__dict__}"
+        )
+        self._kill_event.clear()
+
+        try:
+            self._process.start()
+            self._process.join()
+        except Exception as e:
+            logger.error(f"Error processing motion capture data: {e}")
+
+
+        logger.info("Finished processing session folder!")
+
+        self.finished.emit()
