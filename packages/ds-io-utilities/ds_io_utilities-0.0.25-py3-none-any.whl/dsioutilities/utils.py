0c5874bb02d5f34e9e48ccc81ce8d4c965ff2223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:31 +0000

    Add package ds-io-utilities/0.0.25/ds_io_utilities-0.0.25-py3-none-any.whl

---
+++ packages/ds-io-utilities/ds_io_utilities-0.0.25-py3-none-any.whl/dsioutilities/utils.py
@@ -0,0 +1,22 @@
+from alidaargparser import get_asset_property
+from importlib.util import find_spec
+
+def input_or_output(name):
+
+    dir = get_asset_property(name, property="direction")
+    
+    # TODO remove if else when multidataset supported
+    if dir is not None:
+        return dir
+    else:
+        if name == "input_dataset" or name == "input-dataset":
+            return "input"
+        else: 
+            return "output"
+
+
+def packages_are_installed(packages):
+    for package in packages:
+        if find_spec(package) is None:
+            return False
+    return True
