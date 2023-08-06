d90259ef28c965b1a6562d3a6ff50c7d65f4e6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:40 +0000

    Add package enderchest/0.1.0/enderchest-0.1.0.tar.gz

---
+++ packages/enderchest/enderchest-0.1.0.tar.gz/enderchest-0.1.0/enderchest/__init__.py
@@ -0,0 +1,13 @@
+from . import _version
+from .enderchest import EnderChest
+from .instance import InstanceSpec
+from .shulker_box import ShulkerBox
+
+__version__ = _version.get_versions()["version"]
+
+
+__all__ = [
+    "EnderChest",
+    "InstanceSpec",
+    "ShulkerBox",
+]
