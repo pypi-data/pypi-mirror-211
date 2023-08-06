316734722caa21bd0c1d55d7d0e2679a3d732b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:39 +0000

    Add package snewpy/1.4b1/snewpy-1.4b1.tar.gz

---
+++ packages/snewpy/snewpy-1.4b1.tar.gz/snewpy-1.4b1/python/snewpy/neutrino.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""This module implements basic neutrino properties that are used throughout SNEWPY."""
+
+from enum import IntEnum
+from astropy import units as u
+
+
+class MassHierarchy(IntEnum):
+    """Neutrino mass ordering: ``NORMAL`` or ``INVERTED``."""
+    NORMAL = 1
+    INVERTED = 2
+
+
+class Flavor(IntEnum):
+    """Enumeration of CCSN Neutrino flavors."""
+    NU_E = 0
+    NU_X = 1
+    NU_E_BAR = 2
+    NU_X_BAR = 3
+    
+    def to_tex(self):
+        """LaTeX-compatible string representations of flavor."""
+        if '_BAR' in self.name:
+            return r'$\overline{{\nu}}_{0}$'.format(self.name[3].lower())
+        return r'$\{0}$'.format(self.name.lower())
+
+    @property
+    def is_electron(self):
+        """Return ``True`` for ``Flavor.NU_E`` and ``Flavor.NU_E_BAR``."""
+        return self.value in (Flavor.NU_E.value, Flavor.NU_E_BAR.value)
+
+    @property
+    def is_neutrino(self):
+        """Return ``True`` for ``Flavor.NU_E`` and ``Flavor.NU_X``."""
+        return self.value in (Flavor.NU_E.value, Flavor.NU_X.value)
+
+    @property
+    def is_antineutrino(self):
+        return self.value in (Flavor.NU_E_BAR.value, Flavor.NU_X_BAR.value)
+
+
+class MixingParameters:
+    """Mixing angles and mass differences, assuming three neutrino flavors.
+    
+    This class contains the default values used throughout SNEWPY, which are
+    based on `NuFIT 5.0 <http://www.nu-fit.org>`_ results from July 2020,
+    published in `JHEP 09 (2020) 178 <https://dx.doi.org/10.1007/JHEP09(2020)178>`_
+    [`arXiv:2007.14792 <https://arxiv.org/abs/2007.14792>`_].
+    Note that the best fit values vary between normal and inverted mass hierarchy.
+    """
+    def __init__(self, mh=MassHierarchy.NORMAL):
+        """Initialize the neutrino mixing parameters.
+
+        Parameters
+        ----------
+        mh : MassHierarchy
+            Desired mass ordering: NORMAL or INVERTED.
+        """
+        if type(mh) == MassHierarchy:
+            self.mass_order = mh
+        else:
+            raise TypeError('mh must be of type MassHierarchy')
+
+        # Values from JHEP 09 (2020) 178 [arXiv:2007.14792] and www.nu-fit.org.
+        # The reported precision is not significant given current
+        # uncertainties, but is useful for comparing to the table of
+        # parameters presented on nu-fit.org.
+        if self.mass_order == MassHierarchy.NORMAL:
+            # Note: in NH, the mass splittings are: m1..m2..............m3.
+            self.theta12 = 33.44 * u.deg
+            self.theta13 =  8.57 * u.deg
+            self.theta23 = 49.20 * u.deg
+            self.deltaCP = 197 * u.deg
+            self.dm21_2  = 7.42e-5 * u.eV**2
+            self.dm32_2  = 2.517e-3 * u.eV**2
+        else:
+            # Note: in IH, the mass splittings are: m3..............m1..m2.
+            self.theta12 = 33.45 * u.deg
+            self.theta13 =  8.60 * u.deg
+            self.theta23 = 49.30 * u.deg
+            self.deltaCP = 282 * u.deg
+            self.dm21_2  = 7.42e-5 * u.eV**2
+            self.dm31_2  = -2.498e-3 * u.eV**2
+
+    def get_mixing_angles(self):
+        """Mixing angles of the PMNS matrix.
+        
+        Returns
+        -------
+        tuple
+            Angles theta12, theta13, theta23.
+        """
+        return (self.theta12, self.theta13, self.theta23)
+
---
+++ packages/snewpy/snewpy-1.4b1.tar.gz/snewpy-1.4b1/python/snewpy/test/test_05_snowglobes.py
@@ -0,0 +1,58 @@
+import pytest
+from snewpy.snowglobes import generate_fluence, SimpleRate
+from pathlib import Path
+import tarfile
+
+pytestmark=pytest.mark.snowglobes
+
+models = ['Bollig_2016/s11.2c', 'Bollig_2016/s27.0c']
+transformations = ['AdiabaticMSW_NMO','AdiabaticMSW_IMO']
+
+@pytest.mark.parametrize('model',models)
+@pytest.mark.parametrize('transformation',transformations)
+def test_generate_fluence(splr, model, transformation):
+    """make some input fluxes"""
+    model_path = Path('./models')/model
+    model_type = model_path.parent.stem
+    output_name = f'fluence_{model.replace("/","_")}_{transformation}'
+    fname = generate_fluence(model_path,model_type, transformation, d=10, output_filename=output_name)
+    with tarfile.open(fname) as tar:
+        tar.extractall(model_path.parent)
+
+#numbers from SNEWSv2 paper - for approximate checks 
+crosscheck_table=[  ('icecube',          320e3),
+                    ('wc100kt30prct',4044.8/0.32),
+                    ('ar40kt',              2700),
+                    ('novaND',                40),
+                    ('novaFD',              1900),
+                    ('halo1',                  4),
+                    ('halo2',                 53)]
+
+#numbers from the SNEWPY SNOwGLoBES interface - for approximate checks 
+#of simple rate computation (no smearing)
+crosscheck_table_unsmeared=[  ('icecube',          7.2352e6),
+                    ('wc100kt30prct',4486.9/0.32),
+                    ('ar40kt',              2858.7),
+                    ('novaND',                41.7231),
+                    ('novaFD',              1947.0768),
+                    ('halo1',                  11.5709),
+                    ('halo2',                 146.4671)]
+
+
+@pytest.fixture(autouse=True)
+def splr():
+    return SimpleRate()
+
+@pytest.mark.parametrize('detector, expected_total_unsmeared',crosscheck_table_unsmeared)
+def test_simplerate_crosscheck(splr, detector, expected_total_unsmeared):
+    flux = './models/Bollig_2016/fluence_Bollig_2016_s11.2c_AdiabaticMSW_NMO.dat'
+    data = splr.run(flux,detector)
+    total = data[0].weighted.unsmeared.sum().sum()
+    assert total == pytest.approx(expected_total_unsmeared, 0.01)
+
+@pytest.mark.parametrize('detector, expected_total',crosscheck_table)
+def test_simplerate_smear_crosscheck(splr, detector, expected_total):
+    flux = './models/Bollig_2016/fluence_Bollig_2016_s11.2c_AdiabaticMSW_NMO.dat'
+    data = splr.run(flux,detector)
+    total = data[0].weighted.smeared.sum().sum()
+    assert total == pytest.approx(expected_total, 0.1)
