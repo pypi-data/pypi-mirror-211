3e43bae9ef9273c0e0cdbb4d371808cc0cf2a69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:51 +0000

    Add package pyinaturalist/0.19.0.dev1/pyinaturalist-0.19.0.dev1-py3-none-any.whl

---
+++ packages/pyinaturalist/pyinaturalist-0.19.0.dev1-py3-none-any.whl/pyinaturalist/controllers/controlled_term_controller.py
@@ -0,0 +1,48 @@
+from typing import Dict, List
+
+from pyinaturalist.controllers import BaseController
+from pyinaturalist.docs import document_controller_params
+from pyinaturalist.models import Annotation, ControlledTerm
+from pyinaturalist.v1 import get_controlled_terms, get_controlled_terms_for_taxon
+
+
+class ControlledTermController(BaseController):
+    """:fa:`tag` Controller for ControlledTerm and Annotation requests"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._term_lookup: Dict[int, ControlledTerm] = {}
+
+    @property
+    def term_lookup(self) -> Dict[int, ControlledTerm]:
+        """Get a lookup table of controlled term IDs to term objects"""
+        if not self._term_lookup:
+            self._term_lookup = {term.id: term for term in self.all()}
+        return self._term_lookup
+
+    @document_controller_params(get_controlled_terms)
+    def all(self, **params) -> List[ControlledTerm]:
+        response = get_controlled_terms(**params)
+        return ControlledTerm.from_json_list(response['results'])
+
+    @document_controller_params(get_controlled_terms_for_taxon)
+    def for_taxon(self, taxon_id: int, **params) -> List[ControlledTerm]:
+        response = get_controlled_terms_for_taxon(taxon_id, **params)
+        return ControlledTerm.from_json_list(response['results'])
+
+    def lookup(self, annotations: List[Annotation]) -> List[Annotation]:
+        """Fill in missing information for the specified annotations. If only term and value IDs are
+        present, this will look up, cache, and add complete controlled term details.
+
+        Args:
+            annotations: Observation annotations
+
+        Returns:
+            Annotation objects with ``controlled_attribute`` and ``controlled_value`` populated
+        """
+        for annotation in annotations or []:
+            term = self.term_lookup.get(annotation.controlled_attribute.id)
+            if term:
+                annotation.controlled_attribute = term
+                annotation.controlled_value = term.get_value_by_id(annotation.controlled_value.id)
+        return annotations
---
+++ packages/pyinaturalist/pyinaturalist-0.19.0.dev1-py3-none-any.whl/pyinaturalist/models/life_list.py
@@ -0,0 +1,112 @@
+"""Models for user life lists"""
+from itertools import groupby
+from typing import List, Optional
+
+from pyinaturalist.constants import JsonResponse, TableRow
+from pyinaturalist.models import (
+    BaseModelCollection,
+    Taxon,
+    TaxonCount,
+    define_model,
+    define_model_collection,
+    field,
+)
+
+
+@define_model
+class LifeListTaxon(TaxonCount):
+    """:fa:`dove` :fa:`list` A single :py:class:`.Taxon` in a user's :py:class:`.LifeList`"""
+
+    descendant_obs_count: int = field(default=0, doc='Number of observations of taxon children')
+    direct_obs_count: int = field(
+        default=0, doc='Number of observations of this exact taxon (excluding children)'
+    )
+
+    @property
+    def indent_level(self) -> int:
+        """Indentation level corresponding to this item's rank level"""
+        return int(((70 - self.rank_level) / 5))
+
+    @property
+    def _row(self) -> TableRow:
+        return {
+            'ID': self.id,
+            'Rank': self.rank,
+            'Name': self.name,
+            'Count': self.count,
+        }
+
+    @property
+    def _str_attrs(self) -> List[str]:
+        return ['id', 'rank', 'name', 'count']
+
+
+@define_model_collection
+class LifeList(BaseModelCollection):
+    """:fa:`dove` :fa:`list` A user's life list, based on the schema of ``GET /observations/taxonomy``"""
+
+    data: List[LifeListTaxon] = field(factory=list, converter=LifeListTaxon.from_json_list)
+    count_without_taxon: int = field(default=0, doc='Number of observations without a taxon')
+    user_id: int = field(default=None)
+
+    @classmethod
+    def from_json(cls, value: JsonResponse, user_id: Optional[int] = None, **kwargs) -> 'LifeList':
+        count_without_taxon = value.get('count_without_taxon', 0)
+        if 'results' in value:
+            value = value['results']
+
+        life_list_json = {
+            'data': value,
+            'user_id': user_id,
+            'count_without_taxon': count_without_taxon,
+        }
+        return super(LifeList, cls).from_json(life_list_json)
+
+    def get_count(self, taxon_id: int, count_field='descendant_obs_count') -> int:
+        """Get an observation count for the specified taxon and its descendants, and handle unlisted taxa.
+        **Note:** ``-1`` can be used an alias for ``count_without_taxon``.
+        """
+        if taxon_id == -1:
+            return self.count_without_taxon
+        return super().get_count(taxon_id, count_field=count_field)
+
+    def tree(self):
+        """**Experimental**
+
+        Organize this life list into a taxonomic tree
+
+        Returns:
+            :py:class:`rich.tree.Tree`
+        """
+        return make_tree(self.data)
+
+
+def make_tree(taxa: List[Taxon]):
+    """Organize a list of taxa into a taxonomic tree. Must contain at least one taxon with
+    'Life' (taxon ID 48460) as its parent.
+
+    Returns:
+        :py:class:`rich.tree.Tree`
+    """
+
+    from rich.tree import Tree
+
+    taxa_by_parent_id = _sort_groupby(taxa, key=lambda x: x.parent_id or -1)
+
+    def make_child_tree(node, taxon):
+        """Add a taxon and its children to the specified tree node.
+        Base case: leaf taxon (with no children)
+        Recursive case: non-leaf taxon (with children)
+        """
+        node = node.add(taxon.full_name)
+        for child in taxa_by_parent_id.get(taxon.id, []):
+            node.add(make_child_tree(node, child))
+        return node
+
+    tree_root = {'id': 48460, 'name': 'Life', 'rank': 'State of matter'}
+    return make_child_tree(Tree('life list', expanded=False), Taxon.from_json(tree_root))
+
+
+def _sort_groupby(values, key):
+    """Apply sorting then grouping using the same key"""
+    return {k: list(group) for k, group in groupby(sorted(values, key=key), key=key)}
