f38d7f1cde8466919d57a906d266a32a626f6abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:10 +0000

    Add package pg-common/1.0.0/pg_common-1.0.0.tar.gz

---
+++ packages/pg-common/pg_common-1.0.0.tar.gz/pg_common-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+DIST_NAME = "pg_common"
+DIST_VERSION = "1.0.0"
+__author__ = "baozilaji@gmail.com"
+
+setup(
+	name=DIST_NAME,
+	version=DIST_VERSION,
+	description="python game: common lib",
+	packages=['pg_common'],
+	author=__author__,
+)
