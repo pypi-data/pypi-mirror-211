57ae54acd9f2a99501bc64d219918c0ed47d45cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:51 +0000

    Add package feathub-nightly/0.1.dev20230531/feathub_nightly-0.1.dev20230531-py3-none-any.whl

---
+++ packages/feathub-nightly/feathub_nightly-0.1.dev20230531-py3-none-any.whl/feathub/processors/flink/job_submitter/feathub_job_descriptor.py
@@ -0,0 +1,84 @@
+#  Copyright 2022 The FeatHub Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from datetime import datetime
+from typing import Optional, Dict, Any, Union
+
+import pandas as pd
+
+from feathub.feature_tables.feature_table import FeatureTable
+from feathub.table.table_descriptor import TableDescriptor
+
+
+class FeathubJobDescriptor:
+    """Descriptor of a FeatHub job to run in a remote Flink cluster."""
+
+    def __init__(
+        self,
+        features: TableDescriptor,
+        keys: Union[pd.DataFrame, TableDescriptor, None],
+        start_datetime: Optional[datetime],
+        end_datetime: Optional[datetime],
+        sink: FeatureTable,
+        local_registry_tables: Dict[str, TableDescriptor],
+        allow_overwrite: bool,
+        props: Dict,
+    ):
+        """
+        Instantiate a FeathubJobDescriptor.
+
+        :param features: The table descriptor that contains the features to compute.
+        :param keys: Optional. If it is TableDescriptor or DataFrame, it should be
+                     transformed into a table of keys. If it is not None, the
+                     table only include rows whose key fields match at least one
+                     row of the keys.
+        :param start_datetime: Optional. If it is not None, the `features` table should
+                               have a timestamp field. And the table will only
+                               include features whose
+                               timestamp >= start_datetime. If any field (e.g. minute)
+                               is not specified in the start_datetime, we assume this
+                               field has the minimum possible value.
+        :param end_datetime: Optional. If it is not None, the `features` table should
+                             have a timestamp field. And the table will only
+                             include features whose timestamp < end_datetime. If any
+                             field (e.g. minute) is not specified in the end_datetime,
+                             we assume this field has the maximum possible value.
+        :param sink: Where the features write to.
+        :param local_registry_tables: All the table descriptors registered in the local
+                                      registry that are required to compute the given
+                                      table.
+        :param allow_overwrite: If it is false, throw error if the features collide with
+                                existing data in the given sink.
+        :param props: All properties of FeatHub.
+        """
+        self.features = features
+        self.keys = keys
+        self.start_datetime = start_datetime
+        self.end_datetime = end_datetime
+        self.sink = sink
+        self.local_registry_tables = local_registry_tables
+        self.allow_overwrite = allow_overwrite
+        self.props = props
+
+    def __eq__(self, other: Any) -> bool:
+        return (
+            isinstance(other, FeathubJobDescriptor)
+            and self.features == other.features
+            and self.keys == other.keys
+            and self.start_datetime == other.start_datetime
+            and self.end_datetime == other.end_datetime
+            and self.sink == other.sink
+            and self.local_registry_tables == other.local_registry_tables
+            and self.allow_overwrite == other.allow_overwrite
+            and self.props == other.props
+        )
---
+++ packages/feathub-nightly/feathub_nightly-0.1.dev20230531-py3-none-any.whl/feathub/processors/flink/job_submitter/flink_application_cluster_job_entry.py
@@ -0,0 +1,88 @@
+#  Copyright 2022 The FeatHub Authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import logging
+import sys
+
+import cloudpickle
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import (
+    StreamTableEnvironment,
+)
+
+from feathub.processors.flink.flink_class_loader_utils import (
+    get_flink_context_class_loader,
+)
+from feathub.processors.flink.job_submitter.feathub_job_descriptor import (
+    FeathubJobDescriptor,
+)
+from feathub.processors.flink.table_builder.flink_table_builder import (
+    FlinkTableBuilder,
+)
+from feathub.processors.flink.table_builder.source_sink_utils import insert_into_sink
+from feathub.registries.registry import Registry
+
+logger = logging.getLogger(__file__)
+
+
+def run_job(feathub_job_descriptor_path: str) -> None:
+    """
+    Run the FeatHub job with the given feathub job descriptor.
+
+    The method is expected to run at the JobManager in a remote Flink cluster as the
+    Flink job entry point. It parses the FeathubJobDescriptor, including FlinkTable,
+    Sink, etc., from the given feathub_job_path and execute the Flink job that computes
+    features in table and inserts into the given Sink.
+
+    :param feathub_job_descriptor_path: The path of the FeatHub job config.
+    """
+    with open(feathub_job_descriptor_path, "rb") as f:
+        feathub_job_descriptor: FeathubJobDescriptor = cloudpickle.load(f)
+    logger.info(f"Loaded FeatHub job config: {str(feathub_job_descriptor)}")
+
+    env = StreamExecutionEnvironment.get_execution_environment()
+    t_env = StreamTableEnvironment.create(env)
+
+    props = feathub_job_descriptor.props
+    registry = Registry.instantiate(props=props)
+
+    for _, join_table in feathub_job_descriptor.local_registry_tables.items():
+        registry.register_features(feature_descriptors=[join_table], force_update=True)
+
+    flink_table_builder = FlinkTableBuilder(
+        t_env=t_env,
+        class_loader=get_flink_context_class_loader(),
+        registry=registry,
+    )
+
+    native_flink_table = flink_table_builder.build(
+        features=feathub_job_descriptor.features,
+        keys=feathub_job_descriptor.keys,
+        start_datetime=feathub_job_descriptor.start_datetime,
+        end_datetime=feathub_job_descriptor.end_datetime,
+    )
+
+    table_result = insert_into_sink(
+        t_env,
+        native_flink_table,
+        feathub_job_descriptor.features,
+        feathub_job_descriptor.sink,
+    )
+    table_result.wait()
+
+
+if __name__ == "__main__":
+    try:
+        run_job(feathub_job_descriptor_path=sys.argv[1])
+    except BaseException as e:
+        logger.error("Failed to run job", exc_info=e)
