bd442dfad1e3e35c4c3ea5bd951722de00f528ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:47 +0000

    Add package qcs-sdk-python/0.7.1rc0/qcs_sdk_python-0.7.1rc0-cp311-cp311-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/qcs-sdk-python/qcs_sdk_python-0.7.1rc0-cp311-cp311-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/qcs_sdk/qpu/__init__.pyi
@@ -0,0 +1,41 @@
+from typing import List, Optional
+
+from qcs_sdk.client import QCSClient
+from ._result_data import (
+    QPUResultData as QPUResultData,
+    ReadoutValues as ReadoutValues,
+)
+
+class ListQuantumProcessorsError(RuntimeError):
+    """A request to list available Quantum Processors failed."""
+
+    ...
+
+def list_quantum_processors(
+    client: Optional[QCSClient] = None,
+    timeout: Optional[float] = None,
+) -> List[str]:
+    """
+    Returns all available Quantum Processor (QPU) IDs.
+
+    :param client: The ``QCSClient`` to use. Creates one using environment configuration if unset - see https://docs.rigetti.com/qcs/references/qcs-client-configuration
+    :param timeout: Maximum duration to wait for API calls to complete, in seconds.
+
+    :raises ListQuantumProcessorsError: If the request to list available QPU IDs failed.
+    """
+    ...
+
+async def list_quantum_processors_async(
+    client: Optional[QCSClient] = None,
+    timeout: Optional[float] = None,
+) -> List[str]:
+    """
+    Returns all available Quantum Processor IDs.
+    (async analog of ``list_quantum_processors``)
+
+    :param client: The ``QCSClient`` to use. Creates one using environment configuration if unset - see https://docs.rigetti.com/qcs/references/qcs-client-configuration
+    :param timeout: Maximum duration to wait for API calls to complete, in seconds.
+
+    :raises ListQuantumProcessorsError: If the request to list available QPU IDs failed.
+    """
+    ...
