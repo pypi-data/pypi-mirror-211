45fbd4492ecd319239273748caa280401c1f6c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:44 +0000

    Add package netbox-lists/3.1.0/netbox_lists-3.1.0-py3-none-any.whl

---
+++ packages/netbox-lists/netbox_lists-3.1.0-py3-none-any.whl/netbox_lists/api/urls.py
@@ -0,0 +1,41 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    AggregateListViewSet,
+    DevicesListViewSet,
+    DevicesVMsAttrsListViewSet,
+    DevicesVMsListViewSet,
+    IPAddressListViewSet,
+    IPRangeListViewSet,
+    ListsRootView,
+    PrefixListViewSet,
+    PrometheusDeviceSD,
+    PrometheusVirtualMachineSD,
+    PrometheusIPAddressSD,
+    ServiceListviewSet,
+    TagsListViewSet,
+    VirtualMachinesListViewSet,
+)
+
+app_name = "lists"
+
+router = DefaultRouter()
+router.APIRootView = ListsRootView
+
+router.register("prefixes", PrefixListViewSet)
+router.register("ip-addresses", IPAddressListViewSet)
+router.register("ip-ranges", IPRangeListViewSet)
+router.register("aggregates", AggregateListViewSet)
+router.register("services", ServiceListviewSet)
+router.register("tags", TagsListViewSet)
+router.register("devices", DevicesListViewSet, basename="devices")
+router.register(
+    "virtual-machines", VirtualMachinesListViewSet, basename="virtual-machines"
+)
+router.register("prometheus-devices", PrometheusDeviceSD)
+router.register("prometheus-vms", PrometheusVirtualMachineSD)
+router.register("prometheus-ip-addresses", PrometheusIPAddressSD)
+
+router.register("devices-vms", DevicesVMsListViewSet)
+router.register("devices-vms-attrs", DevicesVMsAttrsListViewSet)
+urlpatterns = router.urls
