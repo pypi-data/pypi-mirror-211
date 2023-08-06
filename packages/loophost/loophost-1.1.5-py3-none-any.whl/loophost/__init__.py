f031be166de1bf5cf8ae5a47c2d79b792ac95f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:37 +0000

    Add package loophost/1.1.5/loophost-1.1.5-py3-none-any.whl

---
+++ packages/loophost/loophost-1.1.5-py3-none-any.whl/loophost/__init__.py
@@ -0,0 +1,39 @@
+__version__ = "1.1.5"
+
+import os
+import pathlib
+import platform
+import sys
+
+
+LOOPHOST_DOMAIN = "loophost.dev"
+TUNNEL_DOMAIN = "fling.team"
+
+PYEX = sys.executable
+HUBDIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def GET_LOOPHOST_DIR():
+    DIR = pathlib.Path("/Users", "Shared", ".loophost")
+    if platform.system().lower().startswith("win"):
+        DIR = pathlib.Path("\\Users", "Public", ".loophost")
+    os.makedirs(DIR, exist_ok=True)
+    os.chdir(DIR)
+    return os.path.abspath(os.path.curdir)
+
+
+_USERNAME = None
+
+
+def GET_FLINGUSER_NAME():
+    global _USERNAME
+    if _USERNAME is None:
+        flinguser_path = pathlib.Path(GET_LOOPHOST_DIR(), "flinguser.txt")
+        if os.path.exists(flinguser_path):
+            with open(flinguser_path, "r") as userfile:
+                _USERNAME = userfile.read()
+    return _USERNAME
+
+
+def DATA_FILE_PATH():
+    return pathlib.Path(GET_LOOPHOST_DIR(), "loophost.json")
