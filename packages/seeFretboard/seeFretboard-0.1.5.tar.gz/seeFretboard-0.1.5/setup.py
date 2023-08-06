ab149f87150ea026691a7a9ec6b3818bf53e181c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:31 +0000

    Add package seeFretboard/0.1.5/seeFretboard-0.1.5.tar.gz

---
+++ packages/seeFretboard/seeFretboard-0.1.5.tar.gz/seeFretboard-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setuptools, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='seeFretboard',
+    version='0.1.5',
+    author='Linda Rong Zhang',
+    author_email='ronglindaz@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/LindaRZhang/seeFretboard',
+    project_urls = {
+        "Bug Tracker": "https://github.com/LindaRZhang/seeFretboard/issues"
+    },
+    license='GNU',
+    packages=find_packages(include=['seeFretboard', 'seeFretboard.*']),
+    install_requires=['requests'],
+    include_package_data=True
+)
