a9a9b6280ea476596c42cfe9712bcd3622cb3620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:16 +0000

    Add package lrservice/0.0.5/lrservice-0.0.5-py3-none-any.whl

---
+++ packages/lrservice/lrservice-0.0.5-py3-none-any.whl/lrservice/__init__.py
@@ -0,0 +1,5 @@
+from lrservice.simple_linear_regr import SimpleLinearRegression
+from lrservice.simple_linear_regr_utils import generate_data, evaluate
+
+__all__ = ['SimpleLinearRegression', 'generate_data', 'evaluate']
+name = 'lrservice'
