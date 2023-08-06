818e13375009e4086d1544b65fb3d9a97aa101bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:30 +0000

    Add package tigeropen/2.3.9/tigeropen-2.3.9.tar.gz

---
+++ packages/tigeropen/tigeropen-2.3.9.tar.gz/tigeropen-2.3.9/tigeropen/trade/response/contracts_response.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018/10/31
+
+@author: gaoan
+"""
+import json
+
+from tigeropen.common.response import TigerResponse
+from tigeropen.common.util.string_utils import camel_to_underline, camel_to_underline_obj
+from tigeropen.trade.domain.contract import Contract
+
+CONTRACT_FIELD_MAPPINGS = {'conid': 'contract_id', 'right': 'put_call', 'tradeable': 'trade'}
+
+
+class ContractsResponse(TigerResponse):
+    def __init__(self):
+        super(ContractsResponse, self).__init__()
+        self.contracts = []
+        self._is_success = None
+    
+    def parse_response_content(self, response_content):
+        response = super(ContractsResponse, self).parse_response_content(response_content)
+        if 'is_success' in response:
+            self._is_success = response['is_success']
+        
+        if self.data:
+            data_json = self.data if isinstance(self.data, dict) else json.loads(self.data)
+            if 'items' in data_json:
+                items = data_json['items']
+            else:
+                items = [data_json]
+            for item in items:
+                contract_fields = {}
+                for key, value in item.items():
+                    tag = CONTRACT_FIELD_MAPPINGS[key] if key in CONTRACT_FIELD_MAPPINGS else camel_to_underline(key)
+                    if isinstance(value, (list, dict)):
+                        value = camel_to_underline_obj(value)
+                    contract_fields[tag] = value
+                contract = Contract()
+                for k, v in contract_fields.items():
+                    setattr(contract, k, v)
+                self.contracts.append(contract)
