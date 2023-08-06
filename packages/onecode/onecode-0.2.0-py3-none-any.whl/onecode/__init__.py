54ef5c3c424969721d787f8906056a359cbe552c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:26:51 +0000

    Add package onecode/0.2.0/onecode-0.2.0-py3-none-any.whl

---
+++ packages/onecode/onecode-0.2.0-py3-none-any.whl/onecode/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 DeepLime <contact@deeplime.io>
+# SPDX-License-Identifier: MIT
+
+__version__ = "0.2.0"
+
+
+from .base import *
+from .elements import *
+from .utils import *
