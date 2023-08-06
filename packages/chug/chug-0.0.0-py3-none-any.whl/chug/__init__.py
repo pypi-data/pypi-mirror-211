4a6c5c3478aeabc96ad082bd7975e71cd001a142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:19 +0000

    Add package chug/0.0.0/chug-0.0.0-py3-none-any.whl

---
+++ packages/chug/chug-0.0.0-py3-none-any.whl/chug/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.0dev0'
+
+#from importlib import metadata
+
+#__version__ = metadata.version(__package__)
+
+#del metadata  # optional, avoids polluting the results of dir(__package__)
