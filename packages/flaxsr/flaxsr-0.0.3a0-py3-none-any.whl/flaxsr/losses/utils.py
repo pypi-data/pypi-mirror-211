2db46d5986e5f313b2208a4e93f7a1d6722a6577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:42 +0000

    Add package flaxsr/0.0.3a0/flaxsr-0.0.3a0-py3-none-any.whl

---
+++ packages/flaxsr/flaxsr-0.0.3a0-py3-none-any.whl/flaxsr/losses/utils.py
@@ -0,0 +1,86 @@
+import jax
+import jax.lax as lax
+import jax.numpy as jnp
+
+import flax
+import flax.linen as nn
+
+import numpy as np
+import einops
+
+from functools import partial
+from typing import Sequence, Literal, Any, Optional
+
+import os
+import pickle
+
+from flaxsr._utils import get
+
+
+loss_wrapper = Any
+
+
+def reduce_fn(loss, reduce: Literal['sum', 'mean', None]) -> jnp.ndarray:
+    if reduce == 'sum':
+        return jnp.sum(loss)
+    elif reduce == 'mean':
+        return jnp.mean(loss)
+    elif reduce is None:
+        return loss
+    else:
+        raise ValueError(f"Invalid reduce mode, got {reduce}. Must be ['sum', 'mean', None]")
+
+
+def _get_package_dir():
+    current_file = os.path.realpath(__file__)
+    return os.path.dirname(current_file)
+
+
+def check_vgg_params_exists():
+    dir_path = _get_package_dir()
+    state = os.path.exists(os.path.join(dir_path, 'vgg19_weights.pkl'))
+
+    if not state:
+        print('VGG19 weights not found !!!')
+        print('Downloading VGG19 weights ...')
+        import tensorflow as tf
+        vgg19 = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+        weights = []
+        for layer in vgg19.layers:
+            if isinstance(layer, tf.keras.layers.Conv2D):
+                weights.append(
+                    (
+                        jnp.asarray(layer.weights[0].numpy()),
+                        jnp.asarray(layer.weights[1].numpy())[jnp.newaxis, jnp.newaxis, jnp.newaxis, ...])
+                )
+        with open(os.path.join(dir_path, 'vgg19_weights.pkl'), 'wb') as f:
+            pickle.dump(weights, f)
+        print('Done !')
+
+
+def load_vgg19_params():
+    dir_path = _get_package_dir()
+    params = pickle.load(open(os.path.join(dir_path, 'vgg19_weights.pkl'), 'rb'))
+    return params
+
+
+def get_loss_wrapper(losses: Sequence[str], weights: Sequence[float]) -> loss_wrapper:
+    assert len(losses) == len(weights), \
+        f"Number of losses and weights must be equal, got {len(losses)} and {len(weights)}"
+    return [(get('losses', loss), float(weight)) for loss, weight in zip(losses, weights)]
+
+
+def compute_loss(
+        hr: jnp.ndarray, sr: jnp.ndarray, losses: loss_wrapper, mode: Literal['sum', 'mean', None] = 'sum',
+        mask: Optional[jnp.ndarray] = None
+) -> jnp.ndarray:
+    loss = jnp.zeros(())
+
+    if mask is not None:
+        hr = hr * mask
+        sr = sr * mask
+
+    for loss_fn, weight in losses:
+        loss += weight * reduce_fn(loss_fn(hr, sr), mode)
+
+    return loss
