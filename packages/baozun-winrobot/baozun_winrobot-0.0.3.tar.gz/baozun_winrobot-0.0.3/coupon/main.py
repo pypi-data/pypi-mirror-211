c67e031ec09d244bbd18248b17a06698dace6e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:24 +0000

    Add package baozun-winrobot/0.0.3/baozun_winrobot-0.0.3.tar.gz

---
+++ packages/baozun-winrobot/baozun_winrobot-0.0.3.tar.gz/baozun_winrobot-0.0.3/coupon/main.py
@@ -0,0 +1,13 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import requests;
+from requests.cookies import RequestsCookieJar;
+from conf.tmall import TMALL_DEFAULT_HEADERS;
+
+def get_coupon_info(cookie_list = []):
+  url = "https://shell.mkt.taobao.com/coupon/getActivityInfo"
+  payload = "{\"id\":\"5907348532\",\"queryDetail\":\"true\",\"couponType\":\"0\"}"
+  cookie_jar = RequestsCookieJar()
+  for cookie_item in cookie_list:
+    cookie_jar.set(cookie_item['name'], cookie_item['value'], domain=cookie_item['domain'])
+  response = requests.request("POST", url, headers=TMALL_DEFAULT_HEADERS, data=payload, cookies=cookie_jar)
+  return response.text
