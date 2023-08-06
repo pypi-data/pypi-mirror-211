ecca8fd0adc8cab397547531d6f74ff18964675e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:50 +0000

    Add package cerebrium/0.5.3/cerebrium-0.5.3-py3-none-any.whl

---
+++ packages/cerebrium/cerebrium-0.5.3-py3-none-any.whl/cerebrium/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.5.3"
+
+from cerebrium.core import deploy, model_api_request, save, get, delete, upload
+from cerebrium.flow import ModelType as model_type
+from cerebrium.conduit import Conduit, Hardware as hardware
+from cerebrium.logging.base import LoggingPlatform as logging_platform
