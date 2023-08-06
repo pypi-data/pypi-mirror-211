d52dcbb063c14aae2a15a598d3e94b1e0d3d7c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:12 +0000

    Add package gmcapsule/0.4.0/gmcapsule-0.4.0-py3-none-any.whl

---
+++ packages/gmcapsule/gmcapsule-0.4.0-py3-none-any.whl/gmcapsule/modules/10_rewrite.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2023 Jaakko Keränen <jaakko.keranen@iki.fi>
+# License: BSD-2-Clause
+
+"""Rewriter"""
+
+import re
+
+
+class PathRewriteHandler:
+    def __init__(self, capsule, rewritten_path):
+        self.capsule = capsule
+        self.rewritten_path = rewritten_path
+
+    def __call__(self, req):
+        old_path = req.path
+        req.path = self.rewritten_path
+
+        # Don't allow rewriting the same request too many times.
+        if hasattr(req, 'num_rewrites'):
+            req.num_rewrites += 1
+        else:
+            req.num_rewrites = 1
+        if req.num_rewrites == 100:
+            return 40, "Stuck in rewrite loop: " + req.url()
+
+        print("[rewrite]", old_path, "->", req.path)
+        return self.capsule.call_entrypoint(req)[0]
+
+
+class Responder:
+    def __init__(self, response):
+        self.response = response
+
+    def __call__(self, req):
+        return self.response
+
+
+class Rewriter:
+    def __init__(self, capsule, protocol, host, src_path, dst_path, status):
+        self.capsule = capsule
+        self.protocol = protocol
+        self.host = host
+        self.src_path = src_path
+        self.dst_path = dst_path
+        self.status = status
+
+    def __call__(self, path):
+        # If path matches a rewritten URL, return the handler object that calls the
+        # correct handler for the updated URL.
+        if self.dst_path:
+            new_path = self.src_path.sub(self.dst_path, path)
+            if new_path != path:
+                return PathRewriteHandler(self.capsule, new_path)
+
+        elif self.status:
+            m = self.src_path.match(path)
+            if m:
+                status = self.status
+                for i in range(self.src_path.groups + 1):
+                    cap = m[i]
+                    if cap:
+                        status = status.replace(f'\\{i}', cap)
+                code, meta = status.split()
+                print("[rewrite]", code, meta)
+                return Responder((int(code), meta))
+
+        return None
+
+
+def init(capsule):
+    cfg = capsule.config()
+    for section in cfg.prefixed_sections('rewrite.').values():
+        protocol = section.get('protocol', None)
+        host = section.get('host', cfg.hostnames()[0])
+        src_path = re.compile(section.get('path'))
+        dst_path = section.get('repl', None)
+        status = section.get('status', None)
+        for proto in [protocol] if protocol else ['gemini', 'titan']:
+            capsule.add(Rewriter(capsule, proto, host, src_path, dst_path, status),
+                        None, # `Rewriter` will return a suitable handler callback.
+                        host,
+                        proto)
