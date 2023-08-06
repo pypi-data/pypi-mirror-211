79ce45ba796bf1d547992c51c8321b2eec24d45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:41 +0000

    Add package apigratis-sdk-python/1.0.1/apigratis-sdk-python-1.0.1.tar.gz

---
+++ packages/apigratis-sdk-python/apigratis-sdk-python-1.0.1.tar.gz/apigratis-sdk-python-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='apigratis-sdk-python',
+    version='1.0.1',
+    url='https://github.com/APIBrasil/apigratis-sdk-python',
+    license='MIT License',
+    author='APIBRASIL',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='contato@apibrasil.com.br',
+    keywords='whatsapp api, apibrasil, cnpj, sms, cep, consulta, api, brasil, gratis, free, whatsapp, apiwhatsapp, apigratis, apifree',
+    description=u'Transforme seus projetos em soluções inteligentes com nossa API. Com recursos como API do WhatsApp, geolocalização, rastreamento de encomendas, verificação de CPF/CNPJ e mais, você pode criar soluções eficientes e funcionais',
+    packages=['apigratis-sdk-python'],
+    install_requires=['requests'])
