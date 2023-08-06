ef7e8771620bc5f843aabe599ccca2bafdbe8074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:08 +0000

    Add package spida/0.3.7/spida-0.3.7.tar.gz

---
+++ packages/spida/spida-0.3.7.tar.gz/spida-0.3.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name="spida",
+    version="0.3.7",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT",
+    description="A Stable Diffusion API Wrapper for AUTOMATIC1111/stable-diffusion-webui",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="h2see",
+    url="https://github.com/h2see/spida",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required_packages,
+    python_requires=">=3.10, <3.11",
+)
