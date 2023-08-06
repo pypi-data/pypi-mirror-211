8d9943ec81ba114cd574025c1d2685fd8b2703e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:31 +0000

    Add package blackboxopt/4.15.1/blackboxopt-4.15.1-py3-none-any.whl

---
+++ packages/blackboxopt/blackboxopt-4.15.1-py3-none-any.whl/blackboxopt/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "4.15.1"
+
+from parameterspace import ParameterSpace
+
+from .base import (
+    ConstraintsError,
+    ContextError,
+    EvaluationsError,
+    Objective,
+    ObjectivesError,
+    OptimizationComplete,
+    Optimizer,
+    OptimizerNotReady,
+)
+from .evaluation import Evaluation, EvaluationSpecification
+from .utils import sort_evaluations
