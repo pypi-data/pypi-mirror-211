6193f0158add0b3c2fc6a1ad3cbea2cd12f84682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:14 +0000

    Add package overhave/3.14.4/overhave-3.14.4.tar.gz

---
+++ packages/overhave/overhave-3.14.4.tar.gz/overhave-3.14.4/overhave/storage/scenario_storage.py
@@ -0,0 +1,71 @@
+import abc
+from typing import cast
+
+from overhave import db
+from overhave.storage.converters import ScenarioModel
+
+
+class BaseScenarioStorageException(Exception):
+    """Base exception for :class:`ScenarioStorage`."""
+
+
+class ScenarioNotExistsError(BaseScenarioStorageException):
+    """Exception for situation without scenario."""
+
+
+class IScenarioStorage(abc.ABC):
+    """Abstract class for feature type storage."""
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_scenario(scenario_id: int) -> ScenarioModel | None:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_scenario_by_feature_id(feature_id: int) -> ScenarioModel:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def update_scenario(model: ScenarioModel) -> None:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def create_scenario(model: ScenarioModel) -> int:
+        pass
+
+
+class ScenarioStorage(IScenarioStorage):
+    """Class for feature type storage."""
+
+    @staticmethod
+    def get_scenario(scenario_id: int) -> ScenarioModel | None:
+        with db.create_session() as session:
+            scenario = session.get(db.Scenario, scenario_id)
+            if scenario is not None:
+                return ScenarioModel.from_orm(scenario)
+            return None
+
+    @staticmethod
+    def get_scenario_by_feature_id(feature_id: int) -> ScenarioModel:
+        with db.create_session() as session:
+            scenario: db.Scenario = session.query(db.Scenario).filter(db.Scenario.feature_id == feature_id).one()
+            return ScenarioModel.from_orm(scenario)
+
+    @staticmethod
+    def update_scenario(model: ScenarioModel) -> None:
+        with db.create_session() as session:
+            scenario = session.get(db.Scenario, model.id)
+            if scenario is None:
+                raise ScenarioNotExistsError(f"Scenario with id={model.id} does not exist!")
+            scenario.text = model.text
+
+    @staticmethod
+    def create_scenario(model: ScenarioModel) -> int:
+        with db.create_session() as session:
+            scenario = db.Scenario(feature_id=model.feature_id, text=model.text)
+            session.add(scenario)
+            session.flush()
+            return cast(int, scenario.id)
