5ed629938641a7d9dedd9326056381475c70fd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:15 +0000

    Add package datagen-protocol-core/0.32.0/datagen_protocol_core-0.32.0-py3-none-any.whl

---
+++ packages/datagen-protocol-core/datagen_protocol_core-0.32.0-py3-none-any.whl/datagen_protocol/schema/humans/__init__.py
@@ -0,0 +1,22 @@
+from datagen_protocol.schema.humans.accessories import (
+    Accessories,
+    Color,
+    Glasses,
+    GlassesPosition,
+    Mask,
+    MaskPosition,
+    MaskTexture,
+)
+from datagen_protocol.schema.humans.human import (
+    Expression,
+    ExpressionName,
+    Eyebrows,
+    Eyes,
+    FacialHair,
+    Gaze,
+    Hair,
+    HairColor,
+    Head,
+    Human,
+    HumanDatapoint,
+)
