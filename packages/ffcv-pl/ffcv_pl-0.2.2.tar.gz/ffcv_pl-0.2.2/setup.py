cd8b7bafa564ae1bfdc79c1da97fba6e0365b9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package ffcv-pl/0.2.2/ffcv_pl-0.2.2.tar.gz

---
+++ packages/ffcv-pl/ffcv_pl-0.2.2.tar.gz/ffcv_pl-0.2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='ffcv_pl',
+      version='0.2.2',
+      description='manage fast data loading with ffcv and pytorch lightning',
+      long_description='see [github project](https://github.com/SerezD/ffcv_pytorch_lightning) ',
+      long_description_content_type='text/markdown',
+      url='https://github.com/SerezD/ffcv_pytorch_lightning',
+      author='DSerez',
+      license='MIT',
+      zip_safe=False)
