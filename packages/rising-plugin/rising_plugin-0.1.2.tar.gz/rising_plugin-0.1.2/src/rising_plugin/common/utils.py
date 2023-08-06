e85c2d126b3311c6f5c4840c66ceb7ca76ffe25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:32 +0000

    Add package rising-plugin/0.1.2/rising_plugin-0.1.2.tar.gz

---
+++ packages/rising-plugin/rising_plugin-0.1.2.tar.gz/rising_plugin-0.1.2/src/rising_plugin/common/utils.py
@@ -0,0 +1,28 @@
+import os
+
+# env variables
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_KEY = os.getenv("PINECONE_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
+FIREBASE_ENV = os.getenv("FIREBASE_SERVICE_ACCOUNT_TEST3_83FFC")
+
+# firebase
+FIREBASE_STORAGE_ROOT = "images/"
+FIREBASE_STORAGE_BUCKET = "test3-83ffc.appspot.com"
+
+# pinecone
+PINECONE_NAMESPACE = "risinglangchain-namespace"
+PINECONE_INDEX_NAME = "risinglangchain-index"
+
+
+# indexes of relatedness of embedding
+COMMAND_SMS_INDEXS = [4, 5]
+
+
+class ProgramType:
+    BROWSER = "browser"
+    ALERT = "alert"
+    IMAGE = "image"
+    SMS = "sms"
+    CONTACT = "contact"
+    MESSAGE = "message"
