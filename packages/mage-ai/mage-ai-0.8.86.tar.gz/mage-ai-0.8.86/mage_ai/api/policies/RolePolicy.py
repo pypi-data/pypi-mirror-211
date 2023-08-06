3f1d69b8a1751a38402d0d9da6508987ed1c0f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:10 +0000

    Add package mage-ai/0.8.86/mage-ai-0.8.86.tar.gz

---
+++ packages/mage-ai/mage-ai-0.8.86.tar.gz/mage-ai-0.8.86/mage_ai/api/policies/RolePolicy.py
@@ -0,0 +1,25 @@
+from mage_ai.api.oauth_scope import OauthScope
+from mage_ai.api.operations import constants
+from mage_ai.api.policies.BasePolicy import BasePolicy
+from mage_ai.api.presenters.RolePresenter import RolePresenter
+
+
+class RolePolicy(BasePolicy):
+    pass
+
+
+RolePolicy.allow_actions([
+    constants.LIST,
+], scopes=[
+    OauthScope.CLIENT_PRIVATE,
+], condition=lambda policy: policy.has_at_least_viewer_role())
+
+RolePolicy.allow_read(RolePresenter.default_attributes, scopes=[
+    OauthScope.CLIENT_PRIVATE,
+], condition=lambda policy: policy.has_at_least_viewer_role())
+
+RolePolicy.allow_query([
+    'limit_roles',
+], scopes=[
+    OauthScope.CLIENT_PRIVATE,
+], condition=lambda policy: policy.has_at_least_viewer_role())
