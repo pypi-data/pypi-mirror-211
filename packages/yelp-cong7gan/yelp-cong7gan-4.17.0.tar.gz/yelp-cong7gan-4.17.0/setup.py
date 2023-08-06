7dde33c5c696b18174ae6ec33217403c91103e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:55 +0000

    Add package yelp-cong7gan/4.17.0/yelp-cong7gan-4.17.0.tar.gz

---
+++ packages/yelp-cong7gan/yelp-cong7gan-4.17.0.tar.gz/yelp-cong7gan-4.17.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='yelp-cong7gan',
+    version='4.17.0',
+    description='A logging package for Yelp applications',
+    author='Infohugger',
+    author_email='HIMANSHUxd2022@proton.me',
+    packages=['yelp_logging'],
+    install_requires=[],
+)
