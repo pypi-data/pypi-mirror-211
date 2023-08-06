ab6255aaddf6174b215bb3f0be563c7f12fb5976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:39 +0000

    Add package circuit-knitting-toolbox/0.2.0/circuit_knitting_toolbox-0.2.0-py3-none-any.whl

---
+++ packages/circuit-knitting-toolbox/circuit_knitting_toolbox-0.2.0-py3-none-any.whl/circuit_knitting_toolbox/circuit_cutting/__init__.py
@@ -0,0 +1,48 @@
+# This code is a Qiskit project.
+
+# (C) Copyright IBM 2022.
+
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Circuit Cutting (:mod:`circuit_knitting_toolbox.circuit_cutting`).
+
+.. currentmodule:: circuit_knitting_toolbox.circuit_cutting
+
+.. autosummary::
+   :toctree: ../stubs/
+   :nosignatures:
+
+    cutqc.run_subcircuit_instances
+    cutqc.generate_summation_terms
+    cutqc.build
+    cutqc.verify
+    cutqc.cut_circuit_wires
+    cutqc.evaluate_subcircuits
+    cutqc.reconstruct_full_distribution
+    partition_circuit_qubits
+    partition_problem
+    decompose_gates
+    execute_experiments
+    reconstruct_expectation_values
+"""
+from .cutting_decomposition import (
+    partition_circuit_qubits,
+    partition_problem,
+    decompose_gates,
+)
+from .cutting_evaluation import execute_experiments
+from .cutting_reconstruction import reconstruct_expectation_values
+
+__all__ = [
+    "partition_circuit_qubits",
+    "partition_problem",
+    "decompose_gates",
+    "execute_experiments",
+    "reconstruct_expectation_values",
+]
