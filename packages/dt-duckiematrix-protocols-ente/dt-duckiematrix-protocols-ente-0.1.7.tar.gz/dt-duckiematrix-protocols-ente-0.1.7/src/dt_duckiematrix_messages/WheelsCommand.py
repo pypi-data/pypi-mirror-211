6e57871c292c283eb2832e1f7fa16816211105d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:48 +0000

    Add package dt-duckiematrix-protocols-ente/0.1.7/dt-duckiematrix-protocols-ente-0.1.7.tar.gz

---
+++ packages/dt-duckiematrix-protocols-ente/dt-duckiematrix-protocols-ente-0.1.7.tar.gz/dt-duckiematrix-protocols-ente-0.1.7/src/dt_duckiematrix_messages/WheelsCommand.py
@@ -0,0 +1,10 @@
+from typing import Dict
+
+import dataclasses
+
+from dt_duckiematrix_messages import CBorMessage
+
+
+@dataclasses.dataclass
+class WheelsCommand(CBorMessage):
+    wheels: Dict[str, float]
