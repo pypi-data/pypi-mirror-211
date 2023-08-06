55e7dbcac0f92fea1552d0467aa2b1b0d0c43cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:50 +0000

    Add package pipen-annotate/0.7.3/pipen_annotate-0.7.3.tar.gz

---
+++ packages/pipen-annotate/pipen_annotate-0.7.3.tar.gz/pipen_annotate-0.7.3/pipen_annotate/annotate.py
@@ -0,0 +1,257 @@
+from __future__ import annotations
+
+import re
+import textwrap
+from typing import Any, Callable, Type, MutableMapping
+
+from diot import OrderedDiot
+from pipen import Proc, ProcGroup
+from pipen.utils import mark as proc_mark, get_marked as proc_get_marked
+
+from .sections import (
+    Section,
+    SectionSummary,
+    SectionInput,
+    SectionOutput,
+    SectionEnvs,
+    SectionItems,
+    SectionProcGroupArgs,
+    SectionText,
+)
+
+SECTION_TYPES: MutableMapping[str, Type[Section]] = {
+    "Summary": SectionSummary,
+    "Input": SectionInput,
+    "Output": SectionOutput,
+    "Envs": SectionEnvs,
+    "Args": SectionItems,
+    "Returns": SectionItems,
+    "Raises": SectionItems,
+    "Warns": SectionItems,
+    "See Also": SectionText,
+    "Notes": SectionText,
+    "References": SectionText,
+    "Examples": SectionText,
+    "Todo": SectionText,
+    "Text": SectionText,
+    "Items": SectionItems,
+}
+
+META_CONTAINER = "__meta__"
+
+
+def _mark(cls: type, **kwargs) -> type:
+    """Mark a class, not only a Proc
+
+    Args:
+        cls: The class to be marked
+        kwargs: The marks
+
+    Returns:
+        The marked class
+    """
+    if issubclass(cls, Proc):
+        return proc_mark(**kwargs)(cls)
+
+    if not getattr(cls, META_CONTAINER, None):
+        setattr(cls, META_CONTAINER, {})
+
+    meta = getattr(cls, META_CONTAINER)
+    meta.update(kwargs)
+    return cls
+
+
+def _get_marked(cls: type, mark_name: str, default: Any = None) -> Any:
+    """Get the marked value from a class, not only a Proc
+
+    Args:
+        cls: The class to get marked value from
+        mark_name: The mark name
+        default: The default value if not found
+
+    Returns:
+        The marked value
+    """
+    if issubclass(cls, Proc):
+        return proc_get_marked(cls, mark_name, default)
+
+    if not getattr(cls, META_CONTAINER, None):
+        setattr(cls, META_CONTAINER, {})
+
+    meta = getattr(cls, META_CONTAINER)
+    return meta.get(mark_name, default)
+
+
+def _annotate_uninherited(cls: type) -> OrderedDiot:
+    """Annotate a Proc class with docstring, without inheriting from base.
+
+    Args:
+        cls: The class to be annotated.
+
+    Returns:
+        The annotated dict.
+    """
+    annotated = OrderedDiot()
+    docstring = cls.__doc__
+
+    if docstring:
+        if docstring[0] in (" ", "\t"):
+            docstring = textwrap.dedent(docstring)
+        else:
+            parts = docstring.split("\n", 1)
+            if len(parts) == 1:
+                first, rest = parts[0], ""
+            else:
+                first, rest = parts
+            docstring = f"{first}\n{textwrap.dedent(rest)}"
+
+        section_name = "Summary"
+        section = SectionSummary(cls, section_name)
+        for line in docstring.splitlines():
+            line = line.rstrip()
+            if line and line[-1] == ":":
+                if line[:-1] in SECTION_TYPES:
+                    annotated[section_name] = section.parse()
+                    section_name = line[:-1]
+                    if section_name == "Args" and issubclass(cls, ProcGroup):
+                        section = SectionProcGroupArgs(cls, section_name)
+                    else:
+                        section = SECTION_TYPES[section_name](
+                            cls,
+                            section_name,
+                        )
+                elif re.sub(r"(?!^) ", "", line[:-1]).isidentifier():
+                    annotated[section_name] = section.parse()
+                    section_name = line[:-1]
+                    section = SectionText(cls, section_name)
+                else:
+                    section.consume(line)
+            else:
+                section.consume(line)
+
+        annotated[section_name] = section.parse()
+
+    if issubclass(cls, Proc):
+        if "Summary" not in annotated:
+            annotated.Summary = SectionSummary(cls, "Summary").parse()
+        if "Input" not in annotated:
+            annotated.Input = SectionInput(cls, "Input").parse()
+        if "Output" not in annotated:
+            annotated.Output = SectionOutput(cls, "Output").parse()
+        if "Envs" not in annotated:
+            annotated.Envs = SectionEnvs(cls, "Envs").parse()
+
+    return annotated
+
+
+def _update_annotation(
+    base: OrderedDiot | None,
+    other: OrderedDiot,
+) -> OrderedDiot:
+    """Update the annotation with another annotation."""
+    if base is None:
+        base = OrderedDiot()
+    else:
+        base = base.copy()
+
+    for key, value in other.items():
+        section_class = SECTION_TYPES.get(key)
+        if key not in base or section_class is None:
+            base[key] = value
+        else:
+            base[key] = section_class.update_parsed(base[key], value)
+
+    return base
+
+
+def annotate(cls: Type[Any]) -> OrderedDiot:
+    """Annotate a Proc class with docstring.
+
+    Args:
+        cls: The class to be annotated.
+
+    Returns:
+        The annotated dict.
+    """
+    annotated = _get_marked(cls, "annotate_annotated")
+    inherit = _get_marked(cls, "annotate_inherit", True)
+
+    if not annotated:
+        annotated = _annotate_uninherited(cls)
+        if issubclass(cls, Proc):
+            base = [
+                mro
+                for mro in cls.__mro__
+                if issubclass(mro, Proc)
+                and mro is not Proc
+                and mro is not cls
+                and mro is not object
+            ]
+        else:
+            base = [
+                mro
+                for mro in cls.__mro__
+                if mro is not cls
+                and mro is not object
+            ]
+
+        base = base[0] if base else None
+        annotated_base = annotate(base) if inherit and base else None
+        annotated = _update_annotation(annotated_base, annotated)
+        _mark(cls, annotate_annotated=annotated)
+
+    return annotated
+
+
+def _register_section(
+    section: str,
+    section_class: Type[Section] | str | None = None,
+) -> Callable[[Type[Section]], Type[Section]] | Type[Section]:
+    """Register a section to be parsed.
+
+    Args:
+        section: The section name.
+        section_class: The section class or a shortcut string to
+            builtin section class.
+            summary: SectionSummary
+            input: SectionInput
+            output: SectionOutput
+            envs: SectionEnvs
+            items: SectionItems
+            text: SectionText
+    """
+    # When used as a decorator
+    if section_class is None:
+        return lambda cls: _register_section(section, cls)
+
+    if isinstance(section_class, str):
+        section_class = section_class.title()
+        if section_class not in SECTION_TYPES:
+            raise ValueError(
+                f"Invalid section class shortcut: {section_class}\n"
+                f"Valid shortcuts: {', '.join(SECTION_TYPES)}",
+            ) from None
+        section_class = SECTION_TYPES[section_class]
+
+    SECTION_TYPES[section] = section_class
+    return section_class
+
+
+def _unregister_section(section: str) -> None:
+    """Unregister a section.
+
+    Args:
+        section: The section name.
+    """
+    SECTION_TYPES.pop(section, None)
+
+
+def _no_doc_inherit(proc: type) -> type:
+    """A decorator to disable docstring inheritance for a class"""
+    _mark(proc, annotate_inherit=False)
+    return proc
+
+
+annotate.register_section = _register_section
+annotate.unregister_section = _unregister_section
+annotate.no_inherit = _no_doc_inherit
---
+++ packages/pipen-annotate/pipen_annotate-0.7.3.tar.gz/pipen_annotate-0.7.3/pipen_annotate/sections.py
@@ -0,0 +1,385 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List, Mapping, MutableMapping
+
+import re
+import textwrap
+import warnings
+
+from diot import Diot, OrderedDiot
+from pipen.defaults import ProcInputType
+
+__all__ = (
+    "SectionSummary",
+    "SectionInput",
+    "SectionOutput",
+    "SectionEnvs",
+    "SectionItems",
+    "SectionText",
+)
+
+ITEM_LINE_REGEX = re.compile(
+    r"^(?P<name>[^\s]+)\s*(?:\((?P<attrs>.+?)\))?:(?P<help>.+)?$"
+)
+ITEM_ATTR_REGEX = re.compile(r"^(?P<name>[\w-]+)(?:\s*[:=]\s*(?P<value>.+))?$")
+
+
+class MalFormattedAnnotationError(Exception):
+    """Raised when the annotation is malformatted"""
+
+
+class UnknownAnnotationItemWarning(Warning):
+    """Raised when the annotation item is unknown"""
+
+
+def _dedent(lines: List[str]) -> List[str]:
+    """Dedent a list of lines.
+    """
+    return textwrap.dedent("\n".join(lines)).splitlines()
+
+
+def _end_of_sentence(line: str) -> bool:
+    """Check if a line ends with a sentence.
+    """
+    return (
+        line.endswith(".")
+        or line.endswith("?")
+        or line.endswith("!")
+        or line.endswith(":")
+    )
+
+
+def _parse_terms(
+    lines: List[str],
+    prefix: str | None = None,
+) -> Diot:
+    """Parse a list of lines as terms.
+    """
+    terms = OrderedDiot()
+    lines = _dedent(lines)
+    sublines = []
+    item = None
+    just_matched = False
+    help_continuing = False
+    for line in lines:
+        if prefix:
+            matched = ITEM_LINE_REGEX.match(line[len(prefix):])
+        else:
+            matched = ITEM_LINE_REGEX.match(line)
+
+        lstripped_line = line.lstrip()
+        if matched:
+            if item:
+                # See if we have sub-terms
+                item.terms = _parse_terms(sublines, prefix="- ")
+                sublines.clear()
+
+            # Create a new item
+            name = matched.group("name")
+            attrs = matched.group("attrs")
+            help = matched.group("help")
+            terms[name] = Diot(attrs={}, terms={}, help="")
+
+            if attrs:
+                for attr in attrs.split(";"):
+                    attr = attr.strip()
+                    attr_matched = ITEM_ATTR_REGEX.match(attr)
+                    if not attr_matched:
+                        raise MalFormattedAnnotationError(
+                            f"\nInvalid item attribute: {attr}"
+                            "\nExpecting: <name>[:=]<value>"
+                            f"\nFull attributes: {attrs}"
+                        )
+                    attr_name = attr_matched.group("name")
+                    attr_value = attr_matched.group("value")
+
+                    terms[name].attrs[attr_name] = (
+                        True if attr_value is None else attr_value
+                    )
+
+            if help is not None:
+                terms[name].help = help.strip()
+                if terms[name].help == "|":
+                    help_continuing = True
+
+            item = terms[name]
+            just_matched = True
+        elif item is None:
+            raise MalFormattedAnnotationError(
+                f"\nInvalid item line: {line}"
+                "\nExpecting: <name>[ (<attrs>)]: <help>"
+            )
+        elif just_matched and not lstripped_line.startswith("- "):
+            if help_continuing and item.help == "|":
+                sep = item.help = ""
+            else:
+                sep = (
+                    "\n"
+                    if help_continuing
+                    or _end_of_sentence(item.help)
+                    or lstripped_line.startswith(">>>")
+                    or (
+                        item.help
+                        and item.help.splitlines()[-1].startswith(">>>")
+                    )
+                    else " "
+                )
+            item.help = f"{item.help}{sep}{lstripped_line}"
+        elif lstripped_line.startswith("- "):
+            sublines.append(line)
+            just_matched = False
+            help_continuing = False
+        else:
+            sublines.append(line)
+
+    if item:
+        # See if we have sub-terms
+        item.terms = _parse_terms(sublines, prefix="- ")
+
+    return terms
+
+
+def _parse_one_output(out: str) -> List[str] | None:
+    """Parse one output item, such as `out:file:<outfile>`"""
+    parts = out.split(":")
+    if not parts[0].isidentifier():
+        return None
+    if len(parts) < 3:
+        return [parts[0], ProcInputType.VAR, parts[1]]
+    return parts
+
+
+def _update_attrs_with_cls(
+    parsed: Diot,
+    envs: dict | None,
+    prev_key: str | None = None,
+) -> None:
+    """Update the attrs of parsed terms with the class envs."""
+    envs = envs or {}
+    for key, value in envs.items():
+        whole_key = f"{prev_key}.{key}" if prev_key else key
+
+        if key not in parsed:
+            parsed[key] = Diot(attrs={}, terms={}, help="")
+
+        if (
+            parsed[key].attrs.get("ns", False)
+            or parsed[key].attrs.get("namespace", False)
+            or parsed[key].attrs.get("action", "") in ("ns", "namespace")
+            or (isinstance(value, dict) and parsed[key].terms)
+        ):
+            parsed[key].attrs.setdefault("ns", True)
+            _update_attrs_with_cls(
+                parsed[key].terms,
+                value,
+                prev_key=whole_key,
+            )
+            # continue
+
+        if "default" not in parsed[key].attrs:
+            parsed[key].attrs["default"] = value
+
+
+def _update_terms(base: Mapping, other: Mapping) -> None:
+    """Update the terms of base with the other terms."""
+    base = base.copy()
+    for key, value in other.items():
+        if key not in base:
+            base[key] = value
+        else:
+            if value.help:
+                base[key].help = value.help
+            base[key].attrs.update(value.attrs)
+            _update_terms(base[key].terms, value.terms)
+
+    return base
+
+
+class Section(ABC):
+
+    def __init__(self, cls, name) -> None:
+        self.name = name
+        self._cls = cls
+        self._lines: List[str] = []
+
+    def consume(self, line: str) -> None:
+        self._lines.append(line)
+
+    @abstractmethod
+    def parse(self) -> str | Diot | List[str]:  # pragma: no cover
+        pass
+
+    @classmethod
+    def update_parsed(
+        cls,
+        base: str | List[str] | MutableMapping,
+        other: str | List[str] | MutableMapping,
+    ) -> str | List[str] | MutableMapping:
+        """Update the parsed result with the other result."""
+        if isinstance(other, str):  # pragma: no cover
+            return other
+        if isinstance(other, list):
+            return base + other
+
+        base = base.copy()
+        base.update(other)
+        return base
+
+
+class SectionSummary(Section):
+
+    def parse(self) -> str | Diot | List[str]:
+        """Parse the summary section."""
+        lines = self._lines
+        if lines:
+            if lines[0] and lines[0][0] in (" ", "\t"):
+                lines = _dedent(self._lines)
+            else:
+                lines = [lines[0]] + _dedent(lines[1:])
+
+        short = long = ""
+        for i, line in enumerate(lines):
+            if not line:
+                long = "\n".join(lines[i + 1:])
+                break
+            short += line + " "
+
+        return Diot(short=short.rstrip(), long=long)
+
+    @classmethod
+    def update_parsed(
+        cls,
+        base: str | List[str] | MutableMapping,
+        other: str | List[str] | MutableMapping,
+    ) -> str | List[str] | MutableMapping:
+        """Update the parsed result with the other result."""
+        base = base.copy()
+        if other.short:
+            base.short = other.short
+        if other.long:
+            base.long = other.long
+        return base
+
+
+class SectionItems(Section):
+
+    def parse(self) -> str | Diot | List[str]:
+        try:
+            return _parse_terms(self._lines)
+        except MalFormattedAnnotationError as e:
+            raise MalFormattedAnnotationError(
+                f"[{self._cls.__name__}/{self.name}] {e}"
+            ) from None
+
+    @classmethod
+    def update_parsed(
+        cls,
+        base: str | List[str] | MutableMapping,
+        other: str | List[str] | MutableMapping,
+    ) -> str | List[str] | MutableMapping:
+        """Update the parsed result with the other result."""
+        return _update_terms(base, other)
+
+
+class SectionInput(SectionItems):
+
+    def parse(self) -> str | Diot | List[str]:
+        parsed = super().parse()
+        input_keys = self._cls.input
+
+        if isinstance(input_keys, str):
+            input_keys = [
+                input_key.strip() for input_key in input_keys.split(",")
+            ]
+
+        input_key_names = set()
+        for input_key_type in input_keys or []:
+            if ":" not in input_key_type:
+                input_key_type = f"{input_key_type}:{ProcInputType.VAR}"
+
+            input_key, input_type = input_key_type.split(":", 1)
+            input_key_names.add(input_key)
+            if input_key not in parsed:
+                parsed[input_key] = Diot(
+                    attrs={"itype": input_type},
+                    terms={},
+                    help="",
+                )
+            else:
+                parsed[input_key].attrs["itype"] = input_type
+
+            parsed[input_key].attrs["nargs"] = "+"
+            if input_type in (ProcInputType.FILES, ProcInputType.DIRS):
+                parsed[input_key].attrs["action"] = "append"
+            else:
+                parsed[input_key].attrs["action"] = "extend"
+
+        if set(parsed) - input_key_names:
+            warnings.warn(
+                f"[{self._cls.__name__}] Unknown input keys "
+                f"{set(parsed) - input_key_names}",
+                UnknownAnnotationItemWarning,
+            )
+
+        return parsed
+
+
+class SectionOutput(SectionItems):
+
+    def parse(self) -> str | Diot | List[str]:
+        output = self._cls.output
+        if not output:
+            return Diot()
+
+        parsed = super().parse()
+
+        if not isinstance(output, (list, tuple)):
+            output = [out.strip() for out in output.split(",")]
+
+        out_names = set()
+        for out in output:
+            parts = _parse_one_output(out)
+            if not parts:
+                continue
+
+            out_names.add(parts[0])
+            if parts[0] not in parsed:
+                parsed[parts[0]] = Diot(
+                    attrs={"otype": parts[1], "default": parts[2]},
+                    terms={},
+                    help="",
+                )
+            else:
+                parsed[parts[0]].attrs["otype"] = parts[1]
+                parsed[parts[0]].attrs["default"] = parts[2]
+
+        if set(parsed) - out_names:
+            warnings.warn(
+                f"[{self._cls.__name__}] Unknown output keys "
+                f"{set(parsed) - out_names}",
+                UnknownAnnotationItemWarning,
+            )
+
+        return parsed
+
+
+class SectionEnvs(SectionItems):
+
+    def parse(self) -> str | Diot | List[str]:
+        parsed = super().parse()
+        _update_attrs_with_cls(parsed, self._cls.envs)
+        return parsed
+
+
+class SectionProcGroupArgs(SectionItems):
+
+    def parse(self) -> str | Diot | List[str]:
+        parsed = super().parse()
+        _update_attrs_with_cls(parsed, self._cls.DEFAULTS)
+        return parsed
+
+
+class SectionText(Section):
+
+    def parse(self) -> str | Diot | List[str]:
+        return "\n".join(_dedent(self._lines))
