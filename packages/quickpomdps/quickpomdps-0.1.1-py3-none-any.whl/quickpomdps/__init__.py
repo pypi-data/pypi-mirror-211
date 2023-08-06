1cb64a4918f9d0ff868b8ad14da3597a693be475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:26:38 +0000

    Add package quickpomdps/0.1.1/quickpomdps-0.1.1-py3-none-any.whl

---
+++ packages/quickpomdps/quickpomdps-0.1.1-py3-none-any.whl/quickpomdps/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+import julia
+
+julia.install()
+from julia import Main
+
+
+def install_julia_dependencies():
+    """
+    Install Julia packages required for quickpomdps
+    """
+    from julia import Pkg
+
+    Pkg.add(["PyCall","QuickPOMDPs"])
+
+install_julia_dependencies()
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+Main.include(os.path.join(script_dir, "setup.jl"))
+from julia.QuickPOMDPs import (
+    DiscreteExplicitPOMDP,
+    DiscreteExplicitMDP,
+    QuickMDP,
+    QuickPOMDP,
+    MissingQuickArgument,
+)
