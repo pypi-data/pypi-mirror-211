c760a24da88941445863e4c88d26d4dd634ee836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:08 +0000

    Add package rysk-client/0.1.1/rysk_client-0.1.1.tar.gz

---
+++ packages/rysk-client/rysk_client-0.1.1.tar.gz/rysk_client-0.1.1/rysk_client/src/utils.py
@@ -0,0 +1,30 @@
+"""
+Helper functions for the rysk_client package.
+"""
+import json
+import os
+from copy import deepcopy
+
+from web3 import Web3
+
+from rysk_client.src.constants import ADDRESSES, DEFAULT_ENCODING, RPC_URL
+# 
+
+
+def get_contract(name, web3):
+    """Returns a web3 contract instance for the given contract name"""
+    # we need to set the root of the path to be the installed rysk package
+    path = os.path.join(os.path.dirname(__file__), "..")
+    spec = ADDRESSES[name]
+    with open(f"{path}/{spec['path']}", "r", encoding=DEFAULT_ENCODING) as abi:
+        abi = json.loads(abi.read())["abi"]
+    res = deepcopy(spec)
+    del res["path"]
+    res["abi"] = abi
+    return web3.eth.contract(**res)
+
+
+def get_web3() -> Web3:
+    """Returns a web3 instance connected to RPC_URL"""
+    web3 = Web3(Web3.HTTPProvider(RPC_URL))
+    return web3
