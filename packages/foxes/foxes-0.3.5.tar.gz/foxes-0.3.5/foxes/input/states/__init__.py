68e8db0ae6af5f8edab85eb16b5b706b611c4c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:41 +0000

    Add package foxes/0.3.5/foxes-0.3.5.tar.gz

---
+++ packages/foxes/foxes-0.3.5.tar.gz/foxes-0.3.5/foxes/input/states/__init__.py
@@ -0,0 +1,10 @@
+"""
+Atmospheric input states.
+"""
+from .single import SingleStateStates
+from .scan_ws import ScanWS
+from .states_table import StatesTable, Timeseries
+from .field_data_nc import FieldDataNC
+from .multi_height import MultiHeightStates, MultiHeightTimeseries
+
+from . import create
---
+++ packages/foxes/foxes-0.3.5.tar.gz/foxes-0.3.5/foxes/utils/geom2d/__init__.py
@@ -0,0 +1,7 @@
+"""
+Geometries in two dimensions.
+"""
+from .area_geometry import AreaGeometry, InvertedAreaGeometry
+from .area_geometry import AreaUnion, AreaIntersection
+from .polygon import ClosedPolygon
+from .circle import Circle
