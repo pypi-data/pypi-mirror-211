1629fb4c10d176350b97ab1b717af575ee7bdc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:14 +0000

    Add package databento/0.13.0/databento-0.13.0-py2.py3-none-any.whl

---
+++ packages/databento/databento-0.13.0-py2.py3-none-any.whl/databento/historical/api/symbology.py
@@ -0,0 +1,82 @@
+from datetime import date
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from databento.common.enums import SType
+from databento.common.parsing import (
+    datetime_to_date_string,
+    optional_date_to_string,
+    optional_symbols_list_to_string,
+)
+from databento.common.validation import validate_enum, validate_semantic_string
+from databento.historical.api import API_VERSION
+from databento.historical.http import BentoHttpAPI
+from requests import Response
+
+
+class SymbologyHttpAPI(BentoHttpAPI):
+    """
+    Provides request methods for the symbology HTTP API endpoints.
+    """
+
+    def __init__(self, key: str, gateway: str) -> None:
+        super().__init__(key=key, gateway=gateway)
+        self._base_url = gateway + f"/v{API_VERSION}/symbology"
+
+    def resolve(
+        self,
+        dataset: str,
+        symbols: Union[List[str], str],
+        stype_in: Union[SType, str],
+        stype_out: Union[SType, str],
+        start_date: Union[date, str],
+        end_date: Optional[Union[date, str]] = None,
+        default_value: Optional[str] = "",
+    ) -> Dict[str, Any]:
+        """
+        Request symbology mappings resolution from Databento.
+
+        Makes a `GET /symbology.resolve` HTTP request.
+
+        Parameters
+        ----------
+        dataset : Dataset or str
+            The dataset code (string identifier) for the request.
+        symbols : List[Union[str, int]] or str, optional
+            The symbols to resolve. Takes up to 2,000 symbols per request.
+        stype_in : SType or str, default 'raw_symbol'
+            The input symbology type to resolve from.
+        stype_out : SType or str, default 'instrument_id'
+            The output symbology type to resolve to.
+        start_date : date or str
+            The start date (UTC) of the request time range (inclusive).
+        end_date : date or str, optional
+            The end date (UTC) of the request time range (exclusive).
+        default_value : str, default '' (empty string)
+            The default value to return if a symbol cannot be resolved.
+
+        Returns
+        -------
+        Dict[str, Any]
+            A result including a map of input symbol to output symbol across a
+            date range.
+
+        """
+        stype_in_valid = validate_enum(stype_in, SType, "stype_in")
+        symbols_list = optional_symbols_list_to_string(symbols, stype_in_valid)
+        params: List[Tuple[str, str]] = [
+            ("dataset", validate_semantic_string(dataset, "dataset")),
+            ("symbols", symbols_list),
+            ("stype_in", str(stype_in_valid)),
+            ("stype_out", str(validate_enum(stype_out, SType, "stype_out"))),
+            ("start_date", datetime_to_date_string(start_date)),
+            ("end_date", optional_date_to_string(end_date)),
+            ("default_value", default_value),
+        ]
+
+        response: Response = self._get(
+            url=self._base_url + ".resolve",
+            params=params,
+            basic_auth=True,
+        )
+
+        return response.json()
---
+++ packages/databento/databento-0.13.0-py2.py3-none-any.whl/databento/historical/http.py
@@ -0,0 +1,223 @@
+import sys
+from io import BufferedIOBase
+from json.decoder import JSONDecodeError
+from typing import Any, List, Optional, Tuple
+
+import aiohttp
+import requests
+from aiohttp import ClientResponse, ContentTypeError
+from databento.common.error import BentoClientError, BentoServerError
+from databento.version import __version__
+from requests import Response
+from requests.auth import HTTPBasicAuth
+
+
+_32KB = 1024 * 32  # 32_768
+
+
+class BentoHttpAPI:
+    """The base class for all Databento HTTP API endpoints."""
+
+    TIMEOUT = 100
+
+    def __init__(self, key: str, gateway: str):
+        python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
+        user_agent = f"Databento/{__version__} Python/{python_version}"
+
+        self._key = key
+        self._gateway = gateway
+        self._headers = {"accept": "application/json", "user-agent": user_agent}
+
+    def _check_api_key(self) -> None:
+        if self._key == "YOUR_API_KEY":
+            raise ValueError(
+                "The API key is currently set to 'YOUR_API_KEY'. "
+                "Please replace this value with a valid API key. "
+                "You can find these through your Databento user portal.",
+            )
+
+    def _get(
+        self,
+        url: str,
+        params: Optional[List[Tuple[str, Optional[str]]]] = None,
+        basic_auth: bool = False,
+    ) -> Response:
+        self._check_api_key()
+
+        with requests.get(
+            url=url,
+            params=params,
+            headers=self._headers,
+            auth=HTTPBasicAuth(username=self._key, password="") if basic_auth else None,
+            timeout=(self.TIMEOUT, self.TIMEOUT),
+        ) as response:
+            check_http_error(response)
+            return response
+
+    async def _get_json_async(
+        self,
+        url: str,
+        params: Optional[List[Tuple[str, Optional[str]]]] = None,
+        basic_auth: bool = False,
+    ) -> Any:
+        self._check_api_key()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                url=url,
+                params=params,
+                headers=self._headers,
+                auth=aiohttp.BasicAuth(login=self._key, password="", encoding="utf-8")
+                if basic_auth
+                else None,
+                timeout=self.TIMEOUT,
+            ) as response:
+                await check_http_error_async(response)
+                return await response.json()
+
+    def _post(
+        self,
+        url: str,
+        params: Optional[List[Tuple[str, Optional[str]]]] = None,
+        basic_auth: bool = False,
+    ) -> Response:
+        self._check_api_key()
+
+        with requests.post(
+            url=url,
+            params=params,
+            headers=self._headers,
+            auth=HTTPBasicAuth(username=self._key, password="") if basic_auth else None,
+            timeout=(self.TIMEOUT, self.TIMEOUT),
+        ) as response:
+            check_http_error(response)
+            return response
+
+    def _stream(
+        self,
+        url: str,
+        params: List[Tuple[str, Optional[str]]],
+        basic_auth: bool,
+        writer: BufferedIOBase,
+    ) -> None:
+        self._check_api_key()
+
+        with requests.get(
+            url=url,
+            params=params,
+            headers=self._headers,
+            auth=HTTPBasicAuth(username=self._key, password="") if basic_auth else None,
+            timeout=(self.TIMEOUT, self.TIMEOUT),
+            stream=True,
+        ) as response:
+            check_http_error(response)
+
+            for chunk in response.iter_content(chunk_size=_32KB):
+                writer.write(chunk)
+
+    async def _stream_async(
+        self,
+        url: str,
+        params: List[Tuple[str, Optional[str]]],
+        basic_auth: bool,
+        writer: BufferedIOBase,
+    ) -> None:
+        self._check_api_key()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                url=url,
+                params=[x for x in params if x[1] is not None],
+                headers=self._headers,
+                auth=aiohttp.BasicAuth(login=self._key, password="", encoding="utf-8")
+                if basic_auth
+                else None,
+                timeout=self.TIMEOUT,
+            ) as response:
+                await check_http_error_async(response)
+
+                async for chunk in response.content.iter_chunks():
+                    writer.write(chunk[0])
+
+
+def is_400_series_error(status: int) -> bool:
+    return status // 100 == 4
+
+
+def is_500_series_error(status: int) -> bool:
+    return status // 100 == 5
+
+
+def check_http_error(response: Response) -> None:
+    if is_500_series_error(response.status_code):
+        try:
+            json_body = response.json()
+            message = json_body.get("detail")
+        except JSONDecodeError:
+            json_body = None
+            message = None
+        if response.status_code == 504:
+            message = "The remote gateway timed out."
+        raise BentoServerError(
+            http_status=response.status_code,
+            http_body=response.content,
+            json_body=json_body,
+            message=message,
+            headers=response.headers,
+        )
+    elif is_400_series_error(response.status_code):
+        try:
+            json_body = response.json()
+            message = json_body.get("detail")
+        except JSONDecodeError:
+            json_body = None
+            message = None
+        if response.status_code == 408:
+            message = "The request transmission timed out."
+        raise BentoClientError(
+            http_status=response.status_code,
+            http_body=response.content,
+            json_body=json_body,
+            message=message,
+            headers=response.headers,
+        )
+
+
+async def check_http_error_async(response: ClientResponse) -> None:
+    if is_500_series_error(response.status):
+        try:
+            json_body = await response.json()
+            http_body = await response.read()
+            message = json_body.get("detail", "")
+        except ContentTypeError:
+            http_body = None
+            json_body = None
+            message = ""
+
+        if response.status == 504:
+            message = "The remote gateway timed out."
+        raise BentoServerError(
+            http_status=response.status,
+            http_body=http_body,
+            json_body=json_body,
+            message=message,
+            headers=response.headers,
+        )
+
+    if is_400_series_error(response.status):
+        try:
+            json_body = await response.json()
+            http_body = await response.read()
+            message = json_body.get("detail", "")
+        except ContentTypeError:
+            http_body = None
+            json_body = None
+            message = ""
+        if response.status == 408:
+            message = "The request transmission timed out."
+        raise BentoClientError(
+            http_status=response.status,
+            http_body=http_body,
+            json_body=json_body,
+            message=message,
+            headers=response.headers,
+        )
