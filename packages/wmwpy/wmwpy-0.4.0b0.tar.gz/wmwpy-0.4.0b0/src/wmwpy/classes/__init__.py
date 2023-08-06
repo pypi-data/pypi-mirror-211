ded70f5bc452a4086485d4cb6a52d72a075bdef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:06 +0000

    Add package wmwpy/0.4.0b0/wmwpy-0.4.0b0.tar.gz

---
+++ packages/wmwpy/wmwpy-0.4.0b0.tar.gz/wmwpy-0.4.0b0/src/wmwpy/classes/__init__.py
@@ -0,0 +1,8 @@
+from .object import Object
+from .level import Level
+from .layout import Layout
+from .sprite import Sprite
+from .imagelist import Imagelist
+from .widget import Widget
+
+__all__ = ['Level', 'Layout', 'Sprite', 'Object', 'Imagelist', 'Widget']
