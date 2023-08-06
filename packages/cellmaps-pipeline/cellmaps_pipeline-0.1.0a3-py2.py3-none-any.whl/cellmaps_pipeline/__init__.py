22c91d2eb0fa0f0cf016b09f68cbba2f1eb56038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:13 +0000

    Add package cellmaps-pipeline/0.1.0a3/cellmaps_pipeline-0.1.0a3-py2.py3-none-any.whl

---
+++ packages/cellmaps-pipeline/cellmaps_pipeline-0.1.0a3-py2.py3-none-any.whl/cellmaps_pipeline/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for CM4AI Pipeline."""
+
+__author__ = 'Christopher Churas'
+__email__ = 'cchuras@ucsd.edu'
+__version__ = '0.1.0a3'
+__repo_url__ = 'https://github.com/idekerlab/cellmaps_pipeline'
+__description__ = 'Runs full Cellmaps CM4AI pipeline'
