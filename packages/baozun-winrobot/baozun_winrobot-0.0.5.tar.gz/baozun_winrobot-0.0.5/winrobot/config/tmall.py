017c2840c6b23eede58ae41351fc2065415a6318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:25 +0000

    Add package baozun-winrobot/0.0.5/baozun_winrobot-0.0.5.tar.gz

---
+++ packages/baozun-winrobot/baozun_winrobot-0.0.5.tar.gz/baozun_winrobot-0.0.5/winrobot/config/tmall.py
@@ -0,0 +1,17 @@
+TMALL_DEFAULT_HEADERS = {
+  'authority': 'shell.mkt.taobao.com',
+  'accept': 'application/json',
+  'accept-language': 'zh-CN,zh;q=0.9',
+  'cache-control': 'no-cache',
+  'content-type': 'application/json; charset=utf-8',
+  'origin': 'https://qn.taobao.com',
+  'pragma': 'no-cache',
+  'referer': 'https://qn.taobao.com/',
+  'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+}
