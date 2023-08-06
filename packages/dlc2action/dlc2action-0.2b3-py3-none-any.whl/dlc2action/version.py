8ed8ace7461e3d986c15e1ed50ed5dddea874be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:18 +0000

    Add package dlc2action/0.2b3/dlc2action-0.2b3-py3-none-any.whl

---
+++ packages/dlc2action/dlc2action-0.2b3-py3-none-any.whl/dlc2action/version.py
@@ -0,0 +1,12 @@
+#
+# Copyright 2020-present by A. Mathis Group and contributors. All rights reserved.
+#
+# This project and all its files are licensed under GNU AGPLv3 or later version. A copy is included in dlc2action/LICENSE.AGPL.
+#
+"""
+DLC2Action Toolbox
+© A. Mathis Lab
+"""
+
+__version__ = "0.2b3"
+VERSION = __version__
