2b925aec4a1682d12e026e1156d384e94564548d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:27 +0000

    Add package zar/2.0.3/zar-2.0.3.tar.gz

---
+++ packages/zar/zar-2.0.3.tar.gz/zar-2.0.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='zar',
+    version='2.0.3',
+    description='Zeroincombenze Archive Replica',
+    long_description="""
+ZAR stand for Zeroincombenze® Archive Replica.
+It is a tool kit to backup, restore, replicate files and/or database.
+
+ZAR manages easily backup for Odoo database, keeps last nth copies and purges oldest copies.
+""",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: System Shells',
+    ],
+    keywords='backup, restore, replica',
+    project_urls={
+        'Documentation': 'https://zeroincombenze-tools.readthedocs.io',
+        'Source': 'https://github.com/zeroincombenze/tools',
+    },
+    author='Antonio Maria Vigliotti',
+    author_email='antoniomaria.vigliotti@gmail.com',
+    license='Affero GPL',
+    packages=['zar'],
+    zip_safe=False,
+)
