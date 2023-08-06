4c230af4bf5030bd91464e44b34461459c7e58a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:33 +0000

    Add package arxivai/0.0.33/arxivai-0.0.33.tar.gz

---
+++ packages/arxivai/arxivai-0.0.33.tar.gz/arxivai-0.0.33/arxivai/embedding.py
@@ -0,0 +1,46 @@
+import openai
+import tiktoken
+
+
+def token_length(text: str) -> int:
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(
+        text,
+        disallowed_special=()
+    )
+    return len(tokens)
+
+
+def split_text(text, chunk_size, overlap_size):
+    chunks = []
+    num_chunks = len(text)//(chunk_size-overlap_size)
+    for i in range(num_chunks):
+        start = i * (chunk_size - overlap_size)
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+
+    return chunks
+
+
+def chunks(context: dict, metadata: dict) -> list:
+    results = []
+    # results.append(context['toc'])
+    for page, text in enumerate(context['fulltext']):
+        chunks = split_text(text, 1024, 32)
+        for chunk in chunks:
+            # chunks_with_metadata = f"[PDF-PAGE]: {page};\t[PAPER-TITLE]: {metadata['title']};\t[PAPER-CONTENT]: {chunk};"
+            chunks_with_metadata = f"{chunk}\t"
+            results.append(chunks_with_metadata)
+
+    return results
+
+
+def encode(chunks: list) -> list:
+    response = openai.Embedding.create(
+        input=chunks,
+        model="text-embedding-ada-002",
+    )
+
+    return response['data']
+
