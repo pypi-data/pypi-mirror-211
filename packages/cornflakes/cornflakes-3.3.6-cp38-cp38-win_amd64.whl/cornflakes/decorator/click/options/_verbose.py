676c65ce70032c9605c642efd554a55b5f4d1bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:02 +0000

    Add package cornflakes/3.3.6/cornflakes-3.3.6-cp38-cp38-win_amd64.whl

---
+++ packages/cornflakes/cornflakes-3.3.6-cp38-cp38-win_amd64.whl/cornflakes/decorator/click/options/_verbose.py
@@ -0,0 +1,24 @@
+import logging
+
+from cornflakes.decorator.click.options._global import global_option
+from cornflakes.logging.logger import setup_logging
+
+
+@global_option(
+    ["-v", "--verbose"],
+    help="Base logging level is set to logging.DEBUG.",
+    is_flag=True,
+)
+def verbose_option(verbose):
+    """Default Option for verbose logging."""
+    setup_logging(default_level=logging.DEBUG if verbose else None)
+
+
+@global_option(
+    ["-vv", "--verbose-all"],
+    help="Base logging level is set to logging.DEBUG.",
+    is_flag=True,
+)
+def verbose_option_all(verbose):
+    """Default Option for verbose logging."""
+    setup_logging(default_level=logging.DEBUG if verbose else None, force=True)
