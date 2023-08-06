2c3c20d3490541bb3244d7870c68fd12ab86843e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:19 +0000

    Add package mpbn/3.2/mpbn-3.2.tar.gz

---
+++ packages/mpbn/mpbn-3.2.tar.gz/mpbn-3.2/mpbn/cli/__init__.py
@@ -0,0 +1,23 @@
+
+import mpbn
+
+import sys
+from argparse import ArgumentParser
+
+def main():
+    ap = ArgumentParser(prog=sys.argv[0])
+    ap.add_argument("bnet_file")
+    ap.add_argument("method", choices=["attractors", "fixedpoints", "bn2asp"])
+    ap.add_argument("--encoding", default="auto", choices=["auto", "bdd", "unate-dnf"])
+    ap.add_argument("--limit", type=int, default=0,
+                    help="limit the number of results")
+    args = ap.parse_args()
+    mbn = mpbn.MPBooleanNetwork(args.bnet_file, encoding=args.encoding)
+    if args.method == "attractors":
+        for attractor in mbn.attractors(limit=args.limit):
+            print(attractor)
+    elif args.method == "fixedpoints":
+        for attractor in mbn.fixedpoints(limit=args.limit):
+            print(attractor)
+    elif args.method == "bn2asp":
+        print(mbn.asp_of_bn())
