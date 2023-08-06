e477106dc24b9def77e6176e7186fd4c675bba5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:34 +0000

    Add package referencing/0.29.0/referencing-0.29.0.tar.gz

---
+++ packages/referencing/referencing-0.29.0.tar.gz/referencing-0.29.0/noxfile.py
@@ -0,0 +1,137 @@
+from pathlib import Path
+import os
+
+import nox
+
+ROOT = Path(__file__).parent
+PYPROJECT = ROOT / "pyproject.toml"
+DOCS = ROOT / "docs"
+REFERENCING = ROOT / "referencing"
+
+
+nox.options.sessions = []
+
+
+def session(default=True, **kwargs):
+    def _session(fn):
+        if default:
+            nox.options.sessions.append(kwargs.get("name", fn.__name__))
+        return nox.session(**kwargs)(fn)
+
+    return _session
+
+
+@session(python=["3.8", "3.9", "3.10", "3.11", "pypy3"])
+def tests(session):
+    session.install("-r", ROOT / "test-requirements.txt")
+
+    if session.posargs and session.posargs[0] == "coverage":
+        if len(session.posargs) > 1 and session.posargs[1] == "github":
+            github = os.environ["GITHUB_STEP_SUMMARY"]
+        else:
+            github = None
+
+        session.install("coverage[toml]")
+        session.run("coverage", "run", "-m", "pytest", REFERENCING)
+        if github is None:
+            session.run("coverage", "report")
+        else:
+            with open(github, "a") as summary:
+                summary.write("### Coverage\n\n")
+                summary.flush()  # without a flush, output seems out of order.
+                session.run(
+                    "coverage",
+                    "report",
+                    "--format=markdown",
+                    stdout=summary,
+                )
+    else:
+        session.run("pytest", *session.posargs, REFERENCING)
+
+
+@session()
+def audit(session):
+    session.install("pip-audit", ROOT)
+    session.run("python", "-m", "pip_audit")
+
+
+@session(tags=["build"])
+def build(session):
+    session.install("build")
+    tmpdir = session.create_tmp()
+    session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+
+
+@session(tags=["style"])
+def readme(session):
+    session.install("build", "twine")
+    tmpdir = session.create_tmp()
+    session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+    session.run("python", "-m", "twine", "check", "--strict", tmpdir + "/*")
+
+
+@session(tags=["style"])
+def style(session):
+    session.install("ruff")
+    session.run("ruff", "check", ROOT)
+
+
+@session()
+def typing(session):
+    session.install("pyright==1.1.307", ROOT)
+    session.run("pyright", REFERENCING)
+
+
+@session(tags=["docs"])
+@nox.parametrize(
+    "builder",
+    [
+        nox.param(name, id=name)
+        for name in [
+            "dirhtml",
+            "doctest",
+            "linkcheck",
+            "man",
+            "spelling",
+        ]
+    ],
+)
+def docs(session, builder):
+    session.install("-r", DOCS / "requirements.txt")
+    tmpdir = Path(session.create_tmp())
+    argv = ["-n", "-T", "-W"]
+    if builder != "spelling":
+        argv += ["-q"]
+    session.run(
+        "python",
+        "-m",
+        "sphinx",
+        "-b",
+        builder,
+        DOCS,
+        tmpdir / builder,
+        *argv,
+    )
+
+
+@session(tags=["docs", "style"], name="docs(style)")
+def docs_style(session):
+    session.install(
+        "doc8",
+        "pygments",
+        "pygments-github-lexers",
+    )
+    session.run("python", "-m", "doc8", "--config", PYPROJECT, DOCS)
+
+
+@session(default=False)
+def requirements(session):
+    session.install("pip-tools")
+    for each in [DOCS / "requirements.in", ROOT / "test-requirements.in"]:
+        session.run(
+            "pip-compile",
+            "--resolver",
+            "backtracking",
+            "-U",
+            each.relative_to(ROOT),
+        )
