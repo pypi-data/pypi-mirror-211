db4c0e2c300011eb1fa85ebb3bd1644e59f71388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:42 +0000

    Add package Mirolyubov-lab3-new-version/2.0.1/Mirolyubov_lab3_new_version-2.0.1.tar.gz

---
+++ packages/Mirolyubov-lab3-new-version/Mirolyubov_lab3_new_version-2.0.1.tar.gz/Mirolyubov_lab3_new_version-2.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Mirolyubov_lab3_new_version",
+    version="2.0.1",
+    description="Library for serialization JSON, XML",
+    author="Mirolyubov Ilya",
+    author_email="ilyamir1@mail.ru",
+    packages=find_packages(),
+    include_package_data=True,
+)
