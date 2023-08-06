b9fbf0423d5e1bf145e4fccb70175045dc7fe04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:50 +0000

    Add package customized-chart/0.1.21/customized_chart-0.1.21.tar.gz

---
+++ packages/customized-chart/customized_chart-0.1.21.tar.gz/customized_chart-0.1.21/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name="customized_chart",
+    version="0.1.21",
+    author="Johan Hagelbäck",
+    author_email="johan.hagelback@gmail.com",
+    description="Simplifies creation of matplotlib charts",
+    long_description="Simplifies creation of matplotlib charts.",
+    license="MIT",
+    packages=["customized_chart"],
+    url="https://github.com/jhagelback/customized_chart",
+    install_requires=["termcolor", "matplotlib", "numpy"]
+    )
