b33bad66e343fa06d0cfd854c4d7de3aca696d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:58 +0000

    Add package thipster/0.13.3/thipster-0.13.3-py3-none-any.whl

---
+++ packages/thipster/thipster-0.13.3-py3-none-any.whl/thipster/engine/I_Parser.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from thipster.engine.ParsedFile import ParsedFile
+
+
+class I_Parser(ABC):
+    """Parser module interface
+
+    Methods
+    -------
+    run()
+        Abstract run method
+
+    """
+    @abstractmethod
+    def run(self, path: str) -> ParsedFile:
+        """Abstract run method
+
+        Returns
+        -------
+        ParsedFile
+            Object containing a representation of an input file broken down into
+            individual resources
+
+        Raises
+        ------
+        NotImplementedError
+            If method is not implemented in inheriting classes
+
+        """
+        raise NotImplementedError('Should implement run()')
