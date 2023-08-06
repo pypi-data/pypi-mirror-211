c781bdfe08d763c0f1d7bfe19c8f1cbe1c3b734e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package ShunkoSerialization/0.1.1/ShunkoSerialization-0.1.1-py3-none-any.whl

---
+++ packages/ShunkoSerialization/ShunkoSerialization-0.1.1-py3-none-any.whl/Serialization/__init__.py
@@ -0,0 +1,11 @@
+from Serialization.type_constants import \
+                           nonetype, moduletype, codetype, celltype, \
+                           functype, bldinfunctype, smethodtype, cmethodtype, \
+                           mapproxytype, wrapdesctype, metdesctype, getsetdesctype, \
+                           CODE_PROPS, UNIQUE_TYPES
+
+from Serialization.base_serializer import BaseSerializer
+from Serialization.dict_serializer import DictSerializer
+from Serialization.json_serializer import JsonSerializer
+from Serialization.xml_serializer import XmlSerializer
+from Serialization.serializers_factory import SerializersFactory, SerializerType
