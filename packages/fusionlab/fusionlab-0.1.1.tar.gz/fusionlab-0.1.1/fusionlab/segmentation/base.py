5e089974e2b026074571c409debe223708c3a377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:14 +0000

    Add package fusionlab/0.1.1/fusionlab-0.1.1.tar.gz

---
+++ packages/fusionlab/fusionlab-0.1.1.tar.gz/fusionlab-0.1.1/fusionlab/segmentation/base.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class SegmentationModel(nn.Module):
+    """
+    Base PyTorch class of the segmentation model with Encoder, Bridger, Decoder, Head
+    """
+    def forward(self, x):
+        features = self.encoder(x)
+        feature_fusion = self.bridger(features)
+        decoder_output = self.decoder(feature_fusion)
+        output = self.head(decoder_output)
+        return output
