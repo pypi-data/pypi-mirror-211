5ed9d263388e8921864388a147a0463310e6d271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:49 +0000

    Add package selenium-firefox/2.0.8/selenium_firefox-2.0.8.tar.gz

---
+++ packages/selenium-firefox/selenium_firefox-2.0.8.tar.gz/selenium_firefox-2.0.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium_firefox",
+    version="2.0.8",
+    author="Kovács Kristóf-Attila",
+    description="selenium_firefox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/selenium_firefox",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'geckodriver-autoinstaller>=0.1.0',
+        'kproxy>=0.0.1',
+        'noraise>=0.0.16',
+        'selenium>=4.0.0',
+        'selenium-browser>=0.0.12',
+        'setuptools>=67.8.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
