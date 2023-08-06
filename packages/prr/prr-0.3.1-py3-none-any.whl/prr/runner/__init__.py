566f2e134b794113a8fec4776d6ec323769d040c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:09 +0000

    Add package prr/0.3.1/prr-0.3.1-py3-none-any.whl

---
+++ packages/prr/prr-0.3.1-py3-none-any.whl/prr/runner/__init__.py
@@ -0,0 +1,40 @@
+from prr.runner.prompt_run import PromptRun
+from prr.runner.saver import PromptRunSaver
+from prr.services.service_registry import ServiceRegistry
+
+service_registry = ServiceRegistry()
+service_registry.register_all_services()
+
+
+# high-level class to run prompts based on configuration
+class Runner:
+    def __init__(self, prompt_config):
+        self.prompt_config = prompt_config
+        self.saver = PromptRunSaver(self.prompt_config)
+
+    def run_service(self, service_name, service_options_overrides, save_run=False):
+        service_config = self.prompt_config.service_with_name(service_name)
+
+        service_config.process_option_overrides(service_options_overrides)
+
+        service = service_registry.service_for_service_config(service_config)
+
+        result = PromptRun(self.prompt_config, service, service_config).run()
+
+        if save_run:
+            run_save_directory = self.saver.save(service_name, result)
+        else:
+            run_save_directory = None
+
+        return result, run_save_directory
+
+    # runs all models defined for specified prompt
+    def run_all_configured_services(self, service_options_overrides, save_run=False):
+        results = {}
+
+        for service_name in self.configured_services():
+            results[service_name] = self.run_service(
+                service_name, service_options_overrides, save_run
+            )
+
+        return results
---
+++ packages/prr/prr-0.3.1-py3-none-any.whl/prr/runner/request.py
@@ -0,0 +1,27 @@
+import yaml
+
+
+# request we're sending to the service
+class ServiceRequest:
+    def __init__(self, service_config, rendered_prompt_content):
+        self.service_config = service_config
+        self.prompt_content = rendered_prompt_content
+
+    def to_dict(self):
+        return {
+            "model": self.service_config.model,
+            "options": self.service_config.options.to_dict(),
+        }
+
+    def prompt_text(self, max_len=0):
+        if isinstance(self.prompt_content, str):
+            text = self.prompt_content
+        else:
+            text = yaml.dump(self.prompt_content)
+
+        if max_len > 0 and len(text) > max_len:
+            text = text[0:max_len] + "..."
+
+            return text.replace("\n", " ").replace("  ", " ")
+
+        return text
