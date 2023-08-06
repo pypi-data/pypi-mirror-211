ca4e1f8df19d9e59131f3ee23a3302735a8df134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:33 +0000

    Add package umls-rat/0.6.5/umls_rat-0.6.5-py3-none-any.whl

---
+++ packages/umls-rat/umls_rat-0.6.5-py3-none-any.whl/umlsrat/const.py
@@ -0,0 +1,10 @@
+DEFAULT_UMLS_VERSION = "2023AA"
+
+API_KEY_ENV_VAR = "UMLS_API_KEY"
+
+# Common vocabulary name mapping
+_COMMON_VOCAB_NAMES = {
+    "SNOMED": "SNOMEDCT_US",
+    "SNOMED-CT": "SNOMEDCT_US",
+    "LOINC": "LNC",
+}
