71f7009777d9b0f164ff519cd2a1ea332b70c9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:45 +0000

    Add package glum/2.5.2/glum-2.5.2-cp39-cp39-win_amd64.whl

---
+++ packages/glum/glum-2.5.2-cp39-cp39-win_amd64.whl/glum/__init__.py
@@ -0,0 +1,42 @@
+import pkg_resources
+
+from ._distribution import (
+    BinomialDistribution,
+    ExponentialDispersionModel,
+    GammaDistribution,
+    GeneralizedHyperbolicSecant,
+    InverseGaussianDistribution,
+    NegativeBinomialDistribution,
+    NormalDistribution,
+    PoissonDistribution,
+    TweedieDistribution,
+)
+from ._glm import GeneralizedLinearRegressor, get_family, get_link
+from ._glm_cv import GeneralizedLinearRegressorCV
+from ._link import IdentityLink, Link, LogitLink, LogLink, TweedieLink
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:
+    __version__ = "unknown"
+
+__all__ = [
+    "BinomialDistribution",
+    "ExponentialDispersionModel",
+    "GammaDistribution",
+    "GeneralizedHyperbolicSecant",
+    "InverseGaussianDistribution",
+    "NormalDistribution",
+    "PoissonDistribution",
+    "TweedieDistribution",
+    "NegativeBinomialDistribution",
+    "IdentityLink",
+    "Link",
+    "LogitLink",
+    "LogLink",
+    "TweedieLink",
+    "GeneralizedLinearRegressor",
+    "GeneralizedLinearRegressorCV",
+    "get_family",
+    "get_link",
+]
