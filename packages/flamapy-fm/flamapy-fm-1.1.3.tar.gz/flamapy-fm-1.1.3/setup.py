e022e7fa54d57aa6c0cd9861054e5266f56083b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:43 +0000

    Add package flamapy-fm/1.1.3/flamapy-fm-1.1.3.tar.gz

---
+++ packages/flamapy-fm/flamapy-fm-1.1.3.tar.gz/flamapy-fm-1.1.3/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="flamapy-fm",
+    version="1.1.3",
+    author="Flamapy",
+    author_email="flamapy@us.es",
+    description="flamapy-fm is a plugin to Flamapy module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flamapy/fm_metamodel",
+    packages=setuptools.find_namespace_packages(include=['flamapy.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'flamapy~=1.1.3',
+        'uvlparser~=1.0.2',
+        'afmparser~=1.0.0',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-mock',
+            'prospector',
+            'mypy',
+            'coverage',
+        ]
+    },
+    dependency_links=[
+        'flamapy~=1.1.3'
+    ],
+)
