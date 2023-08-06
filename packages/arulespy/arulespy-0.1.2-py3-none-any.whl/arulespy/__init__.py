8afadd406dfeee36ea71292d0b355aef277589ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:30 +0000

    Add package arulespy/0.1.2/arulespy-0.1.2-py3-none-any.whl

---
+++ packages/arulespy/arulespy-0.1.2-py3-none-any.whl/arulespy/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '0.1.2'
+
+import rpy2.robjects as ro
+import rpy2.robjects.packages as packages
+import os
+
+os.makedirs(ro.r('Sys.getenv("R_LIBS_USER")')[0], exist_ok=True)
+ro.r('.libPaths(c(Sys.getenv("R_LIBS_USER"), .libPaths()))')
+
+utils = packages.importr('utils')
+if not ro.packages.isinstalled('arules'):
+    print("Installing R package arules.")
+    utils.install_packages('arules', 
+                           repos='https://cloud.r-project.org/',
+                           lib = ro.r('Sys.getenv("R_LIBS_USER")[1]'))
+    
+
+if not ro.packages.isinstalled('arulesViz'):
+    print("Installing R package arulesViz (plus dependencies).")
+    utils.install_packages('arulesViz', 
+                           repos='https://cloud.r-project.org/',
+                           lib = ro.r('Sys.getenv("R_LIBS_USER")[1]'))
+
+              
+from .arules import parameters, Associations, ItemMatrix, Rules, Itemsets, Transactions, R, a2py, concat, apriori, eclat, discretizeDF
+from .arulesViz import plot, inspectDT
