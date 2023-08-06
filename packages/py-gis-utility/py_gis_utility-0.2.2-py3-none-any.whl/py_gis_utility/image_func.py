7befcea26994554fa2b44cf36a2c5905f0c1f391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:56 +0000

    Add package py-gis-utility/0.2.2/py_gis_utility-0.2.2-py3-none-any.whl

---
+++ packages/py-gis-utility/py_gis_utility-0.2.2-py3-none-any.whl/py_gis_utility/image_func.py
@@ -0,0 +1,169 @@
+import affine
+import cv2
+import numpy as np
+from typing import Dict, Union
+from osgeo import gdal
+from geopandas import GeoDataFrame
+from rasterio.io import BufferedDatasetWriter, DatasetWriter
+
+from py_gis_utility.helper import (
+    read_image_with_geo_transform,
+    extract_geometry_from_data_frame_row,
+    read_data_frame,
+    create_mesh,
+)
+from py_gis_utility.ops.image_ops import (
+    image_to_collection_generator,
+    copy_geo_reference_to_image,
+    create_bitmap,
+    save_multi_band,
+)
+
+
+def image_obj_to_coordinates_generator(
+    image: Union[BufferedDatasetWriter, DatasetWriter], **kwargs
+) -> Dict:
+    """
+
+    :param image:
+    :param kwargs:
+    :return:
+    """
+    return image_to_collection_generator(
+        image.read(), image.transform, is_shape=False, crs=image.crs, **kwargs
+    )
+
+
+def image_path_to_coordinates_generator(image_path: str, **kwargs) -> Dict:
+    """
+
+    :param image_path:
+    :return:
+    """
+    return image_obj_to_coordinates_generator(
+        read_image_with_geo_transform(image_path), **kwargs
+    )
+
+
+def image_obj_to_shape_generator(
+    image: Union[BufferedDatasetWriter, DatasetWriter], **kwargs
+) -> Dict:
+    """
+
+    :param image:
+    :return:
+    """
+    return image_to_collection_generator(
+        image.read(), image.transform, is_shape=True, crs=image.crs, **kwargs
+    )
+
+
+def image_path_to_shape_generator(image_path: str, **kwargs) -> Dict:
+    """
+
+    :param image_path:
+    :return:
+    """
+    return image_obj_to_shape_generator(
+        read_image_with_geo_transform(image_path), **kwargs
+    )
+
+
+def copy_geo_reference(
+    copy_from_path: str, copy_to_path: str, save_to: str, is_binary: bool = True
+):
+    """
+
+    :param is_binary:
+    :param save_to:
+    :param copy_from_path:
+    :param copy_to_path:
+    :return:
+    """
+    destination_img = (
+        cv2.imread(copy_to_path, cv2.IMREAD_GRAYSCALE)
+        if is_binary
+        else cv2.cvtColor(cv2.imread(copy_to_path), cv2.COLOR_BGR2RGB)
+    )
+    copy_geo_reference_to_image(
+        read_image_with_geo_transform(copy_from_path),
+        destination_img,
+        save_to,
+    )
+
+
+def shape_geometry_to_bitmap_from_data_frame_generator(
+    data_frame: GeoDataFrame,
+    output_image_size: tuple,
+    pixel_resolution: tuple,
+    allow_output_to_overlap: bool = True,
+):
+    """
+
+    :param data_frame:
+    :param output_image_size:
+    :param pixel_resolution:
+    :param allow_output_to_overlap:
+    :return:
+    """
+    bitmap_generator = create_bitmap(
+        create_mesh(
+            data_frame.total_bounds,
+            output_image_size,
+            pixel_resolution,
+            is_overlap=allow_output_to_overlap,
+        ),
+        list(extract_geometry_from_data_frame_row(data_frame)),
+    )
+
+    return bitmap_generator
+
+
+def shape_geometry_to_bitmap_from_shape_geometry_path_generator(
+    shape_geometry_file_path: str,
+    output_image_size: tuple,
+    pixel_resolution: tuple,
+    allow_output_to_overlap: bool = True,
+):
+    """
+
+    :param shape_geometry_file_path:
+    :param output_image_size:
+    :param pixel_resolution:
+    :param allow_output_to_overlap:
+    :return:
+    """
+    return shape_geometry_to_bitmap_from_data_frame_generator(
+        read_data_frame(shape_geometry_file_path),
+        output_image_size,
+        pixel_resolution,
+        allow_output_to_overlap,
+    )
+
+
+def save_8bit_multi_band(
+    image: np.ndarray, geo_transform: affine.Affine, epsg: int, output_file_name: str
+):
+    """
+
+    :param image: image must be of the format h X w X bands
+    :param geo_transform:
+    :param epsg:
+    :param output_file_name:
+    :return:
+    """
+    save_multi_band(image, geo_transform, gdal.GDT_Byte, epsg, output_file_name)
+
+
+def save_16bit_multi_band(
+    image: np.ndarray, geo_transform: affine.Affine, epsg: int, output_file_name: str
+):
+    """
+
+    :param image: image must be of the format h X w X bands
+    :param geo_transform:
+    :param epsg:
+    :param output_file_name:
+    :return:
+    """
+    save_multi_band(image, geo_transform, gdal.GDT_UInt16, epsg, output_file_name)
---
+++ packages/py-gis-utility/py_gis_utility-0.2.2-py3-none-any.whl/py_gis_utility/ops/image_ops.py
@@ -0,0 +1,162 @@
+from dataclasses import dataclass
+from typing import Dict, Union
+
+import affine
+from osgeo import gdal
+from osgeo import osr
+import rasterio
+import numpy as np
+
+from rasterio.features import shapes
+from rasterio.io import BufferedDatasetWriter, DatasetWriter
+from shapely.geometry import shape
+from stitch_n_split.geo_info import get_affine_transform, get_pixel_resolution
+from stitch_n_split.split.mesh import ImageNonOverLapMesh, ImageOverLapMesh
+
+
+@dataclass
+class Bitmap:
+    array: np.ndarray
+    transform: affine.Affine
+
+
+def create_bitmap(
+    mesh: Union[ImageNonOverLapMesh, ImageOverLapMesh], geometry_collection: list
+):
+    """
+
+    :param mesh:
+    :param geometry_collection:
+    :return:
+    """
+    intermediate_bitmap = np.empty((0,))
+    intermediate_bitmap = np.concatenate(
+        (intermediate_bitmap, geometry_collection), axis=0
+    )
+
+    for grid in mesh.extent():
+        transform = get_affine_transform(
+            grid["extent"][0],
+            grid["extent"][-1],
+            *get_pixel_resolution(mesh.mesh_transform),
+        )
+        bitmap_array = rasterio.features.rasterize(
+            ((g, 255) for g in intermediate_bitmap),
+            out_shape=mesh.grid_size,
+            transform=transform,
+        )
+        yield Bitmap(bitmap_array, transform)
+
+
+def image_to_collection_generator(
+    image: np.ndarray, transform: affine.Affine, is_shape=False, **kwargs
+) -> Dict:
+    """
+
+    :param is_shape:
+    :param transform:
+    :param image:
+    :return:
+    """
+    for i, (s, v) in enumerate(
+        shapes(
+            image.astype(rasterio.uint8),
+            mask=(image > 0),
+            connectivity=8,
+            transform=transform,
+        )
+    ):
+        yield {
+            "geometry": shape(s) if is_shape else s,
+            "properties": {"id": v, **kwargs},
+        }
+
+
+def get_geo_referenced_image_obj(
+    copy_from: Union[BufferedDatasetWriter, DatasetWriter], copy_to: np.ndarray
+):
+    """
+
+    :param copy_from:
+    :param copy_to:
+    :return:
+    """
+    bands = copy_to.ndim if copy_to.ndim > 2 else 1
+    geo_referenced_image = rasterio.open(
+        "",
+        mode="w",
+        driver=copy_from.driver,
+        width=copy_from.width,
+        height=copy_from.height,
+        crs=copy_from.crs,
+        transform=copy_from.transform,
+        dtype=copy_to.dtype,
+        count=bands,
+    )
+    return geo_referenced_image
+
+
+def copy_geo_reference_to_image(
+    copy_from: Union[BufferedDatasetWriter, DatasetWriter],
+    copy_to: np.ndarray,
+    save_to: str,
+):
+    """
+
+    :param copy_from:
+    :param copy_to:
+    :param save_to:
+    :return:
+    """
+    bands = copy_to.ndim if copy_to.ndim > 2 else 1
+    geo_referenced_image = rasterio.open(
+        save_to,
+        mode="w",
+        driver=copy_from.driver,
+        width=copy_from.width,
+        height=copy_from.height,
+        crs=copy_from.crs,
+        transform=copy_from.transform,
+        dtype=copy_to.dtype,
+        count=bands,
+    )
+    if bands > 2:
+        for band in range(copy_to.shape[2]):
+            geo_referenced_image.write(copy_to[:, :, band], band + 1)
+    else:
+        geo_referenced_image.write(copy_to, 1)
+
+    copy_from.close()
+    geo_referenced_image.close()
+
+
+def save_multi_band(
+    image: np.ndarray,
+    geo_transform: affine.Affine,
+    gdal_unit,
+    epsg: int,
+    output_file_name: str,
+):
+    """
+
+    :param image: image must be of the format h X w X bands
+    :param geo_transform:
+    :param gdal_unit:
+    :param epsg:
+    :param output_file_name:
+    :return:
+    """
+    assert image.ndim == 3, f"Expected to have 3 dim got {image.ndim}"
+
+    x, y, z = image.shape
+    dst_ds = gdal.GetDriverByName("GTiff").Create(output_file_name, y, x, z, gdal_unit)
+
+    dst_ds.SetGeoTransform(geo_transform)
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(epsg)
+    dst_ds.SetProjection(srs.ExportToWkt())
+
+    for idx in range(0, z):
+        dst_ds.GetRasterBand(idx + 1).WriteArray(image[:, :, idx])
+
+    dst_ds.FlushCache()
