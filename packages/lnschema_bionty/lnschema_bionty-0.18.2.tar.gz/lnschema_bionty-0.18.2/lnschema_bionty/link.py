f1b57100c0375a852ac1cbf52ffe7c5e643dbe16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:26:53 +0000

    Add package lnschema_bionty/0.18.2/lnschema_bionty-0.18.2.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.18.2.tar.gz/lnschema_bionty-0.18.2/lnschema_bionty/link.py
@@ -0,0 +1,19 @@
+"""Link tables.
+
+.. autosummary::
+   :toctree: .
+
+   FeaturesGene
+   FeaturesProtein
+   FeaturesCellMarker
+   FileReadout
+   PathwayGene
+"""
+
+from ._link import (  # noqa
+    FeaturesCellMarker,
+    FeaturesGene,
+    FeaturesProtein,
+    FileReadout,
+    PathwayGene,
+)
