f319c34f1eb755099f7b845bb628f343191171f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:40 +0000

    Add package funnylog/1.0.1/funnylog-1.0.1-py3-none-any.whl

---
+++ packages/funnylog/funnylog-1.0.1-py3-none-any.whl/funnylog/conf.py
@@ -0,0 +1,18 @@
+import os
+from getpass import getuser
+from platform import machine
+
+
+class _Setting:
+
+    SYS_ARCH = machine()
+    USERNAME = getuser()
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    # 日志文件生成的路径
+    LOG_PATH = os.path.join(ROOT_DIR, "logs")
+    # 本机IP
+    HOST_IP = str(os.popen("hostname -I |awk '{print $1}'").read()).strip("\n").strip()
+
+    LOG_LEVEL = "DEBUG"
+
+setting = _Setting()
