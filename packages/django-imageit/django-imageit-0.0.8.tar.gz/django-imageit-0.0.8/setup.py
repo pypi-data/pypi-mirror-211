dd5573d150ab03cbe36a6b3a7c1f28821ac49a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:07 +0000

    Add package django-imageit/0.0.8/django-imageit-0.0.8.tar.gz

---
+++ packages/django-imageit/django-imageit-0.0.8.tar.gz/django-imageit-0.0.8/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("version.txt", "r") as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='django-imageit',
+    version=version,
+    url="https://github.com/byite/django-imageit",
+    author="Scott James",
+    author_email="scottjames@byitegroup.com",
+    description="Image upload & processing plugin built for Django",
+    packages=['imageit'],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+
+    install_requires = [
+        "django >= 3",
+        "pillow > 8.0.0",
+        "filetype > 1.0.0",
+    ],
+    extras_require = {
+        "dev": [
+            "twine >= 3.4",
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: JavaScript',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+    ],
+    keywords='django images imageit image upload scale crop resize',
+)
