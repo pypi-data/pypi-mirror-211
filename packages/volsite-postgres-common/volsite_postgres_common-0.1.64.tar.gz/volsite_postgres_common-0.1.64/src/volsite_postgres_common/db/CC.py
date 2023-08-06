e02261991cf8ad3d5252aa6b7bf9bf541af35502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:43 +0000

    Add package volsite-postgres-common/0.1.64/volsite_postgres_common-0.1.64.tar.gz

---
+++ packages/volsite-postgres-common/volsite_postgres_common-0.1.64.tar.gz/volsite_postgres_common-0.1.64/src/volsite_postgres_common/db/CC.py
@@ -0,0 +1,32 @@
+from typing import Final
+
+
+# ====== CC: Common Table Column ======
+class CC:
+    built_time: Final[str] = 'built_time'
+
+    count_synchronize_version: Final[str] = 'count_synchronize_version'
+
+    description_id: Final[str] = 'description_id'
+
+    event: Final[str] = 'event'
+
+    formatted_description: Final[str] = 'formatted_description'
+
+    info: Final = 'info'
+
+    last_used_time: Final[str] = 'last_used_time'
+    length: Final[str] = 'length'
+
+    note: Final[str] = 'note'
+
+    plain_description: Final[str] = 'plain_description'
+
+    series: Final[str] = 'series'
+
+    synchronize_version: Final[str] = 'synchronize_version'
+
+    text: Final[str] = 'text'
+
+    update_time: Final[str] = 'update_time'
+    used_count: Final[str] = 'used_count'
