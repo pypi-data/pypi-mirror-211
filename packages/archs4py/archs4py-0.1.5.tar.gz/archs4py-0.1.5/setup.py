8136e581d094509cd8a1d1a7e47152c0fc4c4af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:47 +0000

    Add package archs4py/0.1.5/archs4py-0.1.5.tar.gz

---
+++ packages/archs4py/archs4py-0.1.5.tar.gz/archs4py-0.1.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="archs4py",
+    version="0.1.5",
+    author="Alexander Lachmann",
+    author_email="alexander.lachmann@mssm.edu",
+    description="ARCHS4 python package supporting data loading and data queries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maayanlab/archs4py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "archs4py": ["data/*"]
+    },
+    include_package_data=True,
+    install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
+    python_requires='>=3.7',
+)
