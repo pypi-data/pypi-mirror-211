4517ec123ef1a6ea8575f9c297dcac19cee74602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:42 +0000

    Add package dbxs/0.0.1/dbxs-0.0.1.tar.gz

---
+++ packages/dbxs/dbxs-0.0.1.tar.gz/dbxs-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dbxs',
+    version='0.0.1',
+    description="Ignore this, just testing.",
+    long_description='',
+    author='me',
+    author_email='code@glyph.im',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
