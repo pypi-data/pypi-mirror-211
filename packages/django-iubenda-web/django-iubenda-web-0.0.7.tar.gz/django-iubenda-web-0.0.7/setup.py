026a812d726bf06c6e8fca0b19aa4dfcc99c9dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:35 +0000

    Add package django-iubenda-web/0.0.7/django-iubenda-web-0.0.7.tar.gz

---
+++ packages/django-iubenda-web/django-iubenda-web-0.0.7.tar.gz/django-iubenda-web-0.0.7/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name="django-iubenda-web",
+    version="0.0.7",
+    license="Iubenda integration for web application",
+    description="Iubenda integration for web application",
+    author="ShadMod",
+    author_email="support@shadmod.it",
+    url="https://github.com/shadMod/django_iubenda",
+    download_url="https://github.com/shadMod/django_iubenda/archive/refs/tags/0.0.7.tar.gz",
+    packages=[
+        "django_iubenda",
+        "django_iubenda.core",
+        "django_iubenda.templates",
+        "django_iubenda.tests",
+    ],
+    keywords=[
+        "Django iubenda",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
