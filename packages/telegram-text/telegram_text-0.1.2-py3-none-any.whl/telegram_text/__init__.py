3c2796434040bd43b8f140fbf80409d8dda8f437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:02 +0000

    Add package telegram-text/0.1.2/telegram_text-0.1.2-py3-none-any.whl

---
+++ packages/telegram-text/telegram_text-0.1.2-py3-none-any.whl/telegram_text/__init__.py
@@ -0,0 +1,30 @@
+from .bases import Chain, PlainText, Text
+from .custom import TOMLSection
+from .elements import Hashtag, InlineUser, Link, User
+from .markdown import OrderedList, UnorderedList
+from .styles import Bold, Code, InlineCode, Italic, Spoiler, Strikethrough, Underline
+
+__version__ = '0.1.2'
+__all__ = [
+    "Chain",
+    "PlainText",
+    "Text",
+
+    "Bold",
+    "Code",
+    "InlineCode",
+    "Italic",
+    "Spoiler",
+    "Strikethrough",
+    "Underline",
+
+    "Hashtag",
+    "InlineUser",
+    "Link",
+    "User",
+
+    "OrderedList",
+    "UnorderedList",
+
+    "TOMLSection",
+]
