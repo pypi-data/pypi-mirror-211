0992e9671491e528710d3c1a6964d262fcdf7475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package nonebot-plugin-alconna/0.7.0/nonebot_plugin_alconna-0.7.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.7.0-py3-none-any.whl/nonebot_plugin_alconna/typings.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from typing import Any, Awaitable, Callable, Generic, Literal, TypeVar, Union
+from typing_extensions import ParamSpec, TypeAlias
+
+from nepattern import BasePattern, MatchFailed, PatternModel
+from nonebot.internal.adapter.message import Message, MessageSegment
+from tarina import lang
+
+TMS = TypeVar("TMS", bound=MessageSegment)
+P = ParamSpec("P")
+
+
+class SegmentPattern(BasePattern[TMS], Generic[TMS, P]):
+    def __init__(
+        self,
+        name: str,
+        origin: type[TMS],
+        call: Callable[P, TMS],
+        additional: Callable[..., bool] | None = None,
+    ):
+        super().__init__(
+            name,
+            PatternModel.TYPE_CONVERT,
+            origin,
+            alias=name,
+            accepts=[MessageSegment],
+            validators=[additional] if additional else [],
+        )
+        self.call = call
+
+    def match(self, input_: str | Any) -> TMS:
+        if not isinstance(input_, self.origin):
+            raise MatchFailed(
+                lang.require("nepattern", "type_error").format(target=type(input_))
+            )
+        if input_.type != self.pattern:
+            raise MatchFailed(
+                lang.require("nepattern", "content_error").format(target=input_)
+            )
+        return input_
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> TMS:
+        return self.call(*args, **kwargs)  # type: ignore
+
+
+OutputType = Literal["help", "shortcut", "completion"]
+TConvert: TypeAlias = Callable[[OutputType, str], Union[Message, Awaitable[Message]]]
+
+
+def _isinstance(seg: MessageSegment, accepts: set[str]):
+    return seg if seg.type.lower() in accepts else None
+
+
+def gen_unit(
+    name: str, accepts: set[str], additional: Callable[..., bool] | None = None
+) -> BasePattern[MessageSegment]:
+    return BasePattern(
+        name,
+        PatternModel.TYPE_CONVERT,
+        Any,
+        lambda self, x: _isinstance(x, accepts),
+        accepts=[MessageSegment],
+        validators=[additional] if additional else [],
+    )
