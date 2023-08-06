698b8b2714094039a763d7a65b6b05c27e3b2cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:30 +0000

    Add package airunner/1.15.2/airunner-1.15.2.tar.gz

---
+++ packages/airunner/airunner-1.15.2.tar.gz/airunner-1.15.2/src/airunner/canvas.py
@@ -0,0 +1,360 @@
+from PIL import Image
+from PyQt6.QtCore import Qt, QPoint, QRect
+from PyQt6.QtGui import QColor, QPainter, QPen, QBrush, QCursor
+from airunner.cursors.circle_brush import CircleCursor
+from airunner.mixins.canvas_active_grid_area_mixin import CanvasActiveGridAreaMixin
+from airunner.mixins.canvas_brushes_mixin import CanvasBrushesMixin
+from airunner.mixins.canvas_grid_mixin import CanvasGridMixin
+from airunner.mixins.canvas_image_mixin import CanvasImageMixin
+from airunner.mixins.canvas_layer_mixin import CanvasLayerMixin
+from airunner.mixins.canvas_selectionbox_mixin import CanvasSelectionboxMixin
+from airunner.mixins.canvas_widgets_mixin import CanvasWidgetsMixin
+from airunner.models.linedata import LineData
+
+
+class Canvas(
+    CanvasBrushesMixin,
+    CanvasGridMixin,
+    CanvasWidgetsMixin,
+    CanvasImageMixin,
+    CanvasSelectionboxMixin,
+    CanvasActiveGridAreaMixin,
+    CanvasLayerMixin,
+):
+    saving = False
+    select_start = None
+    select_end = None
+    shift_is_pressed = False
+    left_mouse_button_down = False
+    right_mouse_button_down = False
+    brush_start = None
+    last_mouse_pos = None
+
+    @property
+    def current_layer(self):
+        if len(self.layers) == 0:
+            return None
+        return self.layers[self.current_layer_index]
+
+    @property
+    def select_selected(self):
+        return self.settings_manager.settings.current_tool.get() == "select"
+
+    @property
+    def eraser_selected(self):
+        return self.settings_manager.settings.current_tool.get() == "eraser"
+
+    @property
+    def brush_selected(self):
+        return self.settings_manager.settings.current_tool.get() == "brush"
+
+    @property
+    def move_selected(self):
+        return self.settings_manager.settings.current_tool.get() == "move"
+
+    @property
+    def is_dragging(self):
+        return False
+
+    @property
+    def is_zooming(self):
+        return False
+
+    @property
+    def mouse_pos(self):
+        return self.canvas_container.mapFromGlobal(QCursor.pos())
+
+    @property
+    def brush_size(self):
+        return self.settings_manager.settings.mask_brush_size.get()
+
+    @property
+    def canvas_container(self):
+        return self.parent.window.canvas_container
+
+    @property
+    def settings_manager(self):
+        return self.parent.settings_manager
+
+    @property
+    def mouse_position(self):
+        return self.canvas_container.mapFromGlobal(QCursor.pos())
+
+    def __init__(
+        self,
+        parent=None
+    ):
+        self.canvas_rect = QRect(0, 0, 0, 0)
+        self.pos_x = 0
+        self.pos_y = 0
+        self.current_layer_index = 0
+        self.is_erasing = False
+        self.start_drawing_line_index = 0
+        self.stop_drawing_line_index = 0
+        self.parent = parent
+        self.image_pivot_point = QPoint(0, 0)
+        self.image_root_point = QPoint(0, 0)
+
+        CanvasGridMixin.initialize(self)
+        CanvasActiveGridAreaMixin.initialize(self)
+        CanvasLayerMixin.initialize(self)
+
+        # Set initial position and size of the canvas
+        self.canvas_container.setGeometry(QRect(
+            int(self.canvas_rect.x()),
+            int(self.canvas_rect.y()),
+            int(self.canvas_rect.width()),
+            int(self.canvas_rect.height())
+        ))
+
+        # set self.parent paintEvent
+        self.canvas_container.paintEvent = self.paintEvent
+        self.canvas_container.mousePressEvent = self.mouse_press_event
+        self.canvas_container.mouseMoveEvent = self.mouse_move_event
+        self.canvas_container.mouseReleaseEvent = self.mouse_release_event
+
+        # on shift down
+        self.parent.window.keyPressEvent = self.key_press_event
+
+        # on key up
+        self.parent.window.keyReleaseEvent = self.key_release_event
+
+        # on mouse hover
+        self.canvas_container.enterEvent = self.enter_event
+        self.canvas_container.leaveEvent = self.leave_event
+
+        # Set the default brush color for drawing
+        self.brush = QBrush()
+        self.brush.setStyle(Qt.BrushStyle.SolidPattern)
+
+        # Set the initial position for mouse dragging
+        self.drag_pos = QPoint(0, 0)
+
+        self.set_canvas_color()
+
+    def key_press_event(self, event):
+        if event.key() == Qt.Key.Key_Shift:
+            self.shift_is_pressed = True
+
+    def key_release_event(self, event):
+        if event.key() == Qt.Key.Key_Shift:
+            self.shift_is_pressed = False
+
+    def set_canvas_color(self):
+        self.canvas_container.setStyleSheet(f"background-color: {self.settings_manager.settings.canvas_color.get()};")
+        self.canvas_container.setAutoFillBackground(True)
+
+    def paintEvent(self, event):
+        CanvasGridMixin.paintEvent(self, event)
+        layers = self.layers.copy()
+        layers.reverse()
+        for index in range(len(layers)):
+            layer = layers[index]
+            if not layer.visible:
+                continue
+            CanvasImageMixin.draw(self, layer, index)
+            CanvasBrushesMixin.draw(self, layer, index)
+            CanvasWidgetsMixin.draw(self, layer, index)
+        CanvasSelectionboxMixin.paint_event(self, event)
+        CanvasActiveGridAreaMixin.paint_event(self, event)
+
+    def enter_event(self, event):
+        self.update_cursor()
+
+    def update_cursor(self):
+        if self.brush_selected or self.eraser_selected:
+            self.canvas_container.setCursor(CircleCursor(Qt.GlobalColor.white, Qt.GlobalColor.transparent, self.brush_size))
+        elif self.move_selected:
+            self.canvas_container.setCursor(Qt.CursorShape.OpenHandCursor)
+        elif self.active_grid_area_selected:
+            self.canvas_container.setCursor(Qt.CursorShape.DragMoveCursor)
+        else:
+            self.canvas_container.setCursor(QCursor(Qt.CursorShape.ArrowCursor))
+
+    def leave_event(self, event):
+        self.canvas_container.setCursor(QCursor(Qt.CursorShape.ArrowCursor))
+
+    def update(self):
+        self.parent.window.canvas_position.setText(f"X: {self.pos_x}, Y: {self.pos_y}")
+        self.canvas_container.update()
+
+    def clear(self):
+        self.current_layer.lines = []
+        self.current_layer.images = []
+        self.update()
+
+    def recenter(self):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.update()
+
+    def handle_move_canvas(self, event):
+        self.pos_x += event.pos().x() - self.drag_pos.x()
+        self.pos_y += event.pos().y() - self.drag_pos.y()
+        self.drag_pos = event.pos()
+        self.update()
+
+    def handle_move_layer(self, event):
+        point = QPoint(
+            event.pos().x() if self.drag_pos is not None else 0,
+            event.pos().y() if self.drag_pos is not None else 0
+        )
+        # snap to grid
+        grid_size = self.settings_manager.settings.size.get()
+        point.setX(point.x() - (point.x() % grid_size))
+        point.setY(point.y() - (point.y() % grid_size))
+
+        # center the image
+        # point.setX(int((point.x() - self.current_layer.images[0].image.size[0] / 2)))
+        # point.setY(int((point.y() - self.current_layer.images[0].image.size[1] / 2)))
+
+        # establish a rect based on line points - we need the area that is being moved
+        # so that we can center the point on it
+        rect = QRect()
+        for line in self.current_layer.lines:
+            rect = rect.united(QRect(line.start_point, line.end_point))
+
+        try:
+            rect = rect.united(QRect(self.current_layer.images[0].position.x(), self.current_layer.images[0].position.y(), self.current_layer.images[0].image.size[0], self.current_layer.images[0].image.size[1]))
+        except IndexError:
+            pass
+
+        # center the point on the rect
+        point.setX(int(point.x() - int(rect.width() / 2)))
+        point.setY(int(point.y() - int(rect.height() / 2)))
+
+        self.layers[self.current_layer_index].offset = point
+
+    def mouse_press_event(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.select_start = event.pos()
+        if event.button() in (Qt.MouseButton.LeftButton, Qt.MouseButton.RightButton):
+            if self.brush_selected:
+                self.parent.history.add_event({
+                    "event": "draw",
+                    "layer_index": self.current_layer_index,
+                    "lines": self.current_layer.lines.copy()
+                })
+                if not self.left_mouse_button_down and not self.right_mouse_button_down:
+                    self.start_drawing_line_index = len(self.current_layer.lines)
+                start = event.pos() - QPoint(self.pos_x, self.pos_y)
+                end = event.pos() - QPoint(self.pos_x, self.pos_y)
+                pen = self.pen(event)
+                opacity = 255
+                if event.button() == Qt.MouseButton.LeftButton:
+                    opacity = self.primary_brush_opacity
+                elif event.button() == Qt.MouseButton.RightButton:
+                    opacity = self.secondary_brush_opacity
+                line = LineData(start, end, pen, self.current_layer_index, opacity)
+                start += self.layers[self.current_layer_index].offset
+                end += self.layers[self.current_layer_index].offset
+                self.current_layer.lines += [line]
+
+                if event.button() == Qt.MouseButton.LeftButton and not self.left_mouse_button_down:
+                    self.right_mouse_button_down = False
+                    self.left_mouse_button_down = True
+                    self.brush_start = start
+                elif event.button() == Qt.MouseButton.RightButton and not self.right_mouse_button_down:
+                    self.left_mouse_button_down = False
+                    self.right_mouse_button_down = True
+                    self.brush_start = start
+
+            self.handle_tool(event)
+            self.update()
+        elif event.button() == Qt.MouseButton.MiddleButton:
+            # Start dragging the canvas when the middle or right mouse button is pressed
+            self.drag_pos = event.pos()
+
+    def mouse_move_event(self, event):
+        # check if LeftButton is pressed
+        if Qt.MouseButton.LeftButton in event.buttons() or Qt.MouseButton.RightButton in event.buttons():
+            self.last_mouse_pos = event.pos()
+            self.handle_tool(event)
+            self.update()
+        elif self.drag_pos is not None:
+            self.handle_move_canvas(event)
+    
+    def mouse_release_event(self, event):
+        if event.button() in (Qt.MouseButton.LeftButton, Qt.MouseButton.RightButton):
+            self.left_mouse_button_down = False
+            self.right_mouse_button_down = False
+            self.start_drawing_line_index = None
+            if self.brush_selected:
+                self.stop_drawing_line_index = len(self.current_layer.lines)
+                self.update()
+            elif self.eraser_selected:
+                self.last_pos = None
+                self.is_erasing = False
+        elif event.button() == Qt.MouseButton.MiddleButton:
+            # Start dragging the canvas when the middle or right mouse button is pressed
+            self.drag_pos = event.pos()
+        self.rasterize_lines()
+
+    def handle_select(self, event):
+        if self.select_selected:
+            if self.select_start is None:
+                self.select_start = event.pos()
+            else:
+                self.select_end = event.pos()
+
+        # snap to grid if enabled
+        if self.settings_manager.settings.snap_to_grid.get():
+            grid_size = self.settings_manager.settings.size.get()
+            self.select_start.setX(self.select_start.x() - (self.select_start.x() % grid_size))
+            self.select_start.setY(self.select_start.y() - (self.select_start.y() % grid_size))
+            self.select_end.setX(self.select_end.x() - (self.select_end.x() % grid_size))
+            self.select_end.setY(self.select_end.y() - (self.select_end.y() % grid_size))
+
+        self.update()
+
+    def handle_tool(self, event):
+        if self.eraser_selected:
+            if not self.is_erasing:
+                self.parent.history.add_event({
+                    "event": "erase",
+                    "layer_index": self.current_layer_index,
+                    "lines": self.current_layer.lines.copy(),
+                    "images": self.get_image_copy(self.current_layer_index)
+                })
+            self.handle_erase(event)
+            self.parent.is_dirty = True
+        elif self.brush_selected:
+            self.handle_draw(event)
+            self.parent.is_dirty = True
+        elif self.move_selected:
+            self.handle_move_layer(event)
+            self.parent.is_dirty = True
+        elif self.select_selected:
+            self.handle_select(event)
+        elif self.active_grid_area_selected:
+            self.handle_move_active_grid_area(event)
+
+    def handle_move_active_grid_area(self, event):
+        pos = event.pos()
+        point = QPoint(
+            pos.x(),
+            pos.y()
+        )
+
+        # drag from the center of active_grid_area_pivot_point based on the size
+        width = self.settings_manager.settings.working_width.get()
+        height = self.settings_manager.settings.working_height.get()
+        point -= QPoint(
+            int((width / 2) + self.pos_x),
+            int((height / 2) + self.pos_y)
+        )
+
+        if self.settings_manager.settings.snap_to_grid.get():
+            point = QPoint(
+                point.x() - (point.x() % self.grid_size),
+                point.y() - (point.y() % self.grid_size)
+            )
+
+        self.active_grid_area_pivot_point = point
+
+        # trigger draw event
+        self.update()
+
+    def reset_settings(self):
+        self.parent.window.width_slider.setValue(self.settings_manager.settings.working_width.get())
+        self.parent.window.height_slider.setValue(self.settings_manager.settings.working_height.get())
---
+++ packages/airunner/airunner-1.15.2.tar.gz/airunner-1.15.2/src/airunner/mixins/canvas_brushes_mixin.py
@@ -0,0 +1,297 @@
+from PIL import ImageDraw
+from PyQt6.QtCore import Qt, QPointF, QPoint, QSize, QRect
+from PyQt6.QtGui import QPainter, QPainterPath, QColor, QPen, QImage
+from airunner.models.linedata import LineData
+from PIL import Image
+
+
+class CanvasBrushesMixin:
+    _point = None
+    active_canvas_rect = QRect(0, 0, 0, 0)
+    opacity = None
+    color = None
+    width = None
+    left_line_extremity = None
+    right_line_extremity = None
+    top_line_extremity = None
+    bottom_line_extremity = None
+    last_left = 0
+    last_top = 0
+    min_x = 0
+    min_y = 0
+    last_pos = None
+    
+    @property
+    def is_drawing(self):
+        return self.left_mouse_button_down or self.right_mouse_button_down
+
+    @property
+    def primary_color(self):
+        return QColor(self.settings_manager.settings.primary_color.get())
+
+    @property
+    def primary_brush_opacity(self):
+        return self.settings_manager.settings.primary_brush_opacity.get()
+
+    @property
+    def secondary_color(self):
+        return QColor(self.settings_manager.settings.secondary_color.get())
+
+    @property
+    def secondary_brush_opacity(self):
+        return self.settings_manager.settings.secondary_brush_opacity.get()
+
+    def draw(self, layer, index):
+        path = QPainterPath()
+        painter = None
+        for line in layer.lines:
+            pen = line.pen
+            pen.setCapStyle(Qt.PenCapStyle.RoundCap)
+            pen.setJoinStyle(Qt.PenJoinStyle.RoundJoin)
+            if self.width is None or self.width != line.width:
+                self.width = line.width
+                self.draw_path(path, painter)
+                painter = None
+            if self.color is None or self.color != line.color:
+                self.color = line.color
+                self.draw_path(path, painter)
+                painter = None
+            if self.opacity is None or self.opacity != line.opacity:
+                self.opacity = line.opacity
+                self.draw_path(path, painter)
+                painter = None
+            if not painter:
+                painter = QPainter(self.canvas_container)
+                painter.setBrush(self.brush)
+                painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+                path = QPainterPath()
+            painter.setPen(pen)
+            painter.setOpacity(line.opacity / 255)
+
+            start = QPointF(line.start_point.x() + self.pos_x, line.start_point.y() + self.pos_y)
+            end = QPointF(line.end_point.x() + self.pos_x, line.end_point.y() + self.pos_y)
+
+            # also apply the layer offset
+            offset = QPointF(self.current_layer.offset.x(), self.current_layer.offset.y())
+            start += offset
+            end += offset
+
+            # calculate control points for the Bezier curve
+            dx = end.x() - start.x()
+            dy = end.y() - start.y()
+            ctrl1 = QPointF(start.x() + dx / 3, start.y() + dy / 3)
+            ctrl2 = QPointF(end.x() - dx / 3, end.y() - dy / 3)
+
+            # add the curve to the path
+            path.moveTo(start)
+            path.cubicTo(ctrl1, ctrl2, end)
+
+        # draw the entire line with a single drawPath call
+        self.draw_path(path, painter)
+
+    def draw_path(self, path, painter):
+        if painter:
+            painter.drawPath(path)
+            painter.end()
+
+    def handle_erase(self, event):
+        self.is_erasing = True
+        brush_size = int(self.settings_manager.settings.mask_brush_size.get() / 2)
+        image = self.current_layer.images[0].image if len(self.current_layer.images) > 0 else None
+        image_pos = self.current_layer.images[0].position if len(self.current_layer.images) > 0 else None
+        if image is None:
+            return
+        start = event.pos() - QPoint(self.pos_x, self.pos_y) - image_pos
+        if image:
+            image = image.copy()
+            draw = ImageDraw.Draw(image)
+            if self.last_pos is None:
+                self.last_pos = start
+            draw.line([
+                self.last_pos.x(),
+                self.last_pos.y(),
+                start.x(),
+                start.y()
+            ], fill=(0, 0, 0, 0), width=brush_size*2, joint="curve")
+            draw.ellipse((
+                start.x() - brush_size,
+                start.y() - brush_size,
+                start.x() + brush_size,
+                start.y() + brush_size
+            ), fill=(0, 0, 0, 0))
+            self.current_layer.images[0].image = image
+            self.last_pos = start
+            self.update()
+        self.update()
+
+    def pen(self, event):
+        brush_color = "#ffffff"
+        if event.button() == Qt.MouseButton.LeftButton or Qt.MouseButton.LeftButton in event.buttons():
+            brush_color = self.settings_manager.settings.primary_color.get()
+        elif event.button() == Qt.MouseButton.RightButton or Qt.MouseButton.RightButton in event.buttons():
+            brush_color = self.settings_manager.settings.secondary_color.get()
+        brush_color = QColor(brush_color)
+        pen = QPen(
+            brush_color,
+            self.settings_manager.settings.mask_brush_size.get()
+        )
+        return pen
+
+    def handle_draw(self, event):
+        start = event.pos() - QPoint(self.pos_x, self.pos_y)
+        pen = self.pen(event)
+        opacity = 255
+        if event.button() == Qt.MouseButton.LeftButton or Qt.MouseButton.LeftButton in event.buttons():
+            opacity = self.primary_brush_opacity
+        elif event.button() == Qt.MouseButton.RightButton or Qt.MouseButton.RightButton in event.buttons():
+            opacity = self.secondary_brush_opacity
+        if len(self.current_layer.lines) > 0:
+            previous = LineData(self.current_layer.lines[-1].start_point, start, pen, self.current_layer_index, opacity)
+            self.current_layer.lines[-1] = previous
+
+            if self.shift_is_pressed:  # draw a strait line by combining the line segments
+                if len(self.current_layer.lines) > self.start_drawing_line_index:
+                    start_line = self.current_layer.lines[self.start_drawing_line_index]
+                    end_line = self.current_layer.lines[self.stop_drawing_line_index - 1]
+                    new_line_data = LineData(
+                        start_line.start_point,
+                        end_line.end_point,
+                        start_line.pen,
+                        start_line.layer_index,
+                        start_line.opacity
+                    )
+                    self.current_layer.lines = self.current_layer.lines[:self.start_drawing_line_index]
+                    self.current_layer.lines.append(new_line_data)
+
+        end = event.pos() - QPoint(self.pos_x + 1, self.pos_y)
+        line_data = LineData(start, end, pen, self.current_layer_index, opacity)
+        self.current_layer.lines.append(line_data)
+        self.update()
+
+    def get_line_extremities(self):
+        for line in self.current_layer.lines:
+            start_x = line.start_point.x()
+            start_y = line.start_point.y()
+            end_x = line.end_point.x()
+            end_y = line.end_point.y()
+
+            brush_size = int(self.settings_manager.settings.mask_brush_size.get() / 2)
+            min_x = min(start_x, end_x) - brush_size
+            min_y = min(start_y, end_y) - brush_size
+            max_x = max(start_x, end_x) + brush_size
+            max_y = max(start_y, end_y) + brush_size
+            self.min_x = min_x
+            self.min_y = min_y
+
+            if self.left_line_extremity is None or min_x < self.left_line_extremity:
+                self.left_line_extremity = min_x
+            if self.right_line_extremity is None or max_x > self.right_line_extremity:
+                self.right_line_extremity = max_x
+            if self.top_line_extremity is None or min_y < self.top_line_extremity:
+                self.top_line_extremity = min_y
+            if self.bottom_line_extremity is None or max_y > self.bottom_line_extremity:
+                self.bottom_line_extremity = max_y
+        return self.top_line_extremity, self.left_line_extremity, self.bottom_line_extremity, self.right_line_extremity
+
+    def rasterize_lines(self):
+        if len(self.current_layer.lines) == 0:
+            return
+        top, left, bottom, right = self.get_line_extremities()
+
+        # create a QImage with the size of the lines
+        min_x = min(left, right)
+        min_y = min(top, bottom)
+        max_x = max(left, right)
+        max_y = max(top, bottom)
+        width = abs(max_x - min_x)
+        height = abs(max_y - min_y)
+        img = QImage(QSize(width, height), QImage.Format.Format_ARGB32)
+        img.fill(Qt.GlobalColor.transparent)
+        painter = QPainter(img)
+        painter.setBrush(self.brush)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        path = self.create_image_path(painter)
+        painter.drawPath(path)
+        painter.end()
+        self.convert_pixmap_to_pil_image(img, top, left, bottom, right)
+
+    def create_image_path(self, painter):
+        path = QPainterPath()
+        for line in self.current_layer.lines:
+            pen = line.pen
+            pen.setCapStyle(Qt.PenCapStyle.RoundCap)
+            pen.setJoinStyle(Qt.PenJoinStyle.RoundJoin)
+
+            painter.setPen(pen)
+            painter.setOpacity(line.opacity / 255)
+
+            start = QPointF(line.start_point.x() - self.left_line_extremity, line.start_point.y() - self.top_line_extremity)
+            end = QPointF(line.end_point.x() - self.left_line_extremity, line.end_point.y() - self.top_line_extremity)
+
+            # also apply the layer offset
+            offset = QPointF(self.current_layer.offset.x(), self.current_layer.offset.y())
+            start += offset
+            end += offset
+
+            # calculate control points for the Bezier curve
+            dx = end.x() - start.x()
+            dy = end.y() - start.y()
+            ctrl1 = QPointF(start.x() + dx / 3, start.y() + dy / 3)
+            ctrl2 = QPointF(end.x() - dx / 3, end.y() - dy / 3)
+
+            # add the curve to the path
+            path.moveTo(start)
+            path.cubicTo(ctrl1, ctrl2, end)
+        return path
+
+    def convert_pixmap_to_pil_image(self, img: Image, top: int, left: int, bottom: int, right: int):
+        img = Image.fromqpixmap(img)
+        width = img.width
+        height = img.height
+        current_image = self.current_layer.images[0].image.copy() if len(self.current_layer.images) > 0 else None
+        existing_image_width = current_image.width if current_image else 0
+        existing_image_height = current_image.height if current_image else 0
+
+        composite_width = width
+        composite_height = height
+        if existing_image_width > composite_width:
+            composite_width = existing_image_width
+        if existing_image_height > composite_height:
+            composite_height = existing_image_height
+
+        do_new_image = False
+        if composite_width > existing_image_width or composite_height > existing_image_height:
+            composite_image = Image.new('RGBA', (composite_width, composite_height), (0, 0, 0, 0))
+            do_new_image = True
+        else:
+            composite_image = current_image
+        composite_img_dest = QPoint(left, top)
+
+        pos_x = 0
+        pos_y = 0
+
+        if self.last_left != left:
+            last_left = self.last_left
+            self.last_left = left
+            pos_x = -self.last_left + last_left
+        if self.last_top != top:
+            last_top = self.last_top
+            self.last_top = top
+            pos_y = -self.last_top + last_top
+
+        new_img_dest_pos_x = 0
+        new_img_dest_pos_y = 0
+
+        # self.parent.window.debug_label.setText(
+        #     f"W/H: {width}x{height} | imgdest: {new_img_dest_pos_x}, {new_img_dest_pos_y} | ext: {self.left_line_extremity}, {self.top_line_extremity}, {self.right_line_extremity}, {self.bottom_line_extremity} | max: {self.max_left}, {self.max_top} {self.max_right} {self.max_bottom} | last: {self.last_left}, {self.last_top}"
+        # )
+
+        # add current image to the composite image
+        if current_image and do_new_image:
+            existing_img_dest = (pos_x, pos_y)
+            existing_img_source = (0, 0)
+            composite_image.alpha_composite(current_image, existing_img_dest, existing_img_source)
+        new_img_dest = (new_img_dest_pos_x, new_img_dest_pos_y)
+        composite_image.alpha_composite(img, new_img_dest)
+        self.add_image_to_canvas_new(composite_image, composite_img_dest, self.image_root_point)
+        self.current_layer.lines.clear()
