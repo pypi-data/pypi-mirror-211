3a4c56a0bcf3dea59e81f89a55ee398079bd7cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:31 +0000

    Add package arulespy/0.1.2/arulespy-0.1.2.tar.gz

---
+++ packages/arulespy/arulespy-0.1.2.tar.gz/arulespy-0.1.2/src/arulespy/arulesViz.py
@@ -0,0 +1,19 @@
+"""The arules module provides an interface to R's arules package."""
+
+import rpy2.robjects.packages as packages
+
+### activate automatic conversion of pandas dataframes to R dataframes
+#pandas2ri.activate()
+
+arulesViz = packages.importr('arulesViz')
+
+# get the generic for plot
+base = packages.importr('base')
+plot = base.plot
+#plot.__doc__ = r.plot.__doc__
+
+### FIXME: Not quite sure why I cannot set __doc__ here
+inspectDT = arulesViz.inspectDT
+#inspectDT.__doc__ = r.inspectDT.__doc__
+
+
