0723db0c93c0125d58cb3be6ac60c1b3b3d94e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:38 +0000

    Add package tablemaster/1.0.7/tablemaster-1.0.7.tar.gz

---
+++ packages/tablemaster/tablemaster-1.0.7.tar.gz/tablemaster-1.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tablemaster',
+    version='1.0.7',
+    packages=find_packages(),
+    install_requires=[
+        'PyMySQL',
+        'SQLAlchemy==1.4.46',
+        'pandas',
+        'python-dateutil',
+        'gspread',
+        'tqdm',
+        'numpy',
+        'mysql-connector-python',
+    ],
+    author='Livid',
+    author_email='livid.su@gmail.com',
+    description='A Python package makes it easy to manage tables anywhere',
+    url='https://github.com/ilivid/tablemaster'
+)
