af300c23081ca87e035d816a9e3c1b405495ff79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:11 +0000

    Add package brunospackage/0.0.3/brunospackage-0.0.3.tar.gz

---
+++ packages/brunospackage/brunospackage-0.0.3.tar.gz/brunospackage-0.0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="brunospackage", 
+        version=VERSION,
+        author="Bruno Tacchi",
+        author_email="bruno_tacchi@hotmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['matplotlib', 'pandas'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        # import pandas as pd
+        # import matplotlib.pyplot as plt
+        # from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+        # from matplotlib.figure import Figure
+        # import tkinter as tk
+        # import re
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
