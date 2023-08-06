6e0bfa792bcdd83db9795c371a4ef2df367b7e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:01 +0000

    Add package casaconfig/0.0.53/casaconfig-0.0.53.tar.gz

---
+++ packages/casaconfig/casaconfig-0.0.53.tar.gz/casaconfig-0.0.53/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+with open('README.md', "r") as fid:   #encoding='utf-8'
+    long_description = fid.read()
+
+setup(
+    name='casaconfig',
+    version='0.0.53',
+    description='CASA Operational Configuration Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='National Radio Astronomy Observatory',
+    author_email='casa-feedback@nrao.edu',
+    url='https://github.com/casangi/casadata',
+    license='Apache-2.0',
+    packages=find_packages(),
+    package_data={'casaconfig': ['__data__/*']},
+    include_package_data=True,
+    install_requires=['xarray>=0.16.2',
+                      'numpy>=1.18.4',
+                      'GitPython>=3.1.18',
+                      'importlib_metadata>=4.8.1',
+                      'certifi>=2023.5.7']
+)
