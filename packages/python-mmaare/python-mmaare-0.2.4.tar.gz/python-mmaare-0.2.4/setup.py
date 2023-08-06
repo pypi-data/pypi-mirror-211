0a7002bb6fb2969ae00f529baec718c298600e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:52 +0000

    Add package python-mmaare/0.2.4/python-mmaare-0.2.4.tar.gz

---
+++ packages/python-mmaare/python-mmaare-0.2.4.tar.gz/python-mmaare-0.2.4/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+  setup_requires='git-versiointi',
+  name='python-mmaare',
+  description='Python-moduulimääretoteutus',
+  url='https://github.com/an7oine/python-mmaare.git',
+  author='Antti Hautaniemi',
+  author_email='antti.hautaniemi@pispalanit.fi',
+  py_modules=['mmaare'],
+)
