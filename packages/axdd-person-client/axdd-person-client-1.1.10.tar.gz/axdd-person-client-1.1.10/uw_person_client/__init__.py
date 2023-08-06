5c2ca81aec1c93a8873337a91ee700084fe0fa71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:48 +0000

    Add package axdd-person-client/1.1.10/axdd-person-client-1.1.10.tar.gz

---
+++ packages/axdd-person-client/axdd-person-client-1.1.10.tar.gz/axdd-person-client-1.1.10/uw_person_client/__init__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+import logging
+import os
+
+# setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler()
+    ])
+
+if os.environ.get("AXDD_PERSON_CLIENT_ENV") == "PROD":
+    from uw_person_client.clients.core_client import UWPersonClient
+else:
+    from uw_person_client.clients.mock_client import \
+        MockedUWPersonClient as UWPersonClient  # noqa
