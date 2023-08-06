70f5226a46813878b1344767e30f830aca0de283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:00 +0000

    Add package modAL-python/0.4.2.1/modAL-python-0.4.2.1.tar.gz

---
+++ packages/modAL-python/modAL-python-0.4.2.1.tar.gz/modAL-python-0.4.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='modAL-python',
+    version='0.4.2.1',
+    author='Tivadar Danka',
+    author_email='85a5187a@opayq.com',
+    description='A modular active learning framework for Python3',
+    license='MIT',
+    url='https://modAL-python.github.io/',
+    packages=['modAL', 'modAL.models', 'modAL.utils'],
+    classifiers=['Development Status :: 4 - Beta'],
+    install_requires=['numpy', 'scikit-learn>=0.18',
+                      'scipy>=0.18', 'pandas>=1.1.0', 'skorch==0.9.0'],
+)
