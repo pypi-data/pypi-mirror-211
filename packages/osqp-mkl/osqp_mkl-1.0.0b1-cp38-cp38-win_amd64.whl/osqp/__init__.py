721f73b4ba37514264b50b47b4c8141537275ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:10 +0000

    Add package osqp-mkl/1.0.0b1/osqp_mkl-1.0.0b1-cp38-cp38-win_amd64.whl

---
+++ packages/osqp-mkl/osqp_mkl-1.0.0b1-cp38-cp38-win_amd64.whl/osqp/__init__.py
@@ -0,0 +1,38 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_3_7():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, '.'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-osqp_mkl-1.0.0b1')
+        if os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-osqp_mkl-1.0.0b1')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not ctypes.windll.kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 0x00000008):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError()))
+
+
+_delvewheel_init_patch_1_3_7()
+del _delvewheel_init_patch_1_3_7
+# end delvewheel patch
+
+# The _version.py file is managed by setuptools-scm
+#   and is not in version control.
+from osqp._version import version as __version__  # noqa: F401
+from osqp.interface import (  # noqa: F401
+    OSQP,
+    constant,
+    algebra_available,
+    algebras_available,
+    default_algebra,
+)
