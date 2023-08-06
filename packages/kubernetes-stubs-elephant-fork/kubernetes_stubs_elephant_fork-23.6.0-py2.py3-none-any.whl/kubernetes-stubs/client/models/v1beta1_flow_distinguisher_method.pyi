fb761afd8af7d864252d3c53d1f7f6e0475ec278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:36 +0000

    Add package kubernetes-stubs-elephant-fork/23.6.0/kubernetes_stubs_elephant_fork-23.6.0-py2.py3-none-any.whl

---
+++ packages/kubernetes-stubs-elephant-fork/kubernetes_stubs_elephant_fork-23.6.0-py2.py3-none-any.whl/kubernetes-stubs/client/models/v1beta1_flow_distinguisher_method.pyi
@@ -0,0 +1,14 @@
+import datetime
+import typing
+
+import kubernetes.client
+
+class V1beta1FlowDistinguisherMethod:
+    type: str
+    
+    def __init__(self, *, type: str) -> None:
+        ...
+    def to_dict(self) -> V1beta1FlowDistinguisherMethodDict:
+        ...
+class V1beta1FlowDistinguisherMethodDict(typing.TypedDict, total=False):
+    type: str
