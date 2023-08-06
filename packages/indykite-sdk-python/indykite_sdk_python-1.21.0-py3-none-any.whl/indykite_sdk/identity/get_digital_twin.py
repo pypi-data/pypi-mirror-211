df21edba7df7c0a968ca82f8c4e90933f0ecfd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:09 +0000

    Add package indykite-sdk-python/1.21.0/indykite_sdk_python-1.21.0-py3-none-any.whl

---
+++ packages/indykite-sdk-python/indykite_sdk_python-1.21.0-py3-none-any.whl/indykite_sdk/identity/get_digital_twin.py
@@ -0,0 +1,49 @@
+import uuid
+
+from indykite_sdk.identity import helper
+from indykite_sdk.indykite.identity.v1beta2 import identity_management_api_pb2 as pb2
+from indykite_sdk.indykite.identity.v1beta2 import model_pb2 as model
+from indykite_sdk.utils.deserialize_digital_twin_with_token_info import deserialize_digital_twin_with_token_info
+import sys
+import indykite_sdk.utils.logger as logger
+
+
+def get_digital_twin(self, digital_twin_id, tenant_id, fields):
+    sys.excepthook = logger.handle_excepthook
+    try:
+        response = self.stub.GetDigitalTwin(
+            pb2.GetDigitalTwinRequest(
+                id=model.DigitalTwinIdentifier(
+                    digital_twin=model.DigitalTwin(
+                        id=str(digital_twin_id),
+                        tenant_id=str(tenant_id)
+                    )
+                ),
+                properties=helper.create_property_mask(fields)
+            )
+        )
+    except Exception as exception:
+        return logger.logger_error(exception)
+
+    return deserialize_digital_twin_with_token_info(response)
+
+
+def get_digital_twin_by_token(self, token, fields):
+    sys.excepthook = logger.handle_excepthook
+    try:
+        if len(token) < 32:
+            raise Exception("Token must be 32 chars or more.")
+    except Exception as exception:
+        return logger.logger_error(exception)
+
+    try:
+        response = self.stub.GetDigitalTwin(
+                pb2.GetDigitalTwinRequest(
+                    id=model.DigitalTwinIdentifier(access_token=token),
+                    properties=helper.create_property_mask(fields)
+                )
+        )
+    except Exception as exception:
+        return logger.logger_error(exception)
+
+    return deserialize_digital_twin_with_token_info(response)
