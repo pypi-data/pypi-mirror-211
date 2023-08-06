31e3a5b9886b87090aec2c1f3c86e54f7a16260d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:42 +0000

    Add package waffle-hub/0.2.2/waffle_hub-0.2.2.tar.gz

---
+++ packages/waffle-hub/waffle_hub-0.2.2.tar.gz/waffle_hub-0.2.2/waffle_hub/schema/evaluate.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from waffle_hub.schema.base_schema import BaseSchema
+
+
+@dataclass
+class ObjectDetectionMetric(BaseSchema):
+    mAP: float
+
+
+@dataclass
+class ClassificationMetric(BaseSchema):
+    accuracy: float
+
+
+@dataclass
+class InstanceSegmentationMetric(BaseSchema):
+    mAP: float
