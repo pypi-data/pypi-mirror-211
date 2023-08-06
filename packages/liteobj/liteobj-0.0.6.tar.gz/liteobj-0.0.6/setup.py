1e4afcaefd27d4d2b6d3fc12f16525ccbc6c73d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:12 +0000

    Add package liteobj/0.0.6/liteobj-0.0.6.tar.gz

---
+++ packages/liteobj/liteobj-0.0.6.tar.gz/liteobj-0.0.6/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+from version import __version__
+
+repo_name = 'liteobj'
+
+requirements_fn = "requirements.txt"
+install_requirements = open(f"{repo_name}/{requirements_fn}").read().split()
+
+setup(name=repo_name,
+      version=__version__,
+      description='Create lightweight configs for instantiating ML experiments',
+      author='1lint',
+      author_email='105617163+1lint@users.noreply.github.com',
+      url=f'https://github.com/1lint/{repo_name}', 
+      install_requires=install_requirements,
+      packages=[repo_name],
+      package_data={repo_name: [requirements_fn]},
+      entry_points={
+            'console_scripts': [
+                  "lite = liteobj:main"
+            ]
+      },
+      scripts=["version.py"]
+)
+
