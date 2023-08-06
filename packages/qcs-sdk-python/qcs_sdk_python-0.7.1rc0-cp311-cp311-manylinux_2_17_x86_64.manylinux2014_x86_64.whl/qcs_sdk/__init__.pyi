e6dd21433e643045da7deb642b26998023761c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:49 +0000

    Add package qcs-sdk-python/0.7.1rc0/qcs_sdk_python-0.7.1rc0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/qcs-sdk-python/qcs_sdk_python-0.7.1rc0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/qcs_sdk/__init__.pyi
@@ -0,0 +1,29 @@
+from ._execution_data import (
+    # classes
+    ExecutionData as ExecutionData,
+    ResultData as ResultData,
+    RegisterMap as RegisterMap,
+    RegisterMatrix as RegisterMatrix,
+    # errors
+    RegisterMatrixConversionError as RegisterMatrixConversionError,
+)
+
+from ._executable import (
+    # classes
+    Executable as Executable,
+    ExeParameter as ExeParameter,
+    JobHandle as JobHandle,
+    Service as Service,
+    # errors
+    ExecutionError as ExecutionError,
+)
+
+from ._register_data import (
+    # classes
+    RegisterData as RegisterData,
+)
+
+from .client import (
+    # classes
+    QCSClient as QCSClient,
+)
