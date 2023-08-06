18be398d5854933dff6592ca3ee762e91a267669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:56 +0000

    Add package WebtoonScraper/0.0.16/WebtoonScraper-0.0.16-py3-none-any.whl

---
+++ packages/WebtoonScraper/WebtoonScraper-0.0.16-py3-none-any.whl/WebtoonScraper/CanvasScraper.py
@@ -0,0 +1,9 @@
+# canvas2
+from WebtoonScraper import WebtoonsScraper
+class CanvasScraper(WebtoonsScraper):
+    '''Scraping webtoons from webtoons.com'''
+    def __init__(self):
+        super().__init__()
+        self.BASE_URL = 'https://www.webtoons.com/en/challenge/meme-girls'
+
+canvas = CanvasScraper()
