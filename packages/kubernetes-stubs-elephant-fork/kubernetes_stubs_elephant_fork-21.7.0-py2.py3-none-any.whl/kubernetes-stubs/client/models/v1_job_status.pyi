c95dbbe53a399c9b61f2a23f4838e5873989f158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:37 +0000

    Add package kubernetes-stubs-elephant-fork/21.7.0/kubernetes_stubs_elephant_fork-21.7.0-py2.py3-none-any.whl

---
+++ packages/kubernetes-stubs-elephant-fork/kubernetes_stubs_elephant_fork-21.7.0-py2.py3-none-any.whl/kubernetes-stubs/client/models/v1_job_status.pyi
@@ -0,0 +1,26 @@
+import datetime
+import typing
+
+import kubernetes.client
+
+class V1JobStatus:
+    active: typing.Optional[int]
+    completed_indexes: typing.Optional[str]
+    completion_time: typing.Optional[datetime.datetime]
+    conditions: typing.Optional[list[kubernetes.client.V1JobCondition]]
+    failed: typing.Optional[int]
+    start_time: typing.Optional[datetime.datetime]
+    succeeded: typing.Optional[int]
+    
+    def __init__(self, *, active: typing.Optional[int] = ..., completed_indexes: typing.Optional[str] = ..., completion_time: typing.Optional[datetime.datetime] = ..., conditions: typing.Optional[list[kubernetes.client.V1JobCondition]] = ..., failed: typing.Optional[int] = ..., start_time: typing.Optional[datetime.datetime] = ..., succeeded: typing.Optional[int] = ...) -> None:
+        ...
+    def to_dict(self) -> V1JobStatusDict:
+        ...
+class V1JobStatusDict(typing.TypedDict, total=False):
+    active: typing.Optional[int]
+    completedIndexes: typing.Optional[str]
+    completionTime: typing.Optional[datetime.datetime]
+    conditions: typing.Optional[list[kubernetes.client.V1JobConditionDict]]
+    failed: typing.Optional[int]
+    startTime: typing.Optional[datetime.datetime]
+    succeeded: typing.Optional[int]
