a682a917fe5c86fa774166889ffc506c0d104587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:08 +0000

    Add package cloudservices-arg/0.1.4/cloudservices_arg-0.1.4.tar.gz

---
+++ packages/cloudservices-arg/cloudservices_arg-0.1.4.tar.gz/cloudservices_arg-0.1.4/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudservices_arg',
+    version='0.1.4',
+    description='A sample Python project for rach pack',
+    author='Rac',
+    author_email='rachitmahajan6399@gmail.com',
+    url='https://github.com/r-mhjn/QuizCreator',
+    packages=['cloudservices_arg'],
+    install_requires=['google-auth==2.17.3','google-cloud-pubsub==2.13.0','boto3==1.14.60','botocore==1.17.60','apache-libcloud==3.7.0','PyPubSub==4.0.3'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
