ebe78cf4e329b92f180d8944ea4055a12a76a579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:07 +0000

    Add package django-sso-client/0.0.2/django-sso-client-0.0.2.tar.gz

---
+++ packages/django-sso-client/django-sso-client-0.0.2.tar.gz/django-sso-client-0.0.2/django_client_sso/client_office365.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponseRedirect
+from client_base import ClientBase
+
+class ClientOffice365(ClientBase):
+
+    def login(self,token:str):
+        response = HttpResponseRedirect(f'{self.host}/office365/callback/')
+        response['Authorization'] = f'Bearer {token}'
+        return response
---
+++ packages/django-sso-client/django-sso-client-0.0.2.tar.gz/django-sso-client-0.0.2/django_client_sso/decorators.py
@@ -0,0 +1,26 @@
+from rest_framework.response import Response
+from functools import wraps
+from client_base import ClientBase
+from rest_framework import status
+
+def perm_validator(arg1):
+    def decorator_func(func):
+        @wraps(func)
+        def wrapper(self, request):
+            client = ClientBase()
+            token = request.META.get('HTTP_AUTHORIZATION')
+            token = token.replace('Bearer ', '')
+            token_decode = client.decode_token(token)
+
+            required = str(arg1).split()
+            scope = token_decode['scope']
+
+            for r in required:
+                if r in scope:
+                    return func(self,request)
+           
+            return Response(data={'error':'UNAUTHORIZED'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+        return wrapper
+
+    return decorator_func
