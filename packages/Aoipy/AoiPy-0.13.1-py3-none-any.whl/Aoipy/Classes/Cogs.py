6e52da7eb293884ecf32801850a433ff902207f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:24 +0000

    Add package Aoipy/0.13.1/AoiPy-0.13.1-py3-none-any.whl

---
+++ packages/Aoipy/AoiPy-0.13.1-py3-none-any.whl/Aoipy/Classes/Cogs.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+from Aoipy.tools import findBracketPairs, checkArgs, checkArgCheck
+import asyncio
+
+
+class Aoicogs:
+    def Cogs(self, Cog_Group, Name, Code):
+        from Aoipy.Classes.AoiPyClient import bots
+
+        class main_bod:
+            def __init__(self, bot):
+                self.bot = bot
+
+            @commands.command(name=Name)
+            async def go(self, ctx, *args, Code=Code):
+                from Aoipy.Classes.AoiPyClient import TotalFuncs
+                Context = ctx
+                newCode = checkArgs(args, Code)
+                finalCode = await checkArgCheck(args, newCode, Context)
+
+                await findBracketPairs(finalCode, TotalFuncs, Context)
+
+        COGS = type(Cog_Group, (commands.Cog,), {'__init__': main_bod.__init__, 'go': main_bod.go})
+        asyncio.run(bots.add_cog(COGS(bots)))
