884dd17152e6202db97f906875d0c3f1f79b147b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:41 +0000

    Add package availabl-cli/0.0.5/availabl-cli-0.0.5.tar.gz

---
+++ packages/availabl-cli/availabl-cli-0.0.5.tar.gz/availabl-cli-0.0.5/README.md
@@ -0,0 +1,19 @@
+# Availabl CLI
+This cli is used for interacting with availabl.co from the command line.
+
+## Set Up
+```
+pip install availabl-cli
+```
+
+## Usage
+```
+availabl install --profile <AWS cli profile> --region <AWS region> --api-key <API Key> --app-id <App ID>
+```
+
+or 
+
+```
+availabl install --region <AWS region> --api-key <API Key> --app-id <App ID> --role-arn <Exisitng IAM Role ARN>
+```
+
---
+++ packages/availabl-cli/availabl-cli-0.0.5.tar.gz/availabl-cli-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name = 'availabl-cli',
+    version = '0.0.5',
+    author = 'Arran McCabe',
+    author_email = 'arran@availabl.ai',
+    description = 'availabl cli tool',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    py_modules = ['availabl'],
+    packages = find_packages(),
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        availabl=availabl:run
+    '''
+)
