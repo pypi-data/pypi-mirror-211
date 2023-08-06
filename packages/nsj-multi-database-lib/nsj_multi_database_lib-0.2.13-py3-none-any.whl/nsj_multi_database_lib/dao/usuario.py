e4e3677c2e772a6cf3d5ef57bf2efccb13f165fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:30 +0000

    Add package nsj-multi-database-lib/0.2.13/nsj_multi_database_lib-0.2.13-py3-none-any.whl

---
+++ packages/nsj-multi-database-lib/nsj_multi_database_lib-0.2.13-py3-none-any.whl/nsj_multi_database_lib/dao/usuario.py
@@ -0,0 +1,28 @@
+from nsj_rest_lib.dao.dao_base import DAOBase
+from nsj_gcf_utils.db_adapter2 import DBAdapter2
+from nsj_rest_lib.entity.entity_base import EntityBase
+from nsj_rest_lib.exception import NotFoundException
+
+
+class UsuarioDAO():
+    def __init__(self, db: DBAdapter2):
+        self._db = db
+    
+    def get_by_email(self, email):
+        sql = """
+            select login, senha
+            from ns.usuarios
+            where email ilike :email;
+        """
+
+        # Running query
+        resp = self._db.execute_query(
+            sql,
+            email=email
+        )
+
+        if len(resp) <= 0:
+            raise NotFoundException(
+                f'Não foi encontrado um usuário vinculado à Conta Nasajon')
+        
+        return resp[0]
