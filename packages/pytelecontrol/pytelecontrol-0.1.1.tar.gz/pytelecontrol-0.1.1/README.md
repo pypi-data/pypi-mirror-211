2a87cf088b921fc899d125628a8453d9947d2e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:37 +0000

    Add package pytelecontrol/0.1.1/pytelecontrol-0.1.1.tar.gz

---
+++ packages/pytelecontrol/pytelecontrol-0.1.1.tar.gz/pytelecontrol-0.1.1/README.md
@@ -0,0 +1,16 @@
+# pytelecontrol
+Remotely access to your PLC, with auto discover feature
+
+Key Features
+* Auto discover PLC
+* Auto VPN
+
+
+## Maintainers
+
+  * César Freire <cfreire70@gmail.com>
+  * Ricardo oliveira <ricardo.oliveira.pt@gmail.com>
+
+## Running
+
+    $ poetry run python -m pytelecontrol
---
+++ packages/pytelecontrol/pytelecontrol-0.1.1.tar.gz/pytelecontrol-0.1.1/pytelecontrol/__main__.py
@@ -0,0 +1,40 @@
+import argparse
+import sys
+import logging
+
+from . import __version__
+
+REQUIRED_PYTHON = (3, 10)
+
+LOG_FORMAT = '%(asctime)-15s %(levelname)-8s %(name)-15s %(message)s'
+
+
+def validate_python():
+    """Validate that the right Python version is running."""
+    if sys.version_info[:2] < REQUIRED_PYTHON:
+        print(f"Error: {__package__} requires at least Python "
+              f"v{REQUIRED_PYTHON}")
+        sys.exit(1)
+
+
+def log_setup(loglevel='INFO'):
+    """Start logging write version"""
+    logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
+    logger = logging.getLogger(__package__)
+    logger.info(f"Starting {__package__} {__version__}")
+    logger.info(f'Changing log level to: {loglevel}')
+    logger.setLevel(loglevel)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Remotely access to your PLC, with auto discover feature", prog=__package__)
+    parser.add_argument("--version", action="version", version=f'{__package__} {__version__}')
+    parser.add_argument('--loglevel', default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help='Set log level, Defaults to INFO')
+    return parser
+
+
+if __name__ == '__main__':
+    validate_python()
+    args = get_parser().parse_args()
+    log_setup(args.loglevel)
