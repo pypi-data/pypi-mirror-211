737438604af31a38910fbb8ebac189ea17f981db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:35 +0000

    Add package AeholdSerializer/0.2.2/AeholdSerializer-0.2.2.tar.gz

---
+++ packages/AeholdSerializer/AeholdSerializer-0.2.2.tar.gz/AeholdSerializer-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AeholdSerializer",
+    version="0.2.2",
+    packages=find_packages(include=["AeholdSerializer", "AeholdSerializer.*"]),
+    description="XML & JSON serializator.""",
+    author="Me",
+    license="MIT",
+    entry_points={
+        'console_scripts': ['aehold=AeholdSerializer.lib:main']
+    }
+)
