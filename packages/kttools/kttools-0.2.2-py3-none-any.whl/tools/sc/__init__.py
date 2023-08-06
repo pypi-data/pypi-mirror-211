f75ae6df8773e565d397ae70513de4766ae324fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:39 +0000

    Add package kttools/0.2.2/kttools-0.2.2-py3-none-any.whl

---
+++ packages/kttools/kttools-0.2.2-py3-none-any.whl/tools/sc/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# @Author: Kelvin
+# @Date:   2022-07-18 11:49:10
+# @Last Modified by:   Kelvin
+# @Last Modified time: 2022-07-18 12:00:49
+"""single cell module."""
+
+from ._sc import (
+    exportDEres,
+    returnDEres,
+    vmax,
+    vmin,
+    cell_cycle_scoring,
+    combine_two_categories,
+)
+
+__all__ = [
+    # single-cell
+    "exportDEres",
+    "returnDEres",
+    "vmax",
+    "vmin",
+    "cell_cycle_scoring",
+    "combine_two_categories",
+]
