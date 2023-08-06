0b370874b322b4588f8d9edaa616d02c89955b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:44 +0000

    Add package mezcla/1.3.7/mezcla-1.3.7.tar.gz

---
+++ packages/mezcla/mezcla-1.3.7.tar.gz/mezcla-1.3.7/mezcla/tests/template.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python
+#
+# TODO: Test(s) for ../<module>.py
+#
+# Notes:
+# - Fill out TODO's below. Use numbered tests to order (e.g., test_1_usage).
+# - TODO: If any of the setup/cleanup methods defined, make sure to invoke base
+#   (see examples below for setUp and tearDown).
+# - For debugging the tested script, the ALLOW_SUBCOMMAND_TRACING environment
+#   option shows tracing output normally suppressed by  unittest_wrapper.py.
+# - This can be run as follows:
+#   $ PYTHONPATH=".:$PYTHONPATH" python ./mezcla/tests/test_<module>.py
+#
+
+"""TODO: Tests for <module> module"""
+
+# Standard packages
+import re
+
+# Installed packages
+import pytest
+
+# Local packages
+from mezcla.unittest_wrapper import TestWrapper
+from mezcla import debug
+## TODO: from mezcla import system
+from mezcla import glue_helpers as gh
+
+# Note: Two references are used for the module to be tested:
+#    THE_MODULE:            global module object
+## TODO: change template to new name
+THE_MODULE = None           ## TODO: remove this line (n.b., used just to avoid syntax problems with <module> in following)
+## TODO: import mezcla.<module> as THE_MODULE
+#
+# Note: sanity test for customization (TODO: remove if desired)
+if not re.search(__file__, r"\btemplate.py$"):
+    debug.assertion("mezcla.template" not in str(THE_MODULE))
+
+## TODO:
+## # Environment options
+## # Note: These are just intended for internal options, not for end users.
+## # It also allows for enabling options in one place.
+## #
+## FUBAR = system.getenv_bool("FUBAR", False,
+##                            description="Fouled Up Beyond All Recognition processing")
+
+
+class TestTemplate(TestWrapper):
+    """Class for testcase definition"""
+    script_module = TestWrapper.get_testing_module_name(__file__)
+    # -or- non-mezcla: script_module = TestWrapper.get_testing_module_name(__file__, THE_MODULE)
+    #
+    # TODO: use_temp_base_dir = True            # treat TEMP_BASE as directory
+    # note: temp_file defined by parent (along with script_module, temp_base, and test_num)
+
+    ## TODO: optional setup methods
+    ##
+    ## @classmethod
+    ## def setUpClass(cls):
+    ##     """One-time initialization (i.e., for entire class)"""
+    ##     debug.trace(6, f"TestIt.setUpClass(); cls={cls}")
+    ##     # note: should do parent processing first
+    ##     super().setUpClass()
+    ##     ...
+    ##     return
+    ##
+    ## def setUp(self):
+    ##     """Per-test setup"""
+    ##     debug.trace(6, f"TestIt.setUp(); self={self}")
+    ##     # note: must do parent processing first (e.g., for temp file support)
+    ##     super().setUp()
+    ##     ...
+    ##     # TODO: debug.trace_current_context(level=debug.QUITE_DETAILED)
+    ##     return
+
+    ## TODO: use assertEqual, etc.
+    ##   not assertEquals, etc. [maldito unittest!]
+
+    def test_data_file(self):
+        """Makes sure TODO works as expected"""
+        debug.trace(4, "TestIt.test_data_file()")
+        data = ["TODO1", "TODO2"]
+        gh.write_lines(self.temp_file, data)
+        output = self.run_script("", self.temp_file)
+        self.assertTrue(re.search(r"TODO-pattern", 
+                                  output.strip()))
+        return
+
+    def test_something_else(self):
+        """TODO: flesh out test for something else"""
+        debug.trace(4, "TestIt.test_something_else()")
+        self.fail("TODO: code test")
+        ## ex: self.assertEqual(THE_MODULE.TODO_function() == TODO_value)
+        return
+
+
+    ## TODO: optional cleanup methods
+    ##
+    ## def tearDown(self):
+    ##     debug.trace(6, f"TestIt.tearDown(); self={self}")
+    ##     super(TestIt, cls).tearDownClass()
+    ##     ...
+    ##     return
+    ##
+    ## @classmethod
+    ## def tearDownClass(cls):
+    ##     debug.trace(6, f"TestIt.tearDownClass(); cls={cls}")
+    ##     super(TestIt, self).tearDown()
+    ##     ...
+    ##     return
+
+## TODO:
+## #...............................................................................
+##
+## class TestIt2:
+##     """Another class for testcase definition
+##     Note: Needed to avoid error with pytest due to inheritance with unittest.TestCase via TestWrapper"""
+##     pass
+##
+
+#------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
