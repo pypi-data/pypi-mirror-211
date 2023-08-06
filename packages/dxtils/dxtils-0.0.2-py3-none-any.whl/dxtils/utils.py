6fa00e4e362b6d5464b7c0c9027d38242976d540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:54 +0000

    Add package dxtils/0.0.2/dxtils-0.0.2-py3-none-any.whl

---
+++ packages/dxtils/dxtils-0.0.2-py3-none-any.whl/dxtils/utils.py
@@ -0,0 +1,38 @@
+import platform
+
+import undetected_chromedriver as uc
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import read_version_from_cmd, PATTERN
+
+
+PLATFORM = platform.system()
+BRAVE_EXECUTABLE = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+
+
+def set_up_driver(
+    undetected: bool = False, kwargs_only: bool = False
+) -> Chrome | uc.Chrome | dict:
+    options = ChromeOptions()
+    if PLATFORM != "Windows":
+        version = read_version_from_cmd(
+            f'"{BRAVE_EXECUTABLE}" --version', PATTERN["brave-browser"])
+        driver_binary = ChromeDriverManager(version=version).install()
+        options.binary_location = BRAVE_EXECUTABLE
+        executable_path = BRAVE_EXECUTABLE
+    else:
+        driver_binary = ChromeDriverManager().install()
+        executable_path = None        
+    if not undetected:
+        if kwargs_only:
+            return {"service": Service(driver_binary), "options": options}
+        return Chrome(service=Service(driver_binary), options=options)
+    if kwargs_only:
+        return {
+            "driver_executable_path": driver_binary,
+            "browser_executable_path": executable_path
+        }
+    return uc.Chrome(
+        driver_executable_path=driver_binary,
+        browser_executable_path=executable_path)
