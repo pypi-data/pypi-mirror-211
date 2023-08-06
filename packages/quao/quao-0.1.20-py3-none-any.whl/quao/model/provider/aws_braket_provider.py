cd932161bc21ac2b80b20b1c2b4d6631665400b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:41 +0000

    Add package quao/0.1.20/quao-0.1.20-py3-none-any.whl

---
+++ packages/quao/quao-0.1.20-py3-none-any.whl/quao/model/provider/aws_braket_provider.py
@@ -0,0 +1,40 @@
+"""
+    QuaO Project aws_braket_provider.py Copyright © CITYNOW Co. Ltd. All rights reserved.
+"""
+import boto3
+from braket.aws import AwsSession, AwsDevice
+
+from ...enum.provider_type import ProviderType
+from ...model.provider.provider import Provider
+
+
+class AwsBraketProvider(Provider):
+
+    def __init__(self, aws_access_key, aws_secret_access_key, region_name):
+        super().__init__(ProviderType.AWS_BRAKET)
+        self.aws_access_key = aws_access_key
+        self.aws_secret_access_key = aws_secret_access_key
+        self.region_name = region_name
+
+    def get_backend(self, device_specification: str):
+        """
+
+        @param device_specification:
+        """
+
+        session = self.collect_providers()
+        return AwsDevice(
+            arn=device_specification,
+            aws_session=session)
+
+    def collect_providers(self):
+        """
+
+        """
+
+        session = boto3.Session(
+            aws_access_key_id=self.aws_access_key,
+            aws_secret_access_key=self.aws_secret_access_key,
+            region_name=self.region_name)
+
+        return AwsSession(boto_session=session)
