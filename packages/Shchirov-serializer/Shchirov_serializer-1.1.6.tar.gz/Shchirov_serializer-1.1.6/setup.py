ce2f90b90a79f1a6d1bc386b7cc2e1f1f7c2d86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:01 +0000

    Add package Shchirov-serializer/1.1.6/Shchirov_serializer-1.1.6.tar.gz

---
+++ packages/Shchirov-serializer/Shchirov_serializer-1.1.6.tar.gz/Shchirov_serializer-1.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="Shchirov_serializer",
+    version="1.1.6",
+    description="python library for serialization",
+    url="https://github.com/PaShampusik/PythonLabs/Lab_3",
+    author="PaShampusik",
+    author_email="shchirovpavel@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    packages=["serializers/json_serializer", "serializers/base",
+              "serializers/xml_serializer", "serializers"],
+    include_package_data=True
+)
