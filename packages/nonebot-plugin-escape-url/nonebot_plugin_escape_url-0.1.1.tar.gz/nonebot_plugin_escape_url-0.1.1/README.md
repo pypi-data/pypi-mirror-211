8c219cbefffc112b01b267f7406ba6a061464e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package nonebot-plugin-escape-url/0.1.1/nonebot_plugin_escape_url-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-escape-url/nonebot_plugin_escape_url-0.1.1.tar.gz/nonebot_plugin_escape_url-0.1.1/README.md
@@ -0,0 +1,16 @@
+nonebot-plugin-escape-url
+========
+
+自动转换发送消息中的URL🤔
+
+支持适配器：OneBot V11、QQ频道
+
+## 卖家秀
+
+使用前：
+
+![使用前](img/1.png)
+
+使用后：
+
+![使用后](img/2.png)
---
+++ packages/nonebot-plugin-escape-url/nonebot_plugin_escape_url-0.1.1.tar.gz/nonebot_plugin_escape_url-0.1.1/nonebot_plugin_escape_url/qqguild.py
@@ -0,0 +1,32 @@
+from .config import conf
+
+try:
+    from functools import wraps
+
+    from nonebot import logger
+    from nonebot.adapters.qqguild.api import API_HANDLERS
+
+    from .core import escape_text
+
+
+    def _patch(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            content = kwargs.get("content", None)
+            if content is not None:
+                content = escape_text(content, conf.escape_url_replace_dot_by)
+                logger.trace(f"escaped content: {content}")
+                kwargs["content"] = content
+
+            return await func(*args, **kwargs)
+
+        return wrapper
+
+
+    API_HANDLERS["post_messages"] = _patch(API_HANDLERS["post_messages"])
+    API_HANDLERS["post_dms_messages"] = _patch(API_HANDLERS["post_dms_messages"])
+
+    logger.opt(colors=True).success("Patched <y>QQ Guild</y> Adapter")
+
+except ImportError as e:
+    pass
