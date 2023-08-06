434e94a513d8fd8bd2509a12fa2ca821c8c4e13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:23 +0000

    Add package perun/0.3.2/perun-0.3.2.tar.gz

---
+++ packages/perun/perun-0.3.2.tar.gz/perun-0.3.2/perun/__init__.py
@@ -0,0 +1,16 @@
+"""perun module."""
+# flake8: noqa
+__version__ = "0.3.2"
+from perun.configuration import config
+from perun.logging import init_logging
+
+log = init_logging(config.get("debug", "log_lvl"))
+
+import os
+
+os.environ["IBV_FORK_SAFE"] = "1"
+os.environ["RDMAV_FORK_SAFE"] = "1"
+
+from perun.comm import Comm
+
+COMM_WORLD = Comm()
