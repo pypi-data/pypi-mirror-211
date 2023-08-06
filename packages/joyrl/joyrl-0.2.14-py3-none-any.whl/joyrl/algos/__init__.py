1afc612f91eaa9388156d81babc204fa5e401010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:48 +0000

    Add package joyrl/0.2.14/joyrl-0.2.14-py3-none-any.whl

---
+++ packages/joyrl/joyrl-0.2.14-py3-none-any.whl/joyrl/algos/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JiangJi
+Email: johnjim0816@gmail.com
+Date: 2023-01-01 16:20:49
+LastEditor: JiangJi
+LastEditTime: 2023-05-30 23:53:34
+Discription: 
+'''
+from joyrl.algos import base,DQN,DoubleDQN,DuelingDQN
+__all__ = [
+    "base",
+    "DQN",
+    "DoubleDQN",
+    "DuelingDQN",
+]
