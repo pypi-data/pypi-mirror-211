ca7e84bb3bd48692958a48cc062512121dcbdec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:33 +0000

    Add package pathml/2.1.1/pathml-2.1.1.tar.gz

---
+++ packages/pathml/pathml-2.1.1.tar.gz/pathml-2.1.1/setup.py
@@ -0,0 +1,60 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+about = {}
+with open('pathml/_version.py') as f:
+    exec(f.read(), about)
+version = about["__version__"]
+
+setuptools.setup(
+    name="pathml",
+    version=version,
+    author="Jacob Rosenthal, Ryan Carelli et al.",
+    author_email="PathML@dfci.harvard.edu",
+    description="Tools for computational pathology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    project_urls={
+            "Documentation": "https://pathml.readthedocs.io/en/stable",
+            "Source Code": "https://github.com/Dana-Farber-AIOS/pathml",
+    },
+    install_requires=[
+        "pip",
+        "numpy>=1.16.4",
+        "pandas",
+        "scipy",
+        "scikit-image",
+        "statsmodels",
+        "matplotlib",
+        "openslide-python",
+        "pydicom",
+        "h5py",
+        "scikit-learn",
+        "dask[distributed]",
+        "anndata>=0.7.6",
+        "scanpy",
+        "torch",
+        "opencv-contrib-python",
+        "python-bioformats>=4.0.0",
+        "loguru"
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Framework :: Sphinx",
+        "Framework :: Pytest",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
---
+++ packages/pathml/pathml-2.1.1.tar.gz/pathml-2.1.1/tests/test_logging.py
@@ -0,0 +1,46 @@
+"""
+Copyright 2021, Dana-Farber Cancer Institute and Weill Cornell Medicine
+License: GNU GPL 2.0
+"""
+import logging
+
+import pytest
+from _pytest.logging import caplog as _caplog  # noqa: F401
+from loguru import logger
+
+from pathml._logging import PathMLLogger as pml
+from pathml.utils import _test_log
+
+
+@pytest.fixture
+def caplog(_caplog):  # noqa: F811
+    """
+    loguru is not built on the standard library logging module, but pytest's logging functionality is.
+    In this fixture, we make sure that all logs to loguru are also propagated to the standard library logger so that
+    we can test them with pytest.
+    This overrides the `caplog` fixture that comes builtin to pytest
+
+    Based on: https://youtu.be/eFdVlyAGeZU
+    """
+
+    class PropagateHandler(logging.Handler):
+        def emit(self, record):
+            logging.getLogger(record.name).handle(record)
+
+    handler_id = logger.add(PropagateHandler(), format="{message}")
+    yield _caplog
+    logger.remove(handler_id)
+
+
+def test_toggle_logging(caplog):
+    _test_log("this should not be logged because we haven't enabled logging yet")
+    assert (
+        "this should not be logged because we haven't enabled logging yet"
+        not in caplog.text
+    )
+    pml.enable(sink=caplog.handler)
+    _test_log("this should be logged now")
+    assert "this should be logged now" in caplog.text
+    pml.disable()
+    _test_log("this should definitely not be logged")
+    assert "this should definitely not be logged" not in caplog.text
