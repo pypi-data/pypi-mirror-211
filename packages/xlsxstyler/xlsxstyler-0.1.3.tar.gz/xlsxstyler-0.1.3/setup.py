5bfd5f4df5702373fed4b72ed80e7eb817182b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:16 +0000

    Add package xlsxstyler/0.1.3/xlsxstyler-0.1.3.tar.gz

---
+++ packages/xlsxstyler/xlsxstyler-0.1.3.tar.gz/xlsxstyler-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xlsxstyler']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['openpyxl>=3.1.2,<4.0.0', 'validators>=0.20.0,<0.21.0']
+
+setup_kwargs = {
+    'name': 'xlsxstyler',
+    'version': '0.1.3',
+    'description': 'Some rudimentary styling of excel tables and placing pictures using openpyxl',
+    'long_description': None,
+    'author': 'Gwang-Jin Kim',
+    'author_email': 'gwang.jin.kim.phd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
