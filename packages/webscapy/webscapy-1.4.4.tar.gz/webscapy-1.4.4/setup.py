3f1468de27fba19a538bb6c8188c0deb553444f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:30 +0000

    Add package webscapy/1.4.4/webscapy-1.4.4.tar.gz

---
+++ packages/webscapy/webscapy-1.4.4.tar.gz/webscapy-1.4.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="webscapy",
+    version="1.4.4",
+    description="Selenium built for scraping instead of testing",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Rahul Raj",
+    packages=["webscapy"],
+    zip_safe=False,
+    project_urls={
+        "Documentation": "https://pypi.org/project/webscapy/",
+        "Source": "https://pypi.org/project/webscapy/",
+    }
+)
