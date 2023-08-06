8161ec712b3351ed3a7b22678303ae05e2b6b393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:50 +0000

    Add package bcvContext/0.1.4/bcvcontext-0.1.4.tar.gz

---
+++ packages/bcvContext/bcvcontext-0.1.4.tar.gz/bcvcontext-0.1.4/src/bcvContext/bcvContext.py
@@ -0,0 +1,38 @@
+import json
+from subprocess import call
+
+notarizationPath = ""
+isAudit = True
+
+def init(_notarizationPath, _isAudit) :
+    notarizationPath = _notarizationPath
+    isAudit = _isAudit
+
+def scriptContext(filename) :
+    return filename
+
+def notarizationContextRawfile():
+    return "../.." + notarizationPath + "/../rawFile.csv"
+
+def notarizationContext(filename) :
+    return "../.." + notarizationPath + "/" + filename
+
+def dayContext(_m, _filename) :
+    if (_m >= 0) : raise Exception("No history at J", str(_m))
+    day = json.load(open("../.." + notarizationPath + "/.config.json"))["day"] + _m
+    if (day < 0) : raise Exception("No history at J", str(day))
+    lastNotarization = json.load(open("../.." + notarizationPath + "/../../Day" + str(day) + "/.dayConfig.json"))["lastNotarization"]
+    return "../.." + notarizationPath + "/../../Day" + str(day) + "/Notarization" + str(lastNotarization) + "/" + _filename
+
+def finish(*args) :
+    call(["rm", "-r","../__cache__"])
+    if isAudit == "1" :
+        call(["mkdir","../__cache__"])
+        for i in args :
+            call(["mv",str(i),"../__cache__"])
+        print("Final result at :", "../__cache__")
+
+    else : 
+        for i in args : 
+            call(["mv",str(i),"../.." + notarizationPath])
+        print("Final result at :", "../.." + notarizationPath)
