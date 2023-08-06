a59f8e9bd421b188f15d42355ab9afc62b04a415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:47 +0000

    Add package sphinxcontrib-matlabdomain/0.20.0rc2/sphinxcontrib-matlabdomain-0.20.0rc2.tar.gz

---
+++ packages/sphinxcontrib-matlabdomain/sphinxcontrib-matlabdomain-0.20.0rc2.tar.gz/sphinxcontrib-matlabdomain-0.20.0rc2/tests/roots/test_autodoc/target/+package/ClassBar.m
@@ -0,0 +1,26 @@
+classdef ClassBar < handle
+% The Bar and Foo handler, with a doFoo() method.
+
+    properties
+        bars = 'bars' % Number of bars
+
+        % Number of foos, used by doBar() method
+        foos = 10
+    end
+
+    methods
+        function obj = ClassBar()
+            % Initialize the bars and foos
+            obj.bars = 'bars';
+            obj.foos = 42;
+        end
+
+        function doFoo(obj)
+            % Doing foo
+        end
+
+        function doBar(obj)
+            % Doing bar, not called by ClassBar()
+        end
+    end
+end
