ed2c59c08bfd91536afb24e533b7d77889ceac71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:52 +0000

    Add package wordlist-dp/0.1/wordlist_dp-0.1.tar.gz

---
+++ packages/wordlist-dp/wordlist_dp-0.1.tar.gz/wordlist_dp-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='wordlist_dp',
+    version='0.1',
+    author='Deepak Porwal',
+    author_email='dporwal04@gmail.com',
+    description='A list of words',
+    packages=['wordlist'],
+    url='https://github.com/Deepak-porwal04/wordlist',
+    )
---
+++ packages/wordlist-dp/wordlist_dp-0.1.tar.gz/wordlist_dp-0.1/wordlist/__init__.py
@@ -0,0 +1,13 @@
+words = [
+    'should', 'states', 'before', 'united', 'people', 'during', 'public', 'number', 'system', 'within', 'school', 'little',
+    'report', 'county', 'second', 'office', 'action', 'health', 'amount', 'period', 'having', 'social', 'effect', 'called',
+    'person', 'course', 'change', 'family', 'others', 'figure', 'either', 'though', 'common', 'making', 'result', 'matter',
+    'better', 'always', 'rather', 'policy', 'become', 'things', 'around', 'income', 'nature', 'energy', 'method', 'street',
+    'center', 'market', 'almost', 'church', 'rights', 'member', 'record', 'except', 'single', 'review', 'future', 'higher',
+    'itself', 'reason', 'enough', 'months', 'annual', 'design', 'series', 'ground', 'father', 'growth', 'toward', 'groups',
+    'agency', 'return', 'became', 'direct', 'letter', 'source', 'notice', 'mother', 'volume', 'points', 'senate', 'theory',
+    'motion', 'values', 'twenty', 'supply', 'living', 'answer', 'manner', 'normal', 'charge', 'really', 'length', 'needed',
+    'strong', 'placed', 'passed', 'unless', 'degree', 'august', 'proper', 'issues', 'taking', 'credit', 'across', 'french',
+    'region', 'recent', 'claims', 'entire', 'extent', 'appear', 'safety', 'weight', 'bureau', 'longer', 'former', 'modern'
+]
+
