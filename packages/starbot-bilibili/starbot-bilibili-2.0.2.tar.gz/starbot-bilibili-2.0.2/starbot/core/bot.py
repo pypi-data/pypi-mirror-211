cc69bc2a63b463b9e455ecfe44e212054139396c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:25 +0000

    Add package starbot-bilibili/2.0.2/starbot-bilibili-2.0.2.tar.gz

---
+++ packages/starbot-bilibili/starbot-bilibili-2.0.2.tar.gz/starbot-bilibili-2.0.2/starbot/core/bot.py
@@ -0,0 +1,228 @@
+import asyncio
+import json
+import signal
+import sys
+
+from creart import create
+from graia.ariadne import Ariadne
+from graia.broadcast import Broadcast
+from graia.saya import Saya
+from loguru import logger
+
+from .datasource import DataSource
+from .dynamic import dynamic_spider
+from .server import http_init
+from .user import User, RelationType
+from ..exception import LiveException
+from ..exception.DataSourceException import DataSourceException
+from ..exception.RedisException import RedisException
+from ..utils import redis, config
+from ..utils.network import request, get_session
+from ..utils.utils import split_list, get_credential
+
+
+class StarBot:
+    """
+    StarBot 类
+    """
+    VERSION = "2.0.2"
+    STARBOT_ASCII_LOGO = "\n".join(
+        (
+            r"    _____ _             ____        _   ",
+            r"   / ____| |           |  _ \      | |  ",
+            r"  | (___ | |_ __ _ _ __| |_) | ___ | |_ ",
+            r"   \___ \| __/ _` | '__|  _ < / _ \| __|",
+            r"   ____) | || (_| | |  | |_) | (_) | |_ ",
+            r"  |_____/ \__\__,_|_|  |____/ \___/ \__|",
+            f"      StarBot - (v{VERSION})  2023-05-30",
+            r" Github: https://github.com/Starlwr/StarBot",
+            r"",
+            r"",
+        )
+    )
+
+    def __init__(self, datasource: DataSource):
+        """
+        Args:
+            datasource: 推送配置数据源
+        """
+        self.__datasource = datasource
+        Ariadne.options["StarBotDataSource"] = datasource
+
+    async def __main(self):
+        """
+        StarBot 入口
+        """
+        logger.opt(colors=True, raw=True).info(f"<yellow>{self.STARBOT_ASCII_LOGO}</>")
+        if config.get("CHECK_VERSION"):
+            try:
+                response = await get_session().get("https://mirrors.aliyun.com/pypi/web/json/starbot-bilibili")
+                data = await response.text()
+                latest_version = json.loads(data)["info"]["version"]
+                if latest_version != self.VERSION:
+                    logger.warning(f"检测到 StarBot 新版本 v{latest_version}, 建议升级到最新版本, "
+                                   f"升级内容和版本间不兼容说明请参阅官网或 Github 页的迁移指南")
+            except Exception:
+                logger.error("获取 StarBot 最新版本失败")
+        logger.info("开始启动 StarBot")
+
+        # 从数据源中加载配置
+        try:
+            await self.__datasource.load()
+        except DataSourceException as ex:
+            logger.error(ex.msg)
+            return 1
+
+        if not self.__datasource.bots:
+            logger.error("数据源配置为空, 请先在数据源中配置完毕后再重新运行")
+            return 2
+
+        # 连接 Redis
+        try:
+            await redis.init()
+        except RedisException as ex:
+            logger.error(ex.msg)
+            return 3
+
+        # 通过 UID 列表批量获取信息
+        info = {}
+        info_url = "https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids?uids[]="
+        uids = [str(u) for u in self.__datasource.get_uid_list()]
+        uid_lists = split_list(uids, 100)
+        for lst in uid_lists:
+            info.update(await request("GET", info_url + "&uids[]=".join(lst)))
+        for uid in info:
+            base = info[uid]
+            uid = int(uid)
+            up = self.__datasource.get_up(uid)
+            up.uname = base["uname"]
+            up.room_id = base["room_id"]
+            status = base["live_status"]
+            start_time = base["live_time"]
+            logger.opt(colors=True).info(f"初始化 <cyan>{up.uname}</> "
+                                         f"(UID: <cyan>{up.uid}</>, "
+                                         f"房间号: <cyan>{up.room_id}</>) 的直播间状态: "
+                                         f"{'<green>直播中</>' if status == 1 else '<red>未开播</>'}")
+
+            if status == 1 and start_time != await redis.get_live_start_time(up.room_id):
+                await up.accumulate_and_reset_data()
+
+            await redis.set_live_status(up.room_id, status)
+            await redis.set_live_start_time(up.room_id, start_time)
+
+        # 连接直播间
+        for up in self.__datasource.get_up_list():
+            try:
+                await up.connect()
+                await asyncio.sleep(0.2)
+            except LiveException as ex:
+                logger.error(ex.msg)
+        if len(self.__datasource.get_up_list()) > 0:
+            try:
+                wait_time = config.get("WAIT_FOR_ALL_CONNECTION_TIMEOUT")
+                if wait_time == 0:
+                    wait_time = (len(self.__datasource.get_up_list()) + 5) // 5 * 2
+                await asyncio.wait_for(self.__datasource.wait_for_connects(), wait_time)
+            except asyncio.exceptions.TimeoutError:
+                logger.warning("等待连接所有直播间超时, 请检查是否存在未连接成功的直播间")
+
+        # 启动动态推送模块
+        asyncio.get_event_loop().create_task(dynamic_spider(self.__datasource))
+
+        # 启动 HTTP API 服务
+        if config.get("USE_HTTP_API"):
+            asyncio.get_event_loop().create_task(http_init(self.__datasource))
+
+        # 载入命令
+        logger.info("开始载入命令模块")
+        custom_commands = config.get("CUSTOM_COMMANDS_PACKAGE")
+
+        saya = create(Saya)
+        with saya.module_context():
+            saya.require(f"starbot.commands.builtin")
+            logger.success("内置命令模块载入完毕")
+            if custom_commands:
+                saya.require(custom_commands)
+                logger.success("用户自定义命令模块载入完毕")
+
+        # 自动关注打开了动态推送的未关注 UP 主
+        if config.get("AUTO_FOLLOW_OPENED_DYNAMIC_UPDATE_UP"):
+            if config.get("ACCOUNT_UID") is None:
+                logger.warning("未填写 ACCOUNT_UID 配置项, 无法自动关注打开了动态推送的未关注 UP 主, "
+                               "请使用 config.set('ACCOUNT_UID', 您的UID) 设置, 或手动关注所有需要动态推送的 UP 主, "
+                               "否则无法获取未关注用户的动态更新信息, "
+                               "使用 config.set('AUTO_FOLLOW_OPENED_DYNAMIC_UPDATE_UP', False) 可禁用自动关注功能和此警告")
+            else:
+                uid = int(config.get("ACCOUNT_UID"))
+                me = User(uid, get_credential())
+                follows = set()
+                page = 1
+                while True:
+                    res = await me.get_followings(page)
+                    follows = follows.union(set(map(lambda x: x["mid"], res["list"])))
+                    if len(res["list"]) < 20:
+                        break
+                    page += 1
+
+                need_follow_uids = set()
+                for up in self.__datasource.get_up_list():
+                    if any(map(lambda d: d.enabled, map(lambda t: t.dynamic_update, up.targets))):
+                        need_follow_uids.add(up.uid)
+                need_follow_uids.difference_update(follows)
+
+                async def follow_task(uid_set):
+                    logger.info(f"检测到 {len(uid_set)} 个打开了动态推送但未关注的 UP 主, 启动自动关注任务")
+                    for u in uid_set:
+                        follow_user = User(u, get_credential())
+                        await follow_user.modify_relation(RelationType.SUBSCRIBE)
+                        await asyncio.sleep(10)
+                    logger.success(f"已成功关注了 {len(uid_set)} 个 UP 主")
+
+                if len(need_follow_uids) > 0:
+                    asyncio.create_task(follow_task(need_follow_uids))
+
+        # 启动消息推送模块
+        Ariadne.options["default_account"] = self.__datasource.bots[0].qq
+
+        logger.info("开始运行 Ariadne 消息推送模块")
+
+        try:
+            Ariadne.launch_blocking()
+        except RuntimeError as ex:
+            if "This event loop is already running" in str(ex):
+                pass
+            else:
+                logger.error(ex)
+                return 4
+
+    def run(self):
+        """
+        启动 StarBot
+        """
+
+        logger_format = (
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{line}</cyan> | "
+            "<level>{message}</level>"
+        )
+        logger.remove()
+        logger.add(sys.stderr, format=logger_format, level="INFO")
+        if config.get("LOG_TO_FILE"):
+            logger.add("logs/{time:YYYY-MM}/starbot-{time:YYYY-MM-DD}.log", level="INFO", rotation="00:00",
+                       format="{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{line} | {message}")
+        logger.disable("graia.ariadne.model")
+        logger.disable("graia.ariadne.service")
+        logger.disable("graia.saya")
+        logger.disable("launart")
+
+        bcc = create(Broadcast)
+        loop = bcc.loop
+        if loop.run_until_complete(self.__main()):
+            return
+        try:
+            loop.add_signal_handler(signal.SIGINT, lambda: loop.stop())
+        except NotImplementedError:
+            pass
+        loop.run_forever()
+        loop.close()
---
+++ packages/starbot-bilibili/starbot-bilibili-2.0.2.tar.gz/starbot-bilibili-2.0.2/starbot/utils/config.py
@@ -0,0 +1,329 @@
+from typing import Any
+
+SIMPLE_CONFIG = {
+    # 是否检测最新 StarBot 版本
+    "CHECK_VERSION": True,
+
+    # Redis 连接配置 ( 必须 )
+    # Redis 地址
+    "REDIS_HOST": "localhost",
+    # Redis 端口
+    "REDIS_PORT": 6379,
+    # Redis 数据库号
+    "REDIS_DB": 0,
+    #  Redis 用户名
+    "REDIS_USERNAME": None,
+    #  Redis 密码
+    "REDIS_PASSWORD": None,
+
+    # MySQL 数据源连接配置 ( 可选 )
+    # MySQL 地址
+    "MYSQL_HOST": "localhost",
+    # MySQL 端口
+    "MYSQL_PORT": 3306,
+    # MySQL 用户名
+    "MYSQL_USERNAME": "root",
+    # MySQL 密码
+    "MYSQL_PASSWORD": "123456",
+    # MySQL 数据库名
+    "MYSQL_DB": "starbot",
+
+    # Mirai HTTP 及 Websocket 端口
+    "MIRAI_PORT": 7827,
+
+    # 登录 B 站账号所需 Cookie 数据 ( 不登录账号将有部分功能不可用 ) 各字段获取方式查看：https://bot.starlwr.com/depoly/document
+    "SESSDATA": None,
+    "BILI_JCT": None,
+    "BUVID3": None,
+
+    # 以上 Cookie 数据所对应的 B 站账号 UID，自动关注打开了动态推送但没有关注的用户时使用
+    "ACCOUNT_UID": None,
+    # 是否自动关注打开了动态推送但没有关注的用户，推荐打开，否则无法获取未关注用户的动态更新信息
+    "AUTO_FOLLOW_OPENED_DYNAMIC_UPDATE_UP": True,
+
+    # 是否将日志同时输出到文件中
+    "LOG_TO_FILE": False,
+
+    # 成功连接所有主播直播间的最大等待时长，可使得日志输出顺序更加易读，一般无需修改此处，设置为 0 会自适应计算，单位：秒
+    "WAIT_FOR_ALL_CONNECTION_TIMEOUT": 0,
+
+    # 是否自动判断仅连接必要的直播间，即当某直播间的开播、下播、直播报告开关均未开启时，自动跳过连接直播间，以节省性能
+    "ONLY_CONNECT_NECESSARY_ROOM": False,
+    # 是否自动判断仅处理必要的直播事件，例如当某直播间的下播推送和直播报告中均不包含弹幕相关功能，则不再处理此直播间的弹幕事件，以节省性能
+    "ONLY_HANDLE_NECESSARY_EVENT": False,
+
+    # 主播下播后再开播视为主播网络波动断线重连的时间间隔，在此时间内重新开播不会重新计算本次直播数据，且不重复 @全体成员，单位：秒
+    "UP_DISCONNECT_CONNECT_INTERVAL": 120,
+    # 视为主播网络波动断线重连时，需发送的额外提示消息
+    "UP_DISCONNECT_CONNECT_MESSAGE": "检测到下播后短时间内重新开播,可能是由于主播网络波动引起,本次开播不再重复通知",
+
+    # 当日 @全体成员 达到上限后，需发送的额外提示消息
+    "AT_ALL_LIMITED_MESSAGE": "今日@全体成员次数已达上限,将尝试@指定成员,请需要接收单独@消息的群员使用\"开播@我\"命令进行订阅",
+    # 设置了 @全体成员 但没有管理员权限时，需发送的额外提示消息
+    "NO_PERMISSION_MESSAGE": "已设置@全体成员但没有管理员权限,将尝试@指定成员,请需要接收单独@消息的群员使用\"开播@我\"命令进行订阅",
+
+    # 动态推送抓取频率和视为新动态的时间间隔，单位：秒
+    "DYNAMIC_INTERVAL": 10,
+
+    # 绘图器普通字体路径，如需自定义，请将字体放入 resource 文件夹中后，修改配置中的 normal.ttf 为您的字体文件名
+    "PAINTER_NORMAL_FONT": "normal.ttf",
+    # 绘图器粗体字体路径，如需自定义，请将字体放入 resource 文件夹中后，修改配置中的 bold.ttf 为您的字体文件名
+    "PAINTER_BOLD_FONT": "bold.ttf",
+    # 绘图器自适应不覆盖已绘制图形的间距，单位：像素
+    "PAINTER_AUTO_SIZE_BY_LIMIT_MARGIN": 10,
+
+    # 弹幕词云字体路径，如需自定义，请将字体放入 resource 文件夹中后，修改配置中的 cloud.ttf 为您的字体文件名
+    "DANMU_CLOUD_FONT": "cloud.ttf",
+    # 弹幕词云图片背景色
+    "DANMU_CLOUD_BACKGROUND_COLOR": "white",
+    # 弹幕词云最大字号
+    "DANMU_CLOUD_MAX_FONT_SIZE": 200,
+    # 弹幕词云最多词数
+    "DANMU_CLOUD_MAX_WORDS": 80,
+    # 弹幕词云停用词路径，存储时每行一个停用词，以纯文本方式存储，可过滤这些词使其不出现在词云中
+    "DANMU_CLOUD_STOP_WORDS": "",
+    # 弹幕词云自定义词典路径，存储时每行一个词，以纯文本方式存储，在对弹幕进行切词时，词典中的词不会被切分开
+    "DANMU_CLOUD_DICT": "",
+
+    # 需加载的用户自定义命令包
+    "CUSTOM_COMMANDS_PACKAGE": None,
+
+    # Bot 主人 QQ，用于接收部分 Bot 异常通知等
+    "MASTER_QQ": None,
+
+    # HTTP 代理
+    "PROXY": "",
+
+    # 是否使用 HTTP API 推送
+    "USE_HTTP_API": False,
+    # HTTP API 端口
+    "HTTP_API_PORT": 8088,
+    # 默认 HTTP API 推送 Bot QQ，多 Bot 推送时必填
+    "HTTP_API_DEAFULT_BOT": None,
+    
+    # 命令触发前缀
+    "COMMAND_PREFIX": "",
+    # 每个群开播 @ 我命令人数上限，单次 @ 人数过多容易被风控，不推荐修改
+    "COMMAND_LIVE_ON_AT_ME_LIMIT": 20,
+    # 每个群动态 @ 我命令人数上限，单次 @ 人数过多容易被风控，不推荐修改
+    "COMMAND_DYNAMIC_AT_ME_LIMIT": 20,
+
+    # 是否启用风控消息补发，暂未实现
+    "BAN_RESEND": False,
+    # 风控发送失败消息滞留时间上限，消息因风控滞留超出此时长不会进行补发，0 为无限制，单位：秒，暂未实现
+    "RESEND_TIME_LIMIT": 0,
+    # 是否补发开播推送、下播推送、直播报告、动态推送中的 @全体成员 和 @群成员 消息，可能造成不必要的打扰，不推荐开启，暂未实现
+    "RESEND_AT_MESSAGE": False
+}
+
+FULL_CONFIG = {
+    # 是否检测最新 StarBot 版本
+    "CHECK_VERSION": True,
+
+    # Redis 连接配置 ( 必须 )
+    # Redis 地址
+    "REDIS_HOST": "localhost",
+    # Redis 端口
+    "REDIS_PORT": 6379,
+    # Redis 数据库号
+    "REDIS_DB": 0,
+    #  Redis 用户名
+    "REDIS_USERNAME": None,
+    #  Redis 密码
+    "REDIS_PASSWORD": None,
+
+    # MySQL 数据源连接配置 ( 可选 )
+    # MySQL 地址
+    "MYSQL_HOST": "localhost",
+    # MySQL 端口
+    "MYSQL_PORT": 3306,
+    # MySQL 用户名
+    "MYSQL_USERNAME": "root",
+    # MySQL 密码
+    "MYSQL_PASSWORD": "123456",
+    # MySQL 数据库名
+    "MYSQL_DB": "starbot",
+
+    # Mirai HTTP 及 Websocket 端口
+    "MIRAI_PORT": 7827,
+
+    # 登录 B 站账号所需 Cookie 数据 ( 不登录账号将有部分功能不可用 ) 各字段获取方式查看：https://bot.starlwr.com/depoly/document
+    "SESSDATA": None,
+    "BILI_JCT": None,
+    "BUVID3": None,
+
+    # 以上 Cookie 数据所对应的 B 站账号 UID，自动关注打开了动态推送但没有关注的用户时使用
+    "ACCOUNT_UID": None,
+    # 是否自动关注打开了动态推送但没有关注的用户，推荐打开，否则无法获取未关注用户的动态更新信息
+    "AUTO_FOLLOW_OPENED_DYNAMIC_UPDATE_UP": True,
+
+    # 是否将日志同时输出到文件中
+    "LOG_TO_FILE": False,
+
+    # 成功连接所有主播直播间的最大等待时长，可使得日志输出顺序更加易读，一般无需修改此处，设置为 0 会自适应计算，单位：秒
+    "WAIT_FOR_ALL_CONNECTION_TIMEOUT": 0,
+
+    # 是否自动判断仅连接必要的直播间，即当某直播间的开播、下播、直播报告开关均未开启时，自动跳过连接直播间，以节省性能
+    "ONLY_CONNECT_NECESSARY_ROOM": False,
+    # 是否自动判断仅处理必要的直播事件，例如当某直播间的下播推送和直播报告中均不包含弹幕相关功能，则不再处理此直播间的弹幕事件，以节省性能
+    "ONLY_HANDLE_NECESSARY_EVENT": False,
+
+    # 主播下播后再开播视为主播网络波动断线重连的时间间隔，在此时间内重新开播不会重新计算本次直播数据，且不重复 @全体成员，单位：秒
+    "UP_DISCONNECT_CONNECT_INTERVAL": 120,
+    # 视为主播网络波动断线重连时，需发送的额外提示消息
+    "UP_DISCONNECT_CONNECT_MESSAGE": "检测到下播后短时间内重新开播,可能是由于主播网络波动引起,本次开播不再重复通知",
+
+    # 当日 @全体成员 达到上限后，需发送的额外提示消息
+    "AT_ALL_LIMITED_MESSAGE": "今日@全体成员次数已达上限,将尝试@指定成员,请需要接收单独@消息的群员使用\"开播@我\"命令进行订阅",
+    # 设置了 @全体成员 但没有管理员权限时，需发送的额外提示消息
+    "NO_PERMISSION_MESSAGE": "已设置@全体成员但没有管理员权限,将尝试@指定成员,请需要接收单独@消息的群员使用\"开播@我\"命令进行订阅",
+
+    # 动态推送抓取频率和视为新动态的时间间隔，单位：秒
+    "DYNAMIC_INTERVAL": 10,
+
+    # 绘图器普通字体路径，如需自定义，请将字体放入 resource 文件夹中后，修改配置中的 normal.ttf 为您的字体文件名
+    "PAINTER_NORMAL_FONT": "normal.ttf",
+    # 绘图器粗体字体路径，如需自定义，请将字体放入 resource 文件夹中后，修改配置中的 bold.ttf 为您的字体文件名
+    "PAINTER_BOLD_FONT": "bold.ttf",
+    # 绘图器自适应不覆盖已绘制图形的间距，单位：像素
+    "PAINTER_AUTO_SIZE_BY_LIMIT_MARGIN": 10,
+
+    # 弹幕词云字体路径，如需自定义，请将字体放入 resource 文件夹中后，修改配置中的 cloud.ttf 为您的字体文件名
+    "DANMU_CLOUD_FONT": "cloud.ttf",
+    # 弹幕词云图片背景色
+    "DANMU_CLOUD_BACKGROUND_COLOR": "white",
+    # 弹幕词云最大字号
+    "DANMU_CLOUD_MAX_FONT_SIZE": 200,
+    # 弹幕词云最多词数
+    "DANMU_CLOUD_MAX_WORDS": 80,
+    # 弹幕词云停用词路径，存储时每行一个停用词，以纯文本方式存储，可过滤这些词使其不出现在词云中
+    "DANMU_CLOUD_STOP_WORDS": "",
+    # 弹幕词云自定义词典路径，存储时每行一个词，以纯文本方式存储，在对弹幕进行切词时，词典中的词不会被切分开
+    "DANMU_CLOUD_DICT": "",
+
+    # 需加载的用户自定义命令包
+    "CUSTOM_COMMANDS_PACKAGE": None,
+
+    # Bot 主人 QQ，用于接收 Bot 异常通知等
+    "MASTER_QQ": None,
+
+    # HTTP 代理
+    "PROXY": "",
+
+    # 是否使用 HTTP API 推送
+    "USE_HTTP_API": False,
+    # HTTP API 端口
+    "HTTP_API_PORT": 8088,
+    # 默认 HTTP API 推送 Bot QQ，多 Bot 推送时必填
+    "HTTP_API_DEAFULT_BOT": None,
+
+    # 命令触发前缀
+    "COMMAND_PREFIX": "",
+    # 每个群开播 @ 我命令人数上限，单次 @ 人数过多容易被风控，不推荐修改
+    "COMMAND_LIVE_ON_AT_ME_LIMIT": 20,
+    # 每个群动态 @ 我命令人数上限，单次 @ 人数过多容易被风控，不推荐修改
+    "COMMAND_DYNAMIC_AT_ME_LIMIT": 20,
+
+    # 是否启用风控消息补发，暂未实现
+    "BAN_RESEND": True,
+    # 风控发送失败消息滞留时间上限，消息因风控滞留超出此时长不会进行补发，0 为无限制，单位：秒，暂未实现
+    "RESEND_TIME_LIMIT": 0,
+    # 是否补发开播推送、下播推送、直播报告、动态推送中的 @全体成员 和 @群成员 消息，可能造成不必要的打扰，不推荐开启，暂未实现
+    "RESEND_AT_MESSAGE": False
+}
+
+use_config = SIMPLE_CONFIG
+
+user_config = {}
+
+
+def use(**config: Any):
+    """
+    使用用户自定义配置，可以选择自带配置后，传入部分自定义配置覆盖原有配置
+
+    Args:
+        config: 自定义配置字典
+    """
+    user_config.update(config)
+
+
+def use_simple_config():
+    """
+    使用最简配置，默认配置如下：
+
+    自动检测最新版本
+    使用 Redis 默认连接配置 (host: "localhost", port: 6379, db: 0, username: "", password: "")
+    使用 MySQL 默认连接配置 (host: "localhost", port: 3306, db: "starbot", username: "root", password: "123456")
+    Mirai 连接端口 7827
+    未设置登录 B 站账号所需 Cookie 数据
+    不启用节省性能优化
+    主播下播后 2 分钟内开播视为主播网络波动，并发送提示消息 "检测到下播后短时间内重新开播,可能是由于主播网络波动引起,本次开播不再重复通知"
+    未设置 Bot 主人 QQ
+    不使用 HTTP 代理
+    不开启 HTTP API 推送
+    消息发送间隔 0.5 秒
+    无命令触发前缀
+    不开启风控消息补发
+    """
+    global use_config
+    use_config = SIMPLE_CONFIG
+
+
+def use_full_config():
+    """
+    使用推荐配置，默认配置如下：
+
+    自动检测最新版本
+    使用 Redis 默认连接配置 (host: "localhost", port: 6379, db: 0, username: "", password: "")
+    使用 MySQL 默认连接配置 (host: "localhost", port: 3306, db: "starbot", username: "root", password: "123456")
+    Mirai 连接端口 7827
+    未设置登录 B 站账号所需 Cookie 数据
+    不启用节省性能优化
+    主播下播后 2 分钟内开播视为主播网络波动，并发送提示消息 "检测到下播后短时间内重新开播,可能是由于主播网络波动引起,本次开播不再重复通知"
+    未设置 Bot 主人 QQ
+    不使用 HTTP 代理
+    开启 HTTP API 推送 (port: 8088)
+    消息发送间隔 0.5 秒
+    无命令触发前缀
+    开启风控消息补发 仅补发推送消息
+    """
+    global use_config
+    use_config = FULL_CONFIG
+
+
+def set_credential(sessdata: str, bili_jct: str, buvid3: str):
+    """
+    设置登录 B 站账号所需 Cookie 数据，各字段获取方式查看：https://bili.moyu.moe/#/get-credential.md
+
+    Args:
+        sessdata: SESSDATA
+        bili_jct: BILI_JCT
+        buvid3: BUVID3
+    """
+    set("SESSDATA", sessdata)
+    set("BILI_JCT", bili_jct)
+    set("BUVID3", buvid3)
+
+
+def get(key: str) -> Any:
+    """
+    获取配置项的值
+
+    Args:
+        key: 配置项
+
+    Returns:
+        配置项的值
+    """
+    return user_config[key] if key in user_config else use_config[key]
+
+
+def set(key: str, value: Any):
+    """
+    手动设置配置项
+
+    Args:
+        key: 配置项
+        value: 值
+    """
+    user_config[key] = value
