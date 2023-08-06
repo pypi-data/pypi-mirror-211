f32b4624402ebd2a26c89dbd7f71955b0c36205b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:12 +0000

    Add package cast-vue/0.0.4/cast_vue-0.0.4-py3-none-any.whl

---
+++ packages/cast-vue/cast_vue-0.0.4-py3-none-any.whl/cast_vue/static/src/js/cast_vue/components/PostDetail.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <p v-if="isLoading">Loading data...</p>
+    <div v-else>
+      <router-link to="/">Back to Blog</router-link>
+      <post-item :post="post" :detail="true" @comment-posted="handleCommentPosted"></post-item>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import config from '../config';
+import { useRoute } from 'vue-router';
+import PostItem from './PostItem.vue';
+import { useDataStore } from '../stores/dataStore';
+import { Post, PostsFromApi } from './types';
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: "PostDetail",
+  components: {
+    PostItem,
+  },
+  methods: {
+    async handleCommentPosted() {
+      console.log("handleCommentPosted in PostDetail");
+      await this.fetchPostFromAPI(true);
+    }
+  },
+  setup() {
+    const route = useRoute();
+    const dataStore = useDataStore();
+    const wagtailApiUrl = new URL(config.postListUrl.toString()); // make a copy to not modify the original url
+
+    const isLoading = ref(true);
+    const post = ref({} as Post);
+    const visibleDateStr = ref("");
+
+    const fetchPostFromAPI = async (invalidateCache: boolean = false) => {
+      const postSlug = route.params.slug as string;
+      // FIXME maybe use clean detail url? But then we need to have
+      // the page id instead of the slug and and either modify the API
+      // to accept slugs or do a second request to get the page id. :/
+      const postDetailUrl = new URL(wagtailApiUrl.href);
+      postDetailUrl.searchParams.set("type", "cast.Post");
+      postDetailUrl.searchParams.set("slug", postSlug);
+      postDetailUrl.searchParams.set("fields", "html_detail,comments,comments_security_data,comments_are_enabled");
+
+      try {
+        const posts = await dataStore.fetchJson(postDetailUrl, invalidateCache) as unknown as PostsFromApi;
+        post.value = posts.items[0];
+      } catch (error) {
+        console.error('Error fetching data from API: ', error);
+      } finally {
+        isLoading.value = false;
+      }
+    }
+
+    onMounted(fetchPostFromAPI);
+    return { dataStore, isLoading, post, visibleDate: visibleDateStr, fetchPostFromAPI };
+  },
+}
+</script>
---
+++ packages/cast-vue/cast_vue-0.0.4-py3-none-any.whl/cast_vue/static/src/js/cast_vue/stores/dataStore.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia';
+
+// Define the store type
+interface DataStoreState {
+  jsonCache: Record<string, any>;
+}
+
+// Define and export the store
+export const useDataStore = defineStore({
+  id: "main",
+  state: (): DataStoreState => ({
+    jsonCache: {},
+  }),
+  actions: {
+    async fetchJson(url: URL, invalidateCache: boolean = false): Promise<Record<string, unknown>> {
+      // Check if the URL is in the cache.
+      const urlStr = url.toString();
+      // console.log("fetchJson: ", urlStr)
+      if (this.jsonCache[urlStr] && !invalidateCache) {
+        return this.jsonCache[urlStr];
+      }
+
+      // Fetch the JSON if it's not in the cache.
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        // Store the result in the cache.
+        this.jsonCache[urlStr] = data;
+        return data;
+      } catch (error) {
+        console.error("Failed to fetch JSON", error);
+        throw error;
+      }
+    },
+  },
+});
