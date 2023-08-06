dbfeb3179b6b7d0e657cd8f66c6e0fa1e76bf8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:22 +0000

    Add package rach-pack/0.4.0/rach_pack-0.4.0.tar.gz

---
+++ packages/rach-pack/rach_pack-0.4.0.tar.gz/rach_pack-0.4.0/rach_pack/main.py
@@ -0,0 +1,4 @@
+
+def test_package_rachit():
+    print("Hello World")
+    
---
+++ packages/rach-pack/rach_pack-0.4.0.tar.gz/rach_pack-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='rach_pack',
+    version='0.4.0',
+    description='A sample Python project for rach pack',
+    author='Rac',
+    author_email='rachitmahajan6399@gmail.com',
+    url='https://github.com/r-mhjn/QuizCreator',
+    packages=['rach_pack'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
