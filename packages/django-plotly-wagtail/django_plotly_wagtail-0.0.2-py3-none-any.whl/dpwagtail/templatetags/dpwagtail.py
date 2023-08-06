17eb48aafebfe364a1e890ef137331402c5c2142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:50 +0000

    Add package django-plotly-wagtail/0.0.2/django_plotly_wagtail-0.0.2-py3-none-any.whl

---
+++ packages/django-plotly-wagtail/django_plotly_wagtail-0.0.2-py3-none-any.whl/dpwagtail/templatetags/dpwagtail.py
@@ -0,0 +1,23 @@
+"""
+Tags for rendering dash apps
+"""
+
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("dpwagtail/tags/dpw_instance.html", takes_context=True)
+def dpd_instance(context, dash_instance):
+    request = context.get('request')
+    app_args = dash_instance.resolve_instance(request)
+    name, ivalue = app_args
+
+    if name == 'da':
+        is_dash_app = True
+    if name == 'name':
+        is_stateless_app = True
+
+    return locals()
