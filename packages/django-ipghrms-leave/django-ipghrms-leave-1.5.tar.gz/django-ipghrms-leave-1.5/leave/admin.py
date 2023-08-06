e97b4f6012eee07cc6e9be77b0d73a3c91c427c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:35 +0000

    Add package django-ipghrms-leave/1.5/django-ipghrms-leave-1.5.tar.gz

---
+++ packages/django-ipghrms-leave/django-ipghrms-leave-1.5.tar.gz/django-ipghrms-leave-1.5/leave/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(LeaveType)
+admin.site.register(Leave)
+admin.site.register(LeaveCount)
+admin.site.register(LeaveDelegate)
+admin.site.register(LeaveUnit)
+admin.site.register(LeaveHR)
+admin.site.register(LeaveDE)
+admin.site.register(LeaveDep)
+admin.site.register(Year)
+admin.site.register(Month)
+admin.site.register(LeavePeriod)
