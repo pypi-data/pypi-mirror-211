c2a8fcc281246438ec65a44a9a82e35c8d89c364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:31 +0000

    Add package databricks-common/0.1.8/databricks_common-0.1.8.tar.gz

---
+++ packages/databricks-common/databricks_common-0.1.8.tar.gz/databricks_common-0.1.8/databricks_common/utils/get_spark.py
@@ -0,0 +1,10 @@
+try:
+    from databricks.connect import DatabricksSession
+    from databricks.sdk.core import Config
+
+    config = Config(profile="aws-e2-demo")
+    spark = DatabricksSession.builder.sdkConfig(config).getOrCreate()
+except Exception as e:
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder.getOrCreate()
