060e62cf90dcd228f14b1d2d1cabc759a1eecc91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:49 +0000

    Add package pyclam/0.8.0/pyclam-0.8.0.tar.gz

---
+++ packages/pyclam/pyclam-0.8.0.tar.gz/pyclam-0.8.0/pyclam/__init__.py
@@ -0,0 +1,6 @@
+from . import anomaly_detection as anomaly_detection
+from . import classification as classification
+from . import core as core
+from . import search as search
+from . import utils as utils
+from .core import *
