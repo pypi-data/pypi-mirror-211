97ed381d7bb36c05243c97795ac4537c580ea34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:26 +0000

    Add package openi-beta/0.0.3/openi-beta-0.0.3.tar.gz

---
+++ packages/openi-beta/openi-beta-0.0.3.tar.gz/openi-beta-0.0.3/setup.py
@@ -0,0 +1,32 @@
+# python setup.py sdist bdist_wheel  
+# twine upload dist/*    
+
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='openi-beta',
+    version='0.0.3',
+    description='A test packages for openi pypi',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://openi.pcl.ac.cn/OpenIOSSG/openi-pypi',
+    author='chenzh05',
+    author_email='chenzh.ds@outlook.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['emoji>=2.4.0', 'requests>=2.30.0','tqdm>=4.65.0'],
+    python_requires='>=3.6',
+)
