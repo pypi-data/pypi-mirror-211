e11fd6045e35dec34909d4bd6f916ccfd675c642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:42 +0000

    Add package quick-batch/0.1.7/quick_batch-0.1.7.tar.gz

---
+++ packages/quick-batch/quick_batch-0.1.7.tar.gz/quick_batch-0.1.7/quick_batch/main.py
@@ -0,0 +1,23 @@
+import sys
+import fire
+import runner
+import manual_scaler
+
+
+def main(config=None):
+    # check config - used as script fire will not be used
+    if not config:
+        # check argument provided - config or num_processors
+        if sys.argv[1] == 'config':
+            config = sys.argv[2]
+            runner.run(config)
+        elif sys.argv[1] == 'scale':
+            num_processors = int(sys.argv[2])
+            manual_scaler.scaler(num_processors)
+
+    # check config - used as script fire will be used
+    runner.run(config)
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
