2abb9b9022d9adc9b86a027b1cc90bc33c26fec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:05 +0000

    Add package mass-driver/0.13.2/mass_driver-0.13.2-py3-none-any.whl

---
+++ packages/mass-driver/mass_driver-0.13.2-py3-none-any.whl/mass_driver/commands.py
@@ -0,0 +1,165 @@
+"""The different main commands of the mass-driver tool"""
+
+import sys
+from argparse import Namespace
+from pathlib import Path
+
+from pydantic import ValidationError
+
+from mass_driver.discovery import (
+    discover_drivers,
+    discover_forges,
+    get_driver_entrypoint,
+    get_forge_entrypoint,
+    get_scanners,
+)
+from mass_driver.forge_run import main as forge_main
+from mass_driver.forge_run import pause_until_ok
+from mass_driver.migration_run import main as migration_main
+from mass_driver.models.activity import ActivityLoaded, ActivityOutcome
+from mass_driver.scan_run import scan_main
+
+
+def drivers_command(args: Namespace):
+    """Process the CLI for 'Drivers' subcommand"""
+    if args.info:
+        target_driver = args.info
+        try:
+            driver = get_driver_entrypoint(target_driver)
+            print(
+                f"Plugin name: {driver.name}; Import path: {driver.module}; Class: {driver.attr}"
+            )
+            print(driver.load().__doc__)
+            return
+        except ImportError as e:
+            print(str(e), file=sys.stderr)
+            print("Try `mass driver drivers --list`", file=sys.stderr)
+            return
+    # if args.list:  # Implicit
+    drivers = discover_drivers()
+    print("Available drivers:")
+    for driver in drivers:
+        print(f"{driver.name}")
+    return True
+
+
+def forges_command(args: Namespace):
+    """Process the CLI for 'Forges' subcommand"""
+    if args.info:
+        target_forge = args.info
+        try:
+            forge = get_forge_entrypoint(target_forge)
+            print(
+                f"Plugin name: {forge.name}; Import path: {forge.module}; Class: {forge.attr}"
+            )
+            print(forge.load().__doc__)
+            return
+        except ImportError as e:
+            print(str(e), file=sys.stderr)
+            print("Try `mass driver forges --list`", file=sys.stderr)
+            return
+    # if args.list:  # Implicit
+    forges = discover_forges()
+    print("Available forges:")
+    for forge in forges:
+        print(f"{forge.name}")
+    return True
+
+
+def run_command(args: Namespace) -> ActivityOutcome:
+    """Process the CLI for 'run'"""
+    print("Run mode!")
+    repos = read_repolist(args)
+    activity_str = args.activity_file.read()
+    try:
+        activity = ActivityLoaded.from_config(activity_str)
+    except ValidationError as e:
+        forge_config_error_exit(e)
+    if activity.migration is None:
+        print("No migration section: skipping migration")
+        migration_result = ActivityOutcome(
+            repos_input=repos,
+            local_repos_path={r: Path(r) for r in repos},
+        )
+    else:
+        migration_result = migration_main(
+            activity.migration,
+            repos,
+            not args.no_cache,
+        )
+    print("Migration complete!")
+    if activity.forge is None:
+        # Nothing else to do, just print completion and exit
+        print("No Forge available: end")
+        maybe_save_outcome(args, migration_result)
+        return migration_result
+    # Now guaranteed to have a Forge: pause + forge
+    if not args.no_pause:
+        print("Review the commits now.")
+        pause_until_ok("Type y/yes/continue to run the Forge\n")
+    forge_result = forge_main(activity.forge, migration_result)
+    maybe_save_outcome(args, forge_result)
+    return forge_result
+
+
+def scanners_command(args: Namespace):
+    """Process the CLI for 'scan'"""
+    print("Available scanners:")
+    scanners = get_scanners()
+    for scanner in scanners:
+        print(scanner.name)
+    exit(0)
+
+
+def scan_command(args: Namespace) -> ActivityOutcome:
+    """Process the CLI for 'scan'"""
+    print("Scan mode!")
+    repos = read_repolist(args)
+    activity_str = args.activity_file.read()
+    activity = ActivityLoaded.from_config(activity_str)
+    result = scan_main(
+        activity.scan,
+        repo_urls=repos,
+        cache=not args.no_cache,
+    )
+    maybe_save_outcome(args, result)
+    return result
+
+
+def forge_config_error_exit(e: ValidationError):
+    """Exit in case of bad forge config"""
+    for error in e.errors():
+        if error["type"] == "value_error.missing":
+            envvars = ["FORGE_" + var.upper() for var in error["loc"]]
+            print(
+                f"Missing Forge config: Set envvar(s) {', '.join(envvars)}",
+                file=sys.stderr,
+            )
+        else:
+            print(
+                f"Forge config validation error: {error}",
+                file=sys.stderr,
+            )
+    raise e  # exit code = Simulate the argparse behaviour of exiting on bad args
+
+
+def read_repolist(args) -> list[str]:
+    """Read the repo-list or repo-path arg"""
+    repos = args.repo_path
+    if args.repo_filelist:
+        repos = args.repo_filelist.read().strip().split("\n")
+    return repos
+
+
+def maybe_save_outcome(args: Namespace, outcome: ActivityOutcome):
+    """Consider saving the outcome"""
+    if not args.json_outfile:
+        return
+    save_outcome(outcome, args.json_outfile)
+    print("Saved outcome to given JSON file")
+
+
+def save_outcome(outcome: ActivityOutcome, out_file):
+    """Save the output to given JSON file handle"""
+    out_file.write(outcome.json(indent=2))
+    out_file.write("\n")
---
+++ packages/mass-driver/mass_driver-0.13.2-py3-none-any.whl/mass_driver/discovery.py
@@ -0,0 +1,67 @@
+"""Driver discovery system via plugins"""
+
+import sys
+
+from mass_driver.models.forge import Forge
+from mass_driver.models.patchdriver import PatchDriver
+from mass_driver.models.scan import Scanner
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import EntryPoint, EntryPoints, entry_points
+else:
+    from importlib.metadata import EntryPoint, EntryPoints, entry_points
+
+ENTRYPOINT = "massdriver"
+"""The entrypoint we discover all types of plugins from"""
+DRIVER_ENTRYPOINT = f"{ENTRYPOINT}.drivers"
+"""The specific entrypoint for drivers discovery"""
+FORGE_ENTRYPOINT = f"{ENTRYPOINT}.forges"
+"""The specific entrypoint for Forge discovery"""
+SCANNER_ENTRYPOINT = f"{ENTRYPOINT}.scanners"
+"""The specific entrypoint for Scanner discovery"""
+
+
+def discover_drivers() -> EntryPoints:
+    """Discover all Drivers via plugin system"""
+    return entry_points(group=DRIVER_ENTRYPOINT)
+
+
+def get_driver_entrypoint(driver_name: str) -> EntryPoint:
+    """Fetch the given driver Entrypoint, by name"""
+    drivers = discover_drivers()
+    if driver_name not in drivers.names:
+        raise ImportError(f"Driver '{driver_name}' not found in '{DRIVER_ENTRYPOINT}'")
+    (driver,) = drivers.select(name=driver_name)
+    return driver
+
+
+def get_driver(driver_name: str) -> type[PatchDriver]:
+    """Get the given driver Class, by entrypoint name"""
+    driver = get_driver_entrypoint(driver_name)
+    return driver.load()
+
+
+def discover_forges() -> EntryPoints:
+    """Discover all Forges via plugin system"""
+    return entry_points(group=FORGE_ENTRYPOINT)
+
+
+def get_forge_entrypoint(forge_name: str) -> EntryPoint:
+    """Fetch the given forge Entrypoint, by name"""
+    forges = discover_forges()
+    if forge_name not in forges.names:
+        raise ImportError(f"Forge '{forge_name}' not found in '{FORGE_ENTRYPOINT}'")
+    (forge,) = forges.select(name=forge_name)
+    return forge
+
+
+def get_forge(forge_name: str) -> type[Forge]:
+    """Get the given forge Class, by entrypoint name"""
+    forge = get_forge_entrypoint(forge_name)
+    return forge.load()
+
+
+def get_scanners() -> list[Scanner]:
+    """Grab all discovered scanners"""
+    scanners = entry_points(group=SCANNER_ENTRYPOINT)
+    return [Scanner(name=s.name, func=s.load()) for s in scanners]
