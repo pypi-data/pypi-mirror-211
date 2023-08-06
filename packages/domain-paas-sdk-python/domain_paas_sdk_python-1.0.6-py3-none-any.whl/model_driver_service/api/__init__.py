330e3ea27ea6dc91ad35b452f223dab9fa0cc818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:30 +0000

    Add package domain-paas-sdk-python/1.0.6/domain_paas_sdk_python-1.0.6-py3-none-any.whl

---
+++ packages/domain-paas-sdk-python/domain_paas_sdk_python-1.0.6-py3-none-any.whl/model_driver_service/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from model_driver_service.api.health_api import HealthApi
+from model_driver_service.api.model_run_api import ModelRunApi
