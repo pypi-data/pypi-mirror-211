54b605b02652cb0317596478707b7cb603c06414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:54 +0000

    Add package deci-platform-client/5.0.0/deci_platform_client-5.0.0-py3-none-any.whl

---
+++ packages/deci-platform-client/deci_platform_client-5.0.0-py3-none-any.whl/tests/mocks/__init__.py
@@ -0,0 +1,23 @@
+from .accuracy_metric import AccuracyMetricFactory
+from .add_model_response import APIResponseAddModelResponseFactory
+from .gru_model_response import APIResponseGruModelResponseFactory
+from .model import Model
+from .model_benchmark_result_metadata import ModelBenchmarkResultMetadataFactory
+from .model_metadata import (
+    APIResponseBaselineModelResponseMetadataFactory,
+    BaselineModelResponseMetadataFactory,
+    ModelMetadataFactory,
+)
+from .workspace_response import APIResponseWorkspaceBaseFactory
+
+__all__ = [
+    "AccuracyMetricFactory",
+    "ModelBenchmarkResultMetadataFactory",
+    "Model",
+    "ModelMetadataFactory",
+    "BaselineModelResponseMetadataFactory",
+    "APIResponseAddModelResponseFactory",
+    "APIResponseBaselineModelResponseMetadataFactory",
+    "APIResponseWorkspaceBaseFactory",
+    "APIResponseGruModelResponseFactory",
+]
