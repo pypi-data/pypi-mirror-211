b336aeffb11debcf865c333b71ff9eacd5007844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:38 +0000

    Add package scikit-recommender/0.0.3/scikit_recommender-0.0.3-cp310-cp310-win_amd64.whl

---
+++ packages/scikit-recommender/scikit_recommender-0.0.3-cp310-cp310-win_amd64.whl/skrec/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Zhongchuan Sun"
+__email__ = "zhongchuansun@gmail.com"
+
+# __all__ = []
+
+from .io import *
+from .utils.py import *
+from .utils import *
+
+import colorama
+colorama.init()
