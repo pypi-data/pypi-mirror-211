3b96ea56188baea2f12adc5d37817f5519647dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:10 +0000

    Add package PowDeRUL/1.0.0/PowDeRUL-1.0.0.tar.gz

---
+++ packages/PowDeRUL/PowDeRUL-1.0.0.tar.gz/PowDeRUL-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PowDeRUL',
+    version='1.0.0',
+    description='Python package for calculating lifetime of components',
+    author='PGarn',
+    author_email='paul.garnier@ens-rennes.fr',
+    url='https://github.com/PGarn/LifeTime',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'numpy>=1.19.2',
+        'pandas>=1.1.3',
+        'scipy>=1.10.1',
+        'pandas>=2.0.1',
+        'matplotlib>=3.7.1',
+        'numpy>=1.24.1',
+        'rainflow>=3.2.0',
+        'openpyxl>=3.1.2',
+    ],
+)
