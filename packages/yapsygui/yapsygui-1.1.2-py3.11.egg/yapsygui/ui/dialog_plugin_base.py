15ebe0f25e08b6af214bf3b6e0011c7909e0de6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:58 +0000

    Add package yapsygui/1.1.2/yapsygui-1.1.2-py3.11.egg

---
+++ packages/yapsygui/yapsygui-1.1.2-py3.11.egg/yapsygui/ui/dialog_plugin_base.py
@@ -0,0 +1,46 @@
+from abc import abstractmethod
+
+from qtpy.QtCore import QCoreApplication, QMetaObject
+from qtpy.QtWidgets import QDialog, QVBoxLayout
+from yapsygui.utils import findMainWindow
+
+from yapsygui.ui import TwinPanel, RowButtons
+
+TITLE = "Plugins Manager"
+
+
+class DialogPluginsBase(QDialog):
+
+    def __init__(self):
+        parent = findMainWindow()
+        super().__init__(parent)
+        self.setModal(True)
+
+        self.main_layout = QVBoxLayout(self)
+        self.panel = TwinPanel(self)
+        self.main_layout.addWidget(self.panel)
+        self.controls = RowButtons(self)
+        self.main_layout.addLayout(self.controls)
+        self.controls.install.connect(self.installPlugin)
+        self.controls.uninstall.connect(self.uninstallPlugin)
+        self.panel.connect(self.onSelectPlugin)
+
+        QMetaObject.connectSlotsByName(self)
+
+        self.setWindowTitle(self.tr(TITLE))
+
+    @property
+    def plugin(self):
+        return self.panel.plugin
+
+    @abstractmethod
+    def installPlugin(self):
+        pass
+
+    @abstractmethod
+    def uninstallPlugin(self):
+        pass
+
+    @abstractmethod
+    def onSelectPlugin(self, data):
+        pass
