fb7509b5fb197e5025fde1e69b69a443904de552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:39 +0000

    Add package arize-phoenix/0.0.23rc1/arize_phoenix-0.0.23rc1-py3-none-any.whl

---
+++ packages/arize-phoenix/arize_phoenix-0.0.23rc1-py3-none-any.whl/phoenix/server/api/types/Model.py
@@ -0,0 +1,124 @@
+import asyncio
+from typing import List, Optional
+
+import strawberry
+from strawberry.types import Info
+from strawberry.unset import UNSET
+
+from phoenix.config import get_exported_files
+from phoenix.core.model_schema import PRIMARY, REFERENCE
+from phoenix.server.api.context import Context
+
+from ..input_types.DimensionFilter import DimensionFilter
+from .Dataset import Dataset
+from .Dimension import Dimension, to_gql_dimension
+from .EmbeddingDimension import EmbeddingDimension, to_gql_embedding_dimension
+from .ExportedFile import ExportedFile
+from .pagination import Connection, ConnectionArgs, Cursor, connection_from_list
+
+
+@strawberry.type
+class Model:
+    @strawberry.field
+    def dimensions(
+        self,
+        info: Info[Context, None],
+        first: Optional[int] = 50,
+        last: Optional[int] = UNSET,
+        after: Optional[Cursor] = UNSET,
+        before: Optional[Cursor] = UNSET,
+        include: Optional[DimensionFilter] = UNSET,
+        exclude: Optional[DimensionFilter] = UNSET,
+    ) -> Connection[Dimension]:
+        """
+        A non-trivial implementation should efficiently fetch only
+        the necessary books after the offset.
+        For simplicity, here we build the list and then slice it accordingly
+        """
+        model = info.context.model
+        return connection_from_list(
+            [
+                to_gql_dimension(index, dimension)
+                for index, dimension in enumerate(model.scalar_dimensions)
+                if (not isinstance(include, DimensionFilter) or include.matches(dimension))
+                and (not isinstance(exclude, DimensionFilter) or not exclude.matches(dimension))
+            ],
+            args=ConnectionArgs(
+                first=first,
+                after=after if isinstance(after, Cursor) else None,
+                last=last,
+                before=before if isinstance(before, Cursor) else None,
+            ),
+        )
+
+    @strawberry.field
+    def primary_dataset(self, info: Info[Context, None]) -> Dataset:
+        dataset = info.context.model[PRIMARY]
+        start, stop = dataset.time_range
+        return Dataset(
+            start_time=start,
+            end_time=stop,
+            dataset=dataset,
+        )
+
+    @strawberry.field
+    def reference_dataset(self, info: Info[Context, None]) -> Optional[Dataset]:
+        if (dataset := info.context.model[REFERENCE]).empty:
+            return None
+        start, stop = dataset.time_range
+        return Dataset(
+            start_time=start,
+            end_time=stop,
+            dataset=dataset,
+        )
+
+    @strawberry.field
+    def embedding_dimensions(
+        self,
+        info: Info[Context, None],
+        first: Optional[int] = 50,
+        last: Optional[int] = UNSET,
+        after: Optional[Cursor] = UNSET,
+        before: Optional[Cursor] = UNSET,
+    ) -> Connection[EmbeddingDimension]:
+        """
+        A non-trivial implementation should efficiently fetch only
+        the necessary books after the offset.
+        For simplicity, here we build the list and then slice it accordingly
+        """
+        model = info.context.model
+        return connection_from_list(
+            [
+                to_gql_embedding_dimension(index, embedding_dimension)
+                for index, embedding_dimension in enumerate(
+                    model.embedding_dimensions,
+                )
+            ],
+            args=ConnectionArgs(
+                first=first,
+                after=after if isinstance(after, Cursor) else None,
+                last=last,
+                before=before if isinstance(before, Cursor) else None,
+            ),
+        )
+
+    @strawberry.field(
+        description="Returns exported file names sorted by descending modification time.",
+    )  # type: ignore  # https://github.com/strawberry-graphql/strawberry/issues/1929
+    async def exported_files(
+        self,
+        info: Info[Context, None],
+    ) -> List[ExportedFile]:
+        loop = asyncio.get_running_loop()
+        return [
+            ExportedFile(file_name=path.stem)
+            for path in sorted(
+                await loop.run_in_executor(
+                    None,
+                    get_exported_files,
+                    info.context.export_path,
+                ),
+                key=lambda p: p.stat().st_mtime,
+                reverse=True,
+            )
+        ]
