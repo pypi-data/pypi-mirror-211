41f694bdcd80f6074caed8ca8f7240cae0430666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:21 +0000

    Add package airscript/0.0.3/airscript-0.0.3-py3-none-any.whl

---
+++ packages/airscript/airscript-0.0.3-py3-none-any.whl/airscript/action/__init__.py
@@ -0,0 +1,16 @@
+from airscript import graphics
+
+
+# 点击
+def click(x: int | graphics.Point, y=None, dur=20) -> None:
+    if isinstance(x, graphics.Point):
+        point = x
+        x = point.x
+        y = point.y
+    elif y is None:
+        raise ValueError("If the first argument is not a Point, both x and y should be provided.")
+
+
+# 滑动
+def slide(x: int, y: int, y1: int, dur=20) -> None:
+    pass
