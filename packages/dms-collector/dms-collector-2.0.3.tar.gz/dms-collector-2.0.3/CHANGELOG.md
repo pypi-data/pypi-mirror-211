10bc6d0d4861783d81edf508e7f7a3a3e9c916f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:11 +0000

    Add package dms-collector/2.0.3/dms-collector-2.0.3.tar.gz

---
+++ packages/dms-collector/dms-collector-2.0.3.tar.gz/dms-collector-2.0.3/CHANGELOG.md
@@ -0,0 +1,9 @@
+# dms-collector changelog
+
+## version 2.0.3
+
+* Read and connect timeouts options.
+
+## version 2.0.2
+
+* DMS table file cache; table headers and their descriptions can be stored in a file when they are retrieved from DMS. This may reduce one interaction when retrieving DMS metrics. 
---
+++ packages/dms-collector/dms-collector-2.0.3.tar.gz/dms-collector-2.0.3/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: Tomas Vitvar, https://vitvar.com, tomas.vitvar@oracle.com
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import codecs
+import os
+import re
+import sys
+import argparse
+import glob
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+# read file content
+def read(*parts):
+    path = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(path, encoding="utf-8") as fobj:
+        return fobj.read()
+
+
+# finds the version of the package
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# setup main
+# required modules
+install_requires = ["urllib3>=2.0.2", "requests>=2.31.0", "setuptools_scm>=6.3.2"]
+
+setup(
+    name="dms-collector",
+    use_scm_version={
+        "root": ".",
+        "relative_to": __file__,
+        "local_scheme": "node-and-timestamp",
+    },
+    description="Oracle FMW DMS Spy collector utility",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Tomas Vitvar",
+    author_email="tomas@vitvar.com",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=install_requires,
+    python_requires=">=3.6.0",
+    scripts=["bin/dms-collector"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
