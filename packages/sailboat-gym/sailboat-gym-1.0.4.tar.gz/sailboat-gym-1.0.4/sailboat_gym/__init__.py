52fe39dabf9d0e4498b8e0fbba050c7029fcebb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:31 +0000

    Add package sailboat-gym/1.0.4/sailboat-gym-1.0.4.tar.gz

---
+++ packages/sailboat-gym/sailboat-gym-1.0.4.tar.gz/sailboat-gym-1.0.4/sailboat_gym/__init__.py
@@ -0,0 +1,23 @@
+from gymnasium.envs.registration import register
+
+from .envs import *
+from .renderers import *
+from .types import *
+from .utils import *
+
+register(
+    id='SailboatLSAEnv-v0',
+    entry_point='sailboat_gym.envs:SailboatLSAEnv',
+    max_episode_steps=60 * SailboatLSAEnv.SIM_RATE,  # 1 minute
+)
+
+__all__ = [
+    'SailboatLSAEnv',
+    'CV2DRenderer',
+    'Observation',
+    'Action',
+    'GymObservation',
+    'GymAction'
+]
+
+__version__ = '1.0.4'
