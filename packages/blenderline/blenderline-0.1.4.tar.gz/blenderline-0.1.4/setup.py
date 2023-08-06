9e5c4f3799cf43597add63eda6d99ab98d9aeda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:45 +0000

    Add package blenderline/0.1.4/blenderline-0.1.4.tar.gz

---
+++ packages/blenderline/blenderline-0.1.4.tar.gz/blenderline-0.1.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="blenderline",
+    version="0.1.4",
+    description="A pipeline for generating synthetic production line images",
+    packages=find_packages(exclude=["examples*", "data*"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MaxvandenHoven/blenderline",
+    author="Max van den Hoven",
+    author_email="max.hoven@gmail.com",
+    license="GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "blender-stubs>=3.12.27",
+        "bpy>=3.5.0",
+    ],
+    extras_require={
+        "dev": [
+            "black>=23.3.0",
+            "isort>=5.12.0",
+            "flake8>=6.0.0",
+            "twine>=4.0.2",
+        ],
+    },
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "blenderline = blenderline.command_line:cli",
+        ],
+    },
+)
