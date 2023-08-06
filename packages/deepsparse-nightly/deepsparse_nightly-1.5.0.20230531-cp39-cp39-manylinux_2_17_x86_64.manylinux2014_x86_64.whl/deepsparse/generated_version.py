902aee98a72fe35e3c33ee027eb404908038f280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:49 +0000

    Add package deepsparse-nightly/1.5.0.20230531/deepsparse_nightly-1.5.0.20230531-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/deepsparse-nightly/deepsparse_nightly-1.5.0.20230531-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deepsparse/generated_version.py
@@ -0,0 +1,19 @@
+# neuralmagic: no copyright
+# flake8: noqa
+# fmt: off
+# isort: skip_file
+__all__ = ["__version__", "version", "version_major", "version_minor", "version_bug", "version_build", "version_major_minor", "optimized", "is_release", "revision", "splash", "is_nightly", "build_date"]
+__version__ = "1.5.0.20230531"
+
+version = __version__
+version_major, version_minor, version_bug, version_build = version.split(".") + (
+    [None] if len(version.split(".")) < 4 else []
+) # handle conditional for version being 3 parts or 4 
+version_major_minor = f"{version_major}.{version_minor}"
+optimized = 1
+is_release = 0
+is_nightly = 1
+is_enterprise = 0
+revision = "a777c979"
+splash = "DeepSparse, Copyright 2021-present / Neuralmagic, Inc. version: 1.5.0.20230531 (a777c979) (release) (optimized) (system=avx2, binary=avx2)"
+build_date = "20230531"
