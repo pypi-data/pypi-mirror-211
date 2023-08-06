b7502e9d5200cf904621e6d870ad3a3c95ea0519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package modAL-python-mirror/0.4.2/modAL-python-mirror-0.4.2.tar.gz

---
+++ packages/modAL-python-mirror/modAL-python-mirror-0.4.2.tar.gz/modAL-python-mirror-0.4.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='modAL-python-mirror',
+    version='0.4.2',
+    author='Tivadar Danka',
+    author_email='85a5187a@opayq.com',
+    description='A modular active learning framework for Python3 (mirror package)',
+    license='MIT',
+    url='https://modAL-python.github.io/',
+    packages=['modAL', 'modAL.models', 'modAL.utils'],
+    classifiers=['Development Status :: 4 - Beta'],
+    install_requires=['numpy', 'scikit-learn>=0.18',
+                      'scipy>=0.18', 'pandas>=1.1.0', 'skorch==0.9.0'],
+)
