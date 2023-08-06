a074ed55733a4731c5d5a4ba868fc489340d1c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:37 +0000

    Add package abstract-test-package/0.0.1/abstract_test_package-0.0.1.tar.gz

---
+++ packages/abstract-test-package/abstract_test_package-0.0.1.tar.gz/abstract_test_package-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abstract_test_package',
+    version='0.0.1',
+    author='putkoff',
+    author_email='partners@abstractendeavors.com',
+    description='abstract_test_package is a Python package that facilitates testing with abstract scenarios. Utilizing PyTest, it offers extra utilities to streamline the creation and execution of abstract tests.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AbstractEndeavors/abstract_test_package',
+    packages=find_packages(where="src"),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    install_requires=[
+        # Add your project's requirements here, e.g.,
+        # 'numpy>=1.22.0',
+        # 'pandas>=1.3.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'abstract_test_package=abstract_test_package.main:main',
+        ],
+    },
+)
