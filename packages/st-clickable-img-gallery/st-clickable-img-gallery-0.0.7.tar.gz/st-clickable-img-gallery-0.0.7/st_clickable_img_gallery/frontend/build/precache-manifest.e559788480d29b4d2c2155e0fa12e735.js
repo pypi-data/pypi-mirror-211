c0ffa793f1368e061e2843f65a5d5f11789e0174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:48 +0000

    Add package st-clickable-img-gallery/0.0.7/st-clickable-img-gallery-0.0.7.tar.gz

---
+++ packages/st-clickable-img-gallery/st-clickable-img-gallery-0.0.7.tar.gz/st-clickable-img-gallery-0.0.7/st_clickable_img_gallery/frontend/build/precache-manifest.e559788480d29b4d2c2155e0fa12e735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab820d70dfafaa259b1d2b1f742514d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1a22922c185398fcc72",
+    "url": "./static/js/2.60f28383.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.60f28383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf9182042384e0c7055",
+    "url": "./static/js/main.8598a375.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
