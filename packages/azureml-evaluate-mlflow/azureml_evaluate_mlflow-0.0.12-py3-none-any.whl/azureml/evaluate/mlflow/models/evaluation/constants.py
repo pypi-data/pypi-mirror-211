d3cc5305ea1af853588e4dbec11f77ec4d5b6de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:07 +0000

    Add package azureml-evaluate-mlflow/0.0.12/azureml_evaluate_mlflow-0.0.12-py3-none-any.whl

---
+++ packages/azureml-evaluate-mlflow/azureml_evaluate_mlflow-0.0.12-py3-none-any.whl/azureml/evaluate/mlflow/models/evaluation/constants.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+""" Constants used for model evaluation."""
+
+
+class EvaluationSettingLiterals:
+    MULTI_LABEL = "multilabel"
+
+
+class EvaluationMiscLiterals:
+    IMAGE_OUTPUT_LABEL_COLUMN = "labels"
