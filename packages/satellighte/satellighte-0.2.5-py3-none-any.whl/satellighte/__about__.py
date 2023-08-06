d8c37a77ddc8c48a83cc06288f33acba692abb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:02 +0000

    Add package satellighte/0.2.5/satellighte-0.2.5-py3-none-any.whl

---
+++ packages/satellighte/satellighte-0.2.5-py3-none-any.whl/satellighte/__about__.py
@@ -0,0 +1,24 @@
+import time
+
+_this_year = time.strftime("%Y")
+__author__ = "Oguzcan Turan"
+__author_email__ = "can.turan.10@gmail.com"
+__copyright__ = f"Copyright (c) 2022-{_this_year}, {__author__}."
+__description__ = (
+    "PyTorch Lightning Implementations of Recent Satellite Image Classification !"
+)
+__homepage__ = "https://github.com/canturan10/satellighte"
+__license__ = "MIT License"
+__license_url__ = __homepage__ + "/blob/master/LICENSE"
+__pkg_name__ = "satellighte"
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__description__",
+    "__homepage__",
+    "__license__",
+    "__license_url__",
+    "__pkg_name__",
+]
