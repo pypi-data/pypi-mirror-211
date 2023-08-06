58c3f61b8c5280c52995eb8a7f0e8d408c05020a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:37 +0000

    Add package pnsgraph/0.0.1/pnsgraph-0.0.1.tar.gz

---
+++ packages/pnsgraph/pnsgraph-0.0.1.tar.gz/pnsgraph-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='pnsgraph',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    author='jftapia',
+    author_email='',
+    description=''
+)
