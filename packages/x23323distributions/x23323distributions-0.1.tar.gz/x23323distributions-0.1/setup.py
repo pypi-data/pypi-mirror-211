84a2ba963af8b1285638dd3060db39f5bd71eb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:31 +0000

    Add package x23323distributions/0.1/x23323distributions-0.1.tar.gz

---
+++ packages/x23323distributions/x23323distributions-0.1.tar.gz/x23323distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='x23323distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['x23323distributions'],
+      zip_safe=False)
