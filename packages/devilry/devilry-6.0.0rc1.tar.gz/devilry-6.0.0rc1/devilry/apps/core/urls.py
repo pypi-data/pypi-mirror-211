97a83efccb0961ebb270fd3698ae39d9db520f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:13 +0000

    Add package devilry/6.0.0rc1/devilry-6.0.0rc1.tar.gz

---
+++ packages/devilry/devilry-6.0.0rc1.tar.gz/devilry-6.0.0rc1/devilry/apps/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+from django.contrib.auth.decorators import login_required
+
+from .views.download_staticfeedbackfileattachment import DownloadStaticFeedbackFileAttachmentView
+
+urlpatterns = [
+    re_path(r'^feedbackfileattachment/(?P<pk>\d+)/(?P<asciifilename>.+)?$',
+        login_required(DownloadStaticFeedbackFileAttachmentView.as_view()),
+        name='devilry_core_feedbackfileattachment'),
+]
