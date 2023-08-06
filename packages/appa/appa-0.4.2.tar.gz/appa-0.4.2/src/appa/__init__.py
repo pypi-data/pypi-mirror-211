f1b33f285395d558602e84c4eca42c4208c095bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:02 +0000

    Add package appa/0.4.2/appa-0.4.2.tar.gz

---
+++ packages/appa/appa-0.4.2.tar.gz/appa-0.4.2/src/appa/__init__.py
@@ -0,0 +1,10 @@
+
+from .spacecraft import Spacecraft
+from .propagator import Propagator
+from .solarsystem import bodies
+from . import stop_conditions
+from . import toolbox
+
+del spacecraft
+del propagator
+del solarsystem
