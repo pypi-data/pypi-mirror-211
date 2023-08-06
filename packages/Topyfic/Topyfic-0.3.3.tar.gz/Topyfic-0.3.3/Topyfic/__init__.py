49d7e087ff1c22970aac31dd749ef32008270160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:21 +0000

    Add package Topyfic/0.3.3/Topyfic-0.3.3.tar.gz

---
+++ packages/Topyfic/Topyfic-0.3.3.tar.gz/Topyfic-0.3.3/Topyfic/__init__.py
@@ -0,0 +1,8 @@
+from Topyfic.utils.analyseModel import *
+from Topyfic.utils.makeModel import *
+from Topyfic.train import *
+from Topyfic.topic import *
+from Topyfic.topModel import *
+from Topyfic.analysis import *
+
+
