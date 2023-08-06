b109cb4969e9e644eb347e34486c095d451bb226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:16 +0000

    Add package aksh-distributions/1.5/aksh_distributions-1.5.tar.gz

---
+++ packages/aksh-distributions/aksh_distributions-1.5.tar.gz/aksh_distributions-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aksh_distributions',
+      version='1.5',
+      description='Gaussian distributions',
+      packages=['aksh_distributions'],
+      author= 'Abdalkarim Shehab',
+      author_email = 'abdo.r.shehab2@gmail.com',
+      zip_safe=False)
