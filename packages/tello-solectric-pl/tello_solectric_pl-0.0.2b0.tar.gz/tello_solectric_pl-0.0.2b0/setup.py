2c20f1e52b5a4a7309be00a3aa477cd3bf4bb337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:38 +0000

    Add package tello-solectric-pl/0.0.2b0/tello_solectric_pl-0.0.2b0.tar.gz

---
+++ packages/tello-solectric-pl/tello_solectric_pl-0.0.2b0.tar.gz/tello_solectric_pl-0.0.2b0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tello_solectric_pl",
+    version="0.0.2b",
+    author="Adam Jurkiewicz",
+    python_requires='>=3.8',
+    author_email="adam@jurkiewicz.tech",
+    description="Biblioteka dla wsparcia nauczycieli w Polsce dla dronów Tello-EDU oraz Ryzen TT (z wyświetlaczem LCD)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://solectric.pl",
+    keywords='Dron Tello TelloEDU Ryzen RyzenTT DJI',
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'djitellopy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
+    ],
+)
