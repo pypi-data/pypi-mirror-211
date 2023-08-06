146acf98c2a6ed2dc5708b5071e6c46943002e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:53 +0000

    Add package pyRealEstate/0.0.1/pyRealEstate-0.0.1.tar.gz

---
+++ packages/pyRealEstate/pyRealEstate-0.0.1.tar.gz/pyRealEstate-0.0.1/pyRealEstate/RealEstateMetrics.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np
+
+def COD( x, y):
+    ratio = x / y
+    med =  np.median(ratio)
+    dev = np.sum(np.abs(ratio - med))
+    avgdev=dev / len(ratio)
+    cod = 100 * (avgdev / med)
+    return cod
+
+
+def PRD(x,y):
+    ratio = x / y
+    mnratio = np.mean(ratio)
+    mnx = np.mean(x)
+    mny = np.mean(y)
+    prd = mnratio / (mnx /mny)
+    return prd
