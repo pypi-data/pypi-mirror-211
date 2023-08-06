b0755c82a4c73c0b80b6b4214e9bf09825292b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:33 +0000

    Add package mj-package-ipc/0.1.0/mj_package_ipc-0.1.0.tar.gz

---
+++ packages/mj-package-ipc/mj_package_ipc-0.1.0.tar.gz/mj_package_ipc-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mj_package_ipc',
+    version='0.1.0',
+    description='Example Python package',
+    url='',
+    author='Madhusudan J',
+    author_email='madhusudanj021vl@gmil.com',
+    packages=['mj_package_ipc'],
+    install_requires=['numpy'],
+    long_description="Sample pip installable Python package",
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+    ],
+)
