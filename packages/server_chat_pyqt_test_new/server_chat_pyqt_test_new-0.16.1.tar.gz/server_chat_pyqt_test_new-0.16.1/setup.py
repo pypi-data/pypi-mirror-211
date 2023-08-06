083b203a85f044327a103476318ee6c74c13d623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:33 +0000

    Add package server_chat_pyqt_test_new/0.16.1/server_chat_pyqt_test_new-0.16.1.tar.gz

---
+++ packages/server_chat_pyqt_test_new/server_chat_pyqt_test_new-0.16.1.tar.gz/server_chat_pyqt_test_new-0.16.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='server_chat_pyqt_test_new',
+      version='0.16.1',
+      description='Server packet',
+      packages=find_packages(),  # ,Будем искать пакеты тут(включаем авто поиск пакетов)
+      author_email='test@mail.ru',
+      author='test',
+      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+      # зависимости которые нужно до установить
+      )
