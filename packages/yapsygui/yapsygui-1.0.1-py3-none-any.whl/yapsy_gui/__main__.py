25024174995882b3db9c2e83534a7779b5e62ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:21 +0000

    Add package yapsygui/1.0.1/yapsygui-1.0.1-py3-none-any.whl

---
+++ packages/yapsygui/yapsygui-1.0.1-py3-none-any.whl/yapsy_gui/__main__.py
@@ -0,0 +1,71 @@
+#############################################################################
+#
+#   Source Yapsy:
+#   https://github.com/tibonihoo/yapsy
+#
+#   GUI implemented by: Leonel Hernández
+#   Source: https://github.com/leonelhs/yapsy-gui
+#
+##############################################################################
+import os
+import sys
+import os.path as osp
+from qtpy import QtWidgets
+from qtpy.QtWidgets import QMainWindow, QWidget, \
+    QVBoxLayout, QLabel, QPushButton, QTextBrowser
+from yapsy_gui import DialogPlugins
+
+
+# Fixme: Reload list plugins on uninstall plugin
+# Default demo plugins location path
+location = os.path.dirname(os.path.abspath(__file__))
+INSTALL_DIR = os.path.join(location, "plugins")
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.central_widget = QWidget(self)
+        self.main_layout = QVBoxLayout(self.central_widget)
+        self.label = QLabel(self)
+        self.label.setText("Yapsy GUI Plugin Manager")
+        self.main_layout.addWidget(self.label)
+        self.button_plugins = QPushButton(self)
+        self.button_plugins.setText("Show Plugin Manager")
+        self.button_plugins.clicked.connect(self.showPlugins)
+        self.main_layout.addWidget(self.button_plugins)
+        self.setCentralWidget(self.central_widget)
+        # Yapsy plugin manager API is embedded in GUI
+        self.manager = DialogPlugins(self, INSTALL_DIR)
+        self.manager.pluginsUpdate.connect(self.fetchPlugins)
+        self.fetchPlugins()
+
+        self.output = QTextBrowser(self.central_widget)
+        self.main_layout.addWidget(self.output)
+
+    def fetchPlugins(self):
+        for plugin in self.manager.plugins:
+            plugin.plugin_object.context = self
+            button = QPushButton(self)
+            button.setText(plugin.plugin_object.name)
+            button.clicked.connect(plugin.plugin_object.task)
+            self.main_layout.addWidget(button)
+        self.manager.displayPlugins()
+
+    # Plugin callback: make the process at plugin and it putback here the result
+    def plugin_action(self, text):
+        self.output.append(text)
+
+    def showPlugins(self):
+        self.manager.show()
+
+
+def main():
+    app = QtWidgets.QApplication()
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/yapsygui/yapsygui-1.0.1-py3-none-any.whl/yapsy_gui/plugins/plugin1.py
@@ -0,0 +1,15 @@
+from yapsy.IPlugin import IPlugin
+
+
+class PluginTwo(IPlugin):
+
+    def __init__(self):
+        super().__init__()
+        self.context = None
+        self.name = "Plugin test-01"
+
+    def task(self):
+        self.context.plugin_action("This is a text from plugin test-01")
+
+
+
