f100359334f342eb140fe3521f5746ee6fa8e863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:17 +0000

    Add package aeroapi-python/1.1.4/aeroapi_python-1.1.4-py3-none-any.whl

---
+++ packages/aeroapi-python/aeroapi_python-1.1.4-py3-none-any.whl/aeroapi_python/__init__.py
@@ -0,0 +1,6 @@
+from .Airports import Airports
+from .Operators import Operators
+from .History import History
+from .AeroAPI import AeroAPI
+from .Miscellaneous import Miscellaneous
+from .Flights import Flights
