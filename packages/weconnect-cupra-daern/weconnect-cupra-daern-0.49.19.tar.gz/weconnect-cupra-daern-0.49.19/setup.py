30be9fb9cac30c9328b39d8a7d70d6cf3b3d3150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:23 +0000

    Add package weconnect-cupra-daern/0.49.19/weconnect-cupra-daern-0.49.19.tar.gz

---
+++ packages/weconnect-cupra-daern/weconnect-cupra-daern-0.49.19.tar.gz/weconnect-cupra-daern-0.49.19/setup.py
@@ -0,0 +1,48 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+INSTALL_REQUIRED = (HERE / "requirements.txt").read_text()
+IMAGE_EXTRA_REQUIRED = (HERE / "image_extra_requirements.txt").read_text()
+SETUP_REQUIRED = (HERE / "setup_requirements.txt").read_text()
+TEST_REQUIRED = (HERE / "test_requirements.txt").read_text()
+
+setup(
+    name='weconnect-cupra-daern',
+    packages=find_packages(),
+    version=open("weconnect/__version.py").readlines()[-1].split()[-1].strip("\"'"),
+    description='Fork of Python API for the Volkswagen WeConnect Services to support Cupra Born',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Till Steinbach / Alan Gibson / Stuart Hall',
+    keywords='weconnect, we connect, carnet, car net, volkswagen, vw, telemetry',
+    url='https://github.com/daernsinstantfortress/WeConnect-python',
+    project_urls={
+        'Funding': 'https://github.com/sponsors/tillsteinbach',
+        'Source': 'https://github.com/daernsinstantfortress/WeConnect-python',
+        'Bug Tracker': 'https://github.com/tillsteinbach/WeConnect-python/issues'
+    },
+    license='MIT',
+    install_requires=INSTALL_REQUIRED,
+    extras_require={
+        "Images": IMAGE_EXTRA_REQUIRED,
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3.7',
+    setup_requires=SETUP_REQUIRED,
+    tests_require=TEST_REQUIRED,
+    include_package_data=True,
+    zip_safe=False,
+    package_data={'': ['weconnect/api/vw/badges/*.png']}
+)
