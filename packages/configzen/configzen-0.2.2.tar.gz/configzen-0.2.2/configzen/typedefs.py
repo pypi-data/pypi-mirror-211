b7922c4a8740dbde8d98d408684a1f3e53f03c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:19 +0000

    Add package configzen/0.2.2/configzen-0.2.2.tar.gz

---
+++ packages/configzen/configzen-0.2.2.tar.gz/configzen-0.2.2/configzen/typedefs.py
@@ -0,0 +1,26 @@
+import contextlib
+import os
+import pathlib
+import sys
+from typing import TYPE_CHECKING, TextIO, TypeVar, Union
+
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+if TYPE_CHECKING:
+    from aiofiles.base import AiofilesContextManager
+    from aiofiles.threadpool.text import AsyncTextIOWrapper
+
+    from configzen.config import ConfigModel, Route
+
+T = TypeVar("T")
+
+ConfigModelT = TypeVar("ConfigModelT", bound="ConfigModel")
+SupportsRoute: TypeAlias = Union[str, list[str], "Route"]
+
+ConfigIO: TypeAlias = contextlib.AbstractContextManager[TextIO]
+AsyncConfigIO: TypeAlias = "AiofilesContextManager[None, None, AsyncTextIOWrapper]"
+RawResourceT: TypeAlias = Union[ConfigIO, str, int, os.PathLike, pathlib.Path]
+NormalizedResourceT: TypeAlias = Union[ConfigIO, str, int, pathlib.Path]
