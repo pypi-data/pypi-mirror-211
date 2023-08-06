0ed3880b5a7bea5c9f9ea0e74113fed92e640658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package rfc-tidy/0.1.2/rfc-tidy-0.1.2.tar.gz

---
+++ packages/rfc-tidy/rfc-tidy-0.1.2.tar.gz/rfc-tidy-0.1.2/README.md
@@ -0,0 +1,31 @@
+# rfc-tidy
+
+A simple tool that cleans up RFC XML.
+
+## Usage
+
+Install from pypi with:
+
+```sh
+pip install --user rfc-tidy
+```
+
+Like `black`, `rfc-tidy` cannot be tuned.  Feed XML into `stdin`, get XML
+from `stdout` (it also accepts the input file as the first argument).
+
+```sh
+rfc-tidy < draft-blah-blah-blah.xml > draft-tidied.xml
+```
+
+## Features
+
+Adds `<bcp14>` tags for your "MUST", "SHOULD", and "MAY" statements in text.
+
+Removes extraneous XML, comments, and whitespace.
+
+Indents elements consistently.
+
+Replaces non-ASCII characters in text with XML numeric entities so that you
+don't miss them.
+
+Sorts attributes by name.
---
+++ packages/rfc-tidy/rfc-tidy-0.1.2.tar.gz/rfc-tidy-0.1.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import io
+import os
+
+from setuptools import setup, find_packages
+
+# To release, edit this file to update the version and then
+# git tag -am v{version} v{version}; git push origin v{version}
+# rm -rf dist
+# python -m build
+# python -m twine upload dist/*
+
+if __name__ == "__main__":
+    setup(
+        name="rfc-tidy",
+        version="0.1.2",
+        author="Martin Thomson",
+        author_email="mt@lowentropy.net",
+        scripts=["rfc-tidy"],
+        packages=find_packages(),
+        description="Cleanup RFC XML",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+        ],
+        keywords="rfc ietf xml",
+        python_requires=">=3.7",
+        install_requires=[],
+        url="https://github.com/martinthomson/rfc-tidy",
+        license="MIT",
+    )
