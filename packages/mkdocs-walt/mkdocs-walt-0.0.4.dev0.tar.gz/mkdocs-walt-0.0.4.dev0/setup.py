42dd738ef25b9c61b52f2659de534483c441bfb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:31 +0000

    Add package mkdocs-walt/0.0.4.dev0/mkdocs-walt-0.0.4.dev0.tar.gz

---
+++ packages/mkdocs-walt/mkdocs-walt-0.0.4.dev0.tar.gz/mkdocs-walt-0.0.4.dev0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4-dev'
+
+LONG_DESCRIPTION = '''
+# mkdocs-walt: a minimal documentation theme for MkDocs
+
+Walt is a minimal documentation theme that is best suited for single page websites.
+
+## Installation
+
+```sh
+pip install mkdocs-walt
+```
+
+## Usage
+
+Create a new MkDocs project with the `mkdocs` CLI and add the following your
+project's `mkdocs.yml`:
+
+```yaml
+theme:
+  name: walt
+```
+
+See the [full usage example](https://github.com/codesue/walt/examples/mkdocs).
+
+## Acknowledgements
+
+Walt uses [writ.css](https://github.com/programble/writ/tree/master) for styles
+and [Catppuccin](https://github.com/catppuccin/catppuccin) for dark mode
+color palettes.
+'''
+
+setup(
+    name='mkdocs-walt',
+    version=VERSION,
+    author='Suzen Fylke',
+    author_email='codesue@users.noreply.github.com',
+    description='A minimal theme for MkDocs',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/codesue/walt',
+    license='GPLv3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['mkdocs'],
+    entry_points={
+        'mkdocs.themes': [
+            'walt = walt',
+        ]
+    },
+    zip_safe=False
+)
