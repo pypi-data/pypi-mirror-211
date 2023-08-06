74547ccb6c39a2054aef671083ac9a1e62683f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:16 +0000

    Add package wordlexord/0.0.2/wordlexord-0.0.2.tar.gz

---
+++ packages/wordlexord/wordlexord-0.0.2.tar.gz/wordlexord-0.0.2/word_lexord/__init__.py
@@ -0,0 +1,24 @@
+from .base import (
+    decimal_to_base,
+    convert_nums_to_letters,
+    get_word_number,
+    get_word_by_number,
+    get_words_numbers_in_sentence,
+    words_range,
+    nums_to_words
+)
+from .lang import (
+    ALPHABETS
+)
+
+
+__all__ = [
+    "decimal_to_base",
+    "convert_nums_to_letters",
+    "get_word_number",
+    "get_word_by_number",
+    "get_words_numbers_in_sentence",
+    "words_range",
+    "nums_to_words"
+    "ALPHABETS"
+]
