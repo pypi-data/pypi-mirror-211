abdeb2f321bffba2fbbb390b922ba78ef6b5cd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:30 +0000

    Add package dktotoolkit/1.0.0/dktotoolkit-1.0.0.tar.gz

---
+++ packages/dktotoolkit/dktotoolkit-1.0.0.tar.gz/dktotoolkit-1.0.0/src/dktotoolkit/loadenv.py
@@ -0,0 +1,119 @@
+import os, sys
+
+def load(filename=None):
+
+    if filename is None:
+        filename="./.env"
+    #endIf
+
+    sys.stdout.write(F"> Load environment file : {filename}\n")
+
+    with open(filename,'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            l = line.split("\n")[0].split("#")[0].split("=")
+            if len(l) > 1:
+                var = l[0].strip()
+                val = l[1].strip().replace(",",":").replace(";",":")
+
+                sys.stdout.write(F"load_env> {var} = {val} : ")
+                if "\"" in val:
+                    sys.stdout.write(F"warning : character \" in content (character removed)")
+                    val = val.replace("\"","")
+                else:
+                    sys.stdout.write(F"Done")
+                #endIf
+
+                os.environ[var] = val
+                sys.stdout.write("\n")
+
+            #endIf
+        #endFor
+    #endWith
+
+    sys.stdout.write(F"\n")
+
+    #TODO : ajouter valeurs par defaut si pas presentes !
+
+#endWith
+
+
+def getEnvironVar(varname:str):
+    var = os.environ.get(varname)
+
+    def castList(var:str, char:str):
+
+        if "(" in var and ")" in var:
+            sublist=[""]
+            for e in var:
+                if e=="(":
+                    sublist += ["",]
+                elif e == ")":
+                    sublist += ["",]
+                else:
+                    sublist[-1] += str(e)
+                #endIf
+            #endFor
+
+            tr_sb_list = []
+            for e in sublist:
+                if not e or e in ["", ":"]:
+                    continue
+                #endIf
+                tr_sb_list += [castList(e, ":"),]
+            #endFor
+
+            return tr_sb_list
+        #endIf
+
+        try:
+
+            if isinstance(var, list) or isinstance(var, tuple):
+                return var
+            else:
+                L = [int(e.strip()) if e.strip().isdigit() else str(e.strip()) for e in var.split(char)]
+                return L
+            #endIf
+        except ValueError:
+             L = [str(e.strip()) for e in var.split(char)]
+             return L
+        except Exception as e:
+            print(e)
+            raise Exception
+        #endTry
+        return None
+    #endDef
+
+    if var is None :
+        return None
+    #endVar
+    for char in [":",";",","]:  # separators
+        if char in var:
+
+            L = castList(var, char)
+            if L is not None:
+                return L
+            #endIf
+        #endIf
+    #endFor
+
+
+    try:
+        return int(var.strip())
+    except ValueError:
+        return str(var.strip())
+
+    except Exception as e:
+        print(e)
+        raise Exception
+    #endTry
+
+#endDef
+
+
+if __name__=="__main__":
+    #print(getEnvironVar("PWD"))
+    #print(getEnvironVar("PATH"))
+    fname="/home/pierre/Documents/Projets/bot_breviaire_v2/.env"
+    load(fname)
+    print(getEnvironVar("BRPIERE_BELLS"))
