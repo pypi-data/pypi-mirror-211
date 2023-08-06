d14056ed5cac21010ad68348b6955e4801c212a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:33 +0000

    Add package terminusdb-client/10.2.3/terminusdb-client-10.2.3.tar.gz

---
+++ packages/terminusdb-client/terminusdb-client-10.2.3.tar.gz/terminusdb-client-10.2.3/terminusdb_client/__version__.py
@@ -0,0 +1,9 @@
+__title__ = "woqlClient"
+__description__ = "woqlClient library for accessing the Terminus DB API"
+__url__ = ""
+__version__ = "10.2.3"
+__build__ = 00
+__author__ = "TerminusDB group"
+__author_email__ = "team@terminusdb.com"
+__license__ = "GNU General Public License v3 (GPLv3)"
+__copyright__ = "Copyright 2019 datachemist"
