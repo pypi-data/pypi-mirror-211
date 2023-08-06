86ebfae4389318e390368617b0c33f34c30e1937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:58:25 +0000

    Add package tgw/1.0.3/tgw-1.0.3-py3-none-any.whl

---
+++ packages/tgw/tgw-1.0.3-py3-none-any.whl/tgw/base_struct.py
@@ -0,0 +1,17 @@
+class ReplayCfg(object):
+    def __init__(self) -> None:
+        self.begin_date = 0 # int
+        self.end_date = 0 # int
+        self.begin_time = 0 # int
+        self.end_time = 0 #int
+        self.task_id = 0 # int
+        self.req_codes = [] # list of tuple [(market, code), ...] like [101, '000001']
+        self.cq_flag = 0
+        self.cyc_type = 1
+        self.auto_complete = 1
+        self.replay_speed = 0
+        self.cq_date = 0
+        self.cyc_def = 0
+        self.qj_flag = 0
+        self.md_data_type = 0
+        
