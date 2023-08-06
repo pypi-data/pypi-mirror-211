0d8f0cc9c5d1c35c71c67b46c4e1609e5037697d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:12 +0000

    Add package triessentials/0.1.4/triessentials-0.1.4-py3-none-any.whl

---
+++ packages/triessentials/triessentials-0.1.4-py3-none-any.whl/triessentials/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.4'
+
+import sys, time, random, os
+from random import randint
+
+def slowprint(text,delay_time):
+ for character in text:      
+  sys.stdout.write(character) 
+  sys.stdout.flush()
+  time.sleep(delay_time)
+def random(number1,number2):
+ return randint(number1,number2)
+def clear()
+ os.system('clear')
+ 
+  
