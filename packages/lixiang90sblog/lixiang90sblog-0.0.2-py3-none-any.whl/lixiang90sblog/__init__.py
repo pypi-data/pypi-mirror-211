4dace762b351c587339dd4f038a9a1cdea63dfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:27 +0000

    Add package lixiang90sblog/0.0.2/lixiang90sblog-0.0.2-py3-none-any.whl

---
+++ packages/lixiang90sblog/lixiang90sblog-0.0.2-py3-none-any.whl/lixiang90sblog/__init__.py
@@ -0,0 +1,17 @@
+from .blog import blog,article
+from pprint import pprint
+
+content = blog('blog')
+def list_articles():
+    return content.list_articles()
+def display_titles():
+    pprint(content.return_titles())
+def print_article(title):
+    for item in content.blogs:
+        if item.title==title:
+            item.raw_output()
+def print_article_by_id(id):
+    item = content.blogs[id]
+    item.raw_output()
+def client():
+    pass
