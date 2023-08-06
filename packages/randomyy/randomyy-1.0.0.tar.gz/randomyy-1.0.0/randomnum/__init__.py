a11ac89a2588d0cba14e8af6ad747d150252d7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:05 +0000

    Add package randomyy/1.0.0/randomyy-1.0.0.tar.gz

---
+++ packages/randomyy/randomyy-1.0.0.tar.gz/randomyy-1.0.0/randomnum/__init__.py
@@ -0,0 +1,4 @@
+# Time    : 2023/6/1 13:44:27
+# Author  : dabinhuang
+# File    : __init__.py.py
+# Version : python3.7.5
---
+++ packages/randomyy/randomyy-1.0.0.tar.gz/randomyy-1.0.0/setup.py
@@ -0,0 +1,16 @@
+# Time    : 2023/6/1 13:45:16
+# Author  : dabinhuang
+# File    : setup.py
+# Version : python3.7.5
+
+
+from setuptools import setup
+
+setup(
+    name='randomyy',
+    version='1.0.0',
+    author='Your Name',
+    description='Library for generating random numbers',
+    packages=['randomnum'],
+    install_requires=[],
+)
