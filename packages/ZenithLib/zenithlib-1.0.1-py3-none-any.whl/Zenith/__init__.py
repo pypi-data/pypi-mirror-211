7d69616b762054a16328053c1b9877a60026db96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:38 +0000

    Add package ZenithLib/1.0.1/zenithlib-1.0.1-py3-none-any.whl

---
+++ packages/ZenithLib/zenithlib-1.0.1-py3-none-any.whl/Zenith/__init__.py
@@ -0,0 +1,7 @@
+from .Web import WApp
+from .Builder import Builder
+from .Stylesheet import Stylesheet
+from .lang.langAST import *
+from .lang.langParser import *
+from .lang.langTranspiler import *
+from .modules.ReadFromZenithFile import *
