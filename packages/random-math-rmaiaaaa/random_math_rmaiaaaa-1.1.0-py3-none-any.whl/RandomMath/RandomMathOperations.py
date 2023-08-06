bea5b804e29c0ea9296aa694f99c22c4a96aa1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:10 +0000

    Add package random-math-rmaiaaaa/1.1.0/random_math_rmaiaaaa-1.1.0-py3-none-any.whl

---
+++ packages/random-math-rmaiaaaa/random_math_rmaiaaaa-1.1.0-py3-none-any.whl/RandomMath/RandomMathOperations.py
@@ -0,0 +1,21 @@
+import random
+
+
+class RandomMath:
+
+    def __init__(self) -> None:
+        pass
+
+    def random_sum(self, number: int = 0) -> int:
+        '''
+        Realiza a soma de um número de entrada com um número aleatório
+        '''
+        random_int = random.randint(1, number)
+        return number + random_int
+
+    def random_sub(self, number: int = 0) -> int:
+        '''
+        Realiza a subtração de um número de entrada por um número aleatório
+        '''
+        random_int = random.randint(1, number)
+        return number - random_int
