837f08caefdbf1f5187ad23203fa9b874ddfcf60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:56 +0000

    Add package inifix/4.4.0/inifix-4.4.0.tar.gz

---
+++ packages/inifix/inifix-4.4.0.tar.gz/inifix-4.4.0/src/inifix/_typing.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from typing import AnyStr, Dict, Iterable, TypeVar, Union
+
+# not quite typing.AnyStr : this is not a constrained type variable
+StrLike = Union[str, bytes]
+
+if sys.version_info >= (3, 9):
+    PathLike = Union[AnyStr, os.PathLike[AnyStr]]
+else:
+    PathLike = Union[AnyStr, os.PathLike]
+
+T = TypeVar("T")
+Scalar = Union[int, float, bool, str]
+IterableOrSingle = Union[Iterable[T], T]
+
+# these types are used to validate schemas internally at type-checking time
+SectionT = Dict[str, IterableOrSingle[Scalar]]
+InifixConfT = Union[Dict[str, SectionT], SectionT]
