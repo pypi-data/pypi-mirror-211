1e4eec63a24fc299c4bb4513f172d1b2d67fcd2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:48 +0000

    Add package HanLabImport/0.0.4/HanLabImport-0.0.4.tar.gz

---
+++ packages/HanLabImport/HanLabImport-0.0.4.tar.gz/HanLabImport-0.0.4/setup.py
@@ -0,0 +1,29 @@
+# import setuptools
+import setuptools
+from distutils.core import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("HanLabImport/version.py", "r") as f:
+    # Define __version__ in separate file. That way online documentation can read it too
+    exec(f.read())
+
+setup(
+    name="HanLabImport",
+    version=__version__,
+    description="Import tools for DNPLab specific to the Han Lab",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Thorsten Maly",
+    author_email="tmaly@bridge12.com",
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy>=1.24",
+    ],
+    packages=setuptools.find_packages(),
+    url="https://github.com/Bridge12Technologies/HanLabImport",
+    project_urls={
+        "Tracker": "https://github.com/Bridge12Technologies/HanLabImport/issues"
+    },
+)
