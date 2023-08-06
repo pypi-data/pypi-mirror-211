baff4824bcc25ed65a6732fc950c2b456c24c397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:12 +0000

    Add package blaster-server/0.0.434b0/blaster-server-0.0.434b0.tar.gz

---
+++ packages/blaster-server/blaster-server-0.0.434b0.tar.gz/blaster-server-0.0.434b0/blaster/__init__.py
@@ -0,0 +1,38 @@
+'''
+Created on 29-Nov-2017
+
+@author: abhinav
+'''
+from gevent import monkey; monkey.patch_all()  # replaces read , write, send , sleep ...
+from gevent import local, signal_handler, signal
+import sys
+# override config module, hack
+from .config import config
+from .utils import events
+
+
+sys.modules["blaster.config"] = config  # hack
+
+
+# gevent local with some default
+class __ReqCtx(local.local):
+	def __init__(self, **kwargs):
+		self.__dict__.update({"req": None, "timestamp": None, "user": None})
+
+
+req_ctx = __ReqCtx()
+# END gevent local
+
+
+def blaster_exit():
+	# send exit signals
+	# stage 0 -> stop creating new things
+	# stage 1 -> initiate closing all connections, waiting things
+	# ..
+	# stage 5 -> all things cleanedup and done
+	for i in range(6):
+		events.broadcast_event("blaster_exit" + str(i))
+
+
+# sigint event broadcast
+signal_handler(signal.SIGINT, blaster_exit)
