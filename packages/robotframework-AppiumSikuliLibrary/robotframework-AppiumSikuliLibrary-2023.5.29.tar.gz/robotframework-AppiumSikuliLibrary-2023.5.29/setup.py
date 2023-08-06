a77ea5db3939f982c68bf58860050e55a3f7fdf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:07 +0000

    Add package robotframework-AppiumSikuliLibrary/2023.5.29/robotframework-AppiumSikuliLibrary-2023.5.29.tar.gz

---
+++ packages/robotframework-AppiumSikuliLibrary/robotframework-AppiumSikuliLibrary-2023.5.29.tar.gz/robotframework-AppiumSikuliLibrary-2023.5.29/setup.py
@@ -0,0 +1,41 @@
+'''
+Created on 2015/12/10
+
+Origin Author: by wang_yang1980@hotmail.com
+'''
+from setuptools import setup
+
+from os.path import abspath, dirname, join
+with open(join(dirname(abspath(__file__)), 'target', 'src', 'AppiumSikuliLibrary', 'version.py')) as f:
+      exec(f.read())
+
+
+DESCRIPTION = """
+Appium - Sikuli Robot Framework Library provide keywords for Robot Framework to test UI through Sikuli.
+
+Notes: AppiumSikuliLibrary.jar file is OS dependent.
+Suggestion: https://adoptium.net/temurin/releases/?version=11
+
+"""[1:-1]
+CLASSIFIERS = """
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Java
+Topic :: Software Development :: Testing
+"""[1:-1]
+setup(name         = 'robotframework-AppiumSikuliLibrary',
+      version      = VERSION,
+      description  = 'Appium - Sikuli library for Robot Framework',
+      long_description = DESCRIPTION,
+      author       = 'Thinh Nguyen',
+      author_email = 'nguyenvanthinh.dnn@gmail.com',
+      url          = 'https://github.com/thinhntt/robotframework-AppiumSikuliLibrary',
+      license      = 'Apache License 2.0',
+      keywords     = 'robotframework testing testautomation sikuli UI',
+      platforms    = 'any',
+      classifiers  = CLASSIFIERS.splitlines(),
+      package_dir  = {'' : 'target/src'},
+      packages     = ['AppiumSikuliLibrary'],
+      package_data = {'AppiumSikuliLibrary': ['lib/*.jar',
+                                          ]},
+      )
