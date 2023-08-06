43e996078046e3a8254bd5b79d44862ac406739f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:42 +0000

    Add package joyrl/0.2.13/joyrl-0.2.13.tar.gz

---
+++ packages/joyrl/joyrl-0.2.13.tar.gz/joyrl-0.2.13/joyrl/algos/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JiangJi
+Email: johnjim0816@gmail.com
+Date: 2023-01-01 16:20:49
+LastEditor: JiangJi
+LastEditTime: 2023-05-28 20:12:20
+Discription: 
+'''
+from joyrl.algos import base,DQN
+__all__ = [
+    "base",
+    "DQN",
+    "DoubleDQN",
+]
