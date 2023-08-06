d0ac742ab15c8287ad6236f6f18e1286f283b683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:18 +0000

    Add package nssurge-cli/2.0.4/nssurge_cli-2.0.4-py3-none-any.whl

---
+++ packages/nssurge-cli/nssurge_cli-2.0.4-py3-none-any.whl/nssurge_cli/devices_commands.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from nssurge_cli.config import get_config
+from nssurge_cli.types import ChangeDeviceEnum
+from nssurge_cli.utils import (
+    typer_output_dict,
+)
+
+# use_local_nssurge_api_module()
+from nssurge_api import SurgeAPIClient
+from nssurge_api.types import (
+    ChangeDeviceRequest,
+)
+import typer
+import asyncio
+
+app = typer.Typer(name="devices")
+
+
+async def get_devices():
+    async with SurgeAPIClient(*get_config()) as client:
+        devices_resp = await client.get_devices()
+        return await devices_resp.json()
+
+def complete_devices(incomplete: str):
+    """
+    Complete device names.
+    """
+    incomplete = incomplete.lower()
+    devices_dict = asyncio.run(get_devices()) # type: ignore
+    devices: list[dict] = devices_dict['devices']
+    for device in devices:
+        info = ' | '.join(map(lambda x: str(device.get(x)), ['name', 'displayIPAddress', 'totalBytes']))
+        id: str = device.get('identifier') # type: ignore
+        if incomplete in id or incomplete in info.lower():
+            yield id, info
+
+@app.callback(invoke_without_command=True)
+def devices(ctx: typer.Context,
+    output_json: bool = typer.Option(False, "--json", "-j"),
+    pretty_print: bool = typer.Option(False, "--pretty", "-p"),
+    rich_print: bool = typer.Option(False, "--rich", "-r"),
+):
+    """Manage devices"""
+    if ctx.invoked_subcommand is not None:
+        return
+    devices_resp = asyncio.run(get_devices())
+    typer_output_dict(devices_resp, output_json, pretty_print, rich_print)
+
+
+async def get_device_icon(icon_id):
+    async with SurgeAPIClient(*get_config()) as client:
+        icon_resp = await client.get_device_icon(icon_id)
+        return await icon_resp.json()
+
+
+@app.command("icon")
+def icon(icon_id, output_json: bool = typer.Option(False, "--json", "-j"), pretty_print: bool = typer.Option(False, "--pretty", "-p"), rich_print: bool = typer.Option(False, "--rich", "-r")):
+    icon_resp = asyncio.run(get_device_icon(icon_id))
+    typer_output_dict(icon_resp, output_json, pretty_print, rich_print)
+    typer.secho('Warning: the Surge API used is broken on Surge for mac 4.5.0', dim=True, err=True)
+
+
+async def change_device(req: ChangeDeviceRequest):
+    async with SurgeAPIClient(*get_config()) as client:
+        resp = await client.change_device(req)
+        return await resp.json()
+
+@app.command('set')
+def set_device_command(physical_address: str = typer.Argument(..., autocompletion=complete_devices), field: ChangeDeviceEnum = typer.Argument(...), value: str = typer.Argument(...)):
+    req = ChangeDeviceRequest(
+        physicalAddress=physical_address,
+    )
+    # setattr(req, field.value, value)
+    req[field.value] = value
+    resp_dict = asyncio.run(change_device(req))
+    # typer_output_dict(resp_dict)
+    if not resp_dict:
+        # success
+        typer.secho(f'Successfully set {field.value} to {value} for {physical_address}', fg=typer.colors.GREEN)
+    else:
+        # failure
+        typer.secho(f'Failed to set {field.value} to {value} for {physical_address}', fg=typer.colors.RED)
+        typer.secho(f'Error: {resp_dict["error"]}', fg=typer.colors.RED)
+
---
+++ packages/nssurge-cli/nssurge_cli-2.0.4-py3-none-any.whl/nssurge_cli/utils.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import typer
+from aiohttp import ClientResponse
+from nssurge_api import SurgeAPIClient
+from nssurge_api.types import (
+    Capability,
+)
+
+from utils_tddschn.utils import strtobool
+def s2b(s: str) -> bool:
+    return bool(strtobool(s))
+
+
+async def parse_cap_get(resp: ClientResponse) -> bool:
+    if not resp.status == 200:
+        raise ValueError(f"Unexpected status code: {resp.status}")
+    return (await resp.json())["enabled"]
+
+
+async def get_cap_state(client: SurgeAPIClient, capability: Capability) -> bool:
+    get_resp = await client.get_cap(capability)
+    orig_state = await parse_cap_get(get_resp)
+    return orig_state
+
+
+def bool2color(state: bool) -> str:
+    if state:
+        color = typer.colors.GREEN
+    else:
+        color = typer.colors.RED
+    return color
+
+
+def use_local_nssurge_api_module():
+    import sys
+
+    sys.path.insert(0, str(Path.home() / "testdir" / "nssurge-api"))
+    print(sys.path)
+
+
+def typer_output_dict(
+    d: dict,
+    output_json: bool = False,
+    pretty_print: bool = False,
+    rich_print: bool = False,
+) -> None:
+    if output_json:
+        import json
+
+        typer.secho(json.dumps(d, indent=2, ensure_ascii=False))
+    elif pretty_print:
+        from pprint import PrettyPrinter
+
+        pp = PrettyPrinter(indent=2)
+        typer.secho(pp.pformat(d))
+    elif rich_print:
+        try:
+            from rich import print as rprint
+
+            rprint(d)
+        except ImportError:
+            typer.secho("rich module not installed")
+    else:
+        typer.secho(d)
+
+
+# typer_output_dict_typer_options = (    output_json: bool = typer.Option(False, "--json", "-j"),
+#     pretty_print: bool = typer.Option(False, "--pretty", "-p"),)
+rich_print: bool = (typer.Option(False, "--rich", "-r"),)
