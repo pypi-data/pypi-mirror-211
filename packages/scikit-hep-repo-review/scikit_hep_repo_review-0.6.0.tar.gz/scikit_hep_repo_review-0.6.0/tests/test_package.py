42aa840cc60538c80d98f53790dd27379f870964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:08 +0000

    Add package scikit-hep-repo-review/0.6.0/scikit_hep_repo_review-0.6.0.tar.gz

---
+++ packages/scikit-hep-repo-review/scikit_hep_repo_review-0.6.0.tar.gz/scikit_hep_repo_review-0.6.0/tests/test_package.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+import scikit_hep_repo_review as m
+from scikit_hep_repo_review.ghpath import GHPath
+from scikit_hep_repo_review.processor import process
+
+DIR = Path(__file__).parent.resolve()
+
+
+def test_version():
+    assert m.__version__
+
+
+@pytest.mark.skip(reason="Can be rate limited")
+def test_pyodide():
+    package = GHPath(repo="scikit-hep/repo-review", branch="main")
+    results = process(package)
+    assert results
+
+
+def test_local():
+    package = DIR.parent
+    results = process(package)
+    assert results
