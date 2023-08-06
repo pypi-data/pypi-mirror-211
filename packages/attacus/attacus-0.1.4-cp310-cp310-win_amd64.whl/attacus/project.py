86bba5953b300def6c2ac3f7fdeaaf6708692868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:33 +0000

    Add package attacus/0.1.4/attacus-0.1.4-cp310-cp310-win_amd64.whl

---
+++ packages/attacus/attacus-0.1.4-cp310-cp310-win_amd64.whl/attacus/project.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from .pyproject import PyProject
+
+class Project:
+    def __new__(cls, *args, **kwargs):
+        global _project
+        if not _project:
+            _project = super(Project, cls).__new__(cls)
+        return _project
+
+    def __init__(self, path: Path) -> None:
+        self.path: Path = path
+        self.pyproject:PyProject = PyProject.load(path)
+        self.name: str = self.pyproject.name
+
+    @property
+    def flutter_assets(self):
+        if hasattr(self.pyproject.tool.attacus, 'flutter_assets'):
+            return self.pyproject.tool.attacus.flutter_assets
+        return Path.cwd() / 'flutter_assets'
+
+_project: Project = None
