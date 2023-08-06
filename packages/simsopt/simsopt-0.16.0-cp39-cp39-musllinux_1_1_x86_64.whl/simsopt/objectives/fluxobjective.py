3fb1c827dc8e50e5a360be1fa8b03bf45cbd87c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:40 +0000

    Add package simsopt/0.16.0/simsopt-0.16.0-cp39-cp39-musllinux_1_1_x86_64.whl

---
+++ packages/simsopt/simsopt-0.16.0-cp39-cp39-musllinux_1_1_x86_64.whl/simsopt/objectives/fluxobjective.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+import simsoptpp as sopp
+from .._core.optimizable import Optimizable
+from .._core.derivative import derivative_dec
+from .._core.json import GSONable, GSONDecoder, GSONEncoder
+
+
+__all__ = ['SquaredFlux']
+
+
+class SquaredFlux(Optimizable):
+
+    r"""
+    Objective representing the quadratic flux of a field on a surface, that is
+
+    .. math::
+        \frac12 \int_{S} (\mathbf{B}\cdot \mathbf{n} - B_T)^2 ds
+
+    where :math:`\mathbf{n}` is the surface unit normal vector and
+    :math:`B_T` is an optional (zero by default) target value for the
+    magnetic field.
+
+    Args:
+        surface: A :obj:`simsopt.geo.surface.Surface` object on which to compute the flux
+        field: A :obj:`simsopt.field.magneticfield.MagneticField` for which to compute the flux.
+        target: A ``nphi x ntheta`` numpy array containing target values for the flux. Here 
+          ``nphi`` and ``ntheta`` correspond to the number of quadrature points on `surface` 
+          in ``phi`` and ``theta`` direction.
+    """
+
+    def __init__(self, surface, field, Btarget=None):
+        self.surface = surface
+        if Btarget is not None:
+            self.Btarget = np.ascontiguousarray(Btarget)
+        else:
+            self.Btarget = np.zeros(self.surface.normal().shape[:2])
+        self.field = field
+        xyz = self.surface.gamma()
+        self.field.set_points(xyz.reshape((-1, 3)))
+        Optimizable.__init__(self, x0=np.asarray([]), depends_on=[field])
+
+    def J(self):
+        n = self.surface.normal()
+        Bcoil = self.field.B().reshape(n.shape)
+        return sopp.integral_BdotN(Bcoil, self.Btarget, n)
+
+    @derivative_dec
+    def dJ(self):
+        n = self.surface.normal()
+        absn = np.linalg.norm(n, axis=2)
+        unitn = n * (1./absn)[:, :, None]
+        Bcoil = self.field.B().reshape(n.shape)
+        Bcoil_n = np.sum(Bcoil*unitn, axis=2)
+        B_n = (Bcoil_n - self.Btarget)
+        dJdB = (B_n[..., None] * unitn * absn[..., None])/absn.size
+        dJdB = dJdB.reshape((-1, 3))
+        return self.field.B_vjp(dJdB)
+
