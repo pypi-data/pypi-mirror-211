619c676c2bf88e8a61806e71f1949e3a046cd312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:54 +0000

    Add package animedata/0.3.8/animedata-0.3.8-py3-none-any.whl

---
+++ packages/animedata/animedata-0.3.8-py3-none-any.whl/animedata/common/metadata.py
@@ -0,0 +1,17 @@
+import importlib.metadata
+import os
+
+ad_version = importlib.metadata.version("animedata")
+dir_path = os.path.dirname(__file__)
+ad_table = {
+    "repository_url":
+    "https://raw.githubusercontent.com/swarthur/animedata/",
+    "source_file_name" : "animedata_source.json",
+    "local_file_name" : "animedata_local.json",
+    "source_file_path": "..\\resources\\animedata_source.json",
+    "local_file_path": "..\\resources\\animedata_local.json",
+    "anime_name": "anime_name",
+    "seasons": "seasons",
+    "episode_duration": "episode_duration",
+    "episode_release_date": "episode_release_date",
+    "episode_name": "episode_name"}
