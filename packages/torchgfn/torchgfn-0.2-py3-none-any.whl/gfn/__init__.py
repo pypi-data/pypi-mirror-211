d83d47c337a42d66881d73c9aecacab8a05dd949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:04 +0000

    Add package torchgfn/0.2/torchgfn-0.2-py3-none-any.whl

---
+++ packages/torchgfn/torchgfn-0.2-py3-none-any.whl/gfn/__init__.py
@@ -0,0 +1,11 @@
+from .estimators import (
+    LogEdgeFlowEstimator,
+    LogitPBEstimator,
+    LogitPFEstimator,
+    LogStateFlowEstimator,
+    LogZEstimator,
+)
+
+import importlib.metadata as met
+
+__version__ = met.version("torchgfn")
