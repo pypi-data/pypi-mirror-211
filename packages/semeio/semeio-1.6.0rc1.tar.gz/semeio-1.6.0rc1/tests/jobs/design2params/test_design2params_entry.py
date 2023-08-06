eb11f057da8a43ccfceac4cdbeafd77c7c724efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:27 +0000

    Add package semeio/1.6.0rc1/semeio-1.6.0rc1.tar.gz

---
+++ packages/semeio/semeio-1.6.0rc1.tar.gz/semeio-1.6.0rc1/tests/jobs/design2params/test_design2params_entry.py
@@ -0,0 +1,111 @@
+import logging
+import os
+import shutil
+import sys
+
+import pytest
+
+from semeio.jobs.scripts import design2params
+
+
+@pytest.fixture
+def input_data(tmpdir):
+    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+    shutil.copytree(data_dir, tmpdir.strpath, dirs_exist_ok=True)
+
+    cwd = os.getcwd()
+    tmpdir.chdir()
+
+    yield
+
+    os.chdir(cwd)
+
+
+_realization = 1
+_xls = "design_matrix.xlsx"
+_sheet = "DesignSheet01"
+_default_sheet = "DefaultSheet01"
+_parametersfilename = "parameters.txt"
+_log_level = "DEBUG"
+_default_log_level = "WARNING"
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse():
+    args = [str(_realization), _xls, _sheet, _default_sheet]
+    parser = design2params.create_parser()
+    res = parser.parse_args(args)
+    assert res
+    assert res.realization == _realization
+    assert res.xlsfilename == _xls
+    assert res.designsheetname == _sheet
+    assert res.defaultssheetname == _default_sheet
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse_no_default():
+    args = [str(_realization), _xls, _sheet]
+    parser = design2params.create_parser()
+    res = parser.parse_args(args)
+    assert res
+    assert res.realization == _realization
+    assert res.xlsfilename == _xls
+    assert res.designsheetname == _sheet
+    assert res.defaultssheetname is None
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse_with_optionals():
+    args = [
+        str(_realization),
+        _xls,
+        _sheet,
+        "-p",
+        _parametersfilename,
+        "-l",
+        _log_level,
+    ]
+    parser = design2params.create_parser()
+    res = parser.parse_args(args)
+    assert res
+    assert res.realization == _realization
+    assert res.xlsfilename == _xls
+    assert res.designsheetname == _sheet
+    assert res.defaultssheetname is None
+    assert res.parametersfilename == _parametersfilename
+    assert res.log_level == logging.getLevelName(_log_level)
+
+
+def test_argparse_xls_file_not_exists(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["path", str(_realization), "not_a_file", _sheet])
+    with pytest.raises(SystemExit):
+        design2params.main_entry_point()
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse_parameters_file_not_exists():
+    not_existing_parameters_txt = "not_a_file.txt"
+
+    args = [
+        str(_realization),
+        _xls,
+        _sheet,
+        "-p",
+        not_existing_parameters_txt,
+    ]
+    parser = design2params.create_parser()
+    res = parser.parse_args(args)
+    assert res
+    assert res.realization == _realization
+    assert res.xlsfilename == _xls
+    assert res.designsheetname == _sheet
+    assert res.defaultssheetname is None
+    assert res.parametersfilename == not_existing_parameters_txt
+    assert res.log_level == logging.getLevelName(_default_log_level)
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse_design_sheet_missing(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["path", str(_realization), _xls])
+    with pytest.raises(SystemExit):
+        design2params.main_entry_point()
---
+++ packages/semeio/semeio-1.6.0rc1.tar.gz/semeio-1.6.0rc1/tests/jobs/design_kw/test_design_kw_entry.py
@@ -0,0 +1,75 @@
+import logging
+import os
+import shutil
+import sys
+
+import pytest
+
+from semeio.jobs.scripts import design_kw
+
+
+@pytest.fixture
+def input_data(tmpdir):
+    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+    shutil.copytree(data_dir, tmpdir.strpath, dirs_exist_ok=True)
+
+    cwd = os.getcwd()
+    tmpdir.chdir()
+
+    yield
+
+    os.chdir(cwd)
+
+
+_templatefile = "template.yml.tmpl"
+_templatefile_unmatched = "template_unmatched.yml.tmpl"
+_resultfile = "result.txt"
+_log_level = "DEBUG"
+_default_log_level = "WARNING"
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse():
+    args = [_templatefile, _resultfile]
+    parser = design_kw.create_parser()
+    res = parser.parse_args(args)
+
+    assert res
+    assert res.templatefile == _templatefile
+    assert res.resultfile == _resultfile
+    assert res.log_level == logging.getLevelName(_default_log_level)
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse_with_logging():
+    args = [_templatefile, _resultfile, "--log-level", _log_level]
+    parser = design_kw.create_parser()
+    res = parser.parse_args(args)
+    assert res
+    assert res.templatefile == _templatefile
+    assert res.resultfile == _resultfile
+    assert res.log_level == logging.getLevelName(_log_level)
+
+
+def test_argparse_file_not_exists(monkeypatch):
+    monkeypatch.setattr(
+        sys, "argv", ["script_name", "file_not_existing.yml.tmpl", _resultfile]
+    )
+    with pytest.raises(SystemExit):
+        design_kw.main_entry_point()
+
+
+@pytest.mark.usefixtures("input_data")
+def test_argparse_result_file_missing(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["script_name", _templatefile])
+    with pytest.raises(SystemExit):
+        design_kw.main_entry_point()
+
+
+@pytest.mark.usefixtures("input_data")
+def test_unmatched_template(monkeypatch):
+    monkeypatch.setattr(
+        sys, "argv", ["script_name", _templatefile_unmatched, _resultfile]
+    )
+    with pytest.raises(SystemExit):
+        design_kw.main_entry_point()
