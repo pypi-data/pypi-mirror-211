02086c0771372c688887dc9c2f46a7d3af9685f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:45 +0000

    Add package pyrsig/0.4.1/pyrsig-0.4.1-py3-none-any.whl

---
+++ packages/pyrsig/pyrsig-0.4.1-py3-none-any.whl/pyrsig/tests/test_ioapi.py
@@ -0,0 +1,72 @@
+def test_tropomi():
+    from .. import RsigApi
+    import tempfile
+
+    with tempfile.TemporaryDirectory() as td:
+        rsigapi = RsigApi(
+            bdate='2022-03-01', workdir=td,
+            bbox=(-97, 20, -65, 50)
+        )
+        ds = rsigapi.to_ioapi(
+            'tropomi.offl.no2.nitrogendioxide_tropospheric_column'
+        )
+        print(ds.dims)
+
+
+def test_tropomi_cache():
+    from .. import RsigApi
+    import tempfile
+
+    with tempfile.TemporaryDirectory() as td:
+        rsigapi = RsigApi(
+            bdate='2022-03-01', workdir=td,
+            bbox=(-97, 20, -65, 50)
+        )
+        ds = rsigapi.to_ioapi(
+            'tropomi.offl.no2.nitrogendioxide_tropospheric_column'
+        )
+        print(ds.dims)
+        ds = rsigapi.to_ioapi(
+            'tropomi.offl.no2.nitrogendioxide_tropospheric_column'
+        )
+        print(ds.dims)
+        rsigapi = RsigApi(
+            bdate='2022-03-01', workdir=td, overwrite=True,
+            bbox=(-97, 20, -65, 50)
+        )
+        ds = rsigapi.to_ioapi(
+            'tropomi.offl.no2.nitrogendioxide_tropospheric_column'
+        )
+        print(ds.dims)
+
+
+def test_tropomi_encoding_remove():
+    from .. import RsigApi
+    import tempfile
+
+    with tempfile.TemporaryDirectory() as td:
+        rsigapi = RsigApi(
+            bdate='2022-03-02', workdir=td,
+            bbox=(-97, 20, -65, 50), encoding={'zlib': True, 'complevel': 1}
+        )
+        ds = rsigapi.to_ioapi(
+            'tropomi.offl.no2.nitrogendioxide_tropospheric_column',
+            removegz=True
+        )
+        print(ds.dims)
+
+
+def test_tropomi_withmeta():
+    from .. import RsigApi
+    import tempfile
+
+    with tempfile.TemporaryDirectory() as td:
+        rsigapi = RsigApi(
+            bdate='2022-03-02', workdir=td,
+            bbox=(-97, 20, -65, 50), encoding={'zlib': True, 'complevel': 1}
+        )
+        ds = rsigapi.to_ioapi(
+            'tropomi.offl.no2.nitrogendioxide_tropospheric_column',
+            withmeta=True, removegz=True
+        )
+        print(ds.dims, len(ds.attrs['metadata']))
