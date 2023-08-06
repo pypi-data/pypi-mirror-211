d5b1e392a695b069934fcc3063d752312372819b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:35 +0000

    Add package orchestration/1.1.2/orchestration-1.1.2.tar.gz

---
+++ packages/orchestration/orchestration-1.1.2.tar.gz/orchestration-1.1.2/orchestration/orchestrator.py
@@ -0,0 +1,30 @@
+import logging
+import time
+import datetime
+from orchestration.task import Task
+
+
+class Orchestrator:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, cron_expression, script_type):
+        task = Task(cron_expression, script_type)
+        self.tasks.append(task)
+        return task
+
+    def execute_tasks(self):
+        for task in self.tasks:
+            # Check if the task should be skipped
+            if any(skip_condition for skip_condition in task.skips):
+                logging.info(f"Skipping task execution...")
+                continue
+
+            if task.should_execute():
+                for script in task.scripts:
+                    if isinstance(script, tuple) and len(script) == 2:
+                        script_path, parameters_list = script
+                        task.repeat(script_path, parameters_list)
+                    else:
+                        task.execute_script(script)
+                    time.sleep(task.interval)
