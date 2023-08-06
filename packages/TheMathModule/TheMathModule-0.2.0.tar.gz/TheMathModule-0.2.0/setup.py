e822e37c595b92d198df3145e058e36e031e0a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package TheMathModule/0.2.0/TheMathModule-0.2.0.tar.gz

---
+++ packages/TheMathModule/TheMathModule-0.2.0.tar.gz/TheMathModule-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TheMathModule',
+    version='0.2.0',
+    author='Syed Abdullah Saad',
+    author_email='syedabdullahsaad2000@gmail.com',
+    description='A basic module that takes the dataframe as the first argument and a numeric column of the dataframe, and calculates the mean of it (alias it as aom),///from TheMathModule.avg_or_mean import avg_or_mean as aom///'
+,
+    packages=['TheMathModule'],
+)
