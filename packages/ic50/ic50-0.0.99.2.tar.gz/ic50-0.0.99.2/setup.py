cb7d53ddb8e9f49fc0e7f94c4ee79a3b132a500c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:58 +0000

    Add package ic50/0.0.99.2/ic50-0.0.99.2.tar.gz

---
+++ packages/ic50/ic50-0.0.99.2.tar.gz/ic50-0.0.99.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.99.2' 
+DESCRIPTION = 'IC50 prediction'
+LONG_DESCRIPTION = 'Calculate IC50/pIC50 values using a variety of methods.'
+
+setup(
+        name="ic50", 
+        version=VERSION,
+        author="Steve Niu",
+        author_email="<niuacademics@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy','scipy','matplotlib',''],
+        keywords=['python', 'IC50'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
