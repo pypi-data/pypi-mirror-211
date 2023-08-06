8be88255ed07339c5065c9266a270cf9c43f5777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:06 +0000

    Add package ert/5.0.0rc3/ert-5.0.0rc3-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-5.0.0rc3-cp38-cp38-macosx_10_15_x86_64.whl/ert/callbacks.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, List, Tuple
+
+from ert._c_wrappers.enkf.config.parameter_config import ParameterConfig
+from ert._c_wrappers.enkf.enkf_state import (
+    _write_gen_data_to_storage,
+    _write_summary_data_to_storage,
+)
+from ert._c_wrappers.enkf.enums import RealizationStateEnum
+
+from .load_status import LoadResult, LoadStatus
+
+if TYPE_CHECKING:
+    from ert._c_wrappers.enkf import EnsembleConfig, RunArg
+
+logger = logging.getLogger(__name__)
+
+
+def _read_parameters(
+    run_arg: RunArg, parameter_configuration: List[ParameterConfig]
+) -> LoadResult:
+    result = LoadResult(LoadStatus.LOAD_SUCCESSFUL, "")
+    error_msg = ""
+    for config_node in parameter_configuration:
+        if not config_node.forward_init:
+            continue
+        try:
+            config_node.load(
+                Path(run_arg.runpath), run_arg.iens, run_arg.ensemble_storage
+            )
+        except ValueError as err:
+            error_msg += str(err)
+            result = LoadResult(LoadStatus.LOAD_FAILURE, error_msg)
+    return result
+
+
+def forward_model_ok(
+    run_arg: RunArg,
+    ens_conf: EnsembleConfig,
+) -> LoadResult:
+    parameters_result = LoadResult(LoadStatus.LOAD_SUCCESSFUL, "")
+    summary_result = LoadResult(LoadStatus.LOAD_SUCCESSFUL, "")
+    gen_data_result = LoadResult(LoadStatus.LOAD_SUCCESSFUL, "")
+    try:
+        # We only read parameters after the prior, after that, ERT
+        # handles parameters
+        if run_arg.itr == 0:
+            parameters_result = _read_parameters(
+                run_arg, ens_conf.parameter_configuration
+            )
+
+        if parameters_result.status == LoadStatus.LOAD_SUCCESSFUL:
+            summary_result = _write_summary_data_to_storage(ens_conf, run_arg)
+            gen_data_result = _write_gen_data_to_storage(ens_conf, run_arg)
+
+    except Exception as err:
+        logging.exception("Unhandled exception in callback for forward_model")
+        parameters_result = LoadResult(
+            LoadStatus.LOAD_FAILURE,
+            f"Unhandled exception in callback for forward_model {err}",
+        )
+
+    final_result = parameters_result
+    if summary_result.status != LoadStatus.LOAD_SUCCESSFUL:
+        final_result = summary_result
+    elif gen_data_result.status != LoadStatus.LOAD_SUCCESSFUL:
+        final_result = gen_data_result
+
+    run_arg.ensemble_storage.state_map[run_arg.iens] = (
+        RealizationStateEnum.STATE_HAS_DATA
+        if final_result.status == LoadStatus.LOAD_SUCCESSFUL
+        else RealizationStateEnum.STATE_LOAD_FAILURE
+    )
+
+    return final_result
+
+
+def forward_model_exit(run_arg: RunArg, *_: Tuple[Any]) -> LoadResult:
+    run_arg.ensemble_storage.state_map[
+        run_arg.iens
+    ] = RealizationStateEnum.STATE_LOAD_FAILURE
+    return LoadResult(None, "")
---
+++ packages/ert/ert-5.0.0rc3-cp38-cp38-macosx_10_15_x86_64.whl/ert/shared/hook_implementations/workflows/export_misfit_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+from ert._c_wrappers.enkf.enums import RealizationStateEnum
+from ert._c_wrappers.job_queue import ErtScript
+from ert.analysis._es_update import _get_obs_and_measure_data
+from ert.exceptions import StorageError
+
+
+class ExportMisfitDataJob(ErtScript):
+    """
+    Will export misfit per observation and realization to a hdf file.
+    The hdf file has the observation as key, and the misfit as values.
+    The filename is "misfit.hdf" by default, but can be overridden by giving
+    the filename as the first parameter:
+    EXPORT_MISFIT_DATA path/to/output.hdf
+    The misfit its calculated as follows:
+    ((response_value - observation_data) / observation_std)**2
+    """
+
+    def run(self, target_file=None):
+        ert = self.ert()
+
+        if target_file is None:
+            target_file = "misfit.hdf"
+        realizations = self.ensemble.realization_list(
+            RealizationStateEnum.STATE_HAS_DATA
+        )
+
+        if not realizations:
+            raise StorageError("No responses loaded")
+
+        all_observations = [(n.observation_key, []) for n in ert.getObservations()]
+        measured_data, obs_data = _get_obs_and_measure_data(
+            ert.getObservations(), self.ensemble, all_observations, realizations
+        )
+        joined = obs_data.join(measured_data, on=["data_key", "axis"], how="inner")
+        misfit = pd.DataFrame(index=joined.index)
+        for col in measured_data:
+            misfit[col] = ((joined["OBS"] - joined[col]) / joined["STD"]) ** 2
+        misfit.groupby("key").sum().T.to_hdf(target_file, key="misfit", mode="w")
