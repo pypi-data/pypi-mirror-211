8b5ffa9dfcdb0a3c46ad9771181bac3d2f55cd1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:28 +0000

    Add package aix-caller/0.1.0/aix-caller-0.1.0.tar.gz

---
+++ packages/aix-caller/aix-caller-0.1.0.tar.gz/aix-caller-0.1.0/aix_caller/__init__.py
@@ -0,0 +1,4 @@
+__author__ = """Alessio Vaccaro"""
+__version__ = '0.1.0'
+
+from aix_caller.module import makeRequest
---
+++ packages/aix-caller/aix-caller-0.1.0.tar.gz/aix-caller-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='aix-caller',
+    version='0.1.0',    
+    description='',
+    url='',
+    author='Alessio Vaccaro',
+    author_email='',
+    license='BSD 2-clause',
+    packages=['aix_caller'],
+    install_requires=['requests'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
