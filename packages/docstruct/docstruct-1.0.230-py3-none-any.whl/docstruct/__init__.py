50489924bec3a854924a0ab71da6c03f869572ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:55 +0000

    Add package docstruct/1.0.230/docstruct-1.0.230-py3-none-any.whl

---
+++ packages/docstruct/docstruct-1.0.230-py3-none-any.whl/docstruct/__init__.py
@@ -0,0 +1,24 @@
+from .bounding_box import BoundingBox
+from .drawer import Drawer
+from .point import Point
+from .segment import Segment, Segment2D
+from .text_block import (
+    Character,
+    Document,
+    Page,
+    Area,
+    Line,
+    Paragraph,
+    TextBlock,
+    Word,
+    Table,
+    TableColumn,
+    TableCell,
+)
+from .vis_line import VisLine, VisLineOrientation
+from .text_block_splitter import TextBlockSplitter
+from .spatial_grid_indexing import SpatialGrid
+from ..version import VERSION
+
+
+__version__ = VERSION
