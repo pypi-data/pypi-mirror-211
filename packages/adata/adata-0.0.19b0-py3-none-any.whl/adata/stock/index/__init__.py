c741061b427f21b3ac52dc0d48c507f3cd52f09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:20 +0000

    Add package adata/0.0.19b0/adata-0.0.19b0-py3-none-any.whl

---
+++ packages/adata/adata-0.0.19b0-py3-none-any.whl/adata/stock/index/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: 指数相关数据
+@author: 1nchaos
+@time:2022/9/19
+@log: 
+"""
+from adata.stock.index.stock_index import StockIndex
+
+
+class Index(StockIndex):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+index = Index()
