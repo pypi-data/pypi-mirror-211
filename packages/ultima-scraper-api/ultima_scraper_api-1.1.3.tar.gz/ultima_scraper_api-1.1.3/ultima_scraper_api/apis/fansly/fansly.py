b0cfc566e2d7b0c8fe3a46d49610ecaf66743cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:51 +0000

    Add package ultima-scraper-api/1.1.3/ultima_scraper_api-1.1.3.tar.gz

---
+++ packages/ultima-scraper-api/ultima_scraper_api-1.1.3.tar.gz/ultima_scraper_api-1.1.3/ultima_scraper_api/apis/fansly/fansly.py
@@ -0,0 +1,82 @@
+from typing import Any, Literal, Optional, Union
+
+from ultima_scraper_api.apis.api_streamliner import StreamlinedAPI
+from ultima_scraper_api.apis.fansly.classes.auth_model import create_auth
+from ultima_scraper_api.apis.fansly.classes.extras import AuthDetails, endpoint_links
+from ultima_scraper_api.apis.fansly.classes.user_model import create_user
+from ultima_scraper_api.classes.make_settings import Config
+
+
+class FanslyAPI(StreamlinedAPI):
+    def __init__(self, config: Config) -> None:
+        self.site_name: Literal["Fansly"] = "Fansly"
+        StreamlinedAPI.__init__(self, self, config)
+        self.auths: list[create_auth] = []
+        self.users: dict[int, create_user] = {}
+        self.endpoint_links = endpoint_links
+
+    def get_auth(self, identifier: Union[str, int]) -> Optional[create_auth]:
+        final_auth = None
+        for auth in self.auths:
+            if auth.id == identifier:
+                final_auth = auth
+            elif auth.username == identifier:
+                final_auth = auth
+            if final_auth:
+                break
+        return final_auth
+
+    async def remove_invalid_auths(self):
+        for auth in self.auths.copy():
+            if not auth.check_authed():
+                await self.remove_auth(auth)
+
+    async def remove_auth(self, auth: create_auth):
+        await auth.session_manager.active_session.close()
+        self.auths.remove(auth)
+
+    def create_auth_details(self, auth_json: dict[str, Any] = {}) -> AuthDetails:
+        """If you've got a auth.json file, you can load it into python and pass it through here.
+
+        Args:
+            auth_json (dict[str, Any], optional): [description]. Defaults to {}.
+
+        Returns:
+            auth_details: [auth_details object]
+        """
+        return AuthDetails(**auth_json).upgrade_legacy(auth_json)
+
+    class ContentTypes:
+        def __init__(self) -> None:
+            self.Stories = []
+            self.Posts = []
+            self.Chats = []
+            self.Messages = []
+            self.Highlights = []
+            self.MassMessages = []
+
+        def __iter__(self):
+            for attr, value in self.__dict__.items():
+                yield attr, value
+
+        def get_keys(self):
+            return [item[0] for item in self]
+
+    class MediaTypes:
+        def __init__(self) -> None:
+            self.Images = ["photo", "image"]
+            self.Videos = ["video", "stream", "gif"]
+            self.Audios = ["audio"]
+            self.Texts = ["text"]
+
+        def get_keys(self):
+            return [item[0] for item in self.__dict__.items()]
+
+        def find_by_value(self, value: str):
+            final_media_type = None
+            for media_type, alt_media_types in self.__dict__.items():
+                if value in alt_media_types:
+                    final_media_type = media_type
+            if not final_media_type:
+                raise Exception("No media type found")
+            return final_media_type
