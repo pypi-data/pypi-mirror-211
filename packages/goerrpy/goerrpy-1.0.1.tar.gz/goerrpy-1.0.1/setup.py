97ae94577aec9fbda8c998e0f8c938545d7ed26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:18 +0000

    Add package goerrpy/1.0.1/goerrpy-1.0.1.tar.gz

---
+++ packages/goerrpy/goerrpy-1.0.1.tar.gz/goerrpy-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='goerrpy',
+    version='1.0.1',
+    author='Michael Bozhilov',
+    author_email='michaelbozhilov@gmail.com',
+    description="Decorator for simulating Golang's error handling in Python",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/asynchroza/goerrpy',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+)
