a6c25a337ba24c0cf5f0c3913d4b79831184aee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:32 +0000

    Add package nested-django-serializer/1.0.0/nested_django_serializer-1.0.0.tar.gz

---
+++ packages/nested-django-serializer/nested_django_serializer-1.0.0.tar.gz/nested_django_serializer-1.0.0/README.md
@@ -0,0 +1,5 @@
+
+# Nested Django Serializer
+
+An updated version of wadofstuff that is compatible with python3 and Django 4. Used to fully serialize Django model
+foreign key relations.
---
+++ packages/nested-django-serializer/nested_django_serializer-1.0.0.tar.gz/nested_django_serializer-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+import nested_django_serializer.django.serializers
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="nested_django_serializer",
+    version="1.0.0",
+    author="Colton Schneider",
+    description="Modifies the Django serializer such that it fully serializes foreign key relations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=(
+        "nested_django_serializer",
+        "nested_django_serializer.django",
+        "nested_django_serializer.django.serializers"
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Framework :: Django"
+    ],
+    python_requires=">=3.8",
+    install_requires=["django"],
+    license="MIT",
+    keywords="django json serializer"
+)
