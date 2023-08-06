73f0bc6c18c615ac5c42f383f0f466f9cc67d167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package apf-base/1.0.13/apf_base-1.0.13-py3-none-any.whl

---
+++ packages/apf-base/apf_base-1.0.13-py3-none-any.whl/apf/producers/csv.py
@@ -0,0 +1,32 @@
+from apf.producers.generic import GenericProducer
+try:
+    from pandas import json_normalize
+except ImportError:
+    from pandas.io import json_normalize
+
+
+class CSVProducer(GenericProducer):
+    """CSV File Producer.
+
+    .. warning::
+        `CSVProducer` only works for a **single process** step, running it distributed or with
+        multiprocessing can result on issues.
+
+    Parameters
+    ----------
+    FILE_PATH: :class:`str`
+        Output CSV File Path.
+    """
+
+    def __init__(self, config):
+        super().__init__(config=config)
+
+    def produce(self, message=None, **kwargs):
+        """Produce Message to a CSV File.
+
+        Doesn't add the header
+        """
+        serialized_message = json_normalize(message)
+        serialized_message.to_csv(
+            self.config["FILE_PATH"], mode="a+", index=False, header=False
+        )
