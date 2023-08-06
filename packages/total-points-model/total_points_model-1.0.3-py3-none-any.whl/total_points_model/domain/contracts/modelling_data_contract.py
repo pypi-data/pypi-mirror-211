a4525ad5e8c88dfda0731b198a2fd82189a96ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:15 +0000

    Add package total-points-model/1.0.3/total_points_model-1.0.3-py3-none-any.whl

---
+++ packages/total-points-model/total_points_model-1.0.3-py3-none-any.whl/total_points_model/domain/contracts/modelling_data_contract.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+
+@dataclass
+class ModellingDataContract:
+    """ Holds details for defining modelling terms in a single place.
+    """
+    
+    ID_COL = "Match_ID"
+    RESPONSE = "Total_Game_Score"
+    TRAIN_TEST_SPLIT_COL = "ModellingFilter2019"
+    
+    ELO_K_FACTOR = 30
+    
+    team_list = [
+        'Adelaide',
+        'Brisbane Lions',
+        'Carlton',
+        'Collingwood',
+        'Essendon',
+        'Fremantle',
+        'Geelong',
+        'Gold Coast',
+        'Greater Western Sydney',
+        'Hawthorn',
+        'Melbourne',
+        'North Melbourne',
+        'Port Adelaide',
+        'Richmond',
+        'St Kilda',
+        'Sydney',
+        'West Coast',
+        'Western Bulldogs'
+    ]
+    
+    raw_modelling_cols = [
+        "Round_ID",
+        "Home_Ground",
+        "City",
+        "Venue",
+        "Home_Team",
+        "Away_Team",
+        "Q4_Score",
+        "Weather_Description"
+        ]
+    
+    modelling_feature_list = [
+        "Year",
+        "Round",
+        'Kicking_Weather',
+        'State',
+        'Victoria',
+        'Roof',
+        'Finals',
+        'Primary_Home',
+        'Home_Team_Within_State',
+        'Away_Team_Within_State',
+        "ELO_Home",
+        "ELO_Away",
+        'ELO_diff',
+        'ELO_abs_diff',
+        'ELO_probs_Home',
+        'ELO_probs_Away',
+        'ELO_probs_diff',
+        'ELO_probs_abs_diff'
+    ]
