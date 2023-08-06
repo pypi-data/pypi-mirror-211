68da2310074e5c2a52c53cb30d8a648f65b10552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:17 +0000

    Add package imscript/0.7/imscript-0.7.tar.gz

---
+++ packages/imscript/imscript-0.7.tar.gz/imscript-0.7/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools.command.install import install
+class myinstall(install):
+	def run(self):
+		import os
+		os.system("mkdir -p bin")
+		os.system("make -j bin/plambda bin/qauto bin/morsi")
+		install.run(self)
+
+
+import setuptools
+setuptools.setup(
+	name = "imscript",
+	version = "0.7",
+	author = "Enric Meinhardt-Llopis",
+	author_email = "enric.meinhardt@fastmail.com",
+	description = "Image Processing with Unix Pipes",
+	url = "https://github.com/mnhrdt/imscript",
+	classifiers = [
+		"Operating System :: OS Independent",
+		"License :: OSI Approved :: GNU Affero General Public License v3",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Mathematics"
+		],
+	cmdclass = {'install' : myinstall},
+	data_files = [('bin', ['bin/plambda', 'bin/qauto', 'bin/morsi'])]
+)
