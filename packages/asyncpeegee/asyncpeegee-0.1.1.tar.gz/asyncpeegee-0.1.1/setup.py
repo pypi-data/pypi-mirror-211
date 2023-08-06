cface30e15c30b77fe60e59f13801c7a0ce0751d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:58 +0000

    Add package asyncpeegee/0.1.1/asyncpeegee-0.1.1.tar.gz

---
+++ packages/asyncpeegee/asyncpeegee-0.1.1.tar.gz/asyncpeegee-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asyncpeegee",
+    version="0.1.1",
+    license="GPL 3",
+    author="Violet McKinney",
+    author_email="opensource@viomck.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/viomck/peegee",
+    keywords="postgres",
+    install_requires=[
+        "asyncpg",
+        "asyncpg-stubs"
+	],
+    package_data={
+        "peegee": ["__init__.pyi"],
+	}
+)
