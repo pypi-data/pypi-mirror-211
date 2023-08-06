2cc7d2352309c451afcd6a1bc69f5d531a7f055c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:23 +0000

    Add package speedtab/0.1.4.1/speedtab-0.1.4.1.tar.gz

---
+++ packages/speedtab/speedtab-0.1.4.1.tar.gz/speedtab-0.1.4.1/speedtab/__init__.py
@@ -0,0 +1,3 @@
+from speedtab.client import Client, create_token
+from speedtab.enums import MergeType, BorderStyle, HorizontalAlignment, VerticalAlignment, WrapStrategy, ShareRole, ChartType, StackedType, LegendPosition, AxisPosition, DefaultColors, ReadyFormats
+from speedtab.formats import Color, Number, Dollar, Percent, Date, Time, DateTime, Scientific, Border
