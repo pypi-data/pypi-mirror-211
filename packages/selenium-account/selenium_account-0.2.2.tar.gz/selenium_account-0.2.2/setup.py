590824123c706e363a36393140b3ca9f5abe9a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:06 +0000

    Add package selenium-account/0.2.2/selenium_account-0.2.2.tar.gz

---
+++ packages/selenium-account/selenium_account-0.2.2.tar.gz/selenium_account-0.2.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'selenium_account'
+
+setuptools.setup(
+    name="selenium_account",
+    version="0.2.2",
+    author="Kristof",
+    description="selenium_account",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/selenium_account",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'kproxy>=0.0.1',
+        'kstopit>=0.0.13',
+        'selenium-browser>=0.0.12',
+        'setuptools>=67.8.0',
+        'tldextract>=3.1.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
