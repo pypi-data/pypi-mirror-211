787707b246035d82bd7a2a9f3ed138b4a20c4280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:17 +0000

    Add package DE-DataBaseConnect/0.0.68/DE_DataBaseConnect-0.0.68.tar.gz

---
+++ packages/DE-DataBaseConnect/DE_DataBaseConnect-0.0.68.tar.gz/DE_DataBaseConnect-0.0.68/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DE_DataBaseConnect',
+    version='0.0.68',
+    packages=[''],
+    url='https://github.com/DE-DataEng/DE_DataBaseConnect.git',
+    license='MIT',
+    author='Almir J Gomes',
+    author_email='almir.jg@hotmail.com',
+    description='Conector com varias base de dados'
+)
