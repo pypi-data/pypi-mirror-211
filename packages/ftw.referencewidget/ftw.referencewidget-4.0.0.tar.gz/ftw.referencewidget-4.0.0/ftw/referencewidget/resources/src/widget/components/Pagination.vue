b9c451663fc1ccc80581fa28c7693b702f248c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:05 +0000

    Add package ftw.referencewidget/4.0.0/ftw.referencewidget-4.0.0.tar.gz

---
+++ packages/ftw.referencewidget/ftw.referencewidget-4.0.0.tar.gz/ftw.referencewidget-4.0.0/ftw/referencewidget/resources/src/widget/components/Pagination.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav aria-label="Pagination for this listing" v-if="batching">
+    <ul class="pagination">
+      <li :class="batching.prev ? 'page-item' : 'page-item disabled'">
+        <a href="#" @click.prevent="triggerPrevious" class="page-link">
+          {{ $i18n("Previous") }}
+        </a>
+      </li>
+      <li :class="batching.next ? 'page-item' : 'page-item disabled'">
+        <a href="#" @click.prevent="triggerNext" class="page-link">
+          {{ $i18n("Next") }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  props: {
+    batching: {
+      type: Object,
+      required: true,
+      default: () => {
+        return null;
+      },
+    },
+  },
+  methods: {
+    triggerNext() {
+      this.$emit("next", this.batching.next);
+    },
+    triggerPrevious() {
+      this.$emit("previous", this.batching.prev);
+    },
+  },
+};
+</script>
