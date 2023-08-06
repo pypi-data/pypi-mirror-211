e33392ce7b88191a8bd0118c1d9c6877affbff44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:34 +0000

    Add package domain-paas-sdk-python/1.0.5/domain_paas_sdk_python-1.0.5-py3-none-any.whl

---
+++ packages/domain-paas-sdk-python/domain_paas_sdk_python-1.0.5-py3-none-any.whl/digital_twin_service/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from digital_twin_service.api.da_config_api import DAConfigApi
+from digital_twin_service.api.device_indicator_api import DeviceIndicatorApi
+from digital_twin_service.api.model_boundary_api import ModelBoundaryApi
+from digital_twin_service.api.model_point_api import ModelPointApi
+from digital_twin_service.api.point_data_type_map_api import PointDataTypeMapApi
+from digital_twin_service.api.point_map_api import PointMapApi
+from digital_twin_service.api.static_config_api import StaticConfigApi
