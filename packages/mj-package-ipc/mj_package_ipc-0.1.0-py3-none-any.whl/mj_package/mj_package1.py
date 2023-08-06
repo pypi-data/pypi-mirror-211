88180b0a62063cff23f8687b9193ce3d382f0879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:33 +0000

    Add package mj-package-ipc/0.1.0/mj_package_ipc-0.1.0-py3-none-any.whl

---
+++ packages/mj-package-ipc/mj_package_ipc-0.1.0-py3-none-any.whl/mj_package/mj_package1.py
@@ -0,0 +1,10 @@
+""" Simple python module """
+class MyPackage:
+    """
+    A simple class to publish py package
+    """
+
+    def print_params(self, name, account_id):
+        """ Method to print name"""
+        print(name, account_id)
+
---
+++ packages/mj-package-ipc/mj_package_ipc-0.1.0-py3-none-any.whl/mj_package/mj_package2.py
@@ -0,0 +1,3 @@
+from mj_package1 import MyPackage
+
+MyPackage().print_params("Madhusudan J", "P3198345")
