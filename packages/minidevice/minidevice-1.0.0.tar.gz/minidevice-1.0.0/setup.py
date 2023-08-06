bef6260356e2e493ecd0c136de8dfe3c6d416d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:26 +0000

    Add package minidevice/1.0.0/minidevice-1.0.0.tar.gz

---
+++ packages/minidevice/minidevice-1.0.0.tar.gz/minidevice-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='minidevice',
+      version='1.0.0',
+      description='Android Auto Pypi',
+      author='KateTseng',
+      author_email='Kate.TsengK@outlook.com',
+      url='https://www.python.org/',
+      license='MIT',
+      keywords='game',
+      project_urls={},
+      packages=['minidevice'],
+      install_requires=['opencv-python>=4.7.0.72', 'uiautomator2>=2.16.23'],
+      python_requires='>=3'
+     )
