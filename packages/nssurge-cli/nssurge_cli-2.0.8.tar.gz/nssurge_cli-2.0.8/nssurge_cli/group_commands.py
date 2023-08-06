af71499eb6ef50306a8f145c45a88520ea1ea72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:05 +0000

    Add package nssurge-cli/2.0.8/nssurge_cli-2.0.8.tar.gz

---
+++ packages/nssurge-cli/nssurge_cli-2.0.8.tar.gz/nssurge_cli-2.0.8/nssurge_cli/group_commands.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+from nssurge_cli.config import get_config
+from nssurge_cli.utils import (
+    typer_output_dict,
+)
+
+# use_local_nssurge_api_module()
+from nssurge_api.api import SurgeAPIClient
+from nssurge_api.types import (
+    Policy,
+    PolicyGroup,
+)
+import typer
+import asyncio
+from nssurge_cli.policy_commands import (
+    complete_policy,
+    complete_policy_for_policy_group,
+)
+
+app = typer.Typer(name="group")
+
+
+async def get_policy_group(policy_group: PolicyGroup = typer.Argument(None)) -> dict:
+    """
+    Get all policy groups, or a specific one.
+    """
+    async with SurgeAPIClient(*get_config()) as client:
+        try:
+            return await (await client.get_policy_group(policy_group)).json()
+        except Exception as e:
+            typer.secho(f"Error: {e}", err=True)
+            typer.secho(
+                'Using proxy as an arg causes Surge for Mac to crash as of version 4.5.0',
+                err=True,
+                fg='red',
+            )
+            return {}
+
+
+# @app.command('group')
+# @app.callback(invoke_without_command=True)
+@app.command('policy')
+def policy_group(  # ctx: typer.Context,
+    policy_group: PolicyGroup = typer.Argument(
+        ..., help="Policy group name", autocompletion=complete_policy
+    ),
+    # output_json: bool = typer.Option(False, "--json", "-j"),
+    # pretty_print: bool = typer.Option(False, "--pretty", "-p"),
+    # rich_print: bool = typer.Option(False, "--rich", "-r"),
+):
+    """
+    Get all policy groups, or a specific one.
+    """
+    # if ctx.invoked_subcommand is not None:
+    #     return
+    policy_group_dict = asyncio.run(get_policy_group(policy_group))
+    # typer_output_dict(policy_group_dict, output_json, pretty_print, rich_print)
+    policy = policy_group_dict['policy']
+    if policy is None:
+        typer.echo(f"Policy group {policy_group} not found")
+    else:
+        typer.secho(f'The policy for group {policy_group} is {policy}')
+
+
+async def get_policy_group_test_results() -> dict:
+    """
+    Get all policy groups, or a specific one.
+    """
+    async with SurgeAPIClient(*get_config()) as client:
+        return await (await client.get_policy_group_test_results()).json()
+
+
+@app.command("test-results")
+def policy_group_test_results(
+    output_json: bool = typer.Option(False, "--json", "-j"),
+    pretty_print: bool = typer.Option(False, "--pretty", "-p"),
+    rich_print: bool = typer.Option(False, "--rich", "-r"),
+):
+    """
+    Get all policy groups, or a specific one.
+    """
+    policy_group_test_dict = asyncio.run(get_policy_group_test_results())
+    typer_output_dict(policy_group_test_dict, output_json, pretty_print, rich_print)
+
+
+async def select_group(policy_group: PolicyGroup, policy: Policy):
+    """
+    Select a policy group.
+    """
+    async with SurgeAPIClient(*get_config()) as client:
+        return await client.set_policy_group(policy_group, policy)
+
+
+@app.command("select")
+def select_group_command(
+    policy: PolicyGroup = typer.Argument(
+        ..., help='Policy group name', autocompletion=complete_policy
+    ),
+    policy_selected: Policy = typer.Argument(
+        ..., help='Policy name', autocompletion=complete_policy_for_policy_group
+    ),
+):
+    """
+    Select a policy group.
+    """
+    asyncio.run(select_group(policy, policy_selected))
+
+
+async def test_policy_group(policy_group: PolicyGroup) -> dict:
+    """
+    Test a policy group.
+    """
+    async with SurgeAPIClient(*get_config()) as client:
+        resp = await client.test_policy_group(policy_group)
+        return await resp.json()
+
+
+@app.command("test")
+def test_policy_group_command(
+    policy_group: PolicyGroup = typer.Argument(
+        ..., help="Policy group name", autocompletion=complete_policy
+    ),
+    output_json: bool = typer.Option(False, "--json", "-j"),
+    pretty_print: bool = typer.Option(False, "--pretty", "-p"),
+    rich_print: bool = typer.Option(False, "--rich", "-r"),
+):
+    """
+    Test a policy group.
+    """
+    test_dict = asyncio.run(test_policy_group(policy_group))
+    typer_output_dict(test_dict, output_json, pretty_print, rich_print)
+
+
+@app.callback()
+def group_command_callback():
+    """For policy groups"""
---
+++ packages/nssurge-cli/nssurge_cli-2.0.8.tar.gz/nssurge_cli-2.0.8/nssurge_cli/outbound_commands.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from nssurge_cli.config import get_config
+
+# use_local_nssurge_api_module()
+from nssurge_api.api import SurgeAPIClient
+from nssurge_api.types import OutboundMode
+import typer
+import asyncio
+
+app = typer.Typer(name="outbound")
+
+
+async def get_set_outbound(
+    outbound_mode: OutboundMode = typer.Argument(None),
+) -> OutboundMode:
+    """
+    Get or set the outbound mode.
+    """
+    async with SurgeAPIClient(*get_config()) as client:
+        if outbound_mode is not None:
+            await client.set_outbound_mode(outbound_mode)
+        new_outbound_mode = (await (await client.get_outbound_mode()).json())["mode"]
+        match new_outbound_mode:
+            case 'direct':
+                color = 'green'
+            case 'proxy':
+                color = 'yellow'
+            case 'rule':
+                color = 'blue'
+            case _:
+                raise ValueError(f"Unknown outbound mode: {new_outbound_mode}")
+        new_outbound_mode_colored = typer.style(new_outbound_mode, fg=color)
+        if outbound_mode is not None:
+            typer.secho(f"Set outbound mode to {new_outbound_mode_colored}")
+        else:
+            typer.secho(f"Current outbound mode: {new_outbound_mode_colored}")
+        return OutboundMode[new_outbound_mode]
+
+
+# @app.command("outbound")
+@app.callback(invoke_without_command=True)
+def outbound(ctx: typer.Context, outbound_mode: OutboundMode = typer.Argument(None)):
+    """
+    Get or set the outbound mode.
+    """
+    if ctx.invoked_subcommand is not None:
+        return
+    asyncio.run(get_set_outbound(outbound_mode))
