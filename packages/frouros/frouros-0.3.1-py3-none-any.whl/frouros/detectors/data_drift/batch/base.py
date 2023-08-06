ec322089e0feb47f9c961d370c738baef529ecec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:30 +0000

    Add package frouros/0.3.1/frouros-0.3.1-py3-none-any.whl

---
+++ packages/frouros/frouros-0.3.1-py3-none-any.whl/frouros/detectors/data_drift/batch/base.py
@@ -0,0 +1,110 @@
+"""Base data drift batch module."""
+
+import abc
+from typing import Any, Dict, Optional, List, Tuple, Union
+
+import numpy as np  # type: ignore
+
+from frouros.callbacks.base import BaseCallback
+from frouros.detectors.data_drift.base import (
+    BaseDataDrift,
+    BaseDataType,
+    BaseStatisticalType,
+)
+from frouros.detectors.data_drift.exceptions import (
+    MismatchDimensionError,
+)
+
+
+class BaseDataDriftBatch(BaseDataDrift):
+    """Abstract class representing a data drift batch detector."""
+
+    def __init__(
+        self,
+        data_type: BaseDataType,
+        statistical_type: BaseStatisticalType,
+        callbacks: Optional[Union[BaseCallback, List[BaseCallback]]] = None,
+    ) -> None:
+        """Init method.
+
+        :param data_type: data type
+        :type data_type: BaseDataType
+        :param statistical_type: statistical type
+        :type statistical_type: BaseStatisticalType
+        :param callbacks: callbacks
+        :type callbacks: Optional[Union[Callback], List[BaseCallback]]
+        """
+        super().__init__(
+            callbacks=callbacks,
+            data_type=data_type,
+            statistical_type=statistical_type,
+        )
+        for callback in self.callbacks:  # type: ignore
+            callback.set_detector(detector=self)
+
+    def _fit(
+        self,
+        X: np.ndarray,  # noqa: N803
+    ) -> None:
+        self.X_ref = X  # type: ignore
+
+    def compare(
+        self,
+        X: np.ndarray,  # noqa: N803
+        **kwargs,
+    ) -> Tuple[np.ndarray, Dict[str, Any]]:
+        """Compare values.
+
+        :param X: feature data
+        :type X: numpy.ndarray
+        :return: compare result and callbacks logs
+        :rtype: Tuple[np.ndarray, Dict[str, Any]]
+        """
+        for callback in self.callbacks:  # type: ignore
+            callback.on_compare_start()  # type: ignore
+        result = self._compare(X=X, **kwargs)
+        for callback in self.callbacks:  # type: ignore
+            callback.on_compare_end(  # type: ignore
+                result=result,
+                X_ref=self.X_ref,
+                X_test=X,
+            )
+
+        callbacks_logs = self._get_callbacks_logs()
+        return result, callbacks_logs
+
+    def _check_compare_dimensions(self, X: np.ndarray) -> None:  # noqa: N803
+        try:
+            if self.X_ref.shape[1] != X.shape[1]:  # type: ignore
+                raise MismatchDimensionError(
+                    f"Dimensions of X_ref ({self.X_ref.shape[-1]}) "  # type: ignore
+                    f"and X ({X.shape[-1]}) must be equal"
+                )
+        except IndexError as e:
+            if self.X_ref.ndim != X.ndim:  # type: ignore
+                raise MismatchDimensionError(f"Dimensions of X ({X.ndim})") from e
+
+    def _specific_checks(self, X: np.ndarray) -> None:  # noqa: N803
+        self._check_compare_dimensions(X=X)
+
+    @abc.abstractmethod
+    def _apply_method(
+        self, X_ref: np.ndarray, X: np.ndarray, **kwargs  # noqa: N803
+    ) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def _compare(
+        self,
+        X: np.ndarray,  # noqa: N803
+        **kwargs,
+    ) -> np.ndarray:
+        pass
+
+    def _get_result(
+        self, X: np.ndarray, **kwargs  # noqa: N803
+    ) -> Union[List[float], List[Tuple[float, float]], Tuple[float, float]]:
+        result = self._apply_method(  # type: ignore # pylint: disable=not-callable
+            X_ref=self.X_ref, X=X, **kwargs
+        )
+        return result
