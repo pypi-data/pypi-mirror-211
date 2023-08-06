710a2ddf55fbf1a22e3825e7a2b24a90473e2a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:33 +0000

    Add package selecrawler/1.0.1/SeleCrawler-1.0.1.tar.gz

---
+++ packages/selecrawler/SeleCrawler-1.0.1.tar.gz/SeleCrawler-1.0.1/selecrawler/__init__.py
@@ -0,0 +1,2 @@
+from . import selenium_controller
+
---
+++ packages/selecrawler/SeleCrawler-1.0.1.tar.gz/SeleCrawler-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup_args = dict(
+    name='SeleCrawler',
+    version='1.0.1',
+    description='Collection Of Tools To Ease Selenium Operation',
+    packages=find_packages(),
+    author='Gee Tech Labs',
+    author_email='geetechlabs@gmail.com',
+    keywords=['SeleCrawler', 'Sele Crawler', 'Selenium Crawler', 'Selenium', 'Crawler', 'Browser', 'BrowserAutomation', 'Automation', 'Webdriver', 'web', 'driver', 'web driver']
+)
+
+install_requires = [
+    'webdriver-manager==3.8.5',
+    'selenium==4.8.2',
+    'undetected-chromedriver==3.4.6'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
