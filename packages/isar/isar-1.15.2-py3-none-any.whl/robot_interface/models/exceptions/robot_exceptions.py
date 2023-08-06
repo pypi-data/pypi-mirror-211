9647a18351fd2ce26128e73945deab9b7649818a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:42 +0000

    Add package isar/1.15.2/isar-1.15.2-py3-none-any.whl

---
+++ packages/isar/isar-1.15.2-py3-none-any.whl/robot_interface/models/exceptions/robot_exceptions.py
@@ -0,0 +1,26 @@
+class RobotException(Exception):
+    pass
+
+
+class RobotCommunicationException(RobotException):
+    pass
+
+
+class RobotInfeasibleStepException(RobotException):
+    pass
+
+
+class RobotInfeasibleMissionException(RobotException):
+    pass
+
+
+class RobotInvalidResponseException(RobotException):
+    pass
+
+
+class RobotMapException(RobotException):
+    pass
+
+
+class RobotInvalidTelemetryException(RobotException):
+    pass
