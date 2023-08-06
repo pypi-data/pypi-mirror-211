39b597a7a13a0bbfa7c7b281c7e40450c9bda2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:31 +0000

    Add package intra42/0.1.5/intra42-0.1.5.tar.gz

---
+++ packages/intra42/intra42-0.1.5.tar.gz/intra42-0.1.5/intrascraper/__init__.py
@@ -0,0 +1,7 @@
+from .intrascraper import IntraScraper, SCRAPER
+from .intrascraper import (get_all_notifs,
+                           get_ssh_key,
+                           delete_ssh_key,
+                           set_ssh_key,
+                           get_location_data,
+                           get_clusters_map)
