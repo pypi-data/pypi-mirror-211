e4aabb98a8ff24241422a47853c907df8164d08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:05 +0000

    Add package nqgreenapi/1.0.0a2/nqgreenapi-1.0.0a2.tar.gz

---
+++ packages/nqgreenapi/nqgreenapi-1.0.0a2.tar.gz/nqgreenapi-1.0.0a2/nqgreenapi/serializers.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023 Inqana Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class StatusEnum(str, Enum):
+    sent = "sent"
+    delivered = "delivered"
+    read = "read"
+    failed = "failed"
+    noAccount = "noAccount"
+    notInGroup = "notInGroup"
+
+
+class OutgoingMessageStatusModel(BaseModel):
+    timestamp: int
+    status: StatusEnum
+    idMessage: str
