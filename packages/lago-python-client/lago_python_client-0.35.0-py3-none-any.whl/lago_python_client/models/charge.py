f90e6c2f4bf3de8a2325722704bbbccd322f46df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:14 +0000

    Add package lago-python-client/0.35.0/lago_python_client-0.35.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.35.0-py3-none-any.whl/lago_python_client/models/charge.py
@@ -0,0 +1,41 @@
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel
+
+from ..base_model import BaseResponseModel
+
+
+class GroupProperties(BaseModel):
+    group_id: Optional[str]
+    values: Optional[Dict[str, Any]]
+
+
+class Charge(BaseModel):
+    id: Optional[str]
+    billable_metric_id: Optional[str]
+    charge_model: Optional[str]
+    pay_in_advance: Optional[bool]
+    invoiceable: Optional[bool]
+    min_amount_cents: Optional[int]
+    properties: Optional[Dict[str, Any]]
+    group_properties: Optional[GroupProperties]
+
+
+class Charges(BaseModel):
+    __root__: List[Charge]
+
+
+class ChargeResponse(BaseResponseModel):
+    lago_id: Optional[str]
+    lago_billable_metric_id: Optional[str]
+    billable_metric_code: Optional[str]
+    charge_model: Optional[str]
+    pay_in_advance: Optional[bool]
+    invoiceable: Optional[bool]
+    min_amount_cents: Optional[int]
+    properties: Optional[Dict[str, Any]]
+    group_properties: Optional[GroupProperties]
+
+
+class ChargesResponse(BaseResponseModel):
+    __root__: List[ChargeResponse]
