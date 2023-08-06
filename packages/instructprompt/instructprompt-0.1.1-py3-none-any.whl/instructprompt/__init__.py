cdc4c30c0bc4cb60da7e56ea68c0c257aefe3164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:00 +0000

    Add package instructprompt/0.1.1/instructprompt-0.1.1-py3-none-any.whl

---
+++ packages/instructprompt/instructprompt-0.1.1-py3-none-any.whl/instructprompt/__init__.py
@@ -0,0 +1,28 @@
+__version__ == "0.1.1"
+
+import chromadb
+
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="user_instructprompt_collection")
+
+def add(instruction: str): 
+    collection.add(
+        documents=[instruction],
+        ids=[str(id)]
+    )
+    print("✅ instruction added")
+    return
+
+def list_instructions(): 
+    return collection.get()["documents"]
+
+def query_instructions(query: str): 
+    results = collection.query(
+        query_texts=[query],
+        n_results=5
+    )
+    instructions = ""
+    for document in results["documents"][0]: 
+        instructions += document + "\n"
+    return instructions
+
