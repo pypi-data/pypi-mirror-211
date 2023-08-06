3dab00d75da23e4bb6d4f97abbdc7aab9b6f18ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:50 +0000

    Add package atproto/0.0.11/atproto-0.0.11-py3-none-any.whl

---
+++ packages/atproto/atproto-0.0.11-py3-none-any.whl/atproto/codegen/models/builder.py
@@ -0,0 +1,100 @@
+import typing as t
+from pathlib import Path
+
+from atproto.lexicon import models
+from atproto.lexicon.parser import lexicon_parse_dir
+from atproto.nsid import NSID
+
+_LEX_DEF_TYPES_FOR_PARAMS = {
+    models.LexDefinitionType.QUERY,
+    models.LexDefinitionType.PROCEDURE,
+    models.LexDefinitionType.SUBSCRIPTION,
+}
+_LEX_DEF_TYPES_FOR_RESPONSES = {models.LexDefinitionType.QUERY, models.LexDefinitionType.PROCEDURE}
+_LEX_DEF_TYPES_FOR_REFS = {models.LexDefinitionType.QUERY, models.LexDefinitionType.PROCEDURE}
+_LEX_DEF_TYPES_FOR_DATA = {models.LexDefinitionType.PROCEDURE}
+_LEX_DEF_TYPES_FOR_RECORDS = {models.LexDefinitionType.RECORD}
+_LEX_DEF_TYPES_FOR_DEF = {
+    models.LexDefinitionType.OBJECT,
+    models.LexPrimitiveType.STRING,
+    models.LexDefinitionType.TOKEN,
+    models.LexDefinitionType.ARRAY,
+}
+
+LexDefs = t.Dict[
+    str,
+    t.Union[
+        models.LexXrpcProcedure,
+        models.LexXrpcQuery,
+        models.LexObject,
+        models.LexToken,
+        models.LexString,
+        models.LexRecord,
+    ],
+]
+LexDB = t.Dict[NSID, LexDefs]
+
+
+class _LexiconDir:
+    dir_path: t.Optional[Path]
+
+    def __init__(self, default_path: Path = None) -> None:
+        self.dir_path = default_path
+
+    def set(self, path: Path) -> None:
+        self.dir_path = path
+
+    def get(self) -> Path:
+        return self.dir_path
+
+
+lexicon_dir = _LexiconDir()
+
+
+def _filter_defs_by_type(defs: t.Dict[str, models.LexDefinition], def_types: set) -> LexDefs:
+    return {k: v for k, v in defs.items() if v.type in def_types}
+
+
+def _build_nsid_to_defs_map(lexicons: t.List[models.LexiconDoc], def_types: set) -> LexDB:
+    result = {}
+
+    for lexicon in lexicons:
+        nsid = NSID.from_str(lexicon.id)
+        defs = _filter_defs_by_type(lexicon.defs, def_types)
+        if defs:
+            result[nsid] = defs
+
+    return result
+
+
+def build_params_models() -> LexDB:
+    return _build_nsid_to_defs_map(lexicon_parse_dir(lexicon_dir.get()), _LEX_DEF_TYPES_FOR_PARAMS)
+
+
+def build_data_models() -> LexDB:
+    return _build_nsid_to_defs_map(lexicon_parse_dir(lexicon_dir.get()), _LEX_DEF_TYPES_FOR_DATA)
+
+
+def build_response_models() -> LexDB:
+    return _build_nsid_to_defs_map(lexicon_parse_dir(lexicon_dir.get()), _LEX_DEF_TYPES_FOR_RESPONSES)
+
+
+def build_def_models() -> LexDB:
+    return _build_nsid_to_defs_map(lexicon_parse_dir(lexicon_dir.get()), _LEX_DEF_TYPES_FOR_DEF)
+
+
+def build_record_models() -> LexDB:
+    return _build_nsid_to_defs_map(lexicon_parse_dir(lexicon_dir.get()), _LEX_DEF_TYPES_FOR_RECORDS)
+
+
+def build_refs_models() -> LexDB:
+    return _build_nsid_to_defs_map(lexicon_parse_dir(lexicon_dir.get()), _LEX_DEF_TYPES_FOR_REFS)
+
+
+if __name__ == '__main__':
+    build_params_models()
+    build_data_models()
+    build_response_models()
+    build_def_models()
+    build_record_models()
+    build_refs_models()
