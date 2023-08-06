f69395e932b5f6809f285c29eafc15b0a190200b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:33 +0000

    Add package Python-Road/0.0.0.4/Python_Road-0.0.0.4.tar.gz

---
+++ packages/Python-Road/Python_Road-0.0.0.4.tar.gz/Python_Road-0.0.0.4/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="Python_Road",
+      version="0.0.0.4",
+      description="模块",
+      author="sunruiyang",
+      author_email="dogsmallsmile@163.com",
+      packages=[""])
