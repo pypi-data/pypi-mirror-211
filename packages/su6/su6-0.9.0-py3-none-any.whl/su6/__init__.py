f4ff9eef464c5db5d5891e0274d24067bd350a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:47 +0000

    Add package su6/0.9.0/su6-0.9.0-py3-none-any.whl

---
+++ packages/su6/su6-0.9.0-py3-none-any.whl/su6/__init__.py
@@ -0,0 +1,9 @@
+"""
+This file exposes 'app' to the module.
+"""
+
+# SPDX-FileCopyrightText: 2023-present Robin van der Noord <robinvandernoord@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .cli import app  # noqa: import is there for library reasons
