805dfbc7bea2f306a6a3e711647378f7cd81a948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:21 +0000

    Add package pginter/0.2.1/pginter-0.2.1.tar.gz

---
+++ packages/pginter/pginter-0.2.1.tar.gz/pginter-0.2.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pginter",
+    version="0.2.1",
+    author="Nilusink",
+    author_email="nilusink@protonmail.com",
+    description="A python GUI interface, based on pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nilusink/PgInter",
+    # project_urls={
+    #     "Official Website": "http://info.fridrich.xyz",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.11",
+    include_package_data=True
+    # data_files=[
+    #     ('pginter/theme/themes/', ['src/pginter/theme/themes/default.json'])
+    # ]
+)
