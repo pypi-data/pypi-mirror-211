1320686948f9a5131477a69c823c6f3c1ae4003a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:02 +0000

    Add package airscript/0.0.2/airscript-0.0.2.tar.gz

---
+++ packages/airscript/airscript-0.0.2.tar.gz/airscript-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'airscript Type derivation package'
+
+setup(
+    name="airscript",
+    version=VERSION,
+    author="ITisl",
+    author_email="1831207432@qq.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', encoding="UTF8").read(),
+    packages=find_packages(),
+    keywords=['python', "airscript"],
+    license="MIT",
+    url="https://github.com/itisl2220/airscript",
+)
