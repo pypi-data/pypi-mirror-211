a77fb7400d2b59c0082e5217398bf27710e8a2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:19 +0000

    Add package json2csv/0.0.8/json2csv-0.0.8.tar.gz

---
+++ packages/json2csv/json2csv-0.0.8.tar.gz/json2csv-0.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='json2csv',
+    version='0.0.8',
+    description='Library for converting JSON to CSV',
+    author='Ed',
+    author_email='agabsed@gmail.com',
+    packages=['json2csv'],
+    install_requires=[],
+)
