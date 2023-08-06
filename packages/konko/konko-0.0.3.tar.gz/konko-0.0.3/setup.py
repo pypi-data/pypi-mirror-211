99e47df69b7466b9b981b819d0ae06ae766a99c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:11 +0000

    Add package konko/0.0.3/konko-0.0.3.tar.gz

---
+++ packages/konko/konko-0.0.3.tar.gz/konko-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='konko',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'ray'                 
+    ],
+)
