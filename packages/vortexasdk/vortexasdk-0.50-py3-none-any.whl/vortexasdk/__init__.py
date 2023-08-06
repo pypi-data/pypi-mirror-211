ab7ee17f46a325c75a8ca3e95fe497cdf3150bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:03 +0000

    Add package vortexasdk/0.50/vortexasdk-0.50-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.50-py3-none-any.whl/vortexasdk/__init__.py
@@ -0,0 +1,47 @@
+"""Vortexa Python SDK."""
+# noinspection PyUnresolvedReferences
+from vortexasdk.endpoints import (
+    AssetTanks,
+    Attributes,
+    CargoMovements,
+    CargoTimeSeries,
+    Corporations,
+    Geographies,
+    Products,
+    VesselMovements,
+    Vessels,
+    TonneMilesBreakdown,
+    VesselAvailabilitySearch,
+    VesselAvailabilityTimeseries,
+    VesselAvailabilityBreakdown,
+    Fixtures,
+    FleetUtilisationTimeseries,
+    FleetUtilisationSpeedBreakdown,
+    FleetUtilisationAvgDistanceTimeseries,
+    FleetUtilisationCapacityTimeseries,
+    FleetUtilisationQuantityTimeseries,
+    FleetUtilisationDestinationBreakdown,
+    FleetUtilisationOriginBreakdown,
+    EIAForecasts,
+    OriginBreakdown,
+    DestinationBreakdown,
+    ProductBreakdown,
+    ParentProductBreakdown,
+    MovementStatusBreakdown,
+    VesselClassBreakdown,
+    StorageTerminals,
+    OnshoreInventoriesTimeseries,
+    OnshoreInventoriesSearch,
+    FreightPricingSearch,
+    FreightPricingTimeseries,
+    VoyagesTimeseries,
+    VoyagesCongestionBreakdown,
+    VoyagesTopHits,
+    VoyagesSearchEnriched,
+)
+
+# noinspection PyUnresolvedReferences
+from vortexasdk.version import __version__
+
+# noinspection PyUnresolvedReferences
+from vortexasdk.check_setup import run_all_checks
