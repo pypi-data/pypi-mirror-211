cce3086cc6a600dfd7aa51cd87824bcfb904901b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:36 +0000

    Add package udc/0.3.1/udc-0.3.1-py3-none-any.whl

---
+++ packages/udc/udc-0.3.1-py3-none-any.whl/udc/__init__.py
@@ -0,0 +1,12 @@
+from .main import app  # NOQA F401
+from .types import Getable, Listable, Putable  # NOQA F401
+
+# Legacy vars for quiltplus
+"""
+SEP = "+"
+K_HOST = "_hostname"
+K_PROT = "_protocol"
+K_QRY = "_query"
+K_TOOL = "_tool"
+K_URI = "_uri"
+"""
