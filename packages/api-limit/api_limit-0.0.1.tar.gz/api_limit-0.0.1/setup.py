4a41c983bf88d83930fd93dd558c4af7b8b11dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:15 +0000

    Add package api-limit/0.0.1/api_limit-0.0.1.tar.gz

---
+++ packages/api-limit/api_limit-0.0.1.tar.gz/api_limit-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+import api_limit
+
+setup(
+    name='api_limit',
+    version="v0.0.1",
+    description='API rate limit decorator',
+    long_description="",
+    packages=['api_limit'],
+    install_requires=[],
+)
