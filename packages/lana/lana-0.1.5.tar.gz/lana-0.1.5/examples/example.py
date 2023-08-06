5d756eadcb83f286ad0eb3c5bdf3314ddb49f0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:55 +0000

    Add package lana/0.1.5/lana-0.1.5.tar.gz

---
+++ packages/lana/lana-0.1.5.tar.gz/lana-0.1.5/examples/example.py
@@ -0,0 +1,18 @@
+from lana import Matrix
+
+zeros = Matrix.zeros((3,3))
+print(zeros)
+# print(f"shape: {zeros.shape}, type: {type(zeros)}")
+# print(zeros[0], zeros[0][0])
+for row in zeros:
+     print(row, type(row))
+
+# a = Matrix.ones((2,3))
+# b = Matrix.matrix([[2,2,2],[2,2,2]])
+# c = a + b
+# print(c)
+# d = c - a
+# print(d)
+# e = -d 
+# print(e)
+# print(e.shape, type(e))
