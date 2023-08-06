a03b9541c31f7e69efdb296c0d197fd20b700408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:27 +0000

    Add package ezcord/0.2.2/ezcord-0.2.2-py3-none-any.whl

---
+++ packages/ezcord/ezcord-0.2.2-py3-none-any.whl/ezcord/__init__.py
@@ -0,0 +1,13 @@
+__title__ = "ezcord"
+__author__ = "tibue99"
+__license__ = "MIT"
+__version__ = "0.2.2"
+
+from . import components, emb, logs, times, utils
+from .bot import Bot, BridgeBot, PrefixBot
+from .components import *
+from .emb import *
+from .enums import *
+from .logs import *
+from .times import *
+from .utils import *
