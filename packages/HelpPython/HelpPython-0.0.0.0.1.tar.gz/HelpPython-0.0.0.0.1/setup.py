48aa0335955c89db7ce8489b197e30c7d4c114a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:44 +0000

    Add package HelpPython/0.0.0.0.1/HelpPython-0.0.0.0.1.tar.gz

---
+++ packages/HelpPython/HelpPython-0.0.0.0.1.tar.gz/HelpPython-0.0.0.0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="HelpPython",
+      version="0.0.0.0.1",
+      description="模块",
+      author="sunruiyang",
+      author_email="dogsmallsmile@163.com",
+      packages=[""])
