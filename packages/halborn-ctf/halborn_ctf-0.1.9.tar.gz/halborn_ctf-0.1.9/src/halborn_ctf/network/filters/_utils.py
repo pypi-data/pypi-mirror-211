3832279b12b003afbdaef27c5a31fce8c30b7759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:43 +0000

    Add package halborn-ctf/0.1.9/halborn_ctf-0.1.9.tar.gz

---
+++ packages/halborn-ctf/halborn_ctf-0.1.9.tar.gz/halborn_ctf-0.1.9/src/halborn_ctf/network/filters/_utils.py
@@ -0,0 +1,40 @@
+from ...shell import run as _run
+import json
+
+def generic_filter(filter_file, **kwargs):
+    """Allows running an arbitrary ``mitmdump`` script as a background shell process.
+
+    The ``mitmdump`` will be used in ``upstream`` mode against ``http://{to_host}:{to_port}`` 
+    and listen under ``listen_port``. Any extra arguments provided to the ``generic_filter`` will be
+    transfered to the ``script`` using the ``--set`` command flag on the command line of mitmdump 
+    as JSON encoded string which can be accessed on the script using ``mitmproxy.ctx.options.[varname]``.
+
+    Example:
+        Once the script is created it can be executed using :class:`generic_filter`::
+
+            generic_filter('./filter.py', listen_port=8545, to_port=9999, custom='More data')
+            ...
+            # The script can access the extra **kwargs using ``ctx.options.[kwargname]`` and JSON decoding it
+            custom_data = json.loads(ctx.options.custom)
+
+    Args:
+        script (str): Path of the script to execute
+        listen_port (int): Port that mitmdump will listen on
+        to_port (int): The upstream port to proxy traffic to
+        to_host (str): The upstream host to proxy traffic to. Defaults to '127.0.0.1'.
+    """
+    def _wrapper(listen_port, to_port, to_host='127.0.0.1'):
+        set_args = ' '.join(['--set {0}={1}'.format(k, json.dumps(json.dumps(v))) for k,v in kwargs.items()])
+
+        cmd = f'mitmdump -s {filter_file} --mode upstream:http://{to_host}:{to_port} -p {listen_port} {set_args}'
+        _run(cmd, background=True)
+
+    return _wrapper
+
+# def generic_filter(script, listen_port, to_port, to_host='127.0.0.1', **kwargs):
+
+#     # The double escape is for the cli to handle it as part as the --set definition (ex method="[\"data\"]")
+#     set_args = ' '.join(['--set {0}={1}'.format(k, json.dumps(json.dumps(v))) for k,v in kwargs.items()])
+
+#     cmd = f'mitmdump -s {script} --mode upstream:http://{to_host}:{to_port} -p {listen_port} {set_args}'
+#     _run(cmd, background=True)
