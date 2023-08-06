19aec6bb58e93bdd3802e74e71e3753fb7884c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:07 +0000

    Add package init-thumbnail/0.1.0/init_thumbnail-0.1.0.tar.gz

---
+++ packages/init-thumbnail/init_thumbnail-0.1.0.tar.gz/init_thumbnail-0.1.0/init_thumbnail/main.py
@@ -0,0 +1,58 @@
+import click
+import os
+
+
+
+@click.command(
+        help="Creates template for youtube thumbnail"
+        )
+@click.option(
+        '-i',
+        '--inputpath',
+        type=click.Path(),
+        default="./",
+        show_default=True,
+        help="Input path"
+        )
+def main(inputpath):
+    path_tikz = os.path.dirname(os.path.abspath(inputpath))
+    os.makedirs(f'{path_tikz}/thumbnail', exist_ok=True)
+    path_main = os.path.join(f'{path_tikz}/thumbnail', 'main.tex')
+    
+    try:
+        extract_tex_env(inputfile, outputfile, environment)
+    except:
+        click.echo("Failed to extract_tex_env")
+
+    files = [os.path.basename(f) for f in file_list]
+    with open(path_main, 'w') as file:
+        file.write(f'\\documentclass{{article}}\n')
+        file.write(f'\\usepackage{{v-equation}}\n')
+        file.write(f'\\vgeometry[6][3.375]\n')
+
+        file.write(f'\\begin{{document}}\n')
+        file.write(f'\\vtitle[title]\n')
+        file.write(f'\\vprofile\n')
+        file.write(f'\\vspace*{{\\fill}}\n')
+        file.write(f'\\begin{{center}}\n')
+        file.write(f'\\begin{{tikzpicture}}\n')
+        file.write(f'[remember picture, overlay]\n')
+        file.write(r"""\tzcoor*(current page.center)(O){{$O$}}[bl]""")
+        file.write(f'\n\\end{{tikzpicture}}\n')
+        file.write(f'\\end{{center}}\n')
+
+        file.write(f'\\vspace*{{\\fill}}\n')
+        file.write(f'\\end{{document}}')
+
+
+
+
+
+
+
+
+
+
+
+
+
