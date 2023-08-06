51842c69e430620f0755f88cbf5167480ca7a8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:30 +0000

    Add package userfs/1.1.1/userfs-1.1.1.tar.gz

---
+++ packages/userfs/userfs-1.1.1.tar.gz/userfs-1.1.1/userfs/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=8723ba9122f2cc8413ddb23f05e163b4
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A system-bootstrapping automation and introspection tool."
+PKG_NAME = "userfs"
+VERSION = "1.1.1"
