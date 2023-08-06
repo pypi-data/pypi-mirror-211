82788b24edb2e9de35b0c7f705f2ed4ea6e33abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:15 +0000

    Add package thipster/0.13.2/thipster-0.13.2.tar.gz

---
+++ packages/thipster/thipster-0.13.2.tar.gz/thipster-0.13.2/thipster/engine/I_Repository.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from thipster.engine.ResourceModel import ResourceModel
+
+
+class I_Repository(ABC):
+    """Repository module interface
+
+    Methods
+    -------
+    get()
+        Abstract get method
+
+    """
+    @abstractmethod
+    def get(self, resourceNames: list[str]) -> dict[str, ResourceModel]:
+        pass
