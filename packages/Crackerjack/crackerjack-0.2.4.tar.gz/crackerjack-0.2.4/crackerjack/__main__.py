2cb0b1441d3d0b5d7729268ed10ed2c28d23909a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:18 +0000

    Add package Crackerjack/0.2.4/crackerjack-0.2.4.tar.gz

---
+++ packages/Crackerjack/crackerjack-0.2.4.tar.gz/crackerjack-0.2.4/crackerjack/__main__.py
@@ -0,0 +1,49 @@
+import asyncio
+
+from click import command
+from click import help_option
+from click import option
+from crackerjack import crackerjack_it
+from pydantic import BaseModel
+
+
+class Options(BaseModel):
+    commit = False
+    interactive = False
+    doc = False
+    do_not_update_configs = False
+    publish: str | bool = False
+    verbose = False
+
+
+options = Options()
+
+
+@command()
+@help_option("-h", is_flag=True, help="help")
+@option("-c", is_flag=True, help="commit")
+@option("-i", is_flag=True, help="interactive")
+@option("-d", is_flag=True, help="doc")
+@option("-x", is_flag=True, help="do not update configs")
+@option("-v", is_flag=True, help="verbose")
+@option("-p", help="publish: -p [micro, minor, major]")
+# @option("-f", help="format: -f [module]")
+def crackerjack(c: bool, i: bool, d: bool, v: bool, x: bool, p: str) -> None:
+    if c:
+        options.commit = c
+    if i:
+        options.interactive = i
+    if d:
+        options.doc = d
+    if x:
+        options.do_not_update_configs = x
+    if p in ("micro", "minor", "major"):
+        options.publish = p
+    if v:
+        print("-v not currently implemented")
+        options.verbose = v
+    asyncio.run(crackerjack_it(options))
+
+
+if __name__ == "__main__":
+    crackerjack()
