b1d01203c12ae0a32beb324210d7c81523dceddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:31 +0000

    Add package midi-clip/0.1/midi-clip-0.1.tar.gz

---
+++ packages/midi-clip/midi-clip-0.1.tar.gz/midi-clip-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="midi-clip",
+    version="0.1",
+    packages=find_packages(),
+    description="A python package for midi clip.",
+    author="kyaryunha",
+    author_email="kyaryunha@gmail.com",
+    url="https://github.com/kyaryunha/midi-clip",
+    install_requires=[
+        "mido",
+    ],
+)
