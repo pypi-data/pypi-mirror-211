fdbdfa8c90146989467261da75a116feca21d82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:00 +0000

    Add package integreat-cms/2023.5.5/integreat-cms-2023.5.5.tar.gz

---
+++ packages/integreat-cms/integreat-cms-2023.5.5.tar.gz/integreat-cms-2023.5.5/integreat_cms/cms/linklists.py
@@ -0,0 +1,90 @@
+from django.conf import settings
+from linkcheck import Linklist
+
+from .models import Event, EventTranslation, Page, PageTranslation, POITranslation
+
+
+class ContentTranslationLinklist(Linklist):
+    """
+    Base class for content translation link lists
+    """
+
+    html_fields = ["content"]
+
+    @classmethod
+    def filter_callable(cls, objects):
+        """
+        Get only latest versions for non-archived events/locations
+
+        :param objects: Objects to be filtered
+        :type objects: ~django.db.models.query.QuerySet
+
+        :return: Objects that passed the filter
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        return objects.filter(
+            **{f"{objects.model.foreign_field()}__archived": False}
+        ).distinct(f"{objects.model.foreign_field()}__pk", "language__pk")
+
+
+class PageTranslationLinklist(Linklist):
+    """
+    Class for selecting the PageTranslation model for link checks
+    """
+
+    model = PageTranslation
+    html_fields = ["content"]
+
+    @classmethod
+    def filter_callable(cls, objects):
+        """
+        Get only latest versions for non-archived pages
+
+        :param objects: Objects to be filtered
+        :type objects: ~django.db.models.query.QuerySet
+
+        :return: Objects that passed the filter
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        # Since filtering the archived pages causes a lot of overhead, we only want this during the findlinks management command.
+        if settings.LINKCHECK_EXCLUDE_ARCHIVED_PAGES:
+            pages = Page.objects.all().cache_tree(archived=False)
+            objects = objects.filter(page__in=pages)
+        return objects.distinct("page__pk", "language__pk")
+
+
+class EventTranslationLinklist(ContentTranslationLinklist):
+    """
+    Class for selecting the EventTranslation model for link checks
+    """
+
+    model = EventTranslation
+
+    @classmethod
+    def filter_callable(cls, objects):
+        """
+        Get only translations of upcoming events
+
+        :param objects: Objects to be filtered
+        :type objects: ~django.db.models.query.QuerySet
+
+        :return: Objects that passed the filter
+        :rtype: ~django.db.models.query.QuerySet
+        """
+        upcoming_events = Event.objects.filter_upcoming()
+        return objects.filter(event__in=upcoming_events)
+
+
+class POITranslationLinklist(ContentTranslationLinklist):
+    """
+    Class for selecting the POITranslation model for link checks
+    """
+
+    model = POITranslation
+
+
+linklists = {
+    "PageTranslations": PageTranslationLinklist,
+    "EventTranslations": EventTranslationLinklist,
+    "POITranslations": POITranslationLinklist,
+}
---
+++ packages/integreat-cms/integreat-cms-2023.5.5.tar.gz/integreat-cms-2023.5.5/integreat_cms/cms/views/linkcheck/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains views for the broken link checker
+"""
+from .linkcheck_list_view import LinkcheckListView
+from .linkcheck_redirect_view import LinkcheckRedirectView
+from .linkcheck_stats_view import LinkcheckStatsView
