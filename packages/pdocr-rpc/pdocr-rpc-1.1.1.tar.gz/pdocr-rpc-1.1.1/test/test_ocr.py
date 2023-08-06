9568d2733f4153948b0c485eddbf42250a2ed959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:16 +0000

    Add package pdocr-rpc/1.1.1/pdocr-rpc-1.1.1.tar.gz

---
+++ packages/pdocr-rpc/pdocr-rpc-1.1.1.tar.gz/pdocr-rpc-1.1.1/test/test_ocr.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import pytest
+
+from pdocr_rpc.ocr import ocr
+
+from pdocr_rpc.setting import setting
+
+setting.SERVER_IP = "10.8.13.78"
+setting.PORT = "8890"
+
+def test_ocr():
+    res = ocr()
+    assert res is not None
+
+if __name__ == '__main__':
+    pytest.main()
