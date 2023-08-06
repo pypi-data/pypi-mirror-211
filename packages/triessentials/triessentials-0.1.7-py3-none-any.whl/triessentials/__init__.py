3a70238ab9bc285fddb8158bf4b99e5803c16bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:08 +0000

    Add package triessentials/0.1.7/triessentials-0.1.7-py3-none-any.whl

---
+++ packages/triessentials/triessentials-0.1.7-py3-none-any.whl/triessentials/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '0.1.7'
+
+import sys, time, random, os, time
+
+def slowprint(text,delay_time):
+ for character in text:      
+  sys.stdout.write(character) 
+  sys.stdout.flush()
+  time.sleep(delay_time)
+def random(number1,number2):
+ return random.randint(number1,number2)
+def clear():
+ os.system('clear')
+def wait(seconds):
+  time.sleep(seconds)
+def line():
+  print()
+ 
+ 
+  
