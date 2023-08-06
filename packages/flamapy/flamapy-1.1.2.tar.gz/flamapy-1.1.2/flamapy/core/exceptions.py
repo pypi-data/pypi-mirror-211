33bb0ebcbcae0946d9a9d5ef1cfcbcd046df96b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:50 +0000

    Add package flamapy/1.1.2/flamapy-1.1.2.tar.gz

---
+++ packages/flamapy/flamapy-1.1.2.tar.gz/flamapy-1.1.2/flamapy/core/exceptions.py
@@ -0,0 +1,22 @@
+class PluginNotFound(Exception):
+    pass
+
+
+class OperationNotFound(Exception):
+    pass
+
+
+class TransformationNotFound(Exception):
+    pass
+
+
+class ElementNotFound(Exception):
+    pass
+
+
+class DuplicatedFeature(Exception):
+    pass
+
+
+class ConfigurationNotFound(Exception):
+    pass
