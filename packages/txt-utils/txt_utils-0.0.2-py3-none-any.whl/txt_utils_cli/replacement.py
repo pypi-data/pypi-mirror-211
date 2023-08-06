80ca10e55f7f12d61dd8ab8c5e8625f2778a571c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:53 +0000

    Add package txt-utils/0.0.2/txt_utils-0.0.2-py3-none-any.whl

---
+++ packages/txt-utils/txt_utils-0.0.2-py3-none-any.whl/txt_utils_cli/replacement.py
@@ -0,0 +1,67 @@
+import re
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from typing import cast
+
+from txt_utils_cli.default_args import add_file_and_enc_argument
+from txt_utils_cli.globals import ExecutionResult
+from txt_utils_cli.helper import parse_non_empty
+from txt_utils_cli.logging_configuration import get_file_logger, init_and_get_console_logger
+
+
+def get_replacement_parser(parser: ArgumentParser):
+  parser.description = "This command replaces all matching regex patterns in the text with a custom text."
+  add_file_and_enc_argument(parser)
+  parser.add_argument("text", type=parse_non_empty, metavar="TEXT",
+                      help="replace text")
+  parser.add_argument("replace_with", type=str, metavar="REPLACE-WITH",
+                      help="replace text with this text")
+  parser.add_argument("-d", "--disable-regex", action="store_true",
+                      help="disable parsing TEXT as regex pattern")
+  return replace_ns
+
+
+def replace_ns(ns: Namespace) -> ExecutionResult:
+  logger = init_and_get_console_logger(__name__)
+  flogger = get_file_logger()
+
+  if ns.text == ns.replace_with:
+    logger.error("Parameter 'text' and 'replace_with' need to be different!")
+    return False, False
+
+  path = cast(Path, ns.file)
+
+  logger.info("Loading...")
+  try:
+    content = path.read_text(ns.encoding)
+  except Exception as ex:
+    logger.error("File couldn't be loaded!")
+    flogger.exception(ex)
+    return False, False
+
+  old_content = content
+  logger.info("Replacing...")
+  if ns.disable_regex:
+    if ns.text not in content:
+      logger.info("File did not contained TEXT. Nothing to replace.")
+      return True, False
+    content = content.replace(ns.text, ns.replace_with)
+  else:
+    pattern = re.compile(ns.text)
+    content = re.sub(pattern, ns.replace_with, content)
+
+  changed_anything = content != old_content
+  del old_content
+
+  if changed_anything:
+    logger.info("Saving...")
+    try:
+      path.parent.mkdir(parents=True, exist_ok=True)
+      path.write_text(content, ns.encoding)
+    except Exception as ex:
+      logger.error("File couldn't be saved!")
+      flogger.exception(ex)
+      return False, False
+  del content
+
+  return True, changed_anything
---
+++ packages/txt-utils/txt_utils-0.0.2-py3-none-any.whl/txt_utils_cli/vocabulary_exporting.py
@@ -0,0 +1,124 @@
+from argparse import ArgumentParser, Namespace
+from functools import partial
+from multiprocessing import Pool
+from pathlib import Path
+from typing import List, Optional, Set, cast
+
+from tqdm import tqdm
+
+from txt_utils_cli.default_args import add_file_arguments
+from txt_utils_cli.globals import ExecutionResult
+from txt_utils_cli.helper import add_mp_group, parse_path, split_adv
+from txt_utils_cli.logging_configuration import get_file_logger, init_and_get_console_logger
+
+
+def get_vocabulary_exporting_parser(parser: ArgumentParser):
+  parser.description = "This command exports the unit vocabulary."
+  add_file_arguments(parser, True)
+  parser.add_argument("output", type=parse_path,
+                      help="output file to write the vocabulary")
+  parser.add_argument("--include-empty", action="store_true",
+                      help="include empty text in vocabulary if it occurs")
+  add_mp_group(parser)
+  return extract_vocabulary_ns
+
+
+def extract_vocabulary_ns(ns: Namespace) -> ExecutionResult:
+  logger = init_and_get_console_logger(__name__)
+  flogger = get_file_logger()
+  path = cast(Path, ns.file)
+  logger.info("Loading...")
+  try:
+    content = path.read_text(ns.encoding)
+  except Exception as ex:
+    logger.error("File couldn't be loaded!")
+    flogger.exception(ex)
+    return False, False
+
+  logger.info("Splitting lines...")
+  lines = content.split(ns.lsep)
+  del content
+
+  n_jobs = cast(int, ns.n_jobs)
+  chunksize = cast(int, ns.chunksize)
+  maxtasksperchild = cast(int, ns.maxtasksperchild)
+
+  flogger.debug(f"Lines: {len(lines)}")
+  flogger.debug(f"Chunksize: {chunksize}")
+  flogger.debug(f"Maxtask: {maxtasksperchild}")
+  flogger.debug(f"Jobs: {n_jobs}")
+
+  chunks = get_chunks(lines, chunksize)
+  del lines
+
+  flogger.debug(f"Chunks: {len(chunks)}")
+
+  amount_of_jobs_required = len(chunks)
+  n_jobs = min(n_jobs, amount_of_jobs_required)
+  flogger.debug(f"Jobs (final): {n_jobs}")
+
+  method_proxy = partial(
+    get_vocab_process,
+    wsep=ns.sep,
+  )
+
+  voc = set()
+  with Pool(
+    processes=n_jobs,
+    initializer=__init_pool,
+    initargs=(chunks,),
+    maxtasksperchild=maxtasksperchild,
+  ) as pool:
+    iterator = pool.imap_unordered(method_proxy, range(len(chunks)), chunksize=1)
+    iterator = tqdm(iterator, total=len(chunks), desc="Processing", unit=" chunk(s)")
+    voc.update(*iterator)
+  del chunks
+
+  if not ns.include_empty and "" in voc:
+    voc.remove("")
+
+  logger.info(f"Extracted vocabulary size: {len(voc)}")
+
+  logger.info("Saving...")
+  output = cast(Path, ns.output)
+  voc_text = "\n".join(sorted(voc))
+
+  try:
+    output.parent.mkdir(parents=True, exist_ok=True)
+    output.write_text(voc_text, ns.encoding)
+  except Exception as ex:
+    logger.error("Vocabulary file couldn't be saved!")
+    flogger.exception(ex)
+    return False, False
+  logger.info(f"Written vocabulary to: {output.absolute()}")
+  del voc_text
+  return True, True
+
+
+def get_chunks(keys: List[str], chunk_size: Optional[int]) -> List[List[str]]:
+  if chunk_size is None:
+    chunk_size = len(keys)
+  chunked_list = list(keys[i: i + chunk_size] for i in range(0, len(keys), chunk_size))
+  return chunked_list
+
+
+process_chunks: List[str] = None
+
+
+def get_vocab_process(chunk_nr: int, wsep: str) -> Set[str]:
+  global process_chunks
+  chunk = process_chunks[chunk_nr]
+  return get_vocab(chunk, wsep)
+
+
+def get_vocab(lines: List[str], wsep: str) -> Set[str]:
+  voc = set()
+  for line in lines:
+    tokens = split_adv(line, wsep)
+    voc.update(tokens)
+  return voc
+
+
+def __init_pool(chunks: List[List[str]]) -> None:
+  global process_chunks
+  process_chunks = chunks
