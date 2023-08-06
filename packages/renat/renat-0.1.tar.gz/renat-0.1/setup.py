d04cfe50033a5754d99ece4b18253ef56988e587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:30 +0000

    Add package renat/0.1/renat-0.1.tar.gz

---
+++ packages/renat/renat-0.1.tar.gz/renat-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='renat',
+    version='0.1',
+    author='frozix',
+    author_email='tolibaevrinat4@gmail.com',
+    description='Математический модуль',
+    py_modules=['renat'],
+)
